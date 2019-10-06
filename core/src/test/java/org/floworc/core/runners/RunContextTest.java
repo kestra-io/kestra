e93be4898528a688169f4bd1faea8347d11d2157
tchiotludo <tchiot.ludo@gmail.com>
2019-10-06 20:48:35 +0200

    test(variable): add unittest on variables output

--- core/src/test/java/org/floworc/core/runners/RunContextTest.java
+++ core/src/test/java/org/floworc/core/runners/RunContextTest.java
@@ -1,10 +1,13 @@
 package org.floworc.core.runners;
 
+import org.exparity.hamcrest.date.ZonedDateTimeMatchers;
 import org.floworc.core.AbstractMemoryRunnerTest;
 import org.floworc.core.models.executions.Execution;
 import org.junit.jupiter.api.Test;
 import org.slf4j.event.Level;
 
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.concurrent.TimeoutException;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -20,12 +23,28 @@ class RunContextTest extends AbstractMemoryRunnerTest {
 
         assertThat(execution.getTaskRunList().get(0).getLogs(), hasSize(1));
         assertThat(execution.getTaskRunList().get(0).getLogs().get(0).getLevel(), is(Level.TRACE));
-        assertThat(execution.getTaskRunList().get(0).getLogs().get(0).getMessage(), is("first {{todo}}"));
+        assertThat(execution.getTaskRunList().get(0).getLogs().get(0).getMessage(), is("first 1st"));
 
         assertThat(execution.getTaskRunList().get(1).getLogs(), hasSize(1));
         assertThat(execution.getTaskRunList().get(1).getLogs().get(0).getLevel(), is(Level.WARN));
+        assertThat(execution.getTaskRunList().get(1).getLogs().get(0).getMessage(), is("second org.floworc.core.tasks.debugs.Echo"));
 
         assertThat(execution.getTaskRunList().get(2).getLogs(), hasSize(1));
         assertThat(execution.getTaskRunList().get(2).getLogs().get(0).getLevel(), is(Level.ERROR));
+        assertThat(execution.getTaskRunList().get(2).getLogs().get(0).getMessage(), is("third logs"));
+    }
+
+    @Test
+    void variables() throws TimeoutException {
+        Execution execution = runnerUtils.runOne("return");
+
+        assertThat(execution.getTaskRunList(), hasSize(3));
+
+        assertThat(
+            ZonedDateTime.from(ZonedDateTime.parse((String) execution.getTaskRunList().get(0).getOutputs().get("return"))),
+            ZonedDateTimeMatchers.within(10, ChronoUnit.SECONDS, ZonedDateTime.now())
+        );
+        assertThat(execution.getTaskRunList().get(1).getOutputs().get("return"), is("task-id"));
+        assertThat(execution.getTaskRunList().get(2).getOutputs().get("return"), is("return"));
     }
 }

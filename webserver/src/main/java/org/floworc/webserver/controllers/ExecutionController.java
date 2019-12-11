6272828f858645578a47ee73c878a7e85820cb0e
brahima <brahim.aitelhaj@ext.leroymerlin.fr>
2019-12-11 16:59:44 +0100

    feat(crud) : get and find executions

--- webserver/src/main/java/org/floworc/webserver/controllers/ExecutionController.java
+++ webserver/src/main/java/org/floworc/webserver/controllers/ExecutionController.java
@@ -1,9 +1,11 @@
 package org.floworc.webserver.controllers;
 
+import io.micronaut.data.model.Pageable;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Post;
+import io.micronaut.http.annotation.QueryValue;
 import io.micronaut.http.multipart.StreamingFileUpload;
 import io.micronaut.http.sse.Event;
 import io.micronaut.validation.Validated;
@@ -14,8 +16,10 @@ import org.floworc.core.models.executions.Execution;
 import org.floworc.core.models.flows.Flow;
 import org.floworc.core.queues.QueueFactoryInterface;
 import org.floworc.core.queues.QueueInterface;
+import org.floworc.core.repositories.ExecutionRepositoryInterface;
 import org.floworc.core.repositories.FlowRepositoryInterface;
 import org.floworc.core.runners.RunnerUtils;
+import org.floworc.webserver.responses.PagedResults;
 import org.reactivestreams.Publisher;
 
 import javax.annotation.Nullable;
@@ -26,10 +30,14 @@ import java.util.Optional;
 import java.util.concurrent.atomic.AtomicReference;
 
 @Validated
-@Controller("/api/v1/flows/{namespace}/{id}")
+@Controller("/api/v1/")
+
 public class ExecutionController {
     @Inject
     private FlowRepositoryInterface flowRepository;
+
+    @Inject
+    private ExecutionRepositoryInterface executionRepository;
 
     @Inject
     private RunnerUtils runnerUtils;
@@ -39,18 +47,53 @@ public class ExecutionController {
     protected QueueInterface<Execution> executionQueue;
 
     /**
+     * Get a execution
+     *
+     * @param executionId The execution identifier
+     * @return the execution with the provided identifier
+     */
+    @Get(uri = "executions/{executionId}", produces = MediaType.TEXT_JSON)
+    public Maybe<Execution> get(String executionId) {
+        return executionRepository
+                .findById(executionId)
+                .map(Maybe::just)
+                .orElse(Maybe.empty());
+    }
+
+    /**
+     * Find and returns all executions for a specific namespace and flow identifier
+     *
+     * @param namespace The flow namespace
+     * @param flowId    The flow identifier
+     * @param page      The number of result pages to return
+     * @param size      The number of result by page
+     * @return a list of found executions
+     */
+    @Get(uri = "executions", produces = MediaType.TEXT_JSON)
+    public PagedResults<Execution> findByFlowId(@QueryValue(value = "namespace") String namespace,
+                                                @QueryValue(value = "flowId") String flowId,
+                                                @QueryValue(value = "page", defaultValue = "1") int page,
+                                                @QueryValue(value = "size", defaultValue = "10") int size) {
+        return PagedResults.of(
+                executionRepository
+                        .findByFlowId(namespace, flowId, Pageable.from(page, size))
+        );
+    }
+
+
+    /**
      * Trigger an new execution for current flow
      *
      * @param namespace The flow namespace
-     * @param id The flow id
+     * @param id        The flow id
      * @return execution created
      */
-    @Post(uri = "trigger", produces = MediaType.TEXT_JSON, consumes = MediaType.MULTIPART_FORM_DATA)
+    @Post(uri = "flows/{namespace}/{id}/trigger", produces = MediaType.TEXT_JSON, consumes = MediaType.MULTIPART_FORM_DATA)
     public Maybe<Execution> trigger(
-        String namespace,
-        String id,
-        @Nullable Map<String, String> inputs,
-        @Nullable Publisher<StreamingFileUpload> files
+            String namespace,
+            String id,
+            @Nullable Map<String, String> inputs,
+            @Nullable Publisher<StreamingFileUpload> files
     ) {
         Optional<Flow> find = flowRepository.findById(namespace, id);
         if (find.isEmpty()) {
@@ -58,8 +101,8 @@ public class ExecutionController {
         }
 
         Execution current = runnerUtils.newExecution(
-            find.get(),
-            (flow, execution) -> runnerUtils.typedInputs(flow, execution, inputs, files)
+                find.get(),
+                (flow, execution) -> runnerUtils.typedInputs(flow, execution, inputs, files)
         );
 
         executionQueue.emit(current);
@@ -70,12 +113,12 @@ public class ExecutionController {
     /**
      * Trigger an new execution for current flow and follow execution
      *
-     * @param namespace The flow namespace
-     * @param id The flow id
+     * @param namespace   The flow namespace
+     * @param id          The flow id
      * @param executionId The execution id to follow
      * @return execution sse event
      */
-    @Get(uri = "executions/{executionId}/follow", produces = MediaType.TEXT_JSON)
+    @Get(uri = "flows/{namespace}/{id}/executions/{executionId}/follow", produces = MediaType.TEXT_JSON)
     public Flowable<Event<Execution>> triggerAndFollow(String namespace, String id, String executionId) {
         AtomicReference<Runnable> cancel = new AtomicReference<>();
 
@@ -85,22 +128,22 @@ public class ExecutionController {
         }
 
         return Flowable
-            .<Event<Execution>>create(emitter -> {
-                Runnable receive = this.executionQueue.receive(current -> {
-                    if (current.getId().equals(executionId)) {
-                        emitter.onNext(Event.of(current).id(Execution.class.getSimpleName()));
+                .<Event<Execution>>create(emitter -> {
+                    Runnable receive = this.executionQueue.receive(current -> {
+                        if (current.getId().equals(executionId)) {
+                            emitter.onNext(Event.of(current).id(Execution.class.getSimpleName()));
 
-                        if (current.isTerminatedWithListeners(find.get())) {
-                            emitter.onComplete();
+                            if (current.isTerminatedWithListeners(find.get())) {
+                                emitter.onComplete();
+                            }
                         }
+                    });
+                    cancel.set(receive);
+                }, BackpressureStrategy.BUFFER)
+                .doOnComplete(() -> {
+                    if (cancel.get() != null) {
+                        cancel.get().run();
                     }
                 });
-                cancel.set(receive);
-            }, BackpressureStrategy.BUFFER)
-            .doOnComplete(() -> {
-                if (cancel.get() != null) {
-                    cancel.get().run();
-                }
-            });
     }
 }

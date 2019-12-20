f74932c22ab82838979c4cfdee6666718756e518
Eric Régnier <eric.regnier@ext.leroymerlin.fr>
2019-12-20 17:51:21 +0100

    typo(code): clean code

--- ui/src/stores/layout.js
+++ ui/src/stores/layout.js
@@ -6,7 +6,6 @@ export default {
     actions: {},
     mutations: {
         setTopNavbar(state, value) {
-            console.log('set', value)
             state.topNavbar = value
         }
     },

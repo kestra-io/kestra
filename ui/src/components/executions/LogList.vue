daac7267c352779aee980923aa3a94de919e28a7
Eric Régnier <eric.regnier@ext.leroymerlin.fr>
2020-01-16 16:28:34 +0100

    feat(ui): Display attempts in logs

--- ui/src/components/executions/LogList.vue
+++ ui/src/components/executions/LogList.vue
@@ -2,7 +2,11 @@
     <div v-if="execution" class="bg-dark log-wrapper text-white text-monospace">
         <template v-for="taskItem in execution.taskRunList">
             <template v-if="(!task || task.id === taskItem.id) && taskItem.attempts">
-                <template v-for="attempt in taskItem.attempts">
+                <template v-for="(attempt, index) in taskItem.attempts">
+                    <p :key="index" class="attempt">
+                        <b-badge variant="primary">{{$t('attempt') | cap}} {{index + 1}}</b-badge>
+                        {{attempt.state.startDate | date('LLL:ss') }} - {{attempt.state.endDate | date('LLL:ss') }} <clock/> {{$t('Duration')}} : {{attempt.state.duration | humanizeDuration}}
+                    </p>
                     <template v-if="attempt.logs">
                         <template v-for="log in attempt.logs">
                             <log-line
@@ -21,9 +25,9 @@
 <script>
 import { mapState } from "vuex";
 import LogLine from "./LogLine";
-
+import Clock from "vue-material-design-icons/Clock";
 export default {
-    components: { LogLine },
+    components: { LogLine, Clock },
     props: {
         level: {
             type: String,
@@ -45,8 +49,26 @@ export default {
 .log-wrapper {
     padding: 10px;
     border-radius: 5px;
+
     .line:nth-child(odd) {
         background-color: lighten($dark, 5%);
+    }
+    p.attempt {
+        margin-top: $paragraph-margin-bottom;
+        margin-bottom: $paragraph-margin-bottom/2;
+        border-bottom: 1px solid $gray-600;
+        font-family: $font-family-sans-serif;
+        font-size: $font-size-base;
+        padding-bottom: $paragraph-margin-bottom/2;
+
+        .badge {
+            font-size: $font-size-base;
+            font-weight: bold;
+            margin-right: 5px;
+        }
+    }
+    p:first-child {
+        margin-top: 0;
     }
 }
 </style>

9a7fffe954aee89644eb2a9d7f750568ad3d8667
Eric Régnier <eric.regnier@ext.leroymerlin.fr>
2020-01-21 14:27:00 +0100

    feat(ui): display and download execution inputs and outputs close #29

---
+++ ui/src/components/executions/ExecutionOutput.vue
@@ -0,0 +1,69 @@
+<template>
+    <div v-if="execution && outputs">
+        <hr />
+        <h2>{{$t('outputs') | cap}}</h2>
+        <b-table
+            responsive="xl"
+            striped
+            hover
+            bordered
+            :fields="fields"
+            :items="outputs"
+            class="mb-0"
+        >
+            <template v-slot:cell(value)="row">
+                <b-link
+                    class="btn btn-primary"
+                    v-if="row.item.download"
+                    target="_blank"
+                    :href="itemUrl({uri: row.item.value})"
+                >{{$t('download') | cap}}</b-link>
+                <span v-else>{{row.item.value}}</span>
+            </template>
+        </b-table>
+    </div>
+</template>
+<script>
+import { mapState } from "vuex";
+import { apiRoot } from "../../http";
+
+export default {
+    methods: {
+        itemUrl(value) {
+            return `${apiRoot}executions/${this.execution.id}/file?filePath=${value.uri}&type=${value.type}`;
+        }
+    },
+    computed: {
+        ...mapState("execution", ["execution"]),
+        fields() {
+            return [
+                {
+                    key: "key",
+                    label: this.$t("name")
+                },
+                {
+                    key: "task",
+                    label: this.$t("task")
+                },
+                {
+                    key: "value",
+                    label: this.$t("value")
+                }
+            ];
+        },
+        outputs() {
+            const outputs = [];
+            for (const task of this.execution.taskRunList) {
+                for (const key in task.outputs) {
+                    const item = { key, value: task.outputs[key], task: task.id };
+                    if (task.outputs[key].startsWith("kestra:///")) {
+                        item.download = true;
+                    }
+                    outputs.push(item);
+                }
+            }
+            return outputs;
+        }
+    }
+};
+</script>
--- ui/src/components/executions/Overview.vue
+++ ui/src/components/executions/Overview.vue
@@ -5,13 +5,40 @@
                 <status :status="execution.state.current" />
             </b-col>
         </b-row>
+        <h2>{{$t('execution') | cap}}</h2>
         <b-table responsive="xl" striped hover bordered :items="items" class="mb-0"></b-table>
+        <div v-if="execution.inputs">
+            <hr />
+            <h2>{{$t('inputs') | cap}}</h2>
+            <b-table
+                responsive="xl"
+                striped
+                hover
+                bordered
+                :items="inputs"
+                :fields="fields"
+                class="mb-0"
+            >
+                <template v-slot:cell(value)="row">
+                    <span
+                        v-if="['string', 'optional', 'float', 'int', 'instant'].includes(row.item.key)"
+                    >{{row.item.value}}</span>
+                    <b-link
+                        class="btn btn-primary"
+                        v-if="['optionalFile', 'file'].includes(row.item.key)"
+                        target="_blank"
+                        :href="itemUrl(row.item.value)"
+                    >{{$t('download') | cap}}</b-link>
+                </template>
+            </b-table>
+        </div>
     </div>
 </template>
 <script>
 import { mapState } from "vuex";
 import Status from "../Status";
 import humanizeDuration from "humanize-duration";
+import { apiRoot } from "../../http";
 
 const ts = date => new Date(date).getTime();
 
@@ -19,29 +46,57 @@ export default {
     components: {
         Status
     },
+    methods: {
+        itemUrl(value) {
+            return `${apiRoot}executions/${this.execution.id}/file?filePath=${value.uri}&type=${value.type}`;
+        }
+    },
     computed: {
         ...mapState("execution", ["execution"]),
+        fields() {
+            return [
+                {
+                    key: "key",
+                    label: this.$t("name")
+                },
+                {
+                    key: "value",
+                    label: this.$t("value")
+                }
+            ];
+        },
         items() {
             const startTs = this.execution.state.histories[0].date;
-            const stopTs = this.execution.state.histories[this.execution.state.histories.length - 1].date;
+            const stopTs = this.execution.state.histories[
+                this.execution.state.histories.length - 1
+            ].date;
             const delta = ts(stopTs) - ts(startTs);
             const duration = this.$moment.duration(delta);
             const humanDuration = humanizeDuration(duration);
-            const stepCount = this.execution.taskRunList ? this.execution.taskRunList.length : 0
+            const stepCount = this.execution.taskRunList
+                ? this.execution.taskRunList.length
+                : 0;
             return [
-                {key: this.$t('namespace'), value: this.execution.namespace},
-                {key: this.$t('flow'), value: this.execution.flowId},
-                {key: this.$t('created date'), value: startTs},
-                {key: this.$t('updated date'), value: stopTs},
-                {key: this.$t('duration'), value: humanDuration},
-                {key: this.$t('steps'), value: stepCount},
-            ]
+                { key: this.$t("namespace"), value: this.execution.namespace },
+                { key: this.$t("flow"), value: this.execution.flowId },
+                { key: this.$t("created date"), value: startTs },
+                { key: this.$t("updated date"), value: stopTs },
+                { key: this.$t("duration"), value: humanDuration },
+                { key: this.$t("steps"), value: stepCount }
+            ];
+        },
+        inputs() {
+            const inputs = [];
+            for (const key in this.execution.inputs) {
+                inputs.push({ key, value: this.execution.inputs[key] });
+            }
+            return inputs;
         }
     }
 };
 </script>
 <style scoped lang="scss">
-    /deep/ thead {
-        display: none;
-    }
+/deep/ thead {
+    display: none;
+}
 </style>

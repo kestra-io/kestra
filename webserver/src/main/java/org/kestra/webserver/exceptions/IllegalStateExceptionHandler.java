ddf674930aefbb26b8757bc2b35e45011462cb82
brahima <brahim.aitelhaj@ext.leroymerlin.fr>
2020-01-22 15:36:17 +0100

    feat(restart) : add restart execution feature
    Adds the ability to restart an execution either from a specific
    task or from the last failed task.

---
+++ webserver/src/main/java/org/kestra/webserver/exceptions/IllegalStateExceptionHandler.java
@@ -0,0 +1,20 @@
+package org.kestra.webserver.exceptions;
+
+import io.micronaut.context.annotation.Requires;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Produces;
+import io.micronaut.http.server.exceptions.ExceptionHandler;
+
+import javax.inject.Singleton;
+
+@Produces(value = MediaType.TEXT_PLAIN)
+@Singleton
+@Requires(classes = {IllegalStateException.class, ExceptionHandler.class})
+public class IllegalStateExceptionHandler implements ExceptionHandler<IllegalStateException, HttpResponse> {
+    @Override
+    public HttpResponse handle(HttpRequest request, IllegalStateException exception) {
+        return HttpResponse.unprocessableEntity().body(exception.getMessage());
+    }
+}

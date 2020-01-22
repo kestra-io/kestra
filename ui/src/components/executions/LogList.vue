023672649d4c6cdf202aa1f33636bd077ed59dca
eric regnier <eric.regnier@ext.leroymerlin.fr>
2020-01-22 17:59:53 +0100

    feat(ui): add optional / toggleable output button in logs close (#40)
    close #30

--- ui/src/components/executions/LogList.vue
+++ ui/src/components/executions/LogList.vue
@@ -3,9 +3,25 @@
         <template v-for="taskItem in execution.taskRunList">
             <template v-if="(!task || task.id === taskItem.id) && taskItem.attempts">
                 <template v-for="(attempt, index) in taskItem.attempts">
-                    <p :key="attempt.state.startDate" class="attempt">
+                    <p :key="index" class="attempt">
+                        <b-tooltip
+                            placement="left"
+                            :target="`output-toggle-${taskItem.id}`"
+                            triggers="hover"
+                        >{{$t('toggle output display') | cap}}</b-tooltip>
+                        <b-button
+                            :id="`output-toggle-${taskItem.id}`"
+                            variant="info"
+                            class="output"
+                            size="sm"
+                            @click="toggleShowOutput(taskItem)"
+                        >
+                            <eye />
+                        </b-button>
                         <b-badge variant="primary">{{$t('attempt') | cap}} {{index + 1}}</b-badge>
-                        {{attempt.state.startDate | date('LLL:ss') }} - {{attempt.state.endDate | date('LLL:ss') }} <clock/> {{$t('Duration')}} : {{attempt.state.duration | humanizeDuration}}
+                        {{attempt.state.startDate | date('LLL:ss') }} - {{attempt.state.endDate | date('LLL:ss') }}
+                        <clock />
+                        {{$t('Duration')}} : {{attempt.state.duration | humanizeDuration}}
                     </p>
                     <template v-if="attempt.logs">
                         <template v-for="log in attempt.logs">
@@ -19,6 +35,7 @@
                     </template>
                 </template>
             </template>
+            <pre :key="taskItem.id" v-if="showOutputs[taskItem.id]">{{taskItem.outputs.return}}</pre>
         </template>
     </div>
 </template>
@@ -26,8 +43,9 @@
 import { mapState } from "vuex";
 import LogLine from "./LogLine";
 import Clock from "vue-material-design-icons/Clock";
+import Eye from "vue-material-design-icons/Eye";
 export default {
-    components: { LogLine, Clock },
+    components: { LogLine, Clock, Eye },
     props: {
         level: {
             type: String,
@@ -38,8 +56,19 @@ export default {
             default: ""
         }
     },
+    data() {
+        return {
+            showOutputs: {}
+        };
+    },
     computed: {
         ...mapState("execution", ["execution", "task"])
+    },
+    methods: {
+        toggleShowOutput(task) {
+            this.showOutputs[task.id] = !this.showOutputs[task.id];
+            this.$forceUpdate();
+        }
     }
 };
 </script>
@@ -69,6 +98,16 @@ export default {
     }
     p:first-child {
         margin-top: 0;
+    }
+    .output {
+        margin-right: 5px;
+    }
+    pre {
+        border: 1px solid $light;
+        color: $light;
+        padding: 10px;
+        margin-top: 10px;
+        margin-bottom: 5px;
     }
 }
 </style>

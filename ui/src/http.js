407065f4e73bf235d3763a2559aeb84eb78d7100
eric regnier <eric.regnier@ext.leroymerlin.fr>
2020-02-01 15:40:48 +0100

    feat(ui): add progress bar on ajax calls (#50)

--- ui/src/http.js
+++ ui/src/http.js
@@ -1,18 +1,32 @@
 import Vue from 'vue';
 import VueAxios from 'vue-axios';
 import axios from 'axios';
+import { debounce } from "throttle-debounce";
 
-export default callback => {
+
+export default (callback, store) => {
+    const stopLoad = debounce(500, () => {
+        store.commit('core/setLoading', false)
+    })
+    const instance = axios.create({
+        timeout: 15000,
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    instance.interceptors.request.use(config => {
+        store.commit('core/setLoading', true)
+        return config
+
+    })
+    instance.interceptors.response.use(response => {
+        stopLoad()
+        return response
+    })
     Vue.use(
         VueAxios,
-        axios.create({
-            timeout: 15000,
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
+        instance
     );
-
     Vue.axios.defaults.baseURL = (process.env.VUE_APP_API_URL || "") + "/";
     callback();
 };

be39b695095ea72d1125cb0d62653cfd150400a8
kwangin.jung <inylove82@gmail.com>
2020-03-28 18:48:52 +0900

    Change exception
    Change exception

--- storage-local/src/main/java/org/kestra/storage/local/LocalStorage.java
+++ storage-local/src/main/java/org/kestra/storage/local/LocalStorage.java
@@ -37,7 +37,7 @@ public class LocalStorage implements StorageInterface {
 
         if (!file.exists()) {
             if (!file.mkdirs()) {
-                throw new SecurityException();
+                throw new RuntimeException("Cannot make directory for local storage!");
             }
         }
     }

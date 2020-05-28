28c8e33a3223bee4328b2deb99b3d4ed20bf6ff6
eric regnier <eric.regnier@ext.leroymerlin.fr>
2020-05-28 08:46:11 +0200

    fix(ui): fix moment warning

--- ui/src/main.js
+++ ui/src/main.js
@@ -1,24 +1,25 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './routes/router'
-import store from './stores/store'
-import BootstrapVue from 'bootstrap-vue'
-import configureHttp from './http'
-import VueSidebarMenu from 'vue-sidebar-menu'
-import i18n from './i18n'
-import vSelect from 'vue-select'
-import VueMoment from 'vue-moment'
-import VueSSE from 'vue-sse';
-import moment from 'moment'
 import './filters'
 import 'moment/locale/fr'
 import './utils/global'
 import './custom.scss'
-import VerticalSeparator from './components/layout/VerticalSeparator'
 // @TODO: move to scss
 import 'vue-material-design-icons/styles.css';
+
+import App from './App.vue'
+import BootstrapVue from 'bootstrap-vue'
+import VerticalSeparator from './components/layout/VerticalSeparator'
+import Vue from 'vue'
+import VueMoment from 'vue-moment'
 import VueProgressBar from 'vue-progressbar';
-import {extendMoment} from 'moment-range';
+import VueSSE from 'vue-sse';
+import VueSidebarMenu from 'vue-sidebar-menu'
+import configureHttp from './http'
+import { extendMoment } from 'moment-range';
+import i18n from './i18n'
+import moment from 'moment'
+import router from './routes/router'
+import store from './stores/store'
+import vSelect from 'vue-select'
 
 Vue.use(VueProgressBar, {
   color: 'rgb(143, 255, 199)',
@@ -26,10 +27,10 @@ Vue.use(VueProgressBar, {
   height: '2px'
 })
 Vue.use(VueSSE);
-Vue.use(VueMoment, { moment });
+Vue.use(VueMoment, { moment: extendMoment(moment) });
 Vue.use(VueSidebarMenu);
 Vue.use(BootstrapVue);
-Vue.use(extendMoment(moment));
+
 Vue.component('v-select', vSelect);
 Vue.component('v-s', VerticalSeparator);
 

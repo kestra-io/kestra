291639674fc8bef664152483d0b38c5e41bac8f8
tchiotludo <tchiot.ludo@gmail.com>
2020-08-13 14:36:16 +0200

    fix(ui): sort on flows list is not working

--- ui/src/stores/flow.js
+++ ui/src/stores/flow.js
@@ -10,8 +10,9 @@ export default {
 
     actions: {
         searchAndAggregate({ commit }, options) {
+            const sortString = options.sort ? `?sort=${options.sort}` : ''
             delete options.sort
-            return Vue.axios.get(`/api/v1/flows/searchAndAggregate`, { params: options }).then(response => {
+            return Vue.axios.get(`/api/v1/flows/searchAndAggregate${sortString}`, { params: options }).then(response => {
                 commit('setFlows', response.data.results)
                 commit('setTotal', response.data.total)
             })

b6e3cfb491debac6bf137b8b587ee53de1c63d6f
tchiotludo <tchiot.ludo@gmail.com>
2020-10-30 07:16:32 +0100

    fix(ui): let search look at all the indexed data & handle special characters
    close #109

--- ui/src/utils/queryBuilder.js
+++ ui/src/utils/queryBuilder.js
@@ -1,33 +1,48 @@
 const iso = date => new Date(parseInt(date)).toISOString()
-export default {
-    executionQueryBuilder: (route, fields) => {
-        const q = route.query;
 
-        const query = [
-            `namespace:${q.namespace || '*'}`,
-        ]
+export default class QueryBuilder {
+    static toLucene(q) {
+        let query = q;
+
+        query = query.replace(/</g, "");
+        query = query.replace(/>/g, "");
+        query = query.replace(/\//g, "");
+        query = query.replace(/\\/g, "");
+        query = query.replace(/&/g, "");
+        query = query.replace(/!/g, "");
+        query = query.replace(/([\^~*?:"+-=|(){}[\]])/g, "\\$1")
+
+        return `(*${query}* OR ${query})`;
+    }
+
+    static build(route) {
+        const q = route.query;
+        const query = []
+
+        if (q.namespace) {
+            query.push(`namespace:${q.namespace}`)
+        }
+
         if (q.start) {
             query.push(`state.startDate:[${iso(q.start)} TO *]`)
         }
+
         if (q.end) {
             query.push(`state.endDate:[* TO ${iso(q.end)}]`)
         }
         if (q.q) {
-            // Generate search string on all fields
-            const search = fields
-                .map(f => `${f.key}:*${q.q}* OR ${f.key}:${q.q}`)
-                .join(" OR ")
-            query.push('(' + search + ')');
+            query.push(QueryBuilder.toLucene(q.q));
         }
 
         return query.join(" AND ") || '*'
-    },
-    logQueryBuilder: (route) => {
+    }
+
+    static logQueryBuilder(route) {
         const q = route.query
         const start = q.start ? iso(q.start) : '*'
         const end = q.end ? iso(q.end) : '*'
         return [
-            `message:${q.q ? '*' + q.q + '*' : "*"}`,
+            `${q.q ? QueryBuilder.toLucene(q.q) : "*"}`,
             `timestamp:[${start} TO ${end}]`,
             `namespace:${q.namespace || "*"}`,
         ].join(" AND ")

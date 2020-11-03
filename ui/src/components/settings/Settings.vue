21e1a38891e9871950b717c489d918393c37e13f
eric regnier <eric.regnier@ext.leroymerlin.fr>
2020-11-03 16:20:14 +0100

    feat(ui): Allow fold and unfold code (#195)
    in editor by default with user reference in settings

--- ui/src/components/settings/Settings.vue
+++ ui/src/components/settings/Settings.vue
@@ -1,7 +1,10 @@
 <template>
     <div>
-        <b-form-group :label="$t('Language')">
+        <b-form-group :label="$t('Language')" label-cols-sm="3">
             <b-form-select v-model="lang" :options="langOptions"></b-form-select>
+        </b-form-group>
+        <b-form-group :label="$t('Fold auto')" label-cols-sm="3">
+            <b-checkbox v-model="autofoldTextEditor" value="1" unchecked-value="0" />
         </b-form-group>
     </div>
 </template>
@@ -29,12 +32,23 @@ export default {
                 localStorage.setItem("lang", lang);
                 this.$moment.locale(lang);
                 this.$root.$i18n.locale = lang;
-                this.$toast().success();
+                this.$toast().saved();
             },
             get() {
                 return localStorage.getItem("lang") || "en";
             }
-        }
+        },
+        autofoldTextEditor: {
+            set(value) {
+                localStorage.setItem("autofoldTextEditor", value);
+
+                this.$toast().saved();
+            },
+            get() {
+                return localStorage.getItem('autofoldTextEditor')
+            }
+        },
+
     }
 };
 </script>

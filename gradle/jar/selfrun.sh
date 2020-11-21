ed15b4c230b7c6d2c73f0cc4481dc85a8ccaeb74
tchiotludo <tchiot.ludo@gmail.com>
2020-11-21 08:06:01 +0100

    feat(docker): build a full image with all plugins

--- gradle/jar/selfrun.sh
+++ gradle/jar/selfrun.sh
@@ -2,6 +2,13 @@
 KESTRA_PLUGINS_PATH=${KESTRA_PLUGINS_PATH:-"$(dirname "$0")/plugins"}
 export KESTRA_PLUGINS_PATH=${KESTRA_PLUGINS_PATH}
 
+# Kestra configuration env vars
+KESTRA_CONFIGURATION_PATH=${KESTRA_CONFIGURATION_PATH:-"$(dirname "$0")/confs"}
+if [ "${KESTRA_CONFIGURATION}" ]; then
+    echo "${KESTRA_CONFIGURATION}" > "${KESTRA_CONFIGURATION_PATH}/application.yml"
+    export MICRONAUT_CONFIG_FILES="${KESTRA_CONFIGURATION_PATH}/application.yml"
+fi
+
 # Check java version
 JAVA_FULLVERSION=$(java -fullversion 2>&1)
 case "$JAVA_FULLVERSION" in

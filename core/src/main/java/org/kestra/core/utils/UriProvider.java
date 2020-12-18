ae2504bdc6b5ddf49be09a20d97835c7437ecb1e
tchiotludo <tchiot.ludo@gmail.com>
2020-12-18 10:43:50 +0100

    feat(core): enhance UriProvider methods

--- core/src/main/java/org/kestra/core/utils/UriProvider.java
+++ core/src/main/java/org/kestra/core/utils/UriProvider.java
@@ -3,6 +3,7 @@ package org.kestra.core.utils;
 import io.micronaut.context.annotation.Value;
 import org.apache.commons.lang3.StringUtils;
 import org.kestra.core.models.executions.Execution;
+import org.kestra.core.models.flows.Flow;
 
 import java.net.URI;
 import javax.annotation.Nullable;
@@ -14,14 +15,27 @@ public class UriProvider {
     @Value("${kestra.url:}")
     String uri;
 
-    public URI executionUrl(Execution execution) {
+    private URI build(String url) {
         if (uri == null) {
             return null;
         }
 
-        return URI.create(
-            StringUtils.stripEnd(uri, "/") +
-                "/ui/executions/" + execution.getNamespace() + "/" + execution.getFlowId() + "/" + execution.getId()
-        );
+        return URI.create(StringUtils.stripEnd(uri, "/") + url);
+    }
+
+    public URI rootUrl() {
+        return this.build("/");
+    }
+
+    public URI executionUrl(Execution execution) {
+        return this.build("/ui/executions/" + execution.getNamespace() + "/" + execution.getFlowId() + "/" + execution.getId());
+    }
+
+    public URI flowUrl(Execution execution) {
+        return this.build("/ui/flows/" + execution.getNamespace() + "/" + execution.getFlowId());
+    }
+
+    public URI flowUrl(Flow flow) {
+        return this.build("/ui/flows/" + flow.getNamespace() + "/" + flow.getId());
     }
 }

20a5e7e3f5f578c0ceadb2048d4c2e6473b024e2
Ludovic DEHON <tchiot.ludo@gmail.com>
2021-01-07 07:56:57 +0100

    feat(ui): introduce unit test (#263)

--- ui/src/utils/state.js
+++ ui/src/utils/state.js
@@ -1,127 +1,127 @@
 import _mapValues from "lodash/mapValues";
 import _map from "lodash/map";
 
-export default class State {
-    static States = Object.freeze({
-        CREATED: {
-            name: "CREATED",
-            colorClass: "info",
-            color: "#75bcdd",
-            icon: "pause-circle-outline",
-            isRunning: true,
-            isKillable: true
-        },
-        RESTARTED: {
-            name: "RESTARTED",
-            colorClass: "info",
-            color: "#75bcdd",
-            icon: "restart",
-            isRunning: false,
-            isKillable: true
-        },
-        SUCCESS: {
-            name: "SUCCESS",
-            colorClass: "success",
-            color: "#43ac6a",
-            icon: "check-circle-outline",
-            isRunning: false,
-            isKillable: false
-        },
-        RUNNING: {
-            name: "RUNNING",
-            colorClass: "primary",
-            color: "#1AA5DE",
-            icon: "play-circle-outline",
-            isRunning: true,
-            isKillable: true
-        },
-        KILLING: {
-            name: "KILLING",
-            colorClass: "warning",
-            color: "#FBD10B",
-            icon: "close-circle-outline",
-            isRunning: true,
-            isKillable: false
-        },
-        KILLED: {
-            name: "KILLED",
-            colorClass: "warning",
-            color: "#FBD10B",
-            icon: "stop-circle-outline",
-            isRunning: false,
-            isKillable: false
-        },
-        WARNING: {
-            name: "WARNING",
-            colorClass: "warning",
-            color: "#FBD10B",
-            icon: "alert-circle",
-            isRunning: false,
-            isKillable: false
-        },
-        FAILED: {
-            name: "FAILED",
-            colorClass: "danger",
-            color: "#F04124",
-            icon: "close-circle-outline",
-            isRunning: false,
-            isKillable: false
-        }
-    });
+const STATE = Object.freeze({
+    CREATED: {
+        name: "CREATED",
+        colorClass: "info",
+        color: "#75bcdd",
+        icon: "pause-circle-outline",
+        isRunning: true,
+        isKillable: true
+    },
+    RESTARTED: {
+        name: "RESTARTED",
+        colorClass: "info",
+        color: "#75bcdd",
+        icon: "restart",
+        isRunning: false,
+        isKillable: true
+    },
+    SUCCESS: {
+        name: "SUCCESS",
+        colorClass: "success",
+        color: "#43ac6a",
+        icon: "check-circle-outline",
+        isRunning: false,
+        isKillable: false
+    },
+    RUNNING: {
+        name: "RUNNING",
+        colorClass: "primary",
+        color: "#1AA5DE",
+        icon: "play-circle-outline",
+        isRunning: true,
+        isKillable: true
+    },
+    KILLING: {
+        name: "KILLING",
+        colorClass: "warning",
+        color: "#FBD10B",
+        icon: "close-circle-outline",
+        isRunning: true,
+        isKillable: false
+    },
+    KILLED: {
+        name: "KILLED",
+        colorClass: "warning",
+        color: "#FBD10B",
+        icon: "stop-circle-outline",
+        isRunning: false,
+        isKillable: false
+    },
+    WARNING: {
+        name: "WARNING",
+        colorClass: "warning",
+        color: "#FBD10B",
+        icon: "alert-circle",
+        isRunning: false,
+        isKillable: false
+    },
+    FAILED: {
+        name: "FAILED",
+        colorClass: "danger",
+        color: "#F04124",
+        icon: "close-circle-outline",
+        isRunning: false,
+        isKillable: false
+    }
+});
 
+export default class State {
     static get CREATED() {
-        return this.States.CREATED.name;
+        return STATE.CREATED.name;
     }
 
     static get RESTARTED() {
-        return this.States.RESTARTED.name;
+        return STATE.RESTARTED.name;
     }
 
     static get SUCCESS() {
-        return this.States.SUCCESS.name;
+        return STATE.SUCCESS.name;
     }
 
     static get RUNNING() {
-        return this.States.RUNNING.name;
+        return STATE.RUNNING.name;
     }
 
     static get KILLING() {
-        return this.States.KILLING.name;
+        return STATE.KILLING.name;
     }
 
     static get KILLED() {
-        return this.States.KILLED.name;
+        return STATE.KILLED.name;
     }
 
     static get FAILED() {
-        return this.States.FAILED.name;
+        return STATE.FAILED.name;
     }
 
     static get WARNING() {
-        return this.States.WARNING.name;
+        return STATE.WARNING.name;
     }
 
     static isRunning(state) {
-        return this.States[state] && this.States[state].isRunning;
+        return state[state] && state[state].isRunning;
     }
 
     static isKillable(state) {
-        return this.States[state] && this.States[state].isKillable;
+        return state[state] && state[state].isKillable;
     }
 
     static allStates() {
-        return _map(this.States, "name");
+        return _map(STATE, "name");
     }
 
     static colorClass() {
-        return _mapValues(this.States, state => state.colorClass);
+        return _mapValues(STATE, state => state.colorClass);
     }
 
     static color() {
-        return _mapValues(this.States, state => state.color);
+        return _mapValues(STATE, state => state.color);
     }
 
     static icon() {
-        return _mapValues(this.States, state => state.icon);
+        return _mapValues(STATE, state => state.icon);
     }
 }

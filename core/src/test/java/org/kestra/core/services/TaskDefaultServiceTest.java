34f4d5f04aff9a3944c99658b3a6cd7cdd152516
tchiotludo <tchiot.ludo@gmail.com>
2021-01-19 21:40:54 +0100

    chore(deps): update all deps

--- core/src/test/java/org/kestra/core/services/TaskDefaultServiceTest.java
+++ core/src/test/java/org/kestra/core/services/TaskDefaultServiceTest.java
@@ -1,7 +1,7 @@
 package org.kestra.core.services;
 
 import com.google.common.collect.ImmutableMap;
-import io.micronaut.test.annotation.MicronautTest;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;

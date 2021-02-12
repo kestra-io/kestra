14a2c3aeb896ebb1e9e8d1f439cd64d57e23dc8a
eric regnier <eric.regnier@ext.leroymerlin.fr>
2021-02-12 09:04:11 +0100

    fix(ui) : single icon load query (#290)
    close #289

--- ui/src/components/plugins/TaskIcon.vue
+++ ui/src/components/plugins/TaskIcon.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="wrapper" :class="classes" v-if="ready" :id="uuid">
+    <div class="wrapper" v-if="icon" :class="classes" :id="uuid">
         <div class="icon" :style="styles" :alt="cls" />
         <div v-if="!onlyIcon" class="hover">
             {{ name }}
@@ -27,8 +27,7 @@
         },
         data() {
             return {
-                uuid: Utils.uid(),
-                ready: false
+                uuid: Utils.uid()
             }
         },
         components: {
@@ -36,11 +35,11 @@
         computed: {
             ...mapState("plugin", ["icons"]),
             name() {
-                return this.icons[this.cls] ? this.icons[this.cls].name : this.cls;
+                return this.icon ? this.icon.name : this.cls;
             },
             classes() {
                 return {
-                    "flowable": this.icons[this.cls] ? this.icons[this.cls].flowable : false,
+                    "flowable": this.icon ? this.icon.flowable : false,
                     "only-icon": this.onlyIcon
                 }
             },
@@ -53,17 +52,11 @@
                 return this.name ? this.cls.replace("." + this.name, ".<code>" + this.name + "</code>") : this.cls;
             },
             imageBase64() {
-                return this.icons[this.cls] && this.icons[this.cls].icon ?
-                    this.icons[this.cls].icon :
-                    btoa("<svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" aria-hidden=\"true\" focusable=\"false\" width=\"0.75em\" height=\"1em\" style=\"-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);\" preserveAspectRatio=\"xMidYMid meet\" viewBox=\"0 0 384 512\"><path d=\"M288 32H0v448h384V128l-96-96zm64 416H32V64h224l96 96v288z\" fill=\"#333\"/></svg>");
-            }
-        },
-
-        created() {
-            if (this.icons === undefined) {
-                this.$store.dispatch("plugin/icons").then(() => this.ready = true)
-            } else {
-                this.ready = true;
+                const icon = this.icon ? this.icon.icon : undefined;
+                return icon ? icon : btoa("<svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" aria-hidden=\"true\" focusable=\"false\" width=\"0.75em\" height=\"1em\" style=\"-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);\" preserveAspectRatio=\"xMidYMid meet\" viewBox=\"0 0 384 512\"><path d=\"M288 32H0v448h384V128l-96-96zm64 416H32V64h224l96 96v288z\" fill=\"#333\"/></svg>");
+            },
+            icon() {
+                return (this.icons || {})[this.cls]
             }
         }
     };

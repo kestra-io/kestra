0bb0dd74c457d0d71133e1ddfb35688567dc5575
tchiotludo <tchiot.ludo@gmail.com>
2021-03-03 13:33:32 +0100

    feat(ui): move editor to monaco editor

--- ui/src/components/inputs/Editor.vue
+++ ui/src/components/inputs/Editor.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="editor-wrapper">
-        <b-navbar type="dark" variant="dark">
+    <div>
+        <b-navbar v-if="original === undefined && navbar" type="dark" variant="dark">
             <b-btn-group>
                 <b-button @click="autoFold(true)" size="sm" variant="light" v-b-tooltip.hover.top="$t('Fold content lines')">
                     <unfold-less-horizontal />
@@ -15,151 +15,281 @@
                 </b-button>
             </b-btn-group>
         </b-navbar>
-        <editor
-            ref="aceEditor"
-            :value="value"
-            @input="onInput"
-            @init="editorInit"
-            :lang="lang"
-            theme="merbivore_soft"
-            :width="width"
-            min-lines="5"
-            :height="height"
-        />
+
+        <div class="editor-container" :class="containerClass">
+            <div ref="editorContainer" class="editor-wrapper">
+                <MonacoEditor
+                    ref="monacoEditor"
+                    :theme="themeComputed"
+                    :value="value"
+                    :options="options"
+                    :diff-editor="original !== undefined"
+                    :original="original"
+                    @editorDidMount="editorDidMount"
+                    @change="onInput"
+                    :language="lang"
+                />
+                <div
+                    v-show="showPlaceholder"
+                    class="placeholder"
+                    @click="onPlaceholderClick"
+                >
+                    {{ placeholder }}
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
     import UnfoldLessHorizontal from "vue-material-design-icons/UnfoldLessHorizontal";
     import UnfoldMoreHorizontal from "vue-material-design-icons/UnfoldMoreHorizontal";
-    import {mapGetters} from "vuex";
-    import YamlUtils from "../../utils/yamlUtils";
-    import {debounce} from "throttle-debounce";
+    const MonacoEditor = () => import("./MonacoEditor");
 
     export default {
         props: {
             value: {type: String, required: true},
-            lang: {type: String, required: true},
-            width: {type: String, default: "100%"},
-            height: {type: String, default: "100%"},
+            original: {type: String, default: undefined},
+            lang: {type: String, default: undefined},
+            navbar: {type: Boolean, default: true},
+            input: {type: Boolean, default: false},
+            fullHeight: {type: Boolean, default: true},
+            theme: {type: String, default: "vs-dark"},
+            placeholder: {type: [String, Number], default: ""},
         },
         components: {
-            editor: require("vue2-ace-editor"),
+            MonacoEditor,
             UnfoldLessHorizontal,
             UnfoldMoreHorizontal,
         },
-        computed: {
-            ...mapGetters("flow", ["flow"]),
-            ed() {
-                return this.$refs.aceEditor.editor
-            },
+        data() {
+            const options = {}
+
+            if (this.input) {
+                options.lineNumbers = "off"
+                options.folding = false;
+                options.renderLineHighlight = "none"
+                options.wordBasedSuggestions = false;
+                options.occurrencesHighlight= false
+                options.hideCursorInOverviewRuler = true
+                options.overviewRulerBorder = false
+                options.overviewRulerLanes = 0
+                options.lineNumbersMinChars = 0;
+                options.fontSize = 13;
+                options.minimap =  {
+                    enabled: false
+                }
+                options.scrollBeyondLastColumn = 0;
+                options.scrollbar = {
+                    vertical: "hidden",
+                    alwaysConsumeMouseWheel: false,
+                    horizontalScrollbarSize: 5
+                };
+                options.find = {
+                    addExtraSpaceOnTop: false,
+                    autoFindInSelection: "never",
+                    seedSearchStringFromSelection: false,
+                }
+                options.contextmenu = false;
+                options.lineDecorationsWidth = 0;
+            } else {
+                options.scrollbar = {
+                    alwaysConsumeMouseWheel: false,
+                    vertical: "hidden",
+                };
+            }
+
+            return {
+                options: {
+                    ...{
+                        tabSize: 2,
+                        fontFamily: "'Source Code Pro', SFMono-Regular, Menlo, Monaco, Consolas, \"Liberation Mono\", \"Courier New\", monospace",
+                        fontSize: 12,
+                        showFoldingControls: "always",
+                        scrollBeyondLastLine: false,
+                        roundedSelection: false,
+                    },
+                    ...options
+                },
+                focus: false,
+                editor: undefined
+            };
         },
         created() {
-            this.resize = debounce(300, () => {
-                this.handleResize();
-            });
-            window.addEventListener("resize", this.resize);
+            window.addEventListener("resize", this.onResize);
         },
         beforeDestroy() {
-            window.removeEventListener("resize", this.resize);
+            window.removeEventListener("resize", this.onResize);
+        },
+        computed: {
+            themeComputed() {
+                return this.theme ? this.theme : (!this.input ? "vs-dark" : "vs")
+            },
+            containerClass() {
+                return [
+                    !this.input ? "" : "single-line",
+                    "theme-" + this.themeComputed
+                ]
+            },
+            showPlaceholder() {
+                return this.input === true && !this.focus && !(this.value !== undefined && this.value !== "");
+            },
         },
         methods: {
-            handleResize() {
-                this.setEditorMaxLines();
-            },
-            editorInit: function (editor) {
-                require("brace/mode/yaml");
-                require("brace/theme/merbivore_soft");
-                require("brace/ext/language_tools")
-                require("brace/ext/error_marker")
-                require("brace/ext/searchbox")
-                editor.textInput.focus();
+            editorDidMount(editor) {
+                // avoid double import of monaco editor, use a reference
+                const KeyCode = this.$refs.monacoEditor.monaco.KeyCode;
+                const KeyMod = this.$refs.monacoEditor.monaco.KeyMod;
 
-                this.setEditorMaxLines();
+                this.editor = editor;
 
-                editor.setOptions({
-                    minLines: 5,
-                    fontFamily: "\"Source Code Pro\", SFMono-Regular, Menlo, Monaco, Consolas, \"Liberation Mono\", \"Courier New\", monospace",
-                    showPrintMargin: false,
-                    tabSize: 2,
-                    wrap: false,
-                    newLineMode: "unix",
-                    autoScrollEditorIntoView: true,
-                    enableBasicAutocompletion: true,
-                    enableLiveAutocompletion: true
+                if (this.original) {
+                    this.editor.onDidUpdateDiff(this.onResize);
+                } else {
+                    this.editor.onDidChangeModelDecorations(this.onResize);
+
+                    this.editor.onDidBlurEditorWidget(() => {
+                        this.$emit("focusout", editor.getValue());
+                        this.focus = false;
+                    })
+
+                    this.editor.onDidFocusEditorText(() => {
+                        this.focus = true;
+                    })
+                }
+
+                this.editor.addCommand(KeyMod.CtrlCmd | KeyCode.KEY_S, () => {
+                    this.$emit("onSave", editor.getValue())
                 });
-                editor.commands.addCommand({
-                    name: "save",
-                    bindKey: {win: "Ctrl-S", mac: "Cmd-S"},
-                    exec: (editor) => {
-                        this.$emit("onSave", editor.session.getValue())
-                    },
-                });
-                setTimeout(() => {
-                    this.autoFold(localStorage.getItem("autofoldTextEditor") === "1")
-                })
+
+                if (this.input) {
+                    this.editor.addCommand(KeyMod.CtrlCmd | KeyCode.KEY_F, () => {})
+                    this.editor.addCommand(KeyMod.CtrlCmd | KeyCode.KEY_H, () => {})
+                    this.editor.addCommand(KeyCode.F1, () => {})
+                }
+
+                if (this.original === undefined && this.navbar && this.fullHeight) {
+                    this.editor.addAction({
+                        id: "fold-multiline",
+                        label: "Fold All Multi Lines",
+                        keybindings: [
+                            KeyCode.F10,
+                        ],
+                        contextMenuGroupId: "fold",
+                        contextMenuOrder: 1.5,
+                        run: (ed) => {
+                            const foldingContrib = ed.getContribution("editor.contrib.folding");
+                            foldingContrib.getFoldingModel().then(foldingModel => {
+
+                                let editorModel = foldingModel.textModel;
+                                let regions = foldingModel.regions;
+                                let toToggle = [];
+                                for (let i = regions.length - 1; i >= 0; i--) {
+                                    if (regions.isCollapsed(i) === false) {
+                                        let startLineNumber = regions.getStartLineNumber(i);
+
+                                        if (editorModel.getLineContent(startLineNumber).trim().endsWith("|")) {
+                                            toToggle.push(regions.toRegion(i));
+                                        }
+                                    }
+                                }
+                                foldingModel.toggleCollapseState(toToggle);
+                            });
+
+                            return null;
+                        }
+                    });
+
+                    if (localStorage.getItem("autofoldTextEditor") === "1") {
+                        this.autoFold(true);
+                    }
+                }
+
+                if (this.original !== undefined) {
+                    this.editor.updateOptions({readOnly: true})
+                }
+
+                this.onResize();
             },
-            trimContent(text) {
-                return text.split("\n").map(line => line.trim()).join("\n")
+            onResize() {
+                if (this.$refs.editorContainer && this.editor) {
+                    const container = this.$refs.editorContainer;
+                    const containerParent = container.parentElement.parentElement;
+                    const containerWidth = containerParent.offsetWidth;
+
+                    if (this.original || this.fullHeight) {
+                        const fullHeight = window.innerHeight - this.$refs.editorContainer.getBoundingClientRect().y - 55 - 30;
+                        container.style.height = `${fullHeight}px`;
+                        this.editor.layout({width: containerWidth, height: fullHeight});
+                    } else {
+                        const contentHeight = Math.max(21, this.editor.getContentHeight());
+                        container.style.height = `${contentHeight+3}px`;
+                        container.style.width = `${containerWidth-(this.input ? 20 : 0)}px`;
+                        this.editor.layout({width: containerWidth-(this.input ? 20 : 0),  height: contentHeight});
+                    }
+                }
             },
             autoFold(autoFold) {
-                //we may add try in case content is not serializable a json
-                let trimmedContent = this.trimContent(this.value)
-                const foldableTokens = []
-                let lineDiff = 0
-                try {
-                    this.getFoldLines(YamlUtils.parse(this.value), foldableTokens, autoFold)
-                } catch (err) {
-                    this.$toast().warning(
-                        err.message,
-                        this.$t("invalid yaml"),
-                    );
-                    return;
-                }
-                for (const foldableToken of foldableTokens) {
-                    const search = this.trimContent(foldableToken)
-                    const index = trimmedContent.indexOf(search)
-                    const line = trimmedContent.slice(0, index).split("\n").length + lineDiff
-                    lineDiff = line + search.split("\n").length - 2
-                    trimmedContent = trimmedContent.slice(index + search.length)
-                    this.ed.getSession().$toggleFoldWidget(line - 2, {})
-                }
-            },
-            getFoldLines(node, foldableTokens, autoFold) {
-                if (Array.isArray(node)) {
-                    for (const n of node) {
-                        this.getFoldLines(n, foldableTokens, autoFold)
-                    }
-                } else if (typeof (node) === "object") {
-                    for (const key in node) {
-                        this.getFoldLines(node[key], foldableTokens, autoFold)
-                    }
-                } else if (typeof (node) === "string") {
-                    if (node.split("\n").length > 1 && autoFold) {
-                        foldableTokens.push(node)
-                    }
+                if (autoFold) {
+                    this.editor.trigger("fold", "fold-multiline");
                 }
             },
             unfoldAll() {
-                this.ed.getSession().expandFolds(this.ed.getSession().getAllFolds())
+                this.editor.trigger("unfold", "editor.unfoldAll");
             },
             onInput(value) {
                 this.$emit("input", value);
             },
-            setEditorMaxLines(){
-                // We compute max lines to display in the editor and avoid page scroll
-                var viewPortHeight = (window.innerHeight - 290)
-                var fontSize = this.ed.getOption("fontSize");
-                var nbLines = Math.ceil(viewPortHeight / (fontSize + 2));
-                this.ed.setOptions({
-                    maxLines: nbLines
-                });
-            }
+            onPlaceholderClick() {
+                this.editor.layout()
+                this.editor.focus()
+            },
         },
     };
 </script>
-<style scoped>
+<style scoped lang="scss">
+@import "../../styles/variable";
+
 .navbar {
     border: 0;
 }
+/deep/ .editor-container {
+    //.monaco-editor .suggest-widget, .monaco-editor .suggest-details {
+    //    border-style: hidden;
+    //}
+
+    position: relative;
+    max-width: 100%;
+    display: flex;
+
+    .placeholder {
+        position: absolute;
+        top:  $input-padding-y ;
+        left: $input-padding-x;
+        overflow: hidden;
+        padding-left: inherit;
+        padding-right: inherit;
+        cursor: text;
+        user-select: none;
+        color: $input-placeholder-color;
+    }
+
+    .editor-with-placeholder {
+        ::v-deep .view-lines {
+            opacity: 0;
+        }
+    }
+
+    &.single-line {
+        padding: $input-padding-y $input-padding-x;
+        background: white;
+        border: 1px solid $input-border-color;
+        min-height: 38px;
+
+        &.theme-vs-dark {
+            background: #1e1e1e;
+        }
+    }
+}
+
 </style>
---
+++ ui/src/override/components/flows/TaskEdit.vue
@@ -0,0 +1,110 @@
+<template>
+    <b-modal
+        :id="modalId"
+        :title="`Task ${task.id}`"
+        header-bg-variant="dark"
+        header-text-variant="light"
+        hide-backdrop
+        modal-class="right"
+        size="xl"
+        @shown="onShow"
+    >
+        <template #modal-footer>
+            <b-button @click="saveTask" v-if="canSave">
+                <content-save />&nbsp;
+                <span>{{ $t('save') }}</span>
+            </b-button>
+        </template>
+
+        <editor
+            ref="editor"
+            @onSave="saveTask"
+            v-model="taskYaml"
+            theme="vs"
+            :full-height="false"
+            :navbar="false"
+            lang="yaml"
+        />
+    </b-modal>
+</template>
+<script>
+    import YamlUtils from "../../../utils/yamlUtils";
+    import Editor from "../../../components/inputs/Editor";
+    import ContentSave from "vue-material-design-icons/ContentSave";
+    import {canSaveFlowTemplate} from "../../../utils/flowTemplate";
+    import {mapState} from "vuex";
+
+    export default {
+        components: {
+            Editor,
+            ContentSave,
+        },
+        props: {
+            task: {
+                type: Object,
+                required: true
+            },
+            flowId: {
+                type: String,
+                required: true
+            },
+            namespace: {
+                type: String,
+                required: true
+            },
+            modalId: {
+                type: String,
+                required: true
+            },
+        },
+        methods: {
+            saveTask() {
+                let task;
+                try {
+                    task = YamlUtils.parse(this.taskYaml);
+                } catch (err) {
+                    this.$toast().warning(
+                        err.message,
+                        this.$t("invalid yaml"),
+                    );
+
+                    return;
+                }
+
+                return this.$store
+                    .dispatch("flow/updateFlowTask", {
+                        flow: {
+                            id: this.flowId,
+                            namespace: this.namespace
+                        },
+                        task: task
+                    })
+                    .then((response) => {
+                        this.$toast().saved(response.id);
+                    })
+            },
+            onShow() {
+                if (this.$refs.editor) {
+                    this.$refs.editor.onResize();
+                }
+            }
+        },
+        data() {
+            return {
+                taskYaml: undefined,
+            };
+        },
+        created() {
+            this.taskYaml = YamlUtils.stringify(this.task);
+        },
+        computed: {
+            ...mapState("auth", ["user"]),
+            canSave() {
+                return canSaveFlowTemplate(true, this.user, {namespace:this.namespace}, "flow");
+            }
+        }
+    };
+</script>
+<style scoped lang="scss">
+
+</style>

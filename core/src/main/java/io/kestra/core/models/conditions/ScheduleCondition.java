f989772507b7c18e0267947e98651c0076a1b2bf
tchiotludo <tchiot.ludo@gmail.com>
2021-09-08 12:42:08 +0200

    fix(core): missing jackson JsonTypeInfo annotation for ScheduleCondition

--- core/src/main/java/io/kestra/core/models/conditions/ScheduleCondition.java
+++ core/src/main/java/io/kestra/core/models/conditions/ScheduleCondition.java
@@ -1,7 +1,11 @@
 package io.kestra.core.models.conditions;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import io.kestra.core.exceptions.InternalException;
+import io.micronaut.core.annotation.Introspected;
 
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, property = "type", visible = true, include = JsonTypeInfo.As.EXISTING_PROPERTY)
+@Introspected
 public interface ScheduleCondition {
     boolean test(ConditionContext conditionContext) throws InternalException;
 }

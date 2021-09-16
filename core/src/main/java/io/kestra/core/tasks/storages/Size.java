392602dea1828c72057b735e631e2505a1d61de3
tchiotludo <tchiot.ludo@gmail.com>
2021-09-16 12:03:16 +0200

    feat(tasks): add a storage size task

---
+++ core/src/main/java/io/kestra/core/tasks/storages/Size.java
@@ -0,0 +1,61 @@
+package io.kestra.core.tasks.storages;
+
+import io.kestra.core.models.annotations.Example;
+import io.kestra.core.models.annotations.Plugin;
+import io.kestra.core.models.annotations.PluginProperty;
+import io.kestra.core.models.tasks.RunnableTask;
+import io.kestra.core.models.tasks.Task;
+import io.kestra.core.runners.RunContext;
+import io.kestra.core.storages.StorageInterface;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.net.URI;
+
+@SuperBuilder
+@ToString
+@EqualsAndHashCode
+@Getter
+@NoArgsConstructor
+@Schema(
+    title = "Get a filesize from internal storage."
+)
+@Plugin(
+    examples = {
+        @Example(
+            code = {
+                "uri: \"kestra://long/url/file.txt\""
+            }
+        )
+    }
+)
+public class Size extends Task implements RunnableTask<Size.Output> {
+    @Schema(
+        title = "the file",
+        description = "Must be a `kestra://` storage url"
+    )
+    @PluginProperty(dynamic = true)
+    private String uri;
+
+    @Override
+    public Size.Output run(RunContext runContext) throws Exception {
+        StorageInterface storageInterface = runContext.getApplicationContext().getBean(StorageInterface.class);
+        URI render = URI.create(runContext.render(this.uri));
+
+        Long size = storageInterface.size(render);
+
+        return Output.builder()
+            .size(size)
+            .build();
+    }
+
+    @Builder
+    @Getter
+    public static class Output implements io.kestra.core.models.tasks.Output {
+        @Schema(
+            title = "The size of the file "
+        )
+        private final Long size;
+    }
+}

9685d78003debadedc25a01b6920e26efd8b7512
tchiotludo <tchiot.ludo@gmail.com>
2021-10-14 17:49:53 +0200

    feat(core): add a configuration in order to delete configuration files on server startup

---
+++ cli/src/main/java/io/kestra/cli/listeners/DeleteConfigurationApplicationListeners.java
@@ -0,0 +1,39 @@
+package io.kestra.cli.listeners;
+
+import io.micronaut.context.annotation.Requires;
+import io.micronaut.context.env.Environment;
+import io.micronaut.context.event.StartupEvent;
+import io.micronaut.core.util.StringUtils;
+import io.micronaut.runtime.event.annotation.EventListener;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import static io.kestra.core.utils.Rethrow.throwConsumer;
+
+@Singleton
+@Slf4j
+@Requires(property = "kestra.configurations.delete-files-on-start", value = StringUtils.TRUE, defaultValue = StringUtils.FALSE)
+public class DeleteConfigurationApplicationListeners {
+    @Inject
+    Environment environment;
+
+    @EventListener
+    public void onStartupEvent(StartupEvent event) throws IOException {
+        environment.getPropertySources()
+            .forEach(throwConsumer(source -> {
+                Path path = Path.of(source.getName());
+
+                boolean exists = Files.exists(path);
+
+                if (exists) {
+                    Files.delete(path);
+                }
+            }));
+    }
+}
+

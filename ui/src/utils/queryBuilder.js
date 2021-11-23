835d5960024742b515b56348bac28232e5ecc2f7
tchiotludo <tchiot.ludo@gmail.com>
2021-11-23 22:15:23 +0100

    fix(ui): use better querystring search

--- ui/src/utils/queryBuilder.js
+++ ui/src/utils/queryBuilder.js
@@ -2,7 +2,15 @@ import moment from "moment";
 
 export default class QueryBuilder {
     static toLucene(q) {
-        return "(*" + q.split(/[\W_]+/g).join("* AND *") + "*)";
+        const split = q.split(/[^a-zA-Z0-9_.-]+/g);
+
+        let query = "(*" + split.join("*") + "*)^3 OR (*" + split.join("* AND *") + "*)";
+
+        if (split.length === 1 ) {
+            query = `(${q})^5 OR ${query}`
+        }
+
+        return query;
     }
 
     static iso(date) {

4fb6507e6b43ac6f04ff615bccf76396f3605d49
Ludovic DEHON <tchiot.ludo@gmail.com>
2022-02-10 16:19:35 +0100

    fix(core): loading template to early can lead to infinite wait startup on standalone
    since the FlowRepository need KafkaTemplateExecutor and it's not started when injecting flow

--- core/src/test/java/io/kestra/core/tasks/flows/TemplateTest.java
+++ core/src/test/java/io/kestra/core/tasks/flows/TemplateTest.java
@@ -4,6 +4,7 @@ import com.google.common.collect.ImmutableMap;
 import io.kestra.core.models.executions.LogEntry;
 import io.kestra.core.queues.QueueFactoryInterface;
 import io.kestra.core.queues.QueueInterface;
+import io.kestra.core.runners.ListenersTest;
 import io.kestra.core.tasks.debugs.Echo;
 import org.junit.jupiter.api.Test;
 import io.kestra.core.models.executions.Execution;
@@ -13,10 +14,13 @@ import io.kestra.core.runners.AbstractMemoryRunnerTest;
 import io.kestra.core.runners.RunnerUtils;
 import org.slf4j.event.Level;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.TimeoutException;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
@@ -29,6 +33,7 @@ public class TemplateTest extends AbstractMemoryRunnerTest {
     @Inject
     protected TemplateRepositoryInterface templateRepository;
 
+
     @Inject
     @Named(QueueFactoryInterface.WORKERTASKLOG_NAMED)
     protected QueueInterface<LogEntry> logQueue;
@@ -39,10 +44,9 @@ public class TemplateTest extends AbstractMemoryRunnerTest {
         .tasks(Collections.singletonList(Echo.builder().id("test").type(Echo.class.getName()).format("{{ parent.outputs.args['my-forward'] }}").build())).build();
 
     public static void withTemplate(RunnerUtils runnerUtils, TemplateRepositoryInterface templateRepository, QueueInterface<LogEntry> logQueue) throws TimeoutException {
-        templateRepository.create(TEMPLATE_1);
-
         List<LogEntry> logs = new ArrayList<>();
         logQueue.receive(logs::add);
+
 
         Execution execution = runnerUtils.runOne(
             "io.kestra.tests",
@@ -61,7 +65,10 @@ public class TemplateTest extends AbstractMemoryRunnerTest {
     }
 
     @Test
-    void withTemplate() throws TimeoutException {
+    void withTemplate() throws TimeoutException, IOException, URISyntaxException {
+        templateRepository.create(TEMPLATE_1);
+        repositoryLoader.load(Objects.requireNonNull(ListenersTest.class.getClassLoader().getResource("flows/tests/with-template.yaml")));
+
         TemplateTest.withTemplate(runnerUtils, templateRepository, logQueue);
     }
 }

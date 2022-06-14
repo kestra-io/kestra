8a3bae4e008a624a4b3898336cb930edb67b885d
Ludovic DEHON <tchiot.ludo@gmail.com>
2022-06-14 12:34:16 +0200

    feat(jdbc): add metrics & logs to sql query

--- jdbc/src/main/java/io/kestra/jdbc/runner/JdbcQueueService.java
+++ jdbc/src/main/java/io/kestra/jdbc/runner/JdbcQueueService.java
@@ -3,11 +3,13 @@ package io.kestra.jdbc.runner;
 import io.kestra.core.queues.QueueService;
 import io.kestra.core.runners.ExecutionDelay;
 import io.micronaut.context.annotation.Replaces;
+import io.micronaut.context.annotation.Requires;
 import jakarta.inject.Singleton;
 
 @Singleton
 @Replaces(QueueService.class)
-public class JdbcQueueService extends QueueService{
+@Requires(property = "kestra.queue.type", pattern = "mysql|postgres|h2")
+public class JdbcQueueService extends QueueService {
     public String key(Object object) {
         if (object.getClass() == JdbcExecutorState.class) {
             return ((JdbcExecutorState) object).getExecutionId();

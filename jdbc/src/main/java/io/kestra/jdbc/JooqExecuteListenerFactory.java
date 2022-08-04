48b72f06256398e303cfef15c2516de2d0f0399d
Ludovic DEHON <tchiot.ludo@gmail.com>
2022-08-04 22:12:30 +0200

    fix(jdbc): restore metric on jdbc queries

---
+++ jdbc/src/main/java/io/kestra/jdbc/JooqExecuteListenerFactory.java
@@ -0,0 +1,52 @@
+package io.kestra.jdbc;
+
+import io.kestra.core.metrics.MetricRegistry;
+import io.micronaut.context.annotation.EachBean;
+import io.micronaut.context.annotation.Factory;
+import lombok.extern.slf4j.Slf4j;
+import org.jooq.ExecuteContext;
+import org.jooq.ExecuteListener;
+import org.jooq.impl.DefaultExecuteListener;
+
+import java.time.Duration;
+import javax.sql.DataSource;
+import javax.validation.constraints.NotNull;
+
+@Slf4j
+@Factory
+public class JooqExecuteListenerFactory {
+    @EachBean(DataSource.class)
+    public org.jooq.ExecuteListenerProvider jooqConfiguration(MetricRegistry metricRegistry) {
+        return new org.jooq.ExecuteListenerProvider() {
+            @Override
+            public @NotNull ExecuteListener provide() {
+                return new DefaultExecuteListener() {
+                    Long startTime;
+
+                    @Override
+                    public void executeStart(ExecuteContext ctx) {
+                        super.executeStart(ctx);
+                        startTime = System.currentTimeMillis();
+                    }
+
+                    @Override
+                    public void executeEnd(ExecuteContext ctx) {
+                        super.executeEnd(ctx);
+
+                        Duration duration = Duration.ofMillis(System.currentTimeMillis() - startTime);
+
+                        metricRegistry.timer(MetricRegistry.JDBC_QUERY_DURATION, "sql", ctx.sql())
+                            .record(duration);
+
+                        if (log.isTraceEnabled()) {
+                            log.trace("[Duration: {}] [Rows: {}] [Query: {}]", duration, ctx.rows() , ctx.query().toString());
+                        } else if (log.isDebugEnabled()) {
+                            log.debug("[Duration: {}] [Rows: {}] [Query: {}]", duration, ctx.rows() , ctx.sql());
+                        }
+                    }
+                };
+            }
+        };
+    }
+
+}

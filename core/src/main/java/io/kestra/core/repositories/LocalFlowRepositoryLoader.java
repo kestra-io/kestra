dbfe207da0af3aa772c80dcf2614b4eb0ff86eb7
Ludovic DEHON <tchiot.ludo@gmail.com>
2022-08-29 20:48:05 +0200

    feat(core): handle core test included from jar

--- core/src/main/java/io/kestra/core/repositories/LocalFlowRepositoryLoader.java
+++ core/src/main/java/io/kestra/core/repositories/LocalFlowRepositoryLoader.java
@@ -6,15 +6,20 @@ import io.kestra.core.serializers.YamlFlowParser;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.nio.file.*;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
+import org.apache.commons.io.FileUtils;
+
 import javax.validation.ConstraintViolationException;
+
+import static io.kestra.core.utils.Rethrow.throwConsumer;
 
 @Singleton
 @Slf4j
@@ -26,7 +31,29 @@ public class LocalFlowRepositoryLoader {
     private FlowRepositoryInterface flowRepository;
 
     public void load(URL basePath) throws IOException, URISyntaxException {
-        this.load(new File(basePath.toURI()));
+        URI uri = basePath.toURI();
+
+        if (uri.getScheme().equals("jar")) {
+            try (FileSystem fileSystem = FileSystems.newFileSystem(uri, Collections.emptyMap())) {
+                String substring = uri.toString().substring(uri.toString().indexOf("!") + 1);
+
+                Path tempDirectory = Files.createTempDirectory("loader");
+
+                for (Path path1 : fileSystem.getRootDirectories()) {
+                    Files
+                        .walk(path1)
+                        .filter(path -> Files.isRegularFile(path) && path.startsWith(substring))
+                        .forEach(throwConsumer(path -> FileUtils.copyURLToFile(
+                            path.toUri().toURL(),
+                            tempDirectory.resolve(path.toString().substring(1)).toFile())
+                        ));
+                }
+
+                this.load(tempDirectory.toFile());
+            }
+        } else {
+            this.load(Paths.get(uri).toFile());
+        }
     }
 
     public void load(File basePath) throws IOException {

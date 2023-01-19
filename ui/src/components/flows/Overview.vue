885cd0d943c153f9cc914838049292cc927f8578
Yann C <yann.coornaert62@orange.fr>
2023-01-19 14:57:06 +0100

    feat(): Added overview in flow + home in menu

---
+++ ui/src/components/flows/Overview.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <home
+            ref="home"
+            v-if="flow"
+            :namespace="flow.namespace"
+            :flow-id="flow.id"
+            :prevent-route-info="true"
+            :restore-url="false"
+        />
+    </div>
+</template>
+
+<script>
+    import Home from "../home/Home.vue";
+    import {mapGetters} from "vuex";
+
+    export default {
+        components: {
+            Home
+        },
+        computed: {
+            ...mapGetters("flow", ["flow"]),
+        }
+    }
+</script>

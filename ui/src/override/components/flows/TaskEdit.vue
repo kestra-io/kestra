941242084cdb97038e3a60c7416ed9636c5e457b
Yann C <yann.coornaert62@orange.fr>
2023-01-24 09:29:46 +0100

    feat(): removed noise

--- ui/src/override/components/flows/TaskEdit.vue
+++ ui/src/override/components/flows/TaskEdit.vue
@@ -34,92 +34,91 @@
 </template>
 
 <script setup>
-import CodeTags from "vue-material-design-icons/CodeTags.vue";
-import ContentSave from "vue-material-design-icons/ContentSave.vue";
+    import CodeTags from "vue-material-design-icons/CodeTags.vue";
+    import ContentSave from "vue-material-design-icons/ContentSave.vue";
 </script>
 
 <script>
-import YamlUtils from "../../../utils/yamlUtils";
-import Editor from "../../../components/inputs/Editor.vue";
-import {canSaveFlowTemplate, saveFlowTemplate} from "../../../utils/flowTemplate";
-import {mapGetters, mapState} from "vuex";
-import Utils from "../../../utils/utils";
+    import YamlUtils from "../../../utils/yamlUtils";
+    import Editor from "../../../components/inputs/Editor.vue";
+    import {canSaveFlowTemplate, saveFlowTemplate} from "../../../utils/flowTemplate";
+    import {mapGetters, mapState} from "vuex";
+    import Utils from "../../../utils/utils";
 
-export default {
-    components: {Editor},
-    props: {
-        component: {
-            type: String,
-            default: "el-button"
+    export default {
+        components: {Editor},
+        props: {
+            component: {
+                type: String,
+                default: "el-button"
+            },
+            task: {
+                type: Object,
+                default: undefined
+            },
+            taskId: {
+                type: String,
+                default: undefined
+            },
+            flowId: {
+                type: String,
+                required: true
+            },
+            namespace: {
+                type: String,
+                required: true
+            },
         },
-        task: {
-            type: Object,
-            default: undefined
-        },
-        taskId: {
-            type: String,
-            default: undefined
-        },
-        flowId: {
-            type: String,
-            required: true
-        },
-        namespace: {
-            type: String,
-            required: true
-        },
-    },
-    methods: {
-        load(taskId) {
-            return YamlUtils.extractTask(this.sourceCode, taskId);
-        },
-        saveTask() {
-            let task;
-            let updatedSource;
-            try {
-                task = YamlUtils.parse(this.taskYaml);
-                updatedSource = YamlUtils.replaceTaskInDocument(this.sourceCode, this.taskIndex, this.taskYaml)
-            } catch (err) {
-                this.$toast().warning(
-                    err.message,
-                    this.$t("invalid yaml"),
-                );
+        methods: {
+            load(taskId) {
+                return YamlUtils.extractTask(this.sourceCode, taskId);
+            },
+            saveTask() {
+                let task;
+                let updatedSource;
+                try {
+                    updatedSource = YamlUtils.replaceTaskInDocument(this.sourceCode, this.taskIndex, this.taskYaml)
+                } catch (err) {
+                    this.$toast().warning(
+                        err.message,
+                        this.$t("invalid yaml"),
+                    );
 
-                return;
+                    return;
+                }
+                saveFlowTemplate(this, updatedSource, "flow")
+                    .then((response) => {
+                        this.isModalOpen = false;
+                    })
+            },
+            onShow() {
+                this.isModalOpen = !this.isModalOpen;
+                if (this.taskId || this.task.id) {
+                    const value = this.load(this.taskId ? this.taskId : this.task.id)
+                    this.taskIndex = value.index
+                    this.taskYaml = value.task;
+                } else {
+                    this.taskYaml = YamlUtils.stringify(this.task);
+                }
+            },
+        },
+        data() {
+            return {
+                uuid: Utils.uid(),
+                taskYaml: undefined,
+                taskIndex: undefined,
+                isModalOpen: false,
+            };
+        },
+        created() {
+
+        },
+        computed: {
+            ...mapGetters("flow", ["sourceCode"]),
+            ...mapState("auth", ["user"]),
+            canSave() {
+                return canSaveFlowTemplate(true, this.user, {namespace: this.namespace}, "flow");
             }
-            saveFlowTemplate(this, updatedSource, "flow")
-                .then((response) => {
-                    this.isModalOpen = false;
-                })
-        },
-        onShow() {
-            this.isModalOpen = !this.isModalOpen;
-            if (this.taskId || this.task.id) {
-                const value = this.load(this.taskId ? this.taskId : this.task.id)
-                this.taskIndex = value.index
-                this.taskYaml = value.task;
-            } else {
-                this.taskYaml = YamlUtils.stringify(this.task);
-            }
-        },
-    },
-    data() {
-        return {
-            uuid: Utils.uid(),
-            taskYaml: undefined,
-            taskIndex: undefined,
-            isModalOpen: false,
-        };
-    },
-    created() {
-
-    },
-    computed: {
-        ...mapGetters("flow", ["sourceCode"]),
-        ...mapState("auth", ["user"]),
-        canSave() {
-            return canSaveFlowTemplate(true, this.user, {namespace: this.namespace}, "flow");
         }
-    }
-};
+    };
 </script>

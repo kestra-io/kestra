507942c62bf7bb90e9da7bb27284de573efc4aac
Loïc Mathieu <loikeseke@gmail.com>
2023-01-25 11:19:28 +0100

    fix: add more tests

--- core/src/test/java/io/kestra/core/tasks/flows/CurrentOutputFunctionTest.java
+++ core/src/test/java/io/kestra/core/tasks/flows/CurrentOutputFunctionTest.java
@@ -15,20 +15,24 @@ public class CurrentOutputFunctionTest  extends AbstractMemoryRunnerTest {
     void parallel() throws TimeoutException {
         Execution execution = runnerUtils.runOne("io.kestra.tests", "test-current-output");
 
-        var output = (Map<String, Object>) execution.outputs().get("1-1-1_return");
-        var outputv11 = (Map<String, Object>) output.get("v11");
+        var output1 = (Map<String, Object>) execution.outputs().get("1-1-1_return");
+        var outputv11 = (Map<String, Object>) output1.get("v11");
         var outputv11v21 = (Map<String, Object>) outputv11.get("v21");
         assertThat(((Map<String, Object>) outputv11v21.get("v31")).get("value"), is(equalTo("return-v11-v21-v31")));
         assertThat(((Map<String, Object>) outputv11v21.get("v32")).get("value"), is(equalTo("return-v11-v21-v32")));
         var outputv11v22 = (Map<String, Object>) outputv11.get("v22");
         assertThat(((Map<String, Object>) outputv11v22.get("v31")).get("value"), is(equalTo("return-v11-v22-v31")));
         assertThat(((Map<String, Object>) outputv11v22.get("v32")).get("value"), is(equalTo("return-v11-v22-v32")));
-        var outputv12 = (Map<String, Object>) output.get("v12");
+        var outputv12 = (Map<String, Object>) output1.get("v12");
         var outputv12v21 = (Map<String, Object>) outputv12.get("v21");
         assertThat(((Map<String, Object>) outputv12v21.get("v31")).get("value"), is(equalTo("return-v12-v21-v31")));
         assertThat(((Map<String, Object>) outputv12v21.get("v32")).get("value"), is(equalTo("return-v12-v21-v32")));
         var outputv12v22 = (Map<String, Object>) outputv12.get("v22");
         assertThat(((Map<String, Object>) outputv12v22.get("v31")).get("value"), is(equalTo("return-v12-v22-v31")));
         assertThat(((Map<String, Object>) outputv12v22.get("v32")).get("value"), is(equalTo("return-v12-v22-v32")));
+
+        var output2 = (Map<String, Object>) execution.outputs().get("2-1_return");
+        assertThat(((Map<String, Object>) output2.get("v41")).get("value"), is(equalTo("return-v41")));
+        assertThat(((Map<String, Object>) output2.get("v42")).get("value"), is(equalTo("return-v42")));
     }
 }

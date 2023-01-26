5c30c0566f76db881104dc74f9f3e26496247d31
Loïc Mathieu <loikeseke@gmail.com>
2023-01-26 11:40:46 +0100

    feat(core): add variable function currentEachOutput() (#924)
    Co-authored-by: Ludovic DEHON <tchiot.ludo@gmail.com>

---
+++ core/src/main/java/io/kestra/core/runners/pebble/functions/CurrentEachOutputFunction.java
@@ -0,0 +1,42 @@
+package io.kestra.core.runners.pebble.functions;
+
+import io.pebbletemplates.pebble.error.PebbleException;
+import io.pebbletemplates.pebble.extension.Function;
+import io.pebbletemplates.pebble.template.EvaluationContext;
+import io.pebbletemplates.pebble.template.PebbleTemplate;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+public class CurrentEachOutputFunction implements Function {
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public Object execute(Map<String, Object> args, PebbleTemplate self, EvaluationContext context, int lineNumber) {
+        if (!args.containsKey("outputs")) {
+            throw new PebbleException(null, "The 'currentEachOutput' function expects an argument 'outputs'.", lineNumber, self.getName());
+        }
+
+        if (!(args.get("outputs") instanceof Map)) {
+            throw new PebbleException(null, "The 'currentEachOutput' function expects an argument 'outputs' with type map.", lineNumber, self.getName());
+        }
+
+        Map<?, ?> outputs = (Map<?, ?>) args.get("outputs");
+        List<Map<?, ?>> parents = (List<Map<?, ?>>) context.getVariable("parents");
+        if (parents != null && !parents.isEmpty()) {
+            Collections.reverse(parents);
+            for (Map<?, ?> parent : parents) {
+                outputs = (Map<?, ?>) outputs.get(((Map<?, ?>) parent.get("taskrun")).get("value"));
+            }
+        }
+        Map<?, ?> taskrun = (Map<?, ?>) context.getVariable("taskrun");
+
+        return outputs.get(taskrun.get("value"));
+    }
+
+    @Override
+    public List<String> getArgumentNames() {
+        return List.of("outputs");
+    }
+}

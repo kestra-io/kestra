9f305390dbe7534be969b9504cbd0662ad87548d
Loïc Mathieu <loikeseke@gmail.com>
2023-01-30 14:52:20 +0100

    fix(core): handle allOf and $ref type

--- core/src/test/java/io/kestra/core/docs/JsonSchemaGeneratorTest.java
+++ core/src/test/java/io/kestra/core/docs/JsonSchemaGeneratorTest.java
@@ -1,6 +1,18 @@
 package io.kestra.core.docs;
 
+import io.kestra.core.models.annotations.Example;
+import io.kestra.core.models.annotations.PluginProperty;
+import io.kestra.core.models.tasks.RunnableTask;
+import io.kestra.core.models.tasks.VoidOutput;
+import io.kestra.core.runners.RunContext;
+import io.kestra.core.tasks.scripts.ScriptOutput;
 import io.kestra.core.Helpers;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
 import io.kestra.core.models.flows.Flow;
 import io.kestra.core.models.tasks.Task;
 import io.kestra.core.plugins.PluginScanner;
@@ -109,8 +121,55 @@ class JsonSchemaGeneratorTest {
         });
     }
 
+    @Test
+    void testEnum() {
+        Map<String, Object> generate = jsonSchemaGenerator.properties(Task.class, TaskWithEnum.class);
+        System.out.println(generate);
+        assertThat(generate, is(not(nullValue())));
+        assertThat(((Map<String, Map<String, Object>>) generate.get("properties")).size(), is(3));
+    }
+
     @SuppressWarnings("unchecked")
     private Map<String, Map<String, Object>> properties(Map<String, Object> generate) {
         return (Map<String, Map<String, Object>>) generate.get("properties");
+    }
+
+    @SuperBuilder
+    @ToString
+    @EqualsAndHashCode
+    @Getter
+    @NoArgsConstructor
+    private static class TaskWithEnum extends Task implements RunnableTask<VoidOutput>  {
+
+        @PluginProperty
+        @Schema(title = "Title from the attribute")
+        private TestEnum testEnum;
+
+        @PluginProperty
+        @Schema(title = "Title from the attribute")
+        private TestClass testClass;
+
+        @PluginProperty
+        @Schema(
+            title = "Title from the attribute",
+            anyOf = {String.class, Example[].class, Example.class}
+        )
+        private Object testObject;
+
+        @Override
+        public VoidOutput run(RunContext runContext) throws Exception {
+            return null;
+        }
+
+        @Schema(title = "Title from the enum")
+        private enum TestEnum {
+            VALUE1, VALUE2, VALUE3
+        }
+
+        @Schema(title = "Test class")
+        private class TestClass {
+            @Schema(title = "Test property")
+            public String testProperty;
+        }
     }
 }

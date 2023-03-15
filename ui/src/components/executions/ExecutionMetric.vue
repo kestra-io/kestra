fcdf9116f1f4d491ebf87a2356e5282808ff0782
Yann C <yann.coornaert62@orange.fr>
2023-03-15 11:37:58 +0100

    feat: pagination for metrics tab

--- ui/src/components/executions/ExecutionMetric.vue
+++ ui/src/components/executions/ExecutionMetric.vue
@@ -1,6 +1,11 @@
 <template>
-    <div v-if="execution">
-        <collapse>
+    <data-table
+        v-if="execution"
+        @page-changed="onPageChanged"
+        ref="dataTable"
+        :total="total"
+    >
+        <template #navbar>
             <el-form-item>
                 <el-select
                     filterable
@@ -17,74 +22,77 @@
                     />
                 </el-select>
             </el-form-item>
-        </collapse>
-
-        <el-table
-            :data="metricsData"
-            ref="table"
-            :default-sort="{prop: 'name', order: 'ascending'}"
-            stripe
-            table-layout="auto"
-            fixed
-        >
-            <el-table-column prop="task" sortable :label="$t('task')">
-                <template #default="scope">
-                    <p>{{ scope.row.taskId }}</p>
-                </template>
-            </el-table-column>
-            <el-table-column prop="name" sortable :label="$t('name')">
-                <template #default="scope">
-                    <template v-if="scope.row.type === 'timer'">
-                        <kicon><timer /></kicon>
+        </template>
+        <template #table>
+            <el-table
+                :data="metricsData"
+                ref="table"
+                :default-sort="{prop: 'name', order: 'ascending'}"
+                stripe
+                table-layout="auto"
+                fixed
+            >
+                <el-table-column prop="task" sortable :label="$t('task')">
+                    <template #default="scope">
+                        <p>{{ scope.row.taskId }}</p>
                     </template>
-                    <template v-else>
-                        <kicon><counter /></kicon>
+                </el-table-column>
+                <el-table-column prop="name" sortable :label="$t('name')">
+                    <template #default="scope">
+                        <template v-if="scope.row.type === 'timer'">
+                            <kicon><timer /></kicon>
+                        </template>
+                        <template v-else>
+                            <kicon><counter /></kicon>
+                        </template>
+                        &nbsp;<code>{{ scope.row.name }}</code>
                     </template>
-                    &nbsp;<code>{{ scope.row.name }}</code>
-                </template>
-            </el-table-column>
+                </el-table-column>
 
-            <el-table-column prop="tags" sortable :label="$t('tags')">
-                <template #default="scope">
-                    <el-tag
-                        v-for="(value, key) in scope.row.tags"
-                        :key="key"
-                        class="me-1"
-                        type="info"
-                        size="small"
-                        disable-transitions
-                    >
-                        {{ key }}: <strong>{{ value }}</strong>
-                    </el-tag>
-                </template>
-            </el-table-column>
+                <el-table-column prop="tags" sortable :label="$t('tags')">
+                    <template #default="scope">
+                        <el-tag
+                            v-for="(value, key) in scope.row.tags"
+                            :key="key"
+                            class="me-1"
+                            type="info"
+                            size="small"
+                            disable-transitions
+                        >
+                            {{ key }}: <strong>{{ value }}</strong>
+                        </el-tag>
+                    </template>
+                </el-table-column>
 
-            <el-table-column prop="value" sortable :label="$t('value')">
-                <template #default="scope">
-                    <span v-if="scope.row.type === 'timer'">
-                        {{ $filters.humanizeDuration(scope.row.value / 1000) }}
-                    </span>
-                    <span v-else>
-                        {{ $filters.humanizeNumber(scope.row.value) }}
-                    </span>
-                </template>
-            </el-table-column>
-        </el-table>
-    </div>
+                <el-table-column prop="value" sortable :label="$t('value')">
+                    <template #default="scope">
+                        <span v-if="scope.row.type === 'timer'">
+                            {{ $filters.humanizeDuration(scope.row.value / 1000) }}
+                        </span>
+                        <span v-else>
+                            {{ $filters.humanizeNumber(scope.row.value) }}
+                        </span>
+                    </template>
+                </el-table-column>
+            </el-table>
+        </template>
+    </data-table>
 </template>
 <script>
     import {mapState} from "vuex";
-    import Collapse from "../layout/Collapse.vue";
     import Kicon from "../Kicon.vue";
     import Timer from "vue-material-design-icons/Timer.vue";
     import Counter from "vue-material-design-icons/Numeric.vue";
+    import DataTable from "../layout/DataTable.vue";
+    import DataTableActions from "../../mixins/dataTableActions";
 
     export default {
+        mixins: [DataTableActions],
         components: {
-            Collapse,
             Kicon,
             Timer,
             Counter,
+            DataTable
         },
         data() {
             return {
@@ -93,11 +101,11 @@
                 isJson: false,
                 debugError: "",
                 debugStackTrace: "",
-                isModalOpen: false,
+                isModalOpen: false
             };
         },
         mounted() {
-            this.loadMetrics();
+            this.loadData();
         },
         created() {
             if (this.$route.query.search) {
@@ -105,10 +113,15 @@
             }
         },
         methods: {
-            loadMetrics() {
+            loadData(callback) {
+                const params = {
+                    size: parseInt(this.$route.query.size || 25),
+                    page: parseInt(this.$route.query.page || 1)
+                }
                 this.$store.dispatch("execution/loadMetrics", {
                     executionId: this.$route.params.id,
-                })
+                    params: params
+                }).then(metrics => callback())
             },
         },
         computed: {
@@ -126,10 +139,12 @@
             },
             metricsData(){
                 if(this.filter){
-                    console.log(this.filter)
                     return this.metrics.filter(metric => metric.taskRunId === this.filter)
                 }
                 return this.metrics
+            },
+            total(){
+                return this.metrics.length
             }
         }
     };
--- ui/src/stores/executions.js
+++ ui/src/stores/executions.js
@@ -7,7 +7,8 @@ export default {
         task: undefined,
         total: 0,
         logs: [],
-        metrics: []
+        metrics: [],
+        metricsTotal: 0,
     },
     actions: {
         loadExecutions({commit}, options) {
@@ -28,13 +29,13 @@ export default {
         },
         bulkRestartExecution(_, options) {
             return this.$http.post(
-                `/api/v1/executions/restart/by-ids`,
+                "/api/v1/executions/restart/by-ids",
                 options.executionsId
             )
         },
         queryRestartExecution(_, options) {
             return this.$http.post(
-                `/api/v1/executions/restart/by-query`,
+                "/api/v1/executions/restart/by-query",
                 {},
                 {params: options}
             )
@@ -62,10 +63,10 @@ export default {
             return this.$http.delete(`/api/v1/executions/${options.id}/kill`);
         },
         bulkKill(_, options) {
-            return this.$http.delete(`/api/v1/executions/kill/by-ids`, {data: options.executionsId});
+            return this.$http.delete("/api/v1/executions/kill/by-ids", {data: options.executionsId});
         },
         queryKill(_, options) {
-            return this.$http.delete(`/api/v1/executions/kill/by-query`, {params: options});
+            return this.$http.delete("/api/v1/executions/kill/by-query", {params: options});
         },
         loadExecution({commit}, options) {
             return this.$http.get(`/api/v1/executions/${options.id}`).then(response => {
@@ -94,10 +95,10 @@ export default {
             })
         },
         bulkDeleteExecution({commit}, options) {
-            return this.$http.delete(`/api/v1/executions/by-ids`, {data: options.executionsId})
+            return this.$http.delete("/api/v1/executions/by-ids", {data: options.executionsId})
         },
         queryDeleteExecution({commit}, options) {
-            return this.$http.delete(`/api/v1/executions/by-query`, {params: options})
+            return this.$http.delete("/api/v1/executions/by-query", {params: options})
         },
         followExecution(_, options) {
             return new EventSource(`${this.$http.defaults.baseURL}api/v1/executions/${options.id}/follow`);
@@ -116,7 +117,8 @@ export default {
             return this.$http.get(`/api/v1/metrics/${options.executionId}`, {
                 params: options.params
             }).then(response => {
-                commit("setMetrics", response.data)
+                commit("setMetrics", response.data.results)
+                commit("setMetricsTotal", response.data.total)
             })
         }
     },
@@ -141,6 +143,9 @@ export default {
         },
         setMetrics(state, metrics) {
             state.metrics = metrics
+        },
+        setMetricsTotal(state, metrics) {
+            state.metricsTotal = metrics
         },
         appendLogs(state, logs) {
             state.logs.push(logs);

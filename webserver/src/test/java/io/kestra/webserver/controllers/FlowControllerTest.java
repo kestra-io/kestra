31138e569d00a4aab9826505e177c5f77a741c66
Ludovic DEHON <tchiot.ludo@gmail.com>
2023-03-16 20:22:35 +0100

    fix(core): source is not well formatted when submit with json

--- webserver/src/test/java/io/kestra/webserver/controllers/FlowControllerTest.java
+++ webserver/src/test/java/io/kestra/webserver/controllers/FlowControllerTest.java
@@ -310,6 +310,20 @@ class FlowControllerTest extends AbstractMemoryRunnerTest {
     }
 
     @Test
+    void updateFlowMultilineJson() {
+        String flowId = IdUtils.create();
+
+        Flow flow = generateFlowWithFlowable(flowId, "io.kestra.unittest", "\n \n a         \nb\nc");
+
+        Flow result = client.toBlocking().retrieve(POST("/api/v1/flows", flow), Flow.class);
+        assertThat(result.getId(), is(flow.getId()));
+
+        FlowWithSource withSource = client.toBlocking().retrieve(GET("/api/v1/flows/" + flow.getNamespace() + "/" + flow.getId() + "?source=true").contentType(MediaType.APPLICATION_YAML), FlowWithSource.class);
+        assertThat(withSource.getId(), is(flow.getId()));
+        assertThat(withSource.getSource(), containsString("format: |2-"));
+    }
+
+    @Test
     void updateTaskFlow() throws InternalException {
         String flowId = IdUtils.create();
 

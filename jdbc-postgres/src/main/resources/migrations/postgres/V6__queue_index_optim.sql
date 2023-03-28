6884dfc46a7527c39c5a80c3683f9640027d2d20
Andrey <stdrone@ya.ru>
2023-03-28 11:41:58 +0300

    fix posgres migration with non default schema

--- jdbc-postgres/src/main/resources/migrations/postgres/V6__queue_index_optim.sql
+++ jdbc-postgres/src/main/resources/migrations/postgres/V6__queue_index_optim.sql
@@ -1,7 +1,7 @@
 -- We drop the PK and the queues_type__offset, otherwise they are used by the poll query which is sub-optimal.
 -- We create an hash index on offset that will be used instead when filtering on offset.
 
-ALTER TABLE public.queues DROP CONSTRAINT queues_pkey;
+ALTER TABLE queues DROP CONSTRAINT queues_pkey;
 
 DROP INDEX queues_type__offset;
 

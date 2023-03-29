8de4e2a82de1d2618dbb93ece7b614e18cf29ee8
Loïc Mathieu <loikeseke@gmail.com>
2023-03-29 09:35:51 +0200

    fix(jdbc-postgres): fix migration with schema
    Migrations must not have a schema to be able to use a different schema than public.

--- jdbc-postgres/src/main/resources/migrations/postgres/V11__queue_index_optim.sql
+++ jdbc-postgres/src/main/resources/migrations/postgres/V11__queue_index_optim.sql
@@ -1,8 +1,8 @@
 -- We drop the PK and the queues_type__offset, otherwise they are used by the poll query which is sub-optimal.
 -- We create an hash index on offset that will be used instead when filtering on offset.
 
-ALTER TABLE public.queues DROP CONSTRAINT queues_pkey;
+ALTER TABLE queues DROP CONSTRAINT IF EXISTS queues_pkey;
 
-DROP INDEX queues_type__offset;
+DROP INDEX IF EXISTS queues_type__offset;
 
-CREATE INDEX queues_offset ON public.queues USING hash ("offset");
+CREATE INDEX IF NOT EXISTS queues_offset ON public.queues USING hash ("offset");

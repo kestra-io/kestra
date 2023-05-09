f1eb5b9ef64bc416fb865e78e0187fe4a12aad44
brian.mulier <bmmulier@hotmail.fr>
2023-05-09 21:34:59 +0200

    feat: Authentication expiration & failure handled

--- ui/src/utils/axios.js
+++ ui/src/utils/axios.js
@@ -96,9 +96,9 @@ export default (callback, store, router) => {
                 return Promise.reject(errorResponse);
             }
 
-            if (errorResponse.response.status === 401 && (!store.getters["auth/isLogged"] || store.getters["auth/expired"])) {
-                window.location = "/ui/login?from=" + window.location.pathname +
-                    (window.location.search ? "?" + window.location.search : "")
+            if (errorResponse.response.status === 401 && !store.getters["auth/isLogged"]) {
+                window.location = `/ui/login?from=${window.location.pathname +
+                (window.location.search ? "?" + window.location.search : "")}`
             }
 
             if (errorResponse.response.status === 401 &&
@@ -111,6 +111,23 @@ export default (callback, store, router) => {
                 return Promise.reject(errorResponse);
             }
 
+            // Authentication expired
+            if (errorResponse.response.status === 401 &&
+                store.getters["auth/isLogged"]) {
+                document.body.classList.add("login")
+
+                store.dispatch("core/isUnsaved", false);
+                store.commit("auth/setUser", undefined);
+                store.commit("layout/setTopNavbar", undefined);
+                router.push({
+                    name: "login",
+                    query: {
+                        expired: 1,
+                        from: window.location.pathname + (window.location.search ? "?" + window.location.search : "")
+                    }
+                })
+            }
+
             if (errorResponse.response.status === 400){
                 return Promise.reject(errorResponse.response.data)
             }
@@ -121,11 +138,6 @@ export default (callback, store, router) => {
                     content: errorResponse.response.data,
                     variant: "error"
                 })
-
-                if(errorResponse.response.status === 401 &&
-                    store.getters["auth/isLogged"]){
-                    store.commit("auth/setExpired", true);
-                }
 
                 return Promise.reject(errorResponse);
             }
--- ui/src/utils/toast.js
+++ ui/src/utils/toast.js
@@ -27,7 +27,7 @@ export default {
                         return h("span", {innerHTML: message});
                     }
                 },
-                confirm: function(message, callback, callbackIfCancel = () => {}) {
+                confirm: function(message, callback) {
                     ElMessageBox.confirm(
                         this._wrap(message || self.$t("toast confirm")),
                         self.$t("confirmation"),
@@ -37,9 +37,6 @@ export default {
                     )
                         .then(() => {
                             callback();
-                        })
-                        .catch(() => {
-                            callbackIfCancel()
                         })
                 },
                 saved: function(name, title, options) {

467003be0f94c78a21708037d578e6b4beabb98d
YannC <37600690+Skraye@users.noreply.github.com>
2023-06-30 12:36:18 +0200

    fix(ui): prevent recursive flow calls (#1646)
    * fix(ui): Prevent recursive flow calls
    
    close #1474

--- core/src/main/java/io/kestra/core/tasks/flows/Flow.java
+++ core/src/main/java/io/kestra/core/tasks/flows/Flow.java
@@ -15,11 +15,11 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
+import javax.annotation.Nullable;
+import javax.validation.constraints.NotNull;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
-import javax.annotation.Nullable;
-import javax.validation.constraints.NotNull;
 
 @SuperBuilder
 @ToString

c3932700888995bc66a38cf40ea99c23b844e382
brian.mulier <bmmulier@hotmail.fr>
2023-07-03 17:13:16 +0200

    fix(core): add secrets cache
    closes #1679

--- core/src/main/java/io/kestra/core/secret/SecretService.java
+++ core/src/main/java/io/kestra/core/secret/SecretService.java
@@ -3,15 +3,27 @@ package io.kestra.core.secret;
 import io.kestra.core.exceptions.IllegalVariableEvaluationException;
 import jakarta.inject.Singleton;
 
+import javax.annotation.PostConstruct;
 import java.util.Base64;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Singleton
 public class SecretService {
+    private static final String SECRET_PREFIX = "SECRETS_";
+    private Map<String, String> decodedSecrets;
+
+    @PostConstruct
+    private void postConstruct() {
+        decodedSecrets = System.getenv().entrySet().stream().filter(entry -> entry.getKey().startsWith(SECRET_PREFIX)).collect(Collectors.toMap(
+            entry -> entry.getKey().substring(SECRET_PREFIX.length()).toUpperCase(),
+            entry -> new String(Base64.getDecoder().decode(entry.getValue()))
+        ));
+    }
+
     public String findSecret(String key) throws IllegalVariableEvaluationException {
-        String environmentVariable = Optional.ofNullable(System.getenv("SECRETS_" + key.toUpperCase()))
-            .orElseThrow(() -> new IllegalVariableEvaluationException("Unable to find secret '" + key + "'. " +
-                "You should add it in your environment variables as 'SECRETS_" + key.toUpperCase()+"' with base64-encoded value."));
-        return new String(Base64.getDecoder().decode(environmentVariable));
+        return Optional.ofNullable(decodedSecrets.get(key.toUpperCase())).orElseThrow(() -> new IllegalVariableEvaluationException("Unable to find secret '" + key + "'. " +
+            "You should add it in your environment variables as 'SECRETS_" + key.toUpperCase() + "' with base64-encoded value."));
     }
 }

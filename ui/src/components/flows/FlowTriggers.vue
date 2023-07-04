293e177e0dad9551fe191283f055db2d6f25a276
Ludovic DEHON <tchiot.ludo@gmail.com>
2023-07-04 13:23:49 +0200

    cleanup

--- ui/src/components/flows/FlowTriggers.vue
+++ ui/src/components/flows/FlowTriggers.vue
@@ -1,17 +1,16 @@
 <template>
     <el-table
-        :data="this.flow.triggers"
+        v-loading="flowData === undefined"
+        :data="flowData ? flowData.triggers : []"
         stripe
         table-layout="auto"
-        @row-dblclick="goToDetail"
+        @row-dblclick="triggerId = $event.id; isOpen = true"
     >
         <el-table-column prop="id" :label="$t('id')">
             <template #default="scope">
-                <router-link
-                    :to="{name: 'flows/triggers/detail', params: {namespace: flow.namespace, flowId: flow.id, id: scope.row.id}}"
-                >
+                <code>
                     {{ scope.row.id }}
-                </router-link>
+                </code>
             </template>
         </el-table-column>
         <el-table-column prop="type" :label="$t('type')" />
@@ -20,29 +19,77 @@
                 <Markdown :source="scope.row.description" />
             </template>
         </el-table-column>
+
+        <el-table-column column-key="action" class-name="row-action">
+            <template #default="scope">
+                <a href="#" @click="triggerId = scope.row.id; isOpen = true">
+                    <kicon :tooltip="$t('details')" placement="left">
+                        <eye />
+                    </kicon>
+                </a>
+            </template>
+        </el-table-column>
     </el-table>
+
+    <el-drawer
+        v-if="isOpen"
+        v-model="isOpen"
+        destroy-on-close
+        lock-scroll
+        size=""
+        :append-to-body="true"
+    >
+        <template #header>
+            <code>{{ triggerId }}</code>
+        </template>
+        <el-table stripe table-layout="auto" :data="triggerData">
+            <el-table-column prop="key" :label="$t('key')" />
+            <el-table-column prop="value" :label="$t('value')">
+                <template #default="scope">
+                    <vars v-if="scope.row.value instanceof Array || scope.row.value instanceof Object " :data="scope.row.value" />
+                </template>
+            </el-table-column>
+        </el-table>
+    </el-drawer>
+
 </template>
+
+<script setup>
+    import Eye from "vue-material-design-icons/Eye.vue";
+    import Vars from "../executions/Vars.vue";
+</script>
 
 <script>
     import Markdown from "../layout/Markdown.vue";
     import {mapGetters} from "vuex";
+    import Kicon from "../Kicon.vue"
 
     export default {
-        components: {Markdown},
-        methods: {
-            goToDetail(row) {
-                this.$router.push({
-                    name: "flows/triggers/detail",
-                    params: {
-                        flowId: this.flow.id,
-                        namespace: this.flow.namespace,
-                        id: row.id
-                    },
-                });
+        components: {Markdown, Kicon},
+        data() {
+            return {
+                triggerId: undefined,
+                flowData: undefined,
+                isOpen: false
             }
         },
+        created() {
+            this.$http
+                .get(`/api/v1/flows/${this.flow.namespace}/${this.flow.id}?source=false`)
+                .then(value => this.flowData = value.data);
+        },
         computed: {
-            ...mapGetters("flow", ["flow"])
+            ...mapGetters("flow", ["flow"]),
+            triggerData() {
+                return Object
+                    .entries(this.flowData.triggers.filter(trigger => trigger.id === this.triggerId)[0])
+                    .map(([key, value]) => {
+                        return {
+                            key,
+                            value
+                        };
+                    });
+            },
         }
     };
 </script>

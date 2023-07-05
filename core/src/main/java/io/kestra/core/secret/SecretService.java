ed80eb5c06244f00736daf3e01ad07004d321bfc
brian-mulier-p <bmmulier@hotmail.fr>
2023-07-05 12:33:30 +0200

    fix/non-b64-secrets-prevent-crash (#1711)
    * fix(core): non-base64-encoded secrets are no longer crashing the app
    
    closes #1706

--- core/src/main/java/io/kestra/core/secret/SecretService.java
+++ core/src/main/java/io/kestra/core/secret/SecretService.java
@@ -2,6 +2,9 @@ package io.kestra.core.secret;
 
 import io.kestra.core.exceptions.IllegalVariableEvaluationException;
 import jakarta.inject.Singleton;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
@@ -11,16 +14,26 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Singleton
+@Slf4j
 public class SecretService {
     private static final String SECRET_PREFIX = "SECRETS_";
     private Map<String, String> decodedSecrets;
 
     @PostConstruct
     private void postConstruct() {
-        decodedSecrets = System.getenv().entrySet().stream().filter(entry -> entry.getKey().startsWith(SECRET_PREFIX)).collect(Collectors.toMap(
-            entry -> entry.getKey().substring(SECRET_PREFIX.length()).toUpperCase(),
-            entry -> new String(Base64.getDecoder().decode(entry.getValue()))
-        ));
+        decodedSecrets = System.getenv().entrySet().stream()
+            .filter(entry -> entry.getKey().startsWith(SECRET_PREFIX))
+            .<Map.Entry<String, String>>mapMulti((entry, consumer) -> {
+                try {
+                    consumer.accept(Map.entry(entry.getKey(), new String(Base64.getDecoder().decode(entry.getValue()))));
+                } catch (Exception e) {
+                    log.error("Could not decode secret '{}', make sure it is Base64-encoded", entry.getKey(), e);
+                }
+            })
+            .collect(Collectors.toMap(
+                entry -> entry.getKey().substring(SECRET_PREFIX.length()).toUpperCase(),
+                Map.Entry::getValue
+            ));
     }
 
     public String findSecret(String namespace, String key) throws IOException, IllegalVariableEvaluationException {

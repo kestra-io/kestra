c846cff12c73de2347fad9367fe417a448fbe013
Loïc Mathieu <loikeseke@gmail.com>
2023-08-22 11:46:39 +0200

    feat(jdbc-h2): refactor the H2 migrations

--- jdbc-h2/src/main/resources/migrations/h2/V17__trigger_fulltext_col.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE triggers ADD COLUMN IF NOT EXISTS "fulltext" TEXT NOT NULL GENERATED ALWAYS AS (
-    JQ_STRING("value", '.flowId') ||
-    JQ_STRING("value", '.namespace') ||
-    JQ_STRING("value", '.triggerId') ||
-    COALESCE(JQ_STRING("value", '.executionId'), '')
-)
--- jdbc-h2/src/main/resources/migrations/h2/V2__setting.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE settings (
-    "key" VARCHAR(250) NOT NULL PRIMARY KEY,
-    "value" TEXT NOT NULL
-);
-

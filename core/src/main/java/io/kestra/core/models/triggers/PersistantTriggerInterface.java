2b61a0a887eebe7c5d6d17d82f4735605a94826c
Loïc Mathieu <loikeseke@gmail.com>
2023-08-31 11:23:21 +0200

    chore: delete PersistantTriggerInterface as it is not used

--- core/src/main/java/io/kestra/core/models/triggers/PersistantTriggerInterface.java
+++
@@ -1,9 +0,0 @@
-package io.kestra.core.models.triggers;
-
-import io.kestra.core.models.executions.Execution;
-
-import java.util.function.Consumer;
-
-public interface PersistantTriggerInterface {
-    void evaluate(TriggerContext context, Consumer<Execution> consumer);
-}

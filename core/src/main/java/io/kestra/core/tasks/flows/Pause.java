457734b500f9d306ac1a53b65a40ff94a7365c20
Anna Geller <anna.m.geller@gmail.com>
2023-10-09 16:21:42 +0200

    docs(core): Pause task enhancements (#2257)

--- core/src/main/java/io/kestra/core/tasks/flows/Pause.java
+++ core/src/main/java/io/kestra/core/tasks/flows/Pause.java
@@ -30,42 +30,48 @@ import java.util.Optional;
 @Getter
 @NoArgsConstructor
 @Schema(
-    title = "Pause the current execution and wait for a manual approval (changing the task state from the UI) or a delay"
+    title = "Pause the current execution and wait for a manual approval (either by humans or other automated processes). All tasks downstream from the Pause task will be put on hold until the execution is manually resumed from the UI. The Execution will be in a Paused state (_marked in purple_) and you can manually resume it by clicking on the \"Resume\" button in the UI, or by calling the POST API endpoint \"/api/v1/executions/{executionId}/resume\". The execution can also be resumed automatically after a timeout."
 )
 @Plugin(
     examples = {
         @Example(
             full = true,
-            code = {
-                "id: pause",
-                "namespace: io.kestra.tests",
-                "",
-                "tasks:",
-                "  - id: pause",
-                "    type: io.kestra.core.tasks.flows.Pause",
-                "    tasks:",
-                "     - id: ko",
-                "       type: io.kestra.plugin.scripts.shell.Commands",
-                "       commands:",
-                "        - 'echo \"trigger after manual restart\"'",
-                "  - id: last",
-                "    type: io.kestra.core.tasks.debugs.Return",
-                "    format: \"{{task.id}} > {{taskrun.startDate}}\""
-            }
+            code = """
+                id: human_in_the_loop
+                namespace: dev
+
+                tasks:
+                  - id: before_approval
+                    type: io.kestra.core.tasks.debugs.Return
+                    format: Output data that needs to be validated by a human
+
+                  - id: pause
+                    type: io.kestra.core.tasks.flows.Pause
+                    tasks:
+                      - id: run_post_approval
+                        type: io.kestra.plugin.scripts.shell.Commands
+                        runner: PROCESS
+                        commands:
+                          - echo "Manual approval received! Continuing the execution..."
+
+                  - id: post_resume
+                    type: io.kestra.core.tasks.debugs.Return
+                    format: "{{ task.id }} started on {{ taskrun.startDate }} after the Pause"
+                """
         )
     }
 )
 public class Pause extends Sequential implements FlowableTask<VoidOutput> {
     @Schema(
-        title = "Duration of the pause.",
-        description = "If null and no timeout, a manual approval is needed, if not, the delay before continuing the execution"
+        title = "Duration of the pause — useful if you want to pause the execution for a fixed amount of time.",
+        description = "If no delay and no timeout are configured, the execution will never end until it's manually resumed from the UI or API."
     )
     @PluginProperty
     private Duration delay;
 
     @Schema(
-        title = "Timeout of the pause.",
-        description = "If null and no delay, a manual approval is needed, else a manual approval is needed before the timeout or the task will fail"
+        title = "Timeout of the pause — useful to avoid never-ending workflows in a human-in-the-loop scenario. For example, if you want to pause the execution until a human validates some data generated in a previous task, you can set a timeout of e.g. 24 hours. If no manual approval happens within 24 hours, the execution will automatically resume without a prior data validation.",
+        description = "If no delay and no timeout are configured, the execution will never end until it's manually resumed from the UI or API."
     )
     @PluginProperty
     private Duration timeout;

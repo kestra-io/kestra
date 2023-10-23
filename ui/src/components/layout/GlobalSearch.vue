cc11b798ee76eec09632492d57b9acacf015a720
brian-mulier-p <bmmulier@hotmail.fr>
2023-10-23 11:02:16 +0200

    feat(ui) : globalSearch now use el-autocomplete (#2353)
    * feat(ui): global search is now using autocomplete component
    
    * fix(ui): cleanup
    
    ---------
    
    Co-authored-by: YannC <ycoornaert@kestra.io>

--- ui/src/components/layout/GlobalSearch.vue
+++ ui/src/components/layout/GlobalSearch.vue
@@ -1,29 +1,32 @@
 <template>
-    <el-dropdown class="flex-shrink-0" trigger="click" popper-class="hide-arrow overflow-hidden separator-m-0 global-search-popper">
-        <search-field class="align-items-center" @search="search" :router="false" ref="search" placeholder="jump to...">
-            <template #prefix>
-                <magnify />
-            </template>
-            <template #suffix>
-                <keyboard /><span>Ctrl/Cmd + K</span>
-            </template>
-        </search-field>
-        <template #dropdown>
-            <el-dropdown-menu class="bg-transparent p-0">
-                <div v-for="(page, idx) in pages">
-                    <router-link
-                        :to="page.href"
-                        class="d-flex gap-2 el-dropdown-menu__item">
-                        <div class="d-flex gap-2 page-title">
-                            <component :is="page.icon.element" class="align-middle" /> {{ page.title }}
-                        </div>
-                        <arrow-right />
-                    </router-link>
-                    <div class="el-dropdown-menu__item--divided" v-if="idx < pages.length - 1"/>
-                </div>
-            </el-dropdown-menu>
+    <el-autocomplete
+        ref="search"
+        class="flex-shrink-0"
+        v-model="filter"
+        @select="goTo($event)"
+        :fetch-suggestions="search"
+        popper-class="hide-arrow overflow-hidden separator-m-0 global-search-popper"
+        :placeholder="$t('jump to...')"
+        fit-input-width
+    >
+        <template #prefix>
+            <magnify />
         </template>
-    </el-dropdown>
+        <template #suffix>
+            <keyboard /><span>Ctrl/Cmd + K</span>
+        </template>
+        <template #default="{item}">
+            <router-link
+                :to="item.href"
+                class="d-flex gap-2"
+            >
+                <div class="d-flex gap-2 nav-item-title">
+                    <component :is="{...item.icon.element}" class="align-middle" /> {{ item.title }}
+                </div>
+                <arrow-right class="is-justify-end" />
+            </router-link>
+        </template>
+    </el-autocomplete>
 </template>
 
 <script>
@@ -52,18 +55,22 @@
             keyDown(e) {
                 if((e.ctrlKey || e.metaKey) && e.key === "k") {
                     e.preventDefault();
-                    this.$refs.search.$el.querySelector("input").click();
+                    this.$refs.search.inputRef.input.click();
                 }
             },
             routeStartWith(route) {
                 return this.$router.getRoutes().filter(r => r.name.startsWith(route)).map(r => r.name);
             },
-            search(query) {
-                this.filter = query;
+            search(query, cb) {
+                cb(this.navItems.filter(item => item.title.toLowerCase().includes(query.toLowerCase())));
+            },
+            goTo(item) {
+                this.$router.push(item.href);
             }
         },
         mounted() {
             window.addEventListener("keydown", this.keyDown);
+
         },
         unmounted() {
             window.removeEventListener("keydown", this.keyDown);
@@ -71,7 +78,7 @@
         computed: {
             ...mapState("auth", ["user"]),
             ...mapGetters("misc", ["configs"]),
-            pages() {
+            navItems() {
                 return LeftMenu.methods.generateMenu.call(this).flatMap(item => {
                     if(item.hidden) {
                         return [];
@@ -81,16 +88,8 @@
                     }
 
                     return item;
-                }).filter(item => item.title.toLowerCase().includes(this.filter.toLowerCase()));
+                });
             }
         }
     };
 </script>
-
-<style>
-    .global-search-popper {
-        .page-title {
-            width: 30ch;
-        }
-    }
-</style>

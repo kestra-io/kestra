3d07e5c6896c282de4b97301dab73e5ef45274b6
Loïc Mathieu <loikeseke@gmail.com>
2023-10-24 11:22:12 +0200

    fix(core): the tenantId is passed via method argument not inside the Flow class

--- core/src/test/java/io/kestra/core/repositories/AbstractExecutionRepositoryTest.java
+++ core/src/test/java/io/kestra/core/repositories/AbstractExecutionRepositoryTest.java
@@ -348,10 +348,10 @@ public abstract class AbstractExecutionRepositoryTest {
         List<ExecutionCount> result = executionRepository.executionCounts(
             null,
             List.of(
-                new io.kestra.core.models.executions.statistics.Flow(null, NAMESPACE, "first"),
-                new io.kestra.core.models.executions.statistics.Flow(null,NAMESPACE, "second"),
-                new io.kestra.core.models.executions.statistics.Flow(null,NAMESPACE, "third"),
-                new Flow(null,NAMESPACE, "missing")
+                new io.kestra.core.models.executions.statistics.Flow(NAMESPACE, "first"),
+                new io.kestra.core.models.executions.statistics.Flow(NAMESPACE, "second"),
+                new io.kestra.core.models.executions.statistics.Flow(NAMESPACE, "third"),
+                new Flow(NAMESPACE, "missing")
             ),
             null,
             ZonedDateTime.now().minusDays(10),

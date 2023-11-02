e5631921153cb72952fd63efe7516b6a1011c730
Martin <mguibert@kestra.io>
2023-11-02 13:36:33 +0100

    feat(cli): add new command to upload namespace files (#2406)

---
+++ cli/src/main/java/io/kestra/cli/commands/namespaces/NamespaceCommand.java
@@ -0,0 +1,30 @@
+package io.kestra.cli.commands.namespaces;
+
+import io.kestra.cli.AbstractCommand;
+import io.kestra.cli.App;
+import io.kestra.cli.commands.namespaces.files.NamespaceFilesCommand;
+import io.micronaut.configuration.picocli.PicocliRunner;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import picocli.CommandLine;
+
+@CommandLine.Command(
+    name = "namespace",
+    description = "handle namespaces",
+    mixinStandardHelpOptions = true,
+    subcommands = {
+        NamespaceFilesCommand.class
+    }
+)
+@Slf4j
+public class NamespaceCommand extends AbstractCommand {
+    @SneakyThrows
+    @Override
+    public Integer call() throws Exception {
+        super.call();
+
+        PicocliRunner.call(App.class, "namespaces", "--help");
+
+        return 0;
+    }
+}
---
+++ cli/src/main/java/io/kestra/cli/commands/namespaces/files/NamespaceFilesUpdateCommand.java
@@ -0,0 +1,77 @@
+package io.kestra.cli.commands.namespaces.files;
+
+import io.kestra.cli.AbstractValidateCommand;
+import io.kestra.cli.commands.AbstractServiceNamespaceUpdateCommand;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.MediaType;
+import io.micronaut.http.client.exceptions.HttpClientResponseException;
+import io.micronaut.http.client.multipart.MultipartBody;
+import io.micronaut.http.client.netty.DefaultHttpClient;
+import lombok.extern.slf4j.Slf4j;
+import nl.basjes.gitignore.GitIgnore;
+import picocli.CommandLine;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Optional;
+
+@CommandLine.Command(
+    name = "update",
+    description = "update namespace files",
+    mixinStandardHelpOptions = true
+)
+@Slf4j
+public class NamespaceFilesUpdateCommand extends AbstractServiceNamespaceUpdateCommand {
+
+    private static final String KESTRA_IGNORE_FILE = ".kestraignore";
+
+    @Override
+    public Integer call() throws Exception {
+        super.call();
+
+        try (var files = Files.walk(directory); DefaultHttpClient client = client()) {
+            if (delete) {
+                client.toBlocking().exchange(this.requestOptions(HttpRequest.DELETE(apiUri("/files/namespaces/") + namespace + "?path=/", null)));
+            }
+
+            GitIgnore gitIgnore = parseKestraIgnore(directory);
+
+            List<Path> paths = files
+                .filter(Files::isRegularFile)
+                .filter(path -> !path.endsWith(KESTRA_IGNORE_FILE))
+                .filter(path -> !Boolean.TRUE.equals(gitIgnore.isIgnoredFile(path.toString())))
+                .toList();
+            paths.forEach(path -> {
+                MultipartBody body = MultipartBody.builder()
+                    .addPart("fileContent", path.toFile())
+                    .build();
+                Path dest = directory.relativize(path);
+                client.toBlocking().exchange(
+                    this.requestOptions(
+                        HttpRequest.POST(
+                            "/api/v1/files/namespaces/" + namespace + "?path=/" + dest,
+                            body
+                        ).contentType(MediaType.MULTIPART_FORM_DATA)
+                    )
+                );
+                stdOut("Successfully uploaded {0} to /{1}", path, dest);
+            });
+        } catch (HttpClientResponseException e) {
+            AbstractValidateCommand.handleHttpException(e, "namespace");
+            return 1;
+        }
+
+        return 0;
+    }
+
+    private GitIgnore parseKestraIgnore(Path directory) throws IOException {
+        File kestraIgnoreFile = Path.of(directory.toString(), KESTRA_IGNORE_FILE).toFile();
+        if (!kestraIgnoreFile.exists()) {
+            return new GitIgnore("");
+        }
+        return new GitIgnore(kestraIgnoreFile);
+    }
+}

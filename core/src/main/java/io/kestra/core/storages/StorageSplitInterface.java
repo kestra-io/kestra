289bc08ffe4e785eb23154e627d7e46e3980dc4c
Anna Geller <anna.m.geller@gmail.com>
2023-11-13 23:38:10 +0100

    doc: clarify ForEachItem properties and provide a full example

--- core/src/main/java/io/kestra/core/storages/StorageSplitInterface.java
+++ core/src/main/java/io/kestra/core/storages/StorageSplitInterface.java
@@ -30,27 +30,27 @@ import java.util.stream.Collectors;
 
 public interface StorageSplitInterface {
     @Schema(
-        title = "Split by file size.",
-        description = "Can be provided as a string like \"10MB\" or \"200KB\", or the number of bytes. " +
-            "Since we divide storage per line, it's not an hard requirements and files can be a larger."
+        title = "Split a large file into multiple chunks with a maximum file size of `bytes`.",
+        description = "Can be provided as a string in the format \"10MB\" or \"200KB\", or the number of bytes. " +
+            "This allows you to process large files, slit them into smaller chunks by lines and process them in parallel. For example, MySQL by default limits the size of a query size to 16MB per query. Trying to use a bulk insert query with input data larger than 16MB will fail. Splitting the input data into smaller chunks is a common strategy to circumvent this limitation. By dividing a large data set into chunks smaller than the `max_allowed_packet` size (e.g., 10MB), you can insert the data in multiple smaller queries. This approach not only helps to avoid hitting the query size limit but can also be more efficient and manageable in terms of memory utilization, especially for very large datasets. In short, by splitting the file by bytes, you can bulk-insert smaller chunks of e.g. 10MB in parallel to avoid this limitation."
     )
     @PluginProperty(dynamic = true)
     String getBytes();
 
     @Schema(
-        title = "Split by a fixed number of files."
+        title = "Split a file into a fixed number of partitioned files. For example, if you have a file with 1000 lines and you set `partitions` to 10, the file will be split into 10 files with 100 lines each."
     )
     @PluginProperty(dynamic = true)
     Integer getPartitions();
 
     @Schema(
-        title = "Split by file rows count."
+        title = "A number of rows per batch. The file will then be split into chunks with that maximum number of rows."
     )
     @PluginProperty(dynamic = true)
     Integer getRows();
 
     @Schema(
-        title = "The separator to used between rows"
+        title = "The separator used to split a file into chunks. By default, it's a newline `\\n` character. If you are on Windows, you might want to use `\\r\\n` instead."
     )
     @PluginProperty
     String getSeparator();

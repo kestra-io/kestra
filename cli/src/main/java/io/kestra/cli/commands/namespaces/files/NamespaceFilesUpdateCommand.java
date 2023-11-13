b39d93f0b9c1c65e133b23cebdff39695cf0a112
brian-mulier-p <bmmulier@hotmail.fr>
2023-11-13 19:22:21 +0100

    fix(commands): from & to params for ns files update command (#2497)
    closes #2480

--- cli/src/main/java/io/kestra/cli/commands/namespaces/files/NamespaceFilesUpdateCommand.java
+++ cli/src/main/java/io/kestra/cli/commands/namespaces/files/NamespaceFilesUpdateCommand.java
@@ -1,7 +1,7 @@
 package io.kestra.cli.commands.namespaces.files;
 
+import io.kestra.cli.AbstractApiCommand;
 import io.kestra.cli.AbstractValidateCommand;
-import io.kestra.cli.commands.AbstractServiceNamespaceUpdateCommand;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.client.exceptions.HttpClientResponseException;
@@ -23,20 +23,33 @@ import java.util.List;
     mixinStandardHelpOptions = true
 )
 @Slf4j
-public class NamespaceFilesUpdateCommand extends AbstractServiceNamespaceUpdateCommand {
+public class NamespaceFilesUpdateCommand extends AbstractApiCommand {
+    @CommandLine.Parameters(index = "0", description = "the namespace to update")
+    public String namespace;
+
+    @CommandLine.Parameters(index = "1", description = "the local directory containing files for current namespace")
+    public Path from;
+
+    @CommandLine.Parameters(index = "2", description = "the remote namespace path to upload files to", defaultValue = "/")
+    public String to;
+
+    @CommandLine.Option(names = {"--delete"}, negatable = true, description = "if missing should be deleted")
+    public boolean delete = false;
 
     private static final String KESTRA_IGNORE_FILE = ".kestraignore";
 
     @Override
     public Integer call() throws Exception {
         super.call();
+        to = to.startsWith("/") ? to : "/" + to;
+        to = to.endsWith("/") ? to : to + "/";
 
-        try (var files = Files.walk(directory); DefaultHttpClient client = client()) {
+        try (var files = Files.walk(from); DefaultHttpClient client = client()) {
             if (delete) {
-                client.toBlocking().exchange(this.requestOptions(HttpRequest.DELETE(apiUri("/namespaces/") + namespace + "/files?path=/", null)));
+                client.toBlocking().exchange(this.requestOptions(HttpRequest.DELETE(apiUri("/namespaces/") + namespace + "/files?path=" + to, null)));
             }
 
-            GitIgnore gitIgnore = parseKestraIgnore(directory);
+            GitIgnore gitIgnore = parseKestraIgnore(from);
 
             List<Path> paths = files
                 .filter(Files::isRegularFile)
@@ -47,16 +60,17 @@ public class NamespaceFilesUpdateCommand extends AbstractServiceNamespaceUpdateC
                 MultipartBody body = MultipartBody.builder()
                     .addPart("fileContent", path.toFile())
                     .build();
-                Path dest = directory.relativize(path);
+                String relativizedPath = from.relativize(path).toString();
+                String destination = to + relativizedPath;
                 client.toBlocking().exchange(
                     this.requestOptions(
                         HttpRequest.POST(
-                            apiUri("/namespaces/") + namespace + "/files?path=/" + dest,
+                            apiUri("/namespaces/") + namespace + "/files?path=" + destination,
                             body
                         ).contentType(MediaType.MULTIPART_FORM_DATA)
                     )
                 );
-                stdOut("Successfully uploaded {0} to /{1}", path, dest);
+                stdOut("Successfully uploaded {0} to {1}", path.toString(), destination);
             });
         } catch (HttpClientResponseException e) {
             AbstractValidateCommand.handleHttpException(e, "namespace");

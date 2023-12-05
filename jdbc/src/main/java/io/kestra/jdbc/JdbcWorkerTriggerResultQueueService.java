bf3c06af147ea56153597aa760092972c3430210
Loïc Mathieu <loikeseke@gmail.com>
2023-12-05 14:59:18 +0100

    feat(jdbc): handle deserialization error for WorkerTriggerResult

--- jdbc/src/main/java/io/kestra/jdbc/JdbcWorkerTriggerResultQueueService.java
+++ jdbc/src/main/java/io/kestra/jdbc/JdbcWorkerTriggerResultQueueService.java
@@ -1,9 +1,14 @@
 package io.kestra.jdbc;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.kestra.core.exceptions.DeserializationException;
+import io.kestra.core.models.triggers.TriggerContext;
 import io.kestra.core.queues.QueueFactoryInterface;
 import io.kestra.core.queues.QueueInterface;
 import io.kestra.core.runners.WorkerTriggerResult;
+import io.kestra.core.serializers.JacksonMapper;
 import io.kestra.core.utils.Either;
 import io.kestra.jdbc.repository.AbstractJdbcWorkerJobRunningRepository;
 import io.kestra.jdbc.runner.JdbcQueue;
@@ -18,6 +23,8 @@ import java.util.function.Consumer;
 @Singleton
 @Slf4j
 public class JdbcWorkerTriggerResultQueueService {
+    private final static ObjectMapper MAPPER = JacksonMapper.ofJson();
+
     private final JdbcQueue<WorkerTriggerResult> workerTriggerResultQueue;
     @Inject
     private AbstractJdbcWorkerJobRunningRepository jdbcWorkerJobRunningRepository;
@@ -35,13 +42,21 @@ public class JdbcWorkerTriggerResultQueueService {
             eithers.forEach(either -> {
                 if (either.isRight()) {
                     log.error("Unable to deserialize a worker job: {}", either.getRight().getMessage());
+                    try {
+                        JsonNode json = MAPPER.readTree(either.getRight().getRecord());
+                        var triggerContext = MAPPER.treeToValue(json.get("triggerContext"), TriggerContext.class);
+                        jdbcWorkerJobRunningRepository.deleteByKey(triggerContext.uid());
+                    } catch (JsonProcessingException e) {
+                        // ignore the message if we cannot do anything about it
+                        log.error("Unexpected exception when trying to handle a deserialization error", e);
+                    }
                     return;
                 }
 
                 WorkerTriggerResult workerTriggerResult = either.getLeft();
                 jdbcWorkerJobRunningRepository.deleteByKey(workerTriggerResult.getTriggerContext().uid());
-
             });
+
             eithers.forEach(consumer);
         });
         return this.queueStop;

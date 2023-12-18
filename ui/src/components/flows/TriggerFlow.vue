b3eedc19bf01bc475f62555ac5179d2980a1e72d
brian.mulier <bmmulier@hotmail.fr>
2023-12-18 10:33:47 +0100

    fix(ui): reloading flow definition upon flow save in Namespace File editor
    closes #2595

--- ui/src/components/flows/TriggerFlow.vue
+++ ui/src/components/flows/TriggerFlow.vue
@@ -47,16 +47,6 @@
                 }
             };
         },
-        mounted() {
-            if (!this.flow && this.flowId && this.namespace) {
-                this.$store
-                    .dispatch("flow/loadFlow", {
-                        id: this.flowId,
-                        namespace: this.namespace,
-                        allowDeleted: true
-                    });
-            }
-        },
         methods: {
             onClick() {
                 if (this.$tours["guidedTour"].isRunning.value && !this.guidedProperties.executeFlow) {
@@ -78,6 +68,13 @@
             },
             isDisabled() {
                 return this.disabled || this.flow?.deleted;
+            },
+            loadDefinition() {
+                this.$store.dispatch("flow/loadFlow", {
+                    id: this.flowId,
+                    namespace: this.namespace,
+                    allowDeleted: true
+                });
             }
         },
         computed: {
@@ -103,15 +100,13 @@
             },
             flowId: {
                 handler() {
-                    if ((!this.flow || this.flow.id !== this.flowId) && this.flowId && this.namespace) {
-                        this.$store
-                            .dispatch("flow/loadFlow", {
-                                id: this.flowId,
-                                namespace: this.namespace,
-                                allowDeleted: true
-                            });
+                    if (!this.flowId) {
+                        return;
                     }
-                }
+
+                    this.loadDefinition();
+                },
+                immediate: true
             }
         }
     };

2a51145f60938fec78e94c3d95a25f027fc5842e
brian.mulier <bmmulier@hotmail.fr>
2023-12-20 15:54:54 +0100

    fix(webserver): accept tenant in config endpoint

--- webserver/src/main/java/io/kestra/webserver/controllers/MiscController.java
+++ webserver/src/main/java/io/kestra/webserver/controllers/MiscController.java
@@ -67,7 +67,7 @@ public class MiscController {
         return HttpResponse.ok("pong");
     }
 
-    @Get("/api/v1/configs")
+    @Get("/api/v1{/tenant}/configs")
     @ExecuteOn(TaskExecutors.IO)
     @Operation(tags = {"Misc"}, summary = "Get current configurations")
     public Configuration configuration() throws JsonProcessingException {

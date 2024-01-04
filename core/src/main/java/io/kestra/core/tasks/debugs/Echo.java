bf28cdc5e67437ffd34195f87545c9eac8c8a4f9
brian-mulier-p <bmmulier@hotmail.fr>
2024-01-04 11:16:06 +0100

    Revert "fix(docs): corrected the docs on multiple flows (#2799)"
    This reverts commit da69eb24de42867cf20a9a308c225948f591f44b.

--- core/src/main/java/io/kestra/core/tasks/debugs/Echo.java
+++ core/src/main/java/io/kestra/core/tasks/debugs/Echo.java
@@ -1,12 +1,20 @@
 package io.kestra.core.tasks.debugs;
 
-
+import io.kestra.core.models.annotations.PluginProperty;
+import io.kestra.core.tasks.log.Log;
+import io.micronaut.core.annotation.NonNull;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 import io.kestra.core.models.annotations.Example;
 import io.kestra.core.models.annotations.Plugin;
+import io.kestra.core.models.tasks.RunnableTask;
 import io.kestra.core.models.tasks.Task;
+import io.kestra.core.models.tasks.VoidOutput;
+import io.kestra.core.runners.RunContext;
+import org.slf4j.event.Level;
+
+import javax.validation.constraints.NotBlank;
 
 @SuperBuilder
 @ToString
@@ -15,7 +23,7 @@ import io.kestra.core.models.tasks.Task;
 @NoArgsConstructor
 @Schema(
     title = "Log a message in the task logs.",
-    description = "This task is deprecated, please use the `io.kestra.core.tasks.log.Log` task instead.",
+    description = "This task is deprecated, please use the io.kestra.core.tasks.log.Log task instead.",
     deprecated = true
 )
 @Plugin(
@@ -29,4 +37,23 @@ import io.kestra.core.models.tasks.Task;
     }
 )
 @Deprecated
-public class Echo extends Task {}
+public class Echo extends Task implements RunnableTask<VoidOutput> {
+    @NonNull
+    @NotBlank
+    @PluginProperty(dynamic = true)
+    private String format;
+
+    @Builder.Default
+    @PluginProperty
+    private Level level = Level.INFO;
+
+    @Override
+    public VoidOutput run(RunContext runContext) throws Exception {
+        Log log = Log.builder()
+            .level(this.level)
+            .message(this.format)
+            .build();
+        log.run(runContext);
+        return null;
+    }
+}

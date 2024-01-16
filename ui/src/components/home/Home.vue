900f3d080418f5bd83d2ae2cd238b3ef59756069
YannC <37600690+Skraye@users.noreply.github.com>
2024-01-16 15:54:51 +0100

    fix(): load correct tenant & no more unauthorized error (#2875)

--- ui/src/components/home/Home.vue
+++ ui/src/components/home/Home.vue
@@ -152,9 +152,10 @@
     import OnboardingBottom from "../onboarding/OnboardingBottom.vue";
     import DateRange from "../layout/DateRange.vue";
     import TopNavBar from "../layout/TopNavBar.vue";
+    import HomeStartup from "override/mixins/homeStartup"
 
     export default {
-        mixins: [RouteContext, RestoreUrl],
+        mixins: [RouteContext, RestoreUrl, HomeStartup],
         components: {
             DateRange,
             OnboardingBottom,

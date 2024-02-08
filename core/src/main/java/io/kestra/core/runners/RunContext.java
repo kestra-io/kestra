2a4068f5f047876e5dc91bea6a66986a88751803
Loïc Mathieu <loikeseke@gmail.com>
2024-02-08 10:12:41 +0100

    fix(core): init logger before computing variables in the run context

--- core/src/main/java/io/kestra/core/runners/RunContext.java
+++ core/src/main/java/io/kestra/core/runners/RunContext.java
@@ -75,8 +75,8 @@ public class RunContext {
      */
     public RunContext(ApplicationContext applicationContext, Flow flow, Execution execution) {
         this.initBean(applicationContext);
-        this.initContext(flow, null, execution, null);
         this.initLogger(execution);
+        this.initContext(flow, null, execution, null);
     }
 
     /**
@@ -102,9 +102,8 @@ public class RunContext {
      */
     public RunContext(ApplicationContext applicationContext, Flow flow, AbstractTrigger trigger) {
         this.initBean(applicationContext);
-
-        this.variables = this.variables(flow, null, null, null, trigger);
         this.initLogger(flow, trigger);
+        this.variables = this.variables(flow, null, null, null, trigger);
         this.initPluginConfiguration(applicationContext, trigger.getType());
     }
 

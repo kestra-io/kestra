476ab827e31a7aafd1b4111d3d63e27e3ad6f51a
YannC <ycoornaert@kestra.io>
2024-02-08 14:50:04 +0100

    test(SchedulerSchedule): new test for existing backfill (without next execution date)

--- core/src/test/java/io/kestra/core/schedulers/SchedulerScheduleTest.java
+++ core/src/test/java/io/kestra/core/schedulers/SchedulerScheduleTest.java
@@ -4,12 +4,14 @@ import io.kestra.core.models.executions.Execution;
 import io.kestra.core.models.executions.LogEntry;
 import io.kestra.core.models.flows.Flow;
 import io.kestra.core.models.flows.State;
+import io.kestra.core.models.triggers.Trigger;
 import io.kestra.core.models.triggers.types.Schedule;
 import io.kestra.core.queues.QueueFactoryInterface;
 import io.kestra.core.queues.QueueInterface;
 import io.kestra.core.runners.FlowListeners;
 import io.kestra.core.runners.TestMethodScopedWorker;
 import io.kestra.core.runners.Worker;
+import io.kestra.core.utils.Await;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
 import org.junit.jupiter.api.Test;
@@ -122,6 +124,43 @@ public class SchedulerScheduleTest extends AbstractSchedulerTest {
             assertThat(queueCount.getCount(), is(0L));
             assertThat(date.size(), is(3));
             assertThat(executionId.size(), is(3));
+        }
+    }
+
+    // Test to ensure behavior between 0.14 > 0.15
+    @Test
+    void retroSchedule() throws Exception {
+        // mock flow listeners
+        FlowListeners flowListenersServiceSpy = spy(this.flowListenersService);
+
+        Flow flow = createScheduleFlow("Europe/Paris");
+
+        doReturn(List.of(flow))
+            .when(flowListenersServiceSpy)
+            .flows();
+
+        Trigger trigger = Trigger
+            .builder()
+            .triggerId("hourlyfromschedulederschedule")
+            .flowId(flow.getId())
+            .namespace(flow.getNamespace())
+            .date(ZonedDateTime.now())
+            .build();
+
+        triggerState.create(trigger);
+
+        // scheduler
+        try (AbstractScheduler scheduler = scheduler(flowListenersServiceSpy);
+             Worker worker = new TestMethodScopedWorker(applicationContext, 8, null)) {
+            worker.run();
+            scheduler.run();
+
+            Await.until(() -> {
+                Optional<Trigger> optionalTrigger = this.triggerState.findLast(trigger);
+                return optionalTrigger.filter(value -> value.getNextExecutionDate() != null).isPresent();
+            });
+
+            assertThat(this.triggerState.findLast(trigger).get().getNextExecutionDate().isAfter(trigger.getDate()), is(true));
         }
     }
 }

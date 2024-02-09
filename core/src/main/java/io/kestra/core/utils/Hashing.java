ff1f1e9056b66f253c87321bb8d8297a4a592da4
Florian Hussonnois <florian.hussonnois@gmail.com>
2024-02-09 11:10:36 +0100

    chore(core): add required utility methods
    This commit adds required utilities for further improvements.

--- core/src/main/java/io/kestra/core/utils/Hashing.java
+++ core/src/main/java/io/kestra/core/utils/Hashing.java
@@ -2,6 +2,10 @@ package io.kestra.core.utils;
 
 import com.google.common.base.Charsets;
 import com.google.common.hash.HashCode;
+import com.google.common.hash.Hasher;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * Utilities for hashing.
@@ -28,6 +32,33 @@ public final class Hashing {
      */
     public static long hashToLong(final String value) {
         return getHashString(value).asLong();
+    }
+
+    /**
+     * Hashes the given value using SHA-512 algorithm.
+     *
+     * @param value     the value to be hashed.
+     * @param salt      an optional salt to be added to the value.
+     * @return          the digest.
+     */
+    public static byte[] sha512Hash(final byte[] value, byte[] salt) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-512");
+            if (salt != null && salt.length > 0) {
+                md.update(salt);
+            }
+            return md.digest(value);
+        } catch (NoSuchAlgorithmException e) {
+            throw new AssertionError(e);
+        }
+    }
+
+    public static String encodeBytesToHex(final byte[] bytes) {
+        return HashCode.fromBytes(bytes).toString();
+    }
+
+    public static byte[] decodeHexToBytes(final String value) {
+        return HashCode.fromString(value).asBytes();
     }
 
     private static HashCode getHashString(String value) {

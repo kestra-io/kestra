3f71c2bf3bab10262f25320f57374fd624e065e8
Loïc Mathieu <loikeseke@gmail.com>
2024-02-22 09:38:24 +0100

    fix(core): base64 decode the provided encryption key

--- core/src/main/java/io/kestra/core/encryption/EncryptionService.java
+++ core/src/main/java/io/kestra/core/encryption/EncryptionService.java
@@ -15,16 +15,19 @@ import java.util.Base64;
  */
 public class EncryptionService {
     private static final String CIPHER_ALGORITHM = "AES/GCM/NoPadding";
+    private static final String KEY_ALGORITHM = "AES";
     private static final int IV_LENGTH = 12;
     private static final int AUTH_TAG_LENGTH = 128;
     private static final SecureRandom SECURE_RANDOM = new SecureRandom();
 
     /**
      * Encrypt a String using the AES/GCM/NoPadding algorithm and the provided key.
+     * The key must be base64 encoded.
      * The IV is concatenated at the beginning of the string.
      */
     public static String encrypt(String key, String plainText) throws GeneralSecurityException {
-        SecretKey secretKey = new SecretKeySpec(key.getBytes(), "AES");
+        byte[] keyBytes = Base64.getDecoder().decode(key);
+        SecretKey secretKey = new SecretKeySpec(keyBytes, KEY_ALGORITHM);
         Cipher cipher = Cipher.getInstance(CIPHER_ALGORITHM);
         byte[] iv = generateIv();
         GCMParameterSpec ivParameter= new GCMParameterSpec(AUTH_TAG_LENGTH, iv);
@@ -36,10 +39,12 @@ public class EncryptionService {
 
     /**
      * Decrypt a String using the AES/GCM/NoPadding algorithm and the provided key.
+     * The key must be base64 encoded.
      * The IV is recovered from the beginning of the string.
      */
     public static String decrypt(String key, String cipherText) throws GeneralSecurityException {
-        SecretKey secretKey = new SecretKeySpec(key.getBytes(), "AES");
+        byte[] keyBytes = Base64.getDecoder().decode(key);
+        SecretKey secretKey = new SecretKeySpec(keyBytes, KEY_ALGORITHM);
         byte[] input = Base64.getDecoder().decode(cipherText);
         byte[] iv = Arrays.copyOf(input, IV_LENGTH);
         byte[] encrypted =Arrays.copyOfRange(input, IV_LENGTH, input.length);

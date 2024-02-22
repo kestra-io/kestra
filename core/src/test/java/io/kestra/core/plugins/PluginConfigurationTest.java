5b7a24c0dd263e3720e57f62aa8a530eecb03a2d
Loïc Mathieu <loikeseke@gmail.com>
2024-02-22 10:06:00 +0100

    feat(core): Recover Missed Schedule
    Fixes #2219
    
    Allow to specify the behavior in case of misses schedules: recover all, recover last or recover none.

--- core/src/test/java/io/kestra/core/plugins/PluginConfigurationTest.java
+++ core/src/test/java/io/kestra/core/plugins/PluginConfigurationTest.java
@@ -9,24 +9,20 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.IntStream;
 
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 @MicronautTest
 class PluginConfigurationTest {
-
-    private static final String PLUGIN_TEST = "io.kestra.plugin.Test";
 
     @Inject
     private List<PluginConfiguration> configurations;
 
     @Test
     void testInjectEachProperty() {
-        // Given
-        this.configurations.sort(PluginConfiguration.COMPARATOR);
-
-        // Then
-        List<PluginConfiguration> expected = IntStream.range(0, 3)
-            .mapToObj(idx -> new PluginConfiguration(idx, PLUGIN_TEST + idx, Map.of("prop" + idx, "value" + idx)))
-            .toList();
-        Assertions.assertEquals(expected, configurations);
+        assertThat(this.configurations, hasItem(new PluginConfiguration(0, "io.kestra.plugin.Test0", Map.of("prop0", "value0"))));
+        assertThat(this.configurations, hasItem(new PluginConfiguration(1, "io.kestra.plugin.Test1", Map.of("prop1", "value1"))));
+        assertThat(this.configurations, hasItem(new PluginConfiguration(2, "io.kestra.plugin.Test2", Map.of("prop2", "value2"))));
     }
 
 }

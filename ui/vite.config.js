46398324bd7cf61b2bb041e8b6bfc364ad655325
Loïc Mathieu <loikeseke@gmail.com>
2024-02-29 16:17:39 +0100

    fix(ui): lint issue

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -47,7 +47,7 @@ export default defineConfig({
         pluginRewriteAll(),
         visualizer(),
     ],
-    assetsInclude: ['**/*.md'],
+    assetsInclude: ["**/*.md"],
     css: {
         devSourcemap: true
     },

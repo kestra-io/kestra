bf89551447d95f471af78b0cbbe3c35d49766c68
Florian Hussonnois <florian.hussonnois@gmail.com>
2024-03-06 16:56:00 +0100

    chore: move all API Controllers to conditional package
    Changes:
    - relocate all /api controller to a dedicated package to apply
      conditional Micornaut beans
    - add a new root endpoint to get server info
    
    Fix: #3223

---
+++ webserver/src/test/java/io/kestra/webserver/controllers/api/TaskRunControllerTest.java
@@ -0,0 +1,40 @@
+package io.kestra.webserver.controllers.api;
+
+import io.kestra.webserver.controllers.h2.JdbcH2ControllerTest;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpStatus;
+import io.micronaut.http.client.annotation.Client;
+import io.micronaut.http.client.exceptions.HttpClientResponseException;
+import io.micronaut.reactor.http.client.ReactorHttpClient;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskRunControllerTest extends JdbcH2ControllerTest {
+    @Inject
+    @Client("/")
+    private ReactorHttpClient client;
+
+    @Test
+    void search() {
+        HttpClientResponseException e = assertThrows(
+            HttpClientResponseException.class,
+            () -> client.toBlocking().retrieve(HttpRequest.GET("/api/v1/taskruns/search"))
+        );
+
+        assertThat(e.getStatus(), is(HttpStatus.NOT_FOUND));
+    }
+
+    @Test
+    void maxTaskRunSetting() {
+        HttpClientResponseException e = assertThrows(
+            HttpClientResponseException.class,
+            () -> client.toBlocking().retrieve(HttpRequest.GET("/api/v1/taskruns/maxTaskRunSetting"))
+        );
+
+        assertThat(e.getStatus(), is(HttpStatus.NOT_FOUND));
+    }
+}

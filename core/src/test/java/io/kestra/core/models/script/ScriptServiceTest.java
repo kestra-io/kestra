86ca24c5524c1ec56a66ea383f8e721c8278d32d
Loïc Mathieu <loikeseke@gmail.com>
2024-03-27 17:19:58 +0100

    feat(core): ScriptService.labels() and ScriptService.jobName()
    This avoid duplicating the code in multiple plugins

--- core/src/test/java/io/kestra/core/models/script/ScriptServiceTest.java
+++ core/src/test/java/io/kestra/core/models/script/ScriptServiceTest.java
@@ -1,5 +1,11 @@
 package io.kestra.core.models.script;
 
+import io.kestra.core.models.executions.Execution;
+import io.kestra.core.models.executions.TaskRun;
+import io.kestra.core.models.flows.Flow;
+import io.kestra.core.models.flows.State;
+import io.kestra.core.models.tasks.Task;
+import io.kestra.core.runners.RunContext;
 import io.kestra.core.runners.RunContextFactory;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import jakarta.inject.Inject;
@@ -90,5 +96,67 @@ class ScriptServiceTest {
         assertThat(scriptCommands, hasSize(2));
         assertThat(scriptCommands.get(0), is("interpreter"));
         assertThat(scriptCommands.get(1), is("beforeCommand\ncommand"));
+    }
+
+    @Test
+    void labels() {
+        var runContext = runContext(runContextFactory, "very.very.very.very.very.very.very.very.very.very.very.very.long.namespace");
+
+        var labels = ScriptService.labels(runContext, "kestra.io");
+        assertThat(labels.size(), is(6));
+        assertThat(labels.get("kestra.io/namespace"), is("very.very.very.very.very.very.very.very.very.very.very.very.lon"));
+        assertThat(labels.get("kestra.io/flow-id"), is("flowId"));
+        assertThat(labels.get("kestra.io/task-id"), is("task"));
+        assertThat(labels.get("kestra.io/execution-id"), is("executionId"));
+        assertThat(labels.get("kestra.io/taskrun-id"), is("taskrun"));
+        assertThat(labels.get("kestra.io/taskrun-attempt"), is("0"));
+
+        labels = ScriptService.labels(runContext, null, true, true);
+        assertThat(labels.size(), is(6));
+        assertThat(labels.get("namespace"), is("very.very.very.very.very.very.very.very.very.very.very.very.lon"));
+        assertThat(labels.get("flow-id"), is("flowid"));
+        assertThat(labels.get("task-id"), is("task"));
+        assertThat(labels.get("execution-id"), is("executionid"));
+        assertThat(labels.get("taskrun-id"), is("taskrun"));
+        assertThat(labels.get("taskrun-attempt"), is("0"));
+    }
+
+    @Test
+    void jobName() {
+        var runContext = runContext(runContextFactory, "namespace");
+        String jobName = ScriptService.jobName(runContext);
+        assertThat(jobName, startsWith("namespace-flowid-task-"));
+        assertThat(jobName.length(), is(27));
+
+        runContext = runContext(runContextFactory, "very.very.very.very.very.very.very.very.very.very.very.very.long.namespace");
+        jobName = ScriptService.jobName(runContext);
+        assertThat(jobName, startsWith("veryveryveryveryveryveryveryveryveryveryveryverylongnames-"));
+        assertThat(jobName.length(), is(63));
+    }
+
+    private RunContext runContext(RunContextFactory runContextFactory, String namespace) {
+        // create a fake flow and execution
+        Task task = new Task() {
+            @Override
+            public String getId() {
+                return "task";
+            }
+
+            @Override
+            public String getType() {
+                return "Task";
+            }
+        };
+        TaskRun taskRun = TaskRun.builder().id("taskrun").taskId("task").flowId("flowId").namespace(namespace).executionId("executionId")
+            .state(new State().withState(State.Type.RUNNING))
+            .build();
+        Flow flow = Flow.builder().id("flowId").namespace(namespace).revision(1)
+            .tasks(List.of(task))
+            .build();
+        Execution execution = Execution.builder().flowId("flowId").namespace(namespace).id("executionId")
+            .taskRunList(List.of(taskRun))
+            .state(new State().withState(State.Type.RUNNING))
+            .build();
+        return runContextFactory.of(flow, task, execution, taskRun);
     }
 }

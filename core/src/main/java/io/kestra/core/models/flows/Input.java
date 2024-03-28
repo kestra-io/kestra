f5c91105e8e20166af5dea9a0f4072ac55ce0446
brian.mulier <bmmulier@hotmail.fr>
2024-03-28 13:14:57 +0100

    fix(core): properly handle deprecation for input's name property to prevent false warnings

--- core/src/main/java/io/kestra/core/models/flows/Input.java
+++ core/src/main/java/io/kestra/core/models/flows/Input.java
@@ -44,6 +44,9 @@ public abstract class Input<T> implements Data {
     @Pattern(regexp="^[a-zA-Z0-9][.a-zA-Z0-9_-]*")
     String id;
 
+    @Deprecated
+    String name;
+
     @NotNull
     @Valid
     Type type;
@@ -58,15 +61,11 @@ public abstract class Input<T> implements Data {
     public abstract void validate(T input) throws ConstraintViolationException;
 
     @JsonSetter
-    @Deprecated
     public void setName(String name) {
         if (this.id == null) {
             this.id = name;
         }
-    }
 
-    @Deprecated
-    public String getName() {
-        return this.getId();
+        this.name = name;
     }
 }

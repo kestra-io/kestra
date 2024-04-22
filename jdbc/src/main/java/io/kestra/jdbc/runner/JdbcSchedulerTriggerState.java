3f3ce1ce6b043aa1f4d39eba50d3c2c310de97ec
YannC <37600690+Skraye@users.noreply.github.com>
2024-04-22 17:01:26 +0200

    fix(scheduler): better handling of locked triggers (#3603)

--- jdbc/src/main/java/io/kestra/jdbc/runner/JdbcSchedulerTriggerState.java
+++ jdbc/src/main/java/io/kestra/jdbc/runner/JdbcSchedulerTriggerState.java
@@ -84,4 +84,7 @@ public class JdbcSchedulerTriggerState implements SchedulerTriggerStateInterface
     public List<Trigger> findByNextExecutionDateReadyForGivenFlows(List<Flow> flows, ZonedDateTime now, ScheduleContextInterface scheduleContext) {
         throw new NotImplementedException();
     }
+
+    @Override
+    public void unlock(Trigger trigger) {}
 }

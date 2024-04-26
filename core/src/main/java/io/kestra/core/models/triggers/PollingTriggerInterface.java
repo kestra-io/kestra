90858d6a473431af7b8b017e9eef4ee2efdac6ad
Ludovic DEHON <tchiot.ludo@gmail.com>
2024-04-26 23:15:34 +0200

    refactor(core): remove unneeded WorkerTriggerIntervalInterface

--- core/src/main/java/io/kestra/core/models/triggers/PollingTriggerInterface.java
+++ core/src/main/java/io/kestra/core/models/triggers/PollingTriggerInterface.java
@@ -1,12 +1,25 @@
 package io.kestra.core.models.triggers;
 
+import io.kestra.core.models.annotations.PluginProperty;
 import io.kestra.core.models.conditions.ConditionContext;
 import io.kestra.core.models.executions.Execution;
+import io.swagger.v3.oas.annotations.media.Schema;
 
+import java.time.Duration;
 import java.time.ZonedDateTime;
 import java.util.Optional;
 
-public interface PollingTriggerInterface extends WorkerTriggerInterface, WorkerTriggerIntervalInterface {
+public interface PollingTriggerInterface extends WorkerTriggerInterface {
+    @Schema(
+        title = "Interval between polling.",
+        description = "The interval between 2 different polls of schedule, this can avoid to overload the remote system " +
+            "with too many calls. For most of the triggers that depend on external systems, a minimal interval must be " +
+            "at least PT30S.\n" +
+            "See [ISO_8601 Durations](https://en.wikipedia.org/wiki/ISO_8601#Durations) for more information of available interval values."
+    )
+    @PluginProperty
+    Duration getInterval();
+
     Optional<Execution> evaluate(ConditionContext conditionContext, TriggerContext context) throws Exception;
 
     default ZonedDateTime nextEvaluationDate(ConditionContext conditionContext, Optional<? extends TriggerContext> last) throws Exception {

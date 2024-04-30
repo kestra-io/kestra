d78296585291bccd1f753df7b50e82e5e79a1f31
YannC <ycoornaert@kestra.io>
2024-04-30 11:57:23 +0200

    feat: allows to use svg for tenant logo

---
+++ ui/src/components/utils/SvgDisplay.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="wrapper">
+        <div class="icon" :style="styles" />
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "SvgDisplay",
+        props: {
+            encodedSvg: {
+                type: String,
+                default: undefined
+            }
+        },
+        computed: {
+            styles() {
+                return {
+                    backgroundImage: `url(data:image/svg+xml;base64,${this.encodedSvg})`
+                }
+            },
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .wrapper {
+        display: inline-block;
+        width: 100%;
+        height: 100%;
+        position: relative;
+
+        :deep(span) {
+            position: absolute;
+            padding: 1px;
+            left: 0;
+            display: block;
+            width: 100%;
+            height: 100%;
+        }
+
+        :deep(.icon) {
+            width: 100%;
+            height: 100%;
+            display: block;
+            background-size: contain;
+            background-repeat: no-repeat;
+            background-position: center center;
+        }
+    }
+</style>

f4103531761ce78ec88bc0709fc8b8c413b191a1
YannC <ycoornaert@kestra.io>
2024-05-16 17:28:16 +0200

    fix(core): avoid casting TaskForExecution to executableTask in graphs
    close #3757

--- core/src/main/java/io/kestra/core/models/hierarchies/SubflowGraphTask.java
+++ core/src/main/java/io/kestra/core/models/hierarchies/SubflowGraphTask.java
@@ -3,6 +3,8 @@ package io.kestra.core.models.hierarchies;
 import io.kestra.core.models.executions.TaskRun;
 import io.kestra.core.models.tasks.ExecutableTask;
 import io.kestra.core.models.tasks.Task;
+import io.kestra.core.models.tasks.TaskForExecution;
+import io.kestra.core.models.tasks.TaskInterface;
 import lombok.Getter;
 
 import java.util.List;
@@ -14,6 +16,11 @@ public class SubflowGraphTask extends AbstractGraphTask {
     }
 
     public ExecutableTask<?> getExecutableTask() {
-        return (ExecutableTask<?>) super.getTask();
+        TaskInterface task = super.getTask();
+        if (task instanceof ExecutableTask) {
+            return (ExecutableTask<?>) task;
+        } else {
+            return null;
+        }
     }
 }

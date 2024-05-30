d77d148ba57c3c52e4142c4efce99d7d782adf8c
Milos Paunovic <paun992@hotmail.com>
2024-05-30 13:31:04 +0200

    chore(ui): tweaked onboarding tour for both light and dark modes

--- ui/src/components/onboarding/components/buttons/Finish.vue
+++ ui/src/components/onboarding/components/buttons/Finish.vue
@@ -1,22 +1,5 @@
 <template>
-    <el-button class="finish">
+    <el-button class="alternative">
         {{ $t("onboarding.finish") }}
     </el-button>
 </template>
-
-<style scoped lang="scss">
-$background: #2f3342;
-$color: #cac5da;
-
-.finish {
-    background: $background;
-
-    html.light & {
-        color: $color;
-
-        &:hover {
-            color: $background;
-        }
-    }
-}
-</style>
--- ui/src/components/onboarding/components/buttons/Previous.vue
+++ ui/src/components/onboarding/components/buttons/Previous.vue
@@ -1,5 +1,5 @@
 <template>
-    <el-button :icon="ChevronLeft" class="previous">
+    <el-button :icon="ChevronLeft" class="alternative">
         {{ $t("onboarding.previous") }}
     </el-button>
 </template>
@@ -7,20 +7,3 @@
 <script setup lang="ts">
     import ChevronLeft from "vue-material-design-icons/ChevronLeft.vue";
 </script>
-
-<style scoped lang="scss">
-$background: #2f3342;
-$color: #cac5da;
-
-.previous {
-    background: $background;
-
-    html.light & {
-        color: $color;
-
-        &:hover {
-            color: $background;
-        }
-    }
-}
-</style>

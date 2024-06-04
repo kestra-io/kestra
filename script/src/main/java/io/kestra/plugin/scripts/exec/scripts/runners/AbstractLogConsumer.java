a0c8546015c0d5b7d99e7c96ac414f8dcbd7448c
Loïc Mathieu <loikeseke@gmail.com>
2024-06-04 18:01:25 +0200

    chore(*): remove deprecated classes not used anymore

--- script/src/main/java/io/kestra/plugin/scripts/exec/scripts/runners/AbstractLogConsumer.java
+++
@@ -1,8 +0,0 @@
-package io.kestra.plugin.scripts.exec.scripts.runners;
-
-/**
- * @deprecated use {@link io.kestra.core.models.tasks.runners.AbstractLogConsumer} instead.
- */
-@Deprecated
-public abstract class AbstractLogConsumer extends io.kestra.core.models.tasks.runners.AbstractLogConsumer {
-}

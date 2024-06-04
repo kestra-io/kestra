efdb0751557c2c5c584dad0742865a900cd2a4e7
YannC <ycoornaert@kestra.io>
2024-06-04 18:27:54 +0200

    fix(core): Now accept an extension for the file input
    close #3858

--- core/src/main/java/io/kestra/core/models/flows/input/FileInput.java
+++ core/src/main/java/io/kestra/core/models/flows/input/FileInput.java
@@ -1,18 +1,21 @@
 package io.kestra.core.models.flows.input;
 
 import io.kestra.core.models.flows.Input;
+import jakarta.validation.ConstraintViolationException;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 import java.net.URI;
 
-import jakarta.validation.ConstraintViolationException;
-
 @SuperBuilder
 @Getter
 @NoArgsConstructor
 public class FileInput extends Input<URI> {
+    @Builder.Default
+    public String extension = ".upl";
+
     @Override
     public void validate(URI input) throws ConstraintViolationException {
         // no validation yet

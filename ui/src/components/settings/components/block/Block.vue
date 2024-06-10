1e34a5528b7a36d7a7ada95727fcdbbde4cad2cc
Miloš Paunović <paun992@hotmail.com>
2024-06-10 09:50:27 +0200

    chore(ui): added note for timezone settings (#3968)

--- ui/src/components/settings/components/block/Block.vue
+++ ui/src/components/settings/components/block/Block.vue
@@ -1,14 +1,24 @@
 <template>
     <section>
-        <h1 class="heading" v-text="heading" />
+        <h1 class="heading">
+            <el-popover v-if="note" :content="note" trigger="hover" :width="400" class="info">
+                <template #reference>
+                    <InformationOutline />
+                </template>
+            </el-popover>
+            <span>{{ heading }}</span>
+        </h1>
         <slot name="content" />
         <el-divider v-if="!last" />
     </section>
 </template>
 
 <script setup lang="ts">
+    import InformationOutline from "vue-material-design-icons/InformationOutline.vue";
+
     defineProps({
         heading: {type: String, required: true},
+        note: {type: String, default: undefined},
         last: {type: Boolean, default: false},
     });
 </script>
@@ -20,9 +30,16 @@ section {
     margin: calc($spacer * 2);
 
     & > h1.heading {
+        display: flex;
+        align-items: center;
         margin-bottom: calc($spacer * 2);
         font-size: calc($font-size-base * 1.5);
         font-weight: 600;
+
+        & > span.el-tooltip__trigger {
+            cursor: pointer;
+            margin-right: calc($spacer / 2);
+        }
     }
 }
 </style>

4327f080a2a7dfe89db34d543de9fffb8a800bf5
YannC <ycoornaert@kestra.io>
2024-06-10 11:15:47 +0200

    fix(): avoid using link tag in custom markdown plugin
    close #3963

--- ui/src/utils/markdown_plugins/link.ts
+++ ui/src/utils/markdown_plugins/link.ts
@@ -3,7 +3,7 @@ import MarkdownIt from "markdown-it";
 export function linkTag(md: MarkdownIt) {
     const linkTagRegex = /\[\[link(\s+(.*?))?\]\]/g;
 
-    md.inline.ruler.after("emphasis", "link", (state, silent) => {
+    md.inline.ruler.after("emphasis", "link_custom", (state, silent) => {
         if (silent) return false;
 
         // Check if the current position is a link tag
@@ -16,21 +16,21 @@ export function linkTag(md: MarkdownIt) {
             .map(attr => attr.split("="))
             .map(([name, value]) => [name, value.replace(/"/g, "")]);
 
-        const token = state.push("link_open", "link", 1);
+        const token = state.push("link_custom_open", "link", 1);
         token.markup = "[[link]]";
         token.attrs = attrs;
-        state.push("link_close", "link", -1);
+        state.push("link_custom_close", "link_custom", -1);
 
         state.pos = state.pos + match[0].length;
 
         return true;
     });
 
-    md.renderer.rules.link_open = (tokens, idx) => {
+    md.renderer.rules.link_custom_open = (tokens, idx) => {
         const token = tokens[idx];
         const attrs = token.attrs ? token.attrs.map(([name, value]) => `${name}="${value}"`).join("") : "";
 
         return `<router-md ${attrs}>`;
     };
-    md.renderer.rules.link_close = () => "</router-md>";
+    md.renderer.rules.link_custom_close = () => "</router-md>";
 }

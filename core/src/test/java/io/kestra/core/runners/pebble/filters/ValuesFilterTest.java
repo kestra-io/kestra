503d9696b4076c070f998dc7310855dbd02089fe
Ludovic DEHON <tchiot.ludo@gmail.com>
2024-06-24 19:24:26 +0200

    refactor(core): values filter test was flaky

--- core/src/test/java/io/kestra/core/runners/pebble/filters/ValuesFilterTest.java
+++ core/src/test/java/io/kestra/core/runners/pebble/filters/ValuesFilterTest.java
@@ -1,20 +1,23 @@
 package io.kestra.core.runners.pebble.filters;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.google.common.collect.ImmutableMap;
 import io.kestra.core.exceptions.IllegalVariableEvaluationException;
 import io.kestra.core.junit.annotations.KestraTest;
 import io.kestra.core.runners.VariableRenderer;
+import io.kestra.core.serializers.JacksonMapper;
 import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
 
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 
 @KestraTest
 class ValuesFilterTest {
@@ -22,7 +25,7 @@ class ValuesFilterTest {
     VariableRenderer variableRenderer;
 
     @Test
-    void map() throws IllegalVariableEvaluationException {
+    void map() throws IllegalVariableEvaluationException, JsonProcessingException {
         ZonedDateTime date = ZonedDateTime.parse("2013-09-08T16:19:00+02").withZoneSameLocal(ZoneId.systemDefault());
 
         ImmutableMap<String, Object> vars = ImmutableMap.of(
@@ -39,11 +42,13 @@ class ValuesFilterTest {
         );
 
         String render = variableRenderer.render("{{ vars.second.map | values }}", vars);
-        assertThat(render, containsString("\"string\""));
-        assertThat(render, containsString("123"));
+        List<Object> list = JacksonMapper.ofJson().readValue(render, new TypeReference<>() {});
+        assertThat(list, hasItems("string"));
+        assertThat(list, hasItems(1.123));
 
         render = variableRenderer.render("{{ vars.second | values }}", vars);
-        assertThat(render, containsString("\"string\""));
-        assertThat(render, containsString("true,"));
+        list = JacksonMapper.ofJson().readValue(render, new TypeReference<>() {});
+        assertThat(list, hasItems("string"));
+        assertThat(list, hasItems(true));
     }
 }

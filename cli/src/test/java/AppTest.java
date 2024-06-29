d2f8bca48afe16336bbf63e29c9e796a0ae6f5c8
yuri1969 <1969yuri1969@gmail.com>
2024-06-29 10:36:52 +0200

    feat(cli): provide descriptive server start error
    * The validation is based just on 3 basic Kestra properties.
    * Provided a description and link to the docs in the error.
    * The `server local` nor any other non-`server` commands were affected.
    * Added a crude test.

--- cli/src/test/java/AppTest.java
+++ cli/src/test/java/AppTest.java
@@ -1,6 +1,9 @@
+import io.kestra.cli.commands.servers.AbstractServerCommand;
 import io.micronaut.configuration.picocli.PicocliRunner;
 import io.micronaut.context.ApplicationContext;
 import io.micronaut.context.env.Environment;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import io.kestra.cli.App;
 
@@ -9,8 +12,20 @@ import java.io.PrintStream;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class AppTest {
+    @BeforeAll
+    public static void setUp() {
+        // Don't make any assumptions about $HOME
+        System.setProperty("user.home", "/foo");
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        System.clearProperty("user.home");
+    }
+
     @Test
     public void testHelp() {
         ByteArrayOutputStream out = new ByteArrayOutputStream();
@@ -21,5 +36,10 @@ public class AppTest {
 
             assertThat(out.toString(), containsString("kestra"));
         }
+    }
+
+    @Test
+    public void testSeverCommandValidation() {
+        assertThrows(AbstractServerCommand.ServerCommandException.class, () -> App.main(new String[]{"server", "webserver"}));
     }
 }

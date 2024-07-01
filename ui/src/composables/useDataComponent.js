0b770a84556c5eda489f17bf2f1f4d9b7987afea
Milos Paunovic <paun992@hotmail.com>
2024-07-01 14:07:38 +0200

    fix(ui): amended method for extracting component path for data-component attribute

--- ui/src/composables/useDataComponent.js
+++ ui/src/composables/useDataComponent.js
@@ -2,8 +2,5 @@ import {getCurrentInstance} from "vue"
 
 export default function useDataComponent() {
     const instance = getCurrentInstance();
-    let file = instance?.type?.__file?.substring(import.meta.env.__ROOT_DIR__.length + 1);
-    file = file.substring(4, file.length - 4);
-
-    return file;
+    return instance?.type?.__file?.substring(import.meta.env.__ROOT_DIR__.length - 1);
 }

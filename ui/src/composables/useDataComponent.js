c347608a86566b04b0dcfa02117816817ace37ee
Miloš Paunović <paun992@hotmail.com>
2024-07-01 13:40:16 +0200

    fix(ui): properly checking is right property exists (#4187)

--- ui/src/composables/useDataComponent.js
+++ ui/src/composables/useDataComponent.js
@@ -2,7 +2,7 @@ import {getCurrentInstance} from "vue"
 
 export default function useDataComponent() {
     const instance = getCurrentInstance();
-    let file = instance.type.__file.substr(import.meta.env.__ROOT_DIR__.length + 1);
+    let file = instance?.type?.__file?.substring(import.meta.env.__ROOT_DIR__.length + 1);
     file = file.substring(4, file.length - 4);
 
     return file;

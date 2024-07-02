4aa803bfb54df89ca6fa1533f24fe00753a0b27f
Milos Paunovic <paun992@hotmail.com>
2024-07-02 09:56:19 +0200

    feat: implement vite plugin to do file name injection for testing

---
+++ ui/plugins/filename.ts
@@ -0,0 +1,16 @@
+import type {Plugin} from "vite";
+
+const process = (path: string): string => {
+    const match = path.match(/.*\/src\/(.*)\.vue$/);
+    return match ? match[1].toLowerCase() : path;
+};
+
+export function filename(): Plugin {
+    return {
+        name: "filename",
+        transform(code: string, id: string) {
+            if (!id.endsWith(".vue")) return;
+            return {code: code.replace(/FILENAME_PLACEHOLDER/g, process(id)), map: null};
+        },
+    };
+}

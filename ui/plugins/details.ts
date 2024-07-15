08b25643338c432aa5ac16a1fa28e21ae9f8d95d
Miloš Paunović <paun992@hotmail.com>
2024-07-15 13:59:38 +0200

    feat(ui): add commit details to `index.html` file using `vite` plugin system (#4334)

---
+++ ui/plugins/details.ts
@@ -0,0 +1,29 @@
+import type {Plugin} from "vite";
+import {execSync} from "child_process";
+
+const comment = (hash: string, date: string): string => {
+    return `
+<!--
+
+    Last Commit: 
+      
+    URL: https://github.com/kestra-io/kestra/commit/${hash}
+    Date: ${date}
+
+-->`;
+};
+
+export const details = (): Plugin => {
+    const hash: string = execSync("git rev-parse --short HEAD").toString().trim();
+    const date: string = execSync("git log -1 --format=%cd").toString().trim();
+
+    return {
+        name: "details",
+        transformIndexHtml: {
+            enforce: "pre",
+            transform(html: string): string {
+                return comment(hash, date) + html;
+            },
+        },
+    };
+};

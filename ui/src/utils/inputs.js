54c4f1d702c46922df54fde93a6db29ed790fdfc
Loïc Mathieu <loikeseke@gmail.com>
2024-07-18 14:25:09 +0200

    fix(ui): proprely handle `json` type inputs when prefilling executions (#4333)

--- ui/src/utils/inputs.js
+++ ui/src/utils/inputs.js
@@ -4,6 +4,25 @@ export default class Inputs {
     static normalize(type, value) {
         let res = value;
 
+        if (value === null || value === undefined) {
+            res = undefined;
+        } else if (type === "DATE" || type === "DATETIME") {
+            res = moment(res).toISOString()
+        } else if (type === "DURATION" || type === "TIME") {
+            res = moment().startOf("day").add(res, "seconds").toString()
+        } else if (type === "ARRAY" || type === "MULTISELECT" || type === "JSON") {
+            res = JSON.stringify(res).toString();
+        } else if (type === "BOOLEAN" && type === undefined){
+            res = "undefined";
+        } else if (type === "STRING" && Array.isArray(res)){
+            res = res.toString();
+        }
+        return res;
+    }
+
+    static normalizeForComponents(type, value) {
+        let res = value;
+
         if (value === null) {
             res = undefined;
         } else if (type === "DATE" || type === "DATETIME") {

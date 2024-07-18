96803a6cc8829f59e4a2b4bacabdd252e77ee641
yuri <1969yuri1969@gmail.com>
2024-07-18 09:03:33 +0200

    feat(cli): provide descriptive server start error (#4171)
    * feat(cli): provide descriptive server start error
    
    * The validation is based just on 3 basic Kestra properties.
    * Provided a description and link to the docs in the error.
    * The `server local` nor any other non-`server` commands were affected.
    * Added a crude test.
    
    * Describe each missing property separately

--- cli/src/main/java/io/kestra/cli/App.java
+++ cli/src/main/java/io/kestra/cli/App.java
@@ -4,6 +4,7 @@ import io.kestra.cli.commands.configs.sys.ConfigCommand;
 import io.kestra.cli.commands.flows.FlowCommand;
 import io.kestra.cli.commands.namespaces.NamespaceCommand;
 import io.kestra.cli.commands.plugins.PluginCommand;
+import io.kestra.cli.commands.servers.AbstractServerCommand;
 import io.kestra.cli.commands.servers.ServerCommand;
 import io.kestra.cli.commands.sys.SysCommand;
 import io.kestra.cli.commands.templates.TemplateCommand;
@@ -13,16 +14,14 @@ import io.micronaut.context.ApplicationContext;
 import io.micronaut.context.ApplicationContextBuilder;
 import io.micronaut.context.env.Environment;
 import io.micronaut.core.annotation.Introspected;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 import picocli.CommandLine;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 import java.util.concurrent.Callable;
 
 @CommandLine.Command(
@@ -44,6 +43,7 @@ import java.util.concurrent.Callable;
         NamespaceCommand.class,
     }
 )
+@Slf4j
 @Introspected
 public class App implements Callable<Integer> {
     public static void main(String[] args) {
@@ -79,9 +79,7 @@ public class App implements Callable<Integer> {
      * @param args args passed to java app
      * @return the application context created
      */
-    protected static ApplicationContext applicationContext(Class<?> mainClass,
-                                                           String[] args) {
-
+    protected static ApplicationContext applicationContext(Class<?> mainClass, String[] args) {
         ApplicationContextBuilder builder = ApplicationContext
             .builder()
             .mainClass(mainClass)
@@ -96,15 +94,15 @@ public class App implements Callable<Integer> {
         Class<?> cls = commandLine.getCommandSpec().userObject().getClass();
 
         if (AbstractCommand.class.isAssignableFrom(cls)) {
-            // if class have propertiesFromConfig, add configuration files
-            builder.properties(getPropertiesFromMethod(cls, "propertiesFromConfig", commandLine.getCommandSpec().userObject()));
-
             Map<String, Object> properties = new HashMap<>();
 
+            // if class have propertiesFromConfig, add configuration files
+            addPropertiesFromMethod(properties, cls, "propertiesFromConfig", commandLine.getCommandSpec().userObject());
             // if class have propertiesOverrides, add force properties for this class
-            Map<String, Object> propertiesOverrides = getPropertiesFromMethod(cls, "propertiesOverrides", null);
-            if (propertiesOverrides != null) {
-                properties.putAll(propertiesOverrides);
+            addPropertiesFromMethod(properties, cls, "propertiesOverrides", null);
+
+            if (AbstractServerCommand.class.isAssignableFrom(cls)) {
+                validateServerCmdConfig(properties);
             }
 
             // custom server configuration
@@ -121,6 +119,34 @@ public class App implements Callable<Integer> {
             builder.properties(properties);
         }
         return builder.build();
+    }
+
+    private static void addPropertiesFromMethod(Map<String, Object> properties,  Class<?> cls, String methodName, Object instance) {
+        Map<String, Object> propertiesFromMethod = getPropertiesFromMethod(cls, methodName, instance);
+        if (propertiesFromMethod != null) {
+            properties.putAll(propertiesFromMethod);
+        }
+    }
+
+    private static void validateServerCmdConfig(Map<String, Object> propertiesFromConfig) {
+        final Map<String, String> requiredProperties = Map.of(
+            "kestra.queue.type", "https://kestra.io/docs/configuration-guide/setup#queue-configuration",
+            "kestra.repository.type", "https://kestra.io/docs/configuration-guide/setup#repository-configuration",
+            "kestra.storage.type", "https://kestra.io/docs/configuration-guide/setup#internal-storage-configuration"
+        );
+
+        final List<Map.Entry<String, String>> missingProperties = requiredProperties.entrySet().stream()
+            .filter((property) -> !propertiesFromConfig.containsKey(property.getKey()))
+            .toList();
+
+        missingProperties.forEach(property -> log.error("""
+            Server configuration requires the '{}' property to be defined.
+            For more details, please follow the official setup guide at: {}""", property.getKey(), property.getValue())
+        );
+
+        if (!missingProperties.isEmpty()) {
+            throw new AbstractServerCommand.ServerCommandException("Incomplete server configuration - missing required properties");
+        }
     }
 
     @SuppressWarnings("unchecked")

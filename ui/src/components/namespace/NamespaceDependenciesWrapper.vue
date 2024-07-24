d0a7eb928a21bb125f5abb7803ef19d7ccd7a029
Milos Paunovic <paun992@hotmail.com>
2024-07-24 09:32:26 +0200

    fix(ui): pass proper namespace id attribute to dependencies tab

--- ui/src/components/namespace/NamespaceDependenciesWrapper.vue
+++ ui/src/components/namespace/NamespaceDependenciesWrapper.vue
@@ -1,10 +1,14 @@
 <template>
-    <NamespaceDependencies :id="route.params.id" />
+    <NamespaceDependencies :namespace="namespace" />
 </template>
 
 <script setup>
+    import {computed} from "vue";
+
     import {useRoute} from "vue-router";
     const route = useRoute();
+
+    const namespace = computed(() => route.params.id);
 
     import NamespaceDependencies from "./NamespaceDependencies.vue";
 </script>

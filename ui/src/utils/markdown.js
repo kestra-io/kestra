ced6b53457adbade7a3b817d160f1c1e91164c26
Milos Paunovic <paun992@hotmail.com>
2024-08-06 15:09:47 +0200

    chore(deps): handle @shikijs/markdown-it updates with small code refactoring

--- ui/src/utils/markdown.js
+++ ui/src/utils/markdown.js
@@ -4,28 +4,20 @@ import meta from "markdown-it-meta";
 import anchor from "markdown-it-anchor";
 import container from "markdown-it-container";
 import {fromHighlighter} from "@shikijs/markdown-it/core"
-import {getHighlighterCore} from "shiki/core"
+import {createHighlighterCore} from "shiki/core"
 import githubDark from "shiki/themes/github-dark.mjs";
 import githubLight from "shiki/themes/github-light.mjs";
 import {linkTag} from "./markdown_plugins/link";
 
+const highlighter = await createHighlighterCore({
+    langs: [import("shiki/langs/yaml.mjs"), import("shiki/langs/python.mjs"), import("shiki/langs/javascript.mjs")],
+    themes: [githubDark, githubLight],
+    loadWasm: import("shiki/wasm")
+})
 export default class Markdown {
     static async render(markdown, options) {
         githubDark["colors"]["editor.background"] = "var(--bs-gray-500)";
         githubLight["colors"]["editor.background"] = "var(--bs-white)";
-
-        const highlighter = await getHighlighterCore({
-            themes: [
-                githubDark,
-                githubLight
-            ],
-            langs: [
-                import("shiki/langs/yaml.mjs"),
-                import("shiki/langs/python.mjs"),
-                import("shiki/langs/javascript.mjs")
-            ],
-            loadWasm: import("shiki/wasm")
-        })
 
         options = options || {}
 

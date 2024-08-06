ed04c8af4340dbd4af698041ceee555428c21308
Milos Paunovic <paun992@hotmail.com>
2024-08-06 16:33:48 +0200

    chore(ui): making sure shiki highlighter is created as a singleton

--- ui/src/utils/markdown.js
+++ ui/src/utils/markdown.js
@@ -3,41 +3,43 @@ import mark from "markdown-it-mark";
 import meta from "markdown-it-meta";
 import anchor from "markdown-it-anchor";
 import container from "markdown-it-container";
-import {fromHighlighter} from "@shikijs/markdown-it/core"
-import {createHighlighterCore} from "shiki/core"
+import {fromHighlighter} from "@shikijs/markdown-it/core";
+import {createHighlighterCore} from "shiki/core";
 import githubDark from "shiki/themes/github-dark.mjs";
 import githubLight from "shiki/themes/github-light.mjs";
 import {linkTag} from "./markdown_plugins/link";
 
-const highlighter = await createHighlighterCore({
-    langs: [import("shiki/langs/yaml.mjs"), import("shiki/langs/python.mjs"), import("shiki/langs/javascript.mjs")],
-    themes: [githubDark, githubLight],
-    loadWasm: import("shiki/wasm")
-})
+let highlighter = null;
+
+async function getHighlighter() {
+    if (!highlighter) {
+        highlighter = createHighlighterCore({
+            langs: [import("shiki/langs/yaml.mjs"), import("shiki/langs/python.mjs"), import("shiki/langs/javascript.mjs")],
+            themes: [githubDark, githubLight],
+            loadWasm: import("shiki/wasm"),
+        });
+    }
+    return highlighter;
+}
+
 export default class Markdown {
     static async render(markdown, options) {
+        const highlighter = await getHighlighter();
+
         githubDark["colors"]["editor.background"] = "var(--bs-gray-500)";
         githubLight["colors"]["editor.background"] = "var(--bs-white)";
 
-        options = options || {}
+        options = options || {};
 
         const darkTheme = document.getElementsByTagName("html")[0].className.indexOf("dark") >= 0;
 
-        // noinspection JSPotentiallyInvalidConstructorUsage
-        let md = new markdownIt() // jshint ignore:line
+        let md = new markdownIt()
             .use(mark)
             .use(meta)
-            .use(anchor, {
-                permalink: options.permalink ? anchor.permalink.ariaHidden({
-                    placement: "before"
-                }) : undefined
-            })
-            // if more alert types are used inside the task documentation, they need to be configured here also
+            .use(anchor, {permalink: options.permalink ? anchor.permalink.ariaHidden({placement: "before"}) : undefined})
             .use(container, "warning")
             .use(container, "info")
-            .use(fromHighlighter(highlighter, {
-                theme: darkTheme ? "github-dark" : "github-light",
-            }))
+            .use(fromHighlighter(highlighter, {theme: darkTheme ? "github-dark" : "github-light"}))
             .use(linkTag);
 
         md.set({
@@ -48,12 +50,10 @@ export default class Markdown {
             typographer: true,
             langPrefix: "language-",
             quotes: "“”‘’",
-        })
+        });
 
-        md.renderer.rules.table_open = () => "<table class=\"table\">\n"
+        md.renderer.rules.table_open = () => "<table class=\"table\">\n";
 
-        return md.render(
-            markdown
-        );
+        return md.render(markdown);
     }
 }

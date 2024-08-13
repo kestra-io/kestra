9cc435cfce91a97bc2d460fa942838fec64c3cad
YannC <37600690+Skraye@users.noreply.github.com>
2024-08-13 14:56:55 +0200

    tests(jdbc): keep tables name in memory (#4647)
    * tests(jdbc): keep tables name in memory
    
    * fix(jdbc): added @PostConstruct setup for tables

--- jdbc/src/test/java/io/kestra/jdbc/JdbcTestUtils.java
+++ jdbc/src/test/java/io/kestra/jdbc/JdbcTestUtils.java
@@ -2,14 +2,18 @@ package io.kestra.jdbc;
 
 import io.micronaut.flyway.FlywayConfigurationProperties;
 import io.micronaut.flyway.FlywayMigrator;
+import jakarta.annotation.PostConstruct;
 import lombok.SneakyThrows;
 import org.jooq.DSLContext;
 
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
+import org.jooq.Table;
 import org.jooq.impl.DSL;
 
 import javax.sql.DataSource;
+
+import java.util.List;
 
 import static io.kestra.core.utils.Rethrow.throwPredicate;
 
@@ -30,25 +34,35 @@ public class JdbcTestUtils {
     @Inject
     private JdbcTableConfigs tableConfigs;
 
+    List<Table<?>> tables;
+
+    @PostConstruct
+    public void setup() {
+        dslContextWrapper.transaction((configuration) -> {
+            DSLContext dslContext = DSL.using(configuration);
+
+            this.tables = dslContext
+                .meta()
+                .getTables()
+                .stream()
+                .filter(throwPredicate(table -> (table.getSchema().getName().equals(dataSource.getConnection().getCatalog())) ||
+                    table.getSchema().getName().equals("public")  || // for Postgres
+                    table.getSchema().getName().equals("dbo") // for SQLServer
+                ))
+                .filter(table -> tableConfigs.getTableConfigs().stream().anyMatch(conf -> conf.table().equalsIgnoreCase(table.getName())))
+                .toList();
+        });
+    }
+
     @SneakyThrows
     public void drop() {
         var tableNames = tableConfigs.getTableConfigs().stream().map(conf -> conf.table().toLowerCase()).toList();
         dslContextWrapper.transaction((configuration) -> {
             DSLContext dslContext = DSL.using(configuration);
 
-            dslContext
-                .meta()
-                .getTables()
-                .stream()
-                .filter(throwPredicate(table -> (table.getSchema().getName().equals(dataSource.getConnection().getCatalog())) ||
-                    table.getSchema().getName().equals("public")  || // for Postgres
-                    table.getSchema().getName().equals("dbo") // fo SQLServer
-                ))
-                .filter(table -> tableNames.contains(table.getName().toLowerCase()))
-                .forEach(t -> dslContext.delete(t).execute());
+            this.tables.forEach(t -> dslContext.delete(t).execute());
         });
     }
-
     public void migrate() {
         dslContextWrapper.transaction((configuration) -> {
             flywayMigrator.run(config, dataSource);

76f116499f4c3c5c466f352092f6fe0b3b859c6d
MilosPaunovic <paun992@hotmail.com>
2024-09-06 14:38:40 +0200

    chore(ui): amend scope filter buttons component

--- ui/src/components/layout/ScopeFilterButtons.vue
+++ ui/src/components/layout/ScopeFilterButtons.vue
@@ -1,5 +1,5 @@
 <template>
-    <el-select :model-value="value" @update:model-value="onInput" clearable multiple>
+    <el-select :model-value="value" @update:model-value="onInput" clearable :placeholder="$t('scope_filter.all')">
         <el-option v-for="item in options" :key="item.key" :label="item.name" :value="item.key" />
     </el-select>
 </template>
@@ -11,7 +11,10 @@
         emits: ["update:modelValue"],
         data() {
             return {
-                options: [{name: "User only", key: "user"}, {name: "System only", key: "system"}]
+                options: [
+                    {name: this.$t("scope_filter.user"), key: "USER"},
+                    {name: this.$t("scope_filter.system"), key: "SYSTEM"}
+                ]
             };
         },
         methods: {

644110a7a002b7291357f59d9da3d66dd8972a16
Loïc Mathieu <loikeseke@gmail.com>
2024-09-11 09:27:06 +0200

    fix(jdbc): handle duplicate key on flow topology save
    We usually always include `onDuplicateKeyUpdate()` when we save an object in the database, it was missing for batch insert of flow topologies.
    
    Fixes #4825

--- jdbc-mysql/src/main/java/io/kestra/repository/mysql/MysqlFlowTopologyRepository.java
+++ jdbc-mysql/src/main/java/io/kestra/repository/mysql/MysqlFlowTopologyRepository.java
@@ -2,9 +2,13 @@ package io.kestra.repository.mysql;
 
 import io.kestra.core.models.topologies.FlowTopology;
 import io.kestra.jdbc.repository.AbstractJdbcFlowTopologyRepository;
+import io.kestra.jdbc.repository.AbstractJdbcRepository;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
 import jakarta.inject.Singleton;
+import org.jooq.DMLQuery;
+import org.jooq.DSLContext;
+import org.jooq.Record;
 
 @Singleton
 @MysqlRepositoryEnabled
@@ -12,5 +16,14 @@ public class MysqlFlowTopologyRepository extends AbstractJdbcFlowTopologyReposit
     @Inject
     public MysqlFlowTopologyRepository(@Named("flowtopologies") MysqlRepository<FlowTopology> repository) {
         super(repository);
+    }
+
+    @Override
+    protected DMLQuery<Record> buildMergeStatement(DSLContext context, FlowTopology flowTopology) {
+        return context.insertInto(this.jdbcRepository.getTable())
+            .set(AbstractJdbcRepository.field("key"), this.jdbcRepository.key(flowTopology))
+            .set(this.jdbcRepository.persistFields(flowTopology))
+            .onDuplicateKeyUpdate()
+            .set(this.jdbcRepository.persistFields(flowTopology));
     }
 }

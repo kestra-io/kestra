6bf61884523e627a7ca9d52c89638909bc5f752a
Miloš Paunović <paun992@hotmail.com>
2024-09-11 15:01:06 +0200

    fix(ui): amend trigger logs flickering (#4894)

--- ui/src/components/logs/LogsWrapper.vue
+++ ui/src/components/logs/LogsWrapper.vue
@@ -126,7 +126,11 @@
             filters: {
                 type: Object,
                 default: null
-            }
+            },
+            purgeFilters: {
+                type: Boolean,
+                default: false
+            },
         },
         data() {
             return {
@@ -204,7 +208,9 @@
                 this.load();
             },
             loadQuery(base) {
-                let queryFilter = this.filters ? this.filters : this.queryWithFilter();
+                // eslint-disable-next-line no-unused-vars
+                const {triggerId, ...rest} = this.filters || {};
+                let queryFilter = this.filters ? (this.purgeFilters ? rest : this.filters) : this.queryWithFilter();
 
                 if (this.isFlowEdit) {
                     queryFilter["namespace"] = this.namespace;
@@ -225,9 +231,13 @@
             load() {
                 this.isLoading = true
 
-                const data = this.filters
-                    ? {page: this.internalPageNumber, size: this.internalPageSize, ...this.filters}
-                    : {page: this.$route.query.page || this.internalPageNumber, size: this.$route.query.size || this.internalPageSize}
+                // eslint-disable-next-line no-unused-vars
+                const {triggerId, ...rest} = this.filters || {};
+                const data = {
+                    page: this.filters ? this.internalPageNumber : this.$route.query.page || this.internalPageNumber,
+                    size: this.filters ? this.internalPageSize : this.$route.query.size || this.internalPageSize,
+                    ...(this.purgeFilters ? rest : this.filters)
+                };
 
                 this.$store
                     .dispatch("log/findLogs", this.loadQuery({

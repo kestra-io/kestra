98e4f966e4f7a7af5b0801a7a2b5ce8812161dab
MilosPaunovic <paun992@hotmail.com>
2024-09-17 12:48:52 +0200

    feat(ui): initial work on refreshing the dashboard

---
+++ ui/src/components/dashboard/Dashboard.vue
@@ -0,0 +1,80 @@
+<template>
+    <Header />
+
+    <div class="dashboard">
+        <el-row :gutter="20" class="mx-0">
+            <el-col :xs="24" :md="12" :lg="6">
+                <Card
+                    :icon="CheckBold"
+                    :label="t('dashboard.success_ratio')"
+                    value="89%"
+                />
+            </el-col>
+            <el-col :xs="24" :md="12" :lg="6">
+                <Card
+                    :icon="Alert"
+                    :label="t('dashboard.failure_ratio')"
+                    value="11%"
+                />
+            </el-col>
+            <el-col :xs="24" :md="12" :lg="6">
+                <Card :icon="FileTree" :label="t('flows')" value="338" />
+            </el-col>
+            <el-col :xs="24" :md="12" :lg="6">
+                <Card :icon="LightningBolt" :label="t('triggers')" value="29" />
+            </el-col>
+        </el-row>
+
+        <el-row :gutter="20" class="mx-0">
+            <el-col :xs="24" :lg="16">
+                <div>1</div>
+            </el-col>
+            <el-col :xs="24" :lg="8">
+                <div>2</div>
+            </el-col>
+        </el-row>
+
+        <el-row :gutter="20" class="mx-0">
+            <el-col :xs="24" :lg="12">
+                <div>1</div>
+            </el-col>
+            <el-col :xs="24" :lg="12">
+                <div>2</div>
+            </el-col>
+        </el-row>
+    </div>
+</template>
+
+<script setup>
+    import {useI18n} from "vue-i18n";
+
+    import Header from "./components/Header.vue";
+    import Card from "./components/Card.vue";
+
+    import CheckBold from "vue-material-design-icons/CheckBold.vue";
+    import Alert from "vue-material-design-icons/Alert.vue";
+    import LightningBolt from "vue-material-design-icons/LightningBolt.vue";
+    import FileTree from "vue-material-design-icons/FileTree.vue";
+
+    const {t} = useI18n({useScope: "global"});
+</script>
+
+<style lang="scss" scoped>
+$spacing: 20px;
+
+.dashboard {
+    padding: $spacing;
+
+    & .el-row {
+        width: 100%;
+
+        & .el-col {
+            padding-bottom: $spacing;
+
+            & div {
+                background: var(--card-bg);
+            }
+        }
+    }
+}
+</style>
---
+++ ui/src/components/dashboard/components/Card.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="p-4 card">
+        <div class="d-flex pb-2 justify-content-between">
+            <div class="d-flex">
+                <component :is="icon" class="me-2 fs-4 icons" />
+
+                <p class="m-0 fs-6 label">
+                    {{ label }}
+                </p>
+            </div>
+
+            <TextSearchVariant class="fs-4 icons" />
+        </div>
+        <p class="m-0 fs-2 fw-bold">
+            {{ value }}
+        </p>
+    </div>
+</template>
+
+<script setup>
+    import TextSearchVariant from "vue-material-design-icons/TextSearchVariant.vue";
+
+    defineProps({
+        icon: {
+            type: Object,
+            required: true,
+        },
+        label: {
+            type: String,
+            required: true,
+        },
+        value: {
+            type: String,
+            required: true,
+        },
+    });
+</script>
+
+<style lang="scss" scoped>
+$color: #c182ff;
+
+.card {
+    & .icons {
+        color: $color;
+    }
+
+    & .label {
+        color: var(--el-text-color-regular);
+    }
+}
+</style>

b2547f3aca158b1fdb050398aa5e06a6190ed755
YannC <ycoornaert@kestra.io>
2024-09-18 10:18:49 +0200

    fix(webserver): missing params in file paths
    close #4958

--- webserver/src/main/java/io/kestra/webserver/controllers/api/ExecutionController.java
+++ webserver/src/main/java/io/kestra/webserver/controllers/api/ExecutionController.java
@@ -662,7 +662,7 @@ public class ExecutionController {
         @Parameter(description = "The execution id") @PathVariable String executionId,
         @Parameter(description = "The internal storage uri") @QueryValue URI path
     ) throws IOException, URISyntaxException {
-        HttpResponse<StreamedFile> httpResponse = this.validateFile(executionId, path, "/api/v1/executions/{executionId}/file?path=" + path);
+        HttpResponse<StreamedFile> httpResponse = this.validateFile(executionId, path, "/api/v1/" + this.getTenant() + "executions/{executionId}/file?path=" + path);
         if (httpResponse != null) {
             return httpResponse;
         }
@@ -680,7 +680,7 @@ public class ExecutionController {
         @Parameter(description = "The execution id") @PathVariable String executionId,
         @Parameter(description = "The internal storage uri") @QueryValue URI path
     ) throws IOException {
-        HttpResponse<FileMetas> httpResponse = this.validateFile(executionId, path, "/api/v1/executions/{executionId}/file/metas?path=" + path);
+        HttpResponse<FileMetas> httpResponse = this.validateFile(executionId, path, "/api/v1/" + this.getTenant() + "executions/{executionId}/file/metas?path=" + path);
         if (httpResponse != null) {
             return httpResponse;
         }
@@ -1443,7 +1443,7 @@ public class ExecutionController {
         @Parameter(description = "The max row returns") @QueryValue @Nullable Integer maxRows,
         @Parameter(description = "The file encoding as Java charset name. Defaults to UTF-8", example = "ISO-8859-1") @QueryValue(defaultValue = "UTF-8") String encoding
     ) throws IOException {
-        this.validateFile(executionId, path, "/api/v1/executions/{executionId}/file?path=" + path);
+        this.validateFile(executionId, path, "/api/v1/" + this.getTenant() + "executions/{executionId}/file?path=" + path);
 
         String extension = FilenameUtils.getExtension(path.toString());
         Optional<Charset> charset;
@@ -1642,6 +1642,10 @@ public class ExecutionController {
         @Parameter(description = "The namespace") @PathVariable String namespace
     ) {
         return flowRepository.findByNamespaceExecutable(tenantService.resolveTenant(), namespace);
+    }
+
+    public String getTenant() {
+        return tenantService.resolveTenant() != null ? tenantService.resolveTenant() + "/" : "";
     }
 
 }

0ceb0d74df4885196bcece9bdda416ca8c797d71
Loïc Mathieu <loikeseke@gmail.com>
2024-09-24 14:12:18 +0200

    fix(ui): missing translation + wrong property name in the trigger list

--- jdbc/src/main/java/io/kestra/jdbc/repository/AbstractJdbcTriggerRepository.java
+++ jdbc/src/main/java/io/kestra/jdbc/repository/AbstractJdbcTriggerRepository.java
@@ -135,7 +135,7 @@ public abstract class AbstractJdbcTriggerRepository extends AbstractJdbcReposito
             .orderBy(field("next_execution_date").asc())
             .forUpdate()
             .fetch()
-            .map(r -> this.jdbcRepository.deserialize(r.get("value").toString()));
+            .map(r -> this.jdbcRepository.deserialize(r.get("value", String.class)));
     }
 
     public Trigger save(Trigger trigger, ScheduleContextInterface scheduleContextInterface) {

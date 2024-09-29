500fb44c7f0278a3c78842cbb51771e0675facd8
shabith16 <shabith.1981@gmail.com>
2024-09-30 03:36:48 +0530

    UI has been updated, MetadataInputs.vue and MetadataVariables.vue has been removed.

--- ui/src/components/flows/MetadataEditor.vue
+++ ui/src/components/flows/MetadataEditor.vue
@@ -79,12 +79,6 @@
                 </div>
             </div>
         </el-form-item>
-        <!--<el-form-item>
-            <template #label>
-                <code>inputs</code>
-            </template>
-            <metadata-inputs v-model="newMetadata.inputs" :inputs="newMetadata.inputs" />
-        </el-form-item>-->
         <el-form-item>
             <template #label>
                 <code>outputs</code>
@@ -98,12 +92,6 @@
                 @update:model-value="(value) => newMetadata.outputs = value"
             />
         </el-form-item>
-        <!--<el-form-item>
-            <template #label>
-                <code>variables</code>
-            </template>
-            <metadata-variables v-model="newMetadata.variables" :variables="newMetadata.variables" />
-        </el-form-item>-->
         <el-switch
             :model-value="showConcurrency"
             @update:model-value="updateConcurrency"
@@ -152,8 +140,6 @@
 <script>
     import {toRaw} from "vue";
     import markdown from "../layout/Markdown.vue";
-    //import MetadataInputs from "./MetadataInputs.vue";
-    //import MetadataVariables from "./MetadataVariables.vue";
     import yamlUtils from "../../utils/yamlUtils";
     import Editor from "../inputs/Editor.vue";
     import {mapState} from "vuex";
@@ -176,8 +162,6 @@
         components: {
             markdown,
             Editor,
-            //MetadataInputs,
-            //MetadataVariables,
         },
         props: {
             metadata: {

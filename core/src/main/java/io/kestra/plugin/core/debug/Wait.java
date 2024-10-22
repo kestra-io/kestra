7acc11ec639afd8108fe3c437f80b50b272e6b05
yoyounik <sinhanikhil909@gmail.com>
2024-10-23 00:49:52 +0530

    updated the duration and added all other changes except test

--- core/src/main/java/io/kestra/plugin/core/debug/Wait.java
+++ core/src/main/java/io/kestra/plugin/core/debug/Wait.java
@@ -29,7 +29,7 @@ import java.util.concurrent.TimeUnit;
         @Example(
             code = """
                 id: wait
-                type: io.kestra.plugin.core.debug.Wait
+                type: wait.task
                 duration: "PT5S"
             """
         )

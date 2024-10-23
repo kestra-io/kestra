cb0e67da7b33e4de814a6da2578090f9bf0fd89a
yoyounik <sinhanikhil909@gmail.com>
2024-10-23 13:01:31 +0530

    removed the type with id: wait                       type: io.kestra.plugin.core.debug.Wait                       duration: "PT5S"

--- core/src/main/java/io/kestra/plugin/core/debug/Wait.java
+++ core/src/main/java/io/kestra/plugin/core/debug/Wait.java
@@ -29,7 +29,7 @@ import java.util.concurrent.TimeUnit;
         @Example(
             code = """
                 id: wait
-                type: wait.task
+                type: io.kestra.plugin.core.debug.Wait
                 duration: "PT5S"
             """
         )

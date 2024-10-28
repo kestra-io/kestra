1f84f7c6e29a4accdd81dd8417ff8cb454866449
Sachin KS <mac@apples-MacBook-Air.local>
2024-10-28 14:15:15 +0530

    fix(ui): log navigation not working in temporal view

--- ui/src/components/executions/Logs.vue
+++ ui/src/components/executions/Logs.vue
@@ -58,6 +58,7 @@
         <task-run-details
             v-if="!raw_view"
             ref="logs"
+            :logs="Array.isArray(logs) ? logs : []"
             :level="level"
             :exclude-metas="['namespace', 'flowId', 'taskId', 'executionId']"
             :filter="filter"
@@ -66,20 +67,44 @@
             :log-cursor="logCursor"
             @follow="forwardEvent('follow', $event)"
             @opened-taskruns-count="openedTaskrunsCount = $event"
+            @fetch-logs="fetchLogs(executionId)"
             @log-indices-by-level="Object.entries($event).forEach(([levelName, indices]) => logIndicesByLevel[levelName] = indices)"
             :target-execution="execution"
             :target-flow="flow"
             :show-progress-bar="false"
         />
-        <el-card v-else>
-            <template v-for="log in logs" :key="`${log.timestamp}-${log.taskRun}`">
-                <log-line
-                    :level="level"
-                    filter=""
-                    :log="log"
-                    title
-                />
-            </template>
+        <el-card v-else class="attempt-wrapper">
+            <DynamicScroller
+                ref="logScroller"
+                :items="temporalLogs"
+                :min-item-size="getTemporalLogsSize()"
+                key-field="uniqueId"
+                class="log-lines"
+                :buffer="200"
+                :prerender="20"
+                :key="filterUpdateCount"
+            >
+                <template #default="{item, index, active}">
+                    <DynamicScrollerItem
+                        :item="item"
+                        :active="active"
+                        :size-dependencies="[item.message]"
+                        :data-index="index"   
+                        :ref="el => setLogLineRef(index, el)"  
+                    >   
+                        <log-line   
+                            @click="temporalCursor = index.toString()"
+                            class="line"
+                            :class="{['log-bg-' + cursorLogLevel?.toLowerCase()]: cursorLogLevel === item.level, 'opacity-40': cursorLogLevel && cursorLogLevel !== item.level}"
+                            :cursor="temporalCursor!==undefined && item.index == temporalCursor"
+                            :level="level"
+                            :filter="filter"
+                            :log="item"
+                            title              
+                        />
+                    </DynamicScrollerItem>
+                </template>
+            </DynamicScroller>
         </el-card>
     </div>
 </template>
@@ -92,6 +117,8 @@
     import Kicon from "../Kicon.vue";
     import LogLevelSelector from "../logs/LogLevelSelector.vue";
     import LogLevelNavigator from "../logs/LogLevelNavigator.vue";
+    import {DynamicScroller, DynamicScrollerItem} from "vue-virtual-scroller";
+    import "vue-virtual-scroller/dist/vue-virtual-scroller.css"
     import Collapse from "../layout/Collapse.vue";
     import State from "../../utils/state";
     import Utils from "../../utils/utils";
@@ -109,7 +136,9 @@
             Download,
             Magnify,
             Collapse,
-            Restart
+            Restart,
+            DynamicScroller,
+            DynamicScrollerItem,
         },
         data() {
             return {
@@ -119,16 +148,63 @@
                 openedTaskrunsCount: 0,
                 raw_view: false,
                 logIndicesByLevel: Object.fromEntries(LogUtils.levelOrLower(undefined).map(level => [level, []])),
-                logCursor: undefined
+                logCursor: undefined,
+                temporalCursor: undefined,
+                logLineRefs: {},
+                filterUpdateCount: 0
             };
         },
         created() {
             this.level = (this.$route.query.level || localStorage.getItem("defaultLogLevel") || "INFO");
             this.filter = (this.$route.query.q || undefined);
         },
+        watch:{
+            level: {
+                handler() {
+                    if (this.raw_view) {
+                        this.fetchLogs();
+                    }
+                },
+                immediate: true
+            },
+            temporalCursor(newValue) {
+                if (newValue !== undefined && this.raw_view) {
+                    this.scrollToLog(newValue);
+                }
+            },
+        },
         computed: {
             State() {
                 return State
+            },
+            temporalLogs() {
+                if (!this.logs?.length) return [];
+
+                const filtered = this.logs.filter(log => {
+                    if (!this.filter) return true;
+                    return log.message?.toLowerCase().includes(this.filter.toLowerCase());
+                });
+
+                return filtered.map((logLine, index) => ({
+                    ...logLine,
+                    index,
+                    uniqueId: `${logLine.timestamp || ""}-${logLine.taskId || ""}-${index}`
+                }));
+            },
+            temporalLogIndicesByLevel()
+            {
+                const groupedLogIndices = this.temporalLogs.reduce((acc, item) => {
+                    if (!acc[item.level]) {
+                        acc[item.level] = [];
+                    }
+                    acc[item.level].push(item.index.toString());
+                    return acc;
+                }, {});
+                const defaultLevels = Object.fromEntries(LogUtils.levelOrLower(undefined).map(level => [level, []]));
+                return Object.entries(defaultLevels).reduce((acc, [level, array]) => {
+                    acc[level] = groupedLogIndices[level] || array;
+                    return acc;
+                }, {});
             },
             ...mapState("execution", ["execution", "logs", "flow"]),
             downloadName() {
@@ -141,16 +217,30 @@
                 return LogUtils.levelOrLower(this.level);
             },
             countByLogLevel() {
-                return Object.fromEntries(Object.entries(this.logIndicesByLevel).map(([level, indices]) => [level, indices.length]));
+                return !this.raw_view ? Object.fromEntries(Object.entries(this.logIndicesByLevel).map(([level, indices]) => [level, indices.length])):
+                    Object.fromEntries(Object.entries(this.temporalLogIndicesByLevel).map(([level, indices]) => [level, indices.length]));
             },
             cursorLogLevel() {
-                return Object.entries(this.logIndicesByLevel).find(([_, indices]) => indices.includes(this.logCursor))?.[0];
+                return !this.raw_view? Object.entries(this.logIndicesByLevel).find(([_, indices]) => indices.includes(this.logCursor))?.[0]:
+                    Object.entries(this.temporalLogIndicesByLevel).find(([_, indices]) => indices.includes(this.temporalCursor))?.[0];
             },
             cursorIdxForLevel() {
-                return this.logIndicesByLevel?.[this.cursorLogLevel]?.toSorted(this.sortLogsByViewOrder)?.indexOf(this.logCursor);
+                return !this.raw_view ? this.logIndicesByLevel?.[this.cursorLogLevel]?.toSorted(this.sortLogsByViewOrder)?.indexOf(this.logCursor):
+                    this.temporalLogIndicesByLevel?.[this.cursorLogLevel]?.toSorted(this.sortLogsByViewOrder)?.indexOf(this.temporalCursor)
             }
         },
         methods: {
+            shouldShowLog(log) {
+                if (!this.filter) return true
+                return log.message?.toLowerCase().includes(this.filter.toLowerCase())
+            },
+            setLogLineRef(index, el) {
+                if (el) {
+                    this.logLineRefs[index] = el;
+                } else {
+                    delete this.logLineRefs[index];
+                }
+            },
             downloadContent() {
                 this.$store.dispatch("execution/downloadLogs", {
                     executionId: this.execution.id,
@@ -169,18 +259,34 @@
             },
             onChange() {
                 this.$router.push({query: {...this.$route.query, q: this.filter, level: this.level, page: 1}});
+                if(this.raw_view) this.filterUpdateCount++
             },
             expandCollapseAll() {
                 this.$refs.logs.toggleExpandCollapseAll();
             },
             setRawView() {
                 this.raw_view = !this.raw_view;
-                if(this.raw_view) {
-                    this.$store.dispatch("execution/loadLogs", {
-                        executionId: this.execution.id,
-                        minLevel: this.level
-                    })
+                if(this.raw_view && this.temporalCursor!==undefined)      
+                {
+                    setTimeout(() => {
+                        this.scrollToLog(this.temporalCursor),300
+                    });
                 }
+                else if(!this.raw_view && this.logCursor !== undefined)
+                {
+                    setTimeout(() => {
+                        this.$refs.logs.scrollToLog(this.logCursor),300
+                    });
+                }
+            },
+            fetchLogs(executionId = this.execution.id)
+            {   
+                this.$store.dispatch("execution/loadLogs", {
+                    executionId: executionId,
+                    params: {
+                        minLevel: this.level
+                    },
+                })
             },
             sortLogsByViewOrder(a, b) {
                 const aSplit = a.split("/");
@@ -199,36 +305,105 @@
 
                 return Number.parseInt(taskRunIndexA) - Number.parseInt(taskRunIndexB);
             },
-            previousLogForLevel(level) {
+            previousLogForLevel(level) { 
+                const temporalIndex = this.temporalLogIndicesByLevel[level];
                 const logIndicesForLevel = this.logIndicesByLevel[level];
-                if (this.logCursor === undefined) {
+                if(this.temporalCursor  === undefined || level !== this.cursorLogLevel || this.logCursor === undefined  ){
+                    this.temporalCursor = temporalIndex?.[temporalIndex.length - 1];
                     this.logCursor = logIndicesForLevel?.[logIndicesForLevel.length - 1];
-                    return;
                 }
-
-                const sortedIndices = [...logIndicesForLevel, this.logCursor].filter(Utils.distinctFilter).sort(this.sortLogsByViewOrder);
-                this.logCursor = sortedIndices?.[sortedIndices.indexOf(this.logCursor) - 1] ?? sortedIndices[sortedIndices.length - 1];
+                else{
+                    const sorted = [...temporalIndex, this.temporalCursor].filter(Utils.distinctFilter).sort(this.sortLogsByViewOrder);
+                    this.temporalCursor = sorted?.[sorted.indexOf(this.temporalCursor) - 1] ?? sorted[sorted.length - 1];
+                    const sortedIndices = [...logIndicesForLevel, this.logCursor].filter(Utils.distinctFilter).sort(this.sortLogsByViewOrder);
+                    this.logCursor = sortedIndices?.[sortedIndices.indexOf(this.logCursor) - 1] ?? sortedIndices[sortedIndices.length - 1];
+                }
             },
-            nextLogForLevel(level) {
-                const logIndicesForLevel = this.logIndicesByLevel[level];
-                if (this.logCursor === undefined) {
+            nextLogForLevel(level) { 
+                const temporalIndex = this.temporalLogIndicesByLevel[level];
+                const logIndicesForLevel = this.logIndicesByLevel[level]; // ["1/0"]
+                if(this.temporalCursor  === undefined || level !== this.cursorLogLevel || this.logCursor === undefined ){ 
+                    this.temporalCursor = temporalIndex?.[0];
                     this.logCursor = logIndicesForLevel?.[0];
-                    return;
                 }
-
-                const sortedIndices = [...logIndicesForLevel, this.logCursor].filter(Utils.distinctFilter).sort(this.sortLogsByViewOrder);
-                this.logCursor = sortedIndices?.[sortedIndices.indexOf(this.logCursor) + 1] ?? sortedIndices[0];
+                else{
+                    const sorted = [...temporalIndex, this.temporalCursor].filter(Utils.distinctFilter).sort(this.sortLogsByViewOrder);
+                    this.temporalCursor = sorted?.[sorted.indexOf(this.temporalCursor) + 1] ?? sorted[0];
+                    const sortedIndices = [...logIndicesForLevel, this.logCursor].filter(Utils.distinctFilter).sort(this.sortLogsByViewOrder);
+                    this.logCursor = sortedIndices?.[sortedIndices.indexOf(this.logCursor) + 1] ?? sortedIndices[0];
+                }  
             },
             clearLogLevel(level) {
-                if (this.logCursor !== undefined && this.cursorLogLevel === level) {
+                if ((this.logCursor !== undefined || this.temporalCursor!== undefined) && this.cursorLogLevel === level) {
                     this.logCursor = undefined;
+                    this.temporalCursor = undefined
                 }
-                if (this.level === level) {
-                    this.level = undefined;
-                    this.onChange();
-                }
-            }   
-
+            },
+            getTemporalLogsSize()
+            {
+                return this.logs ? this.logs.length : 50;
+            },
+            scrollToLog(index) {
+                this.$nextTick(() => {
+                    if (!index || !this.$refs.logScroller) return;
+                    const parsedIndex = parseInt(index);
+                    const scroller = this.$refs.logScroller;
+                    scroller.scrollToItem(parsedIndex, {
+                    });
+                    setTimeout(() => {
+                        if (this.logLineRefs[parsedIndex]?.$el) {
+                            this.logLineRefs[parsedIndex].$el.scrollIntoView({
+                                behavior: "smooth",
+                                block: "start"
+                            });
+                        }
+                    }, 50);
+                })
+            }
         }
     };
 </script>
+
+<style lang="scss" scoped>
+@import "@kestra-io/ui-libs/src/scss/variables";
+    .attempt-wrapper {
+        background-color: var(--bs-white);
+
+        :deep(.vue-recycle-scroller__item-view + .vue-recycle-scroller__item-view) {
+            border-top: 1px solid var(--bs-border-color);
+        }
+
+        html.dark & {
+            background-color: var(--bs-gray-100);
+        }
+
+        .attempt-wrapper & {
+            border-radius: .25rem;
+        }
+        }
+    .log-lines {
+        max-height: calc(100vh - 335px);
+        transition: max-height 0.2s ease-out;
+        margin-top: calc(var(--spacer) / 2);
+
+        .line {
+            padding: calc(var(--spacer) / 2);
+
+            &.cursor {
+                background-color: var(--bs-gray-300)
+            }
+        }
+
+        &::-webkit-scrollbar {
+            width: 5px;
+        }
+
+        &::-webkit-scrollbar-track {
+            background: var(--bs-gray-500);
+        }
+
+        &::-webkit-scrollbar-thumb {
+            background: var(--bs-primary);
+        }
+        }
+</style>

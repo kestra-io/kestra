880ede0eaea1291eb0856f001bd3ec9126e0da41
Barthélémy Ledoux <ledouxb@me.com>
2024-10-29 14:27:24 +0100

    feat: update design of blueprint header (#5652)
    * add nvmrc to fix version of node
    
    * update layout of blueprints
    
    * adjust css
    
    * support light mode
    
    * fix build process
    
    * chore(translations): auto generate values for languages other than english
    
    * restore the translation files
    
    * chore(translations): auto generate values for languages other than english
    
    * fix layout of tabs when using type card
    
    * fix gradients of icons
    
    * chore(translations): auto generate values for languages other than english
    
    * adjust spaces
    
    * adjust width on oss
    
    ---------
    
    Co-authored-by: Bart Ledoux <bledoux@kestra.io>
    Co-authored-by: GitHub Action <actions@github.com>

---
+++ ui/src/override/components/flows/blueprints/BlueprintsLayout.vue
@@ -0,0 +1,23 @@
+<template>
+    <slot v-if="embed" />
+    <div class="blueprints" v-else>
+        <slot />
+    </div>
+</template>
+
+<script setup lang="ts">
+    defineProps<{
+        embed: boolean;
+    }>();
+</script>
+
+<style scoped lang="scss">
+    .blueprints {
+        background: url('../../../../assets/dots-bg.jpg') no-repeat top left;
+        background-color: #F6F6FA;
+        .dark & {
+            background:  url('../../../../assets/dots-bg-dark.jpg') no-repeat top left;
+            background-color: #1B1E27;
+        }
+    }
+</style>

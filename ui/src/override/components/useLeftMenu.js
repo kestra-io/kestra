920382cf54273f7d7a71df0d4bc4d7d0f4b6bbdc
Bart Ledoux <bledoux@kestra.io>
2024-11-04 11:43:24 +0100

    add hover message to get full title

--- ui/src/override/components/useLeftMenu.js
+++ ui/src/override/components/useLeftMenu.js
@@ -77,7 +77,7 @@ export function useLeftMenu($emit, generateMenu) {
                 },
                 child: store.state.starred.pages.map(p => {
                     return {
-                        component: () => h("a", {href: p.path, style: "display:block;padding: 0 4px;white-space: nowrap;overflow:hidden;text-overflow:ellipsis;"}, p.label),
+                        component: () => h("a", {href: p.path, title: p.label, style: "display:block;padding: 0 4px;white-space: nowrap;overflow:hidden;text-overflow:ellipsis;"}, p.label),
                     }
                 })
             }] : []),

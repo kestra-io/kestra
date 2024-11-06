6cf405794c6b4c26fcba278441f056c1d38574ac
Anna Geller <anna.m.geller@gmail.com>
2024-11-06 13:00:59 +0100

    docs: clarify SLA and examples

--- core/src/main/java/io/kestra/plugin/core/condition/AbstractMultipleCondition.java
+++ core/src/main/java/io/kestra/plugin/core/condition/AbstractMultipleCondition.java
@@ -30,18 +30,18 @@ public abstract class AbstractMultipleCondition extends Condition implements Mul
     @NotNull
     @NotBlank
     @Pattern(regexp="^[a-zA-Z0-9][a-zA-Z0-9_-]*")
-    @Schema(title = "A unique id for the whole flow")
+    @Schema(title = "A unique id for the condition")
     @PluginProperty
     private String id;
 
     @Schema(
-        title = "SLA to define the time period (or window) for evaluating the conditions",
+        title = "SLA to define the time period (or window) for evaluating conditions",
         description = """
-        You can evaluate the conditions on three different ways:
-        1. Using a duration window (`type: DURATION_WINDOW`), this is the default, and is configured by default for a 1 day duration window. A duration window express the evaluation period as a start time and a end time that are moving each time the evaluation time reach the end time, keeping the size of the window to the defined duration. For example, a one day duration windows will always evaluate executions during 24h starting at 00:00:00.
-        2. Using a sliding window (`type: SLIDING_WINDOW`). A sliding window express a period of time that is fixed in size and depends on the evaluation time. For example, a sliding window of 1 hour will evaluate executions of the past hour (so between now and one hour before now). It uses a default window of 1 day.
-        3. Using a daily deadline (`type: DAILY_TIME_DEADLINE`). A daily deadline express the evaluation period as "before a specific time in a day". For example, a daily deadline of 09:00:00 will evaluate executions between 00:00:00 and 09:00:00 each day.
-        4. Using a daily window (`type: DAILY_TIME_WINDOW`). A daily window express the evaluation period as "between two specific time in a day". For example, a daily windows of 06:00:00 and 09:00:00 will evaluate executions between 00:06:00 and 09:00:00 each day.
+        You can set the `type` of `sla` to one of the following values:
+        1. `DURATION_WINDOW`: this is the default `type`. It uses a start time (`windowAdvance`) and end time (`window`) that are moving forward to the next interval whenever the evaluation time reaches the end time, based on the defined duration `window`. For example, with a 1-day window (the default option: `window: PT1D`), the SLA conditions are always evaluated during 24h starting at midnight (i.e. at time 00:00:00) each day. If you set `windowAdvance: PT6H`, the window will start at 6 AM each day. If you set `windowAdvance: PT6H` and you also override the `window` property to `PT6H`, the window will start at 6 AM and last for 6 hours — as a result, Kestra will check the SLA conditions during the following time periods: 06:00 to 12:00, 12:00 to 18:00, 18:00 to 00:00, and 00:00 to 06:00, and so on.
+        2. `SLIDING_WINDOW`: this option also evaluates SLA conditions over a fixed time `window`, but it always goes backward from the current time. For example, a sliding window of 1 hour (`window: PT1H`) will evaluate executions for the past hour (so between now and one hour before now). It uses a default window of 1 day.
+        3. `DAILY_TIME_DEADLINE`: this option declares that some SLA conditions should be met "before a specific time in a day". With the string property `deadline`, you can configure a daily cutoff for checking conditions. For example, `deadline: "09:00:00"` means that the defined SLA conditions should be met from midnight until 9 AM each day; otherwise, the flow will not be triggered.
+        4. `DAILY_TIME_WINDOW`: this option declares that some SLA conditions should be met "within a given time range in a day". For example, a window from `startTime: "06:00:00"` to `endTime: "09:00:00"` evaluates executions within that interval each day. This option is particularly useful for declarative definition of freshness conditions when building data pipelines. For example, if you only need one successful execution within a given time range to guarantee that some data has been successfully refreshed in order for you to proceed with the next steps of your pipeline, this option can be more useful than a strict DAG-based approach. Usually, each failure in your flow would block the entire pipeline, whereas with this option, you can proceed with the next steps of the pipeline as soon as the data is successfully refreshed at least once within the given time range.
         """
     )
     @PluginProperty
@@ -50,11 +50,11 @@ public abstract class AbstractMultipleCondition extends Condition implements Mul
     protected SLA sla = SLA.builder().build();
 
     @Schema(
-        title = "Whether to reset the conditions evaluation result after a first successful evaluation in the time period.",
+        title = "Whether to reset the evaluation results of SLA conditions after a first successful evaluation within the given time period.",
         description = """
-            By default, after a successful evaluation of the set of conditions, the evaluation result is reset, so, the same set of conditions needs to be successfully evaluated again in the same time period to trigger a new execution.
-            This means that to create multiple executions, the same set of conditions needs to be evaluated multiple times.
-            You can disable this by setting this property to false so that, on a same period, each time one of the conditions from the set of conditions is evaluated again after a successful evaluation, it will trigger a new execution."""
+            By default, after a successful evaluation of the set of SLA conditions, the evaluation result is reset, so, the same set of conditions needs to be successfully evaluated again in the same time period to trigger a new execution.
+            This means that to create multiple executions, the same set of conditions needs to be evaluated to `true` multiple times.
+            You can disable this by setting this property to `false` so that, within the same period, each time one of the conditions is satisfied again after a successful evaluation, it will trigger a new execution."""
     )
     @PluginProperty
     @NotNull
@@ -96,7 +96,7 @@ public abstract class AbstractMultipleCondition extends Condition implements Mul
 
         if (result && log.isDebugEnabled()) {
             log.debug(
-                "[namespace: {}] [flow: {}] Multiple conditions validated !",
+                "[namespace: {}] [flow: {}] Multiple conditions validated!",
                 conditionContext.getFlow().getNamespace(),
                 conditionContext.getFlow().getId()
             );

05333f95d2c88d452f1a722c8e8f7919db6bb366
Bart Ledoux <bledoux@kestra.io>
2024-11-07 15:35:54 +0100

    fix: extends the background of blueprint page

--- ui/src/override/components/flows/blueprints/BlueprintsLayout.vue
+++ ui/src/override/components/flows/blueprints/BlueprintsLayout.vue
@@ -15,6 +15,7 @@
     .blueprints {
         background: url('../../../../assets/dots-bg.jpg') no-repeat top left;
         background-color: #F6F6FA;
+        flex: 1;
         .dark & {
             background:  url('../../../../assets/dots-bg-dark.jpg') no-repeat top left;
             background-color: #1B1E27;

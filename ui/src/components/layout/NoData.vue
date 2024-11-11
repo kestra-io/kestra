46902df228dc09d1a82781d670f1f6bd5d756ba5
MilosPaunovic <paun992@hotmail.com>
2024-11-11 13:49:17 +0100

    chore(ui): minor tweaks

--- ui/src/components/layout/NoData.vue
+++ ui/src/components/layout/NoData.vue
@@ -4,20 +4,17 @@
 
 <script setup lang="ts">
     import {computed} from "vue";
+
     import Utils from "../../utils/utils";
-
-    import Dark from "../../assets/dark.png";
-    import Light from "../../assets/light.png";
-
-    const image = computed(() => (Utils.getTheme() === "light" ? Light : Dark));
 
     import {useI18n} from "vue-i18n";
     const {t} = useI18n({useScope: "global"});
 
-    defineProps({
-        description: {
-            type: String,
-            default: t("no_data"), // eslint-disable-line vue/valid-define-props
-        },
-    });
+    import Dark from "../../assets/dark.png";
+    import Light from "../../assets/light.png";
+
+    const props = defineProps({text: {type: String, default: undefined}});
+
+    const image = computed(() => (Utils.getTheme() === "light" ? Light : Dark));
+    const description = computed(() => props.text ?? t("no_data"));
 </script>

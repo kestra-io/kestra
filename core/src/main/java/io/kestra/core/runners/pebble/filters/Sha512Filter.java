fb61339170f8bfe4587bbf57f7d06972f0d38597
hashimzs <hashim.khalifa@array.world>
2024-11-12 11:23:06 +0300

    fix(core): refactor sha filters and add tests for each filter

--- core/src/main/java/io/kestra/core/runners/pebble/filters/Sha512Filter.java
+++ core/src/main/java/io/kestra/core/runners/pebble/filters/Sha512Filter.java
@@ -16,46 +16,9 @@ import java.util.Map;
  *
  * @author Silviu Vergoti
  */
-public class Sha512Filter implements Filter {
+public class Sha512Filter extends ShaBaseFilter {
 
-
-    @Override
-    public List<String> getArgumentNames() {
-        return null;
+    public Sha512Filter() {
+        super("SHA-512");
     }
-
-    @Override
-    public Object apply(Object input, Map<String, Object> args, PebbleTemplate self,
-                        EvaluationContext context, int lineNumber) throws PebbleException {
-        if (input == null) {
-            return null;
-        }
-
-        if (input instanceof String) {
-            MessageDigest digest = null;
-            byte[] encodedHash = null;
-            try {
-                digest = MessageDigest.getInstance("SHA-512");
-                encodedHash = digest.digest(((String) input).getBytes(StandardCharsets.UTF_8));
-            } catch (Exception e) {
-                throw new PebbleException(e, "Hashing exception encountered\n", lineNumber, self.getName());
-            }
-            return bytesToHex(encodedHash);
-        } else {
-            throw new PebbleException(null, "Need a string to hash\n", lineNumber, self.getName());
-        }
-    }
-
-    private static String bytesToHex(byte[] bytes) {
-        StringBuilder hexString = new StringBuilder(2 * bytes.length);
-        for (int i = 0; i < bytes.length; i++) {
-            String hex = Integer.toHexString(0xff & bytes[i]);
-            if (hex.length() == 1) {
-                hexString.append('0');
-            }
-            hexString.append(hex);
-        }
-        return hexString.toString();
-    }
-
 }

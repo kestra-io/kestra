fbe911f928642e428d37b73d926f728c08a1a58f
Bart Ledoux <bledoux@kestra.io>
2024-11-12 21:57:04 +0100

    add style and button to the contextual info bar

--- ui/src/components/ContextInfoBar.vue
+++ ui/src/components/ContextInfoBar.vue
@@ -1,18 +1,63 @@
 <script lang="ts" setup>
     import MessageOutline from "vue-material-design-icons/MessageOutline.vue"
+    import FileDocument from "vue-material-design-icons/FileDocument.vue"
+    import Slack from "vue-material-design-icons/Slack.vue"
+    import Github from "vue-material-design-icons/Github.vue"
+    import Calendar from "vue-material-design-icons/Calendar.vue"
+
+    import OpenInNew from "vue-material-design-icons/OpenInNew.vue"
 </script>
 
 <template>
     <div class="wrapper">
-        <MessageOutline /> docs
+        <button>
+            <MessageOutline class="buttonIcon" />News
+        </button>
+        <button>
+            <FileDocument class="buttonIcon" />Docs
+        </button>
+        <button>
+            <Slack class="buttonIcon" />Help<OpenInNew class="openIcon" />
+        </button>
+        <button>
+            <Github class="buttonIcon" />Open an Issue<OpenInNew class="openIcon" />
+        </button>
+        <button>
+            <Calendar class="buttonIcon" />Get a demo<OpenInNew class="openIcon" />
+        </button>
     </div>
 </template>
 
 <style scoped>
 .wrapper {
     width: 65px;
+    padding: 16px;
     writing-mode: vertical-rl;
     text-orientation: mixed;
     border-left: 1px solid var(--el-border-color);
+    display: flex;
+    gap: 8px;
+}
+
+.wrapper button{
+    background:  #404559;
+    border-radius: 5px;
+    border: 1px solid var(--el-border-color);
+    padding: 10px 5px;
+    width: 32px;
+    display: block;
+    line-height: normal;
+}
+
+.buttonIcon{
+    transform: rotate(90deg);
+    margin-bottom: 8px;
+}
+
+.openIcon{
+    transform: rotate(90deg);
+    margin-top: 8px;
+    color: #C3BBE3;
+    font-size: 12px;
 }
 </style>

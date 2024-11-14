1f553224a0a79aeaa49cc7ebecf37f59d3f80a07
apurvabanka <appu.banka@gmail.com>
2024-11-14 13:59:33 -0500

    Test cases

--- core/src/main/java/io/kestra/core/runners/pebble/functions/FileURIFunction.java
+++ core/src/main/java/io/kestra/core/runners/pebble/functions/FileURIFunction.java
@@ -1,29 +1,29 @@
 package io.kestra.core.runners.pebble.functions;
 
+import java.util.List;
+import java.util.Map;
+
 import io.kestra.core.storages.StorageContext;
 import io.pebbletemplates.pebble.error.PebbleException;
 import io.pebbletemplates.pebble.extension.Function;
 import io.pebbletemplates.pebble.template.EvaluationContext;
 import io.pebbletemplates.pebble.template.PebbleTemplate;
+import jakarta.inject.Singleton;
 
-import java.util.List;
-import java.util.Map;
-
-public class GetFileURI implements Function {
-    private static final String ERROR_MESSAGE = "The 'getFileURI' function expects an argument 'path' that is a path to a namespace file.";
+@Singleton
+public class FileURIFunction implements Function {
+    private static final String ERROR_MESSAGE = "The 'fileURI' function expects an argument 'path' that is a path to a namespace file.";
 
     @Override
     public List<String> getArgumentNames() {
         return List.of("path");
     }
 
-
     @Override
     public Object execute(Map<String, Object> args, PebbleTemplate self, EvaluationContext context, int lineNumber) {
         if (!args.containsKey("path")) {
             throw new PebbleException(null, ERROR_MESSAGE, lineNumber, self.getName());
         }
-
 
         Object path = args.get("path");
         @SuppressWarnings("unchecked")

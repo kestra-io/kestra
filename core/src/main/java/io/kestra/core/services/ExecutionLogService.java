9620c29c5f949bc923f7e75f8110e2a420298ad8
Florian Hussonnois <florian.hussonnois@gmail.com>
2024-11-14 18:12:36 +0100

    feat(core): refactor ExecutionLogService

--- core/src/main/java/io/kestra/core/services/ExecutionLogService.java
+++ core/src/main/java/io/kestra/core/services/ExecutionLogService.java
@@ -13,10 +13,16 @@ import org.slf4j.event.Level;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.FluxSink;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+/**
+ * Service for fetching logs for from an execution.
+ */
 @Singleton
 public class ExecutionLogService {
 
@@ -31,14 +37,14 @@ public class ExecutionLogService {
                                                      final String executionId,
                                                      final Level minLevel) {
 
-        List<String> levels = LogEntry.findLevelsByMin(minLevel).stream().map(Enum::name).toList();
-
         final AtomicReference<Runnable> disposable = new AtomicReference<>();
 
         return Flux.<Event<LogEntry>>create(emitter -> {
                 // fetch repository first
-                fetchExecutionExecutionLogs(tenantId, executionId, minLevel, levels)
+                getExecutionLogs(tenantId, executionId, minLevel, List.of())
                     .forEach(logEntry -> emitter.next(Event.of(logEntry).id("progress")));
+
+                final List<String> levels = LogEntry.findLevelsByMin(minLevel).stream().map(Enum::name).toList();
 
                 // consume in realtime
                 disposable.set(this.logQueue.receive(either -> {
@@ -47,6 +53,7 @@ public class ExecutionLogService {
                     }
 
                     LogEntry current = either.getLeft();
+
                     if (current.getExecutionId() != null && current.getExecutionId().equals(executionId)) {
                         if (levels.contains(current.getLevel().name())) {
                             emitter.next(Event.of(current).id("progress"));
@@ -66,9 +73,42 @@ public class ExecutionLogService {
             });
     }
 
-    public Stream<LogEntry> fetchExecutionExecutionLogs(String tenantId, String executionId, Level minLevel, List<String> levels) {
-        return logRepository.findByExecutionId(tenantId, executionId, minLevel, Pageable.UNPAGED)
+    public InputStream getExecutionLogsAsStream(final String tenantId,
+                                                final String executionId,
+                                                final Level minLevel,
+                                                final String taskRunId,
+                                                final List<String> taskIds,
+                                                final Integer attempt) {
+        List<LogEntry> logs = getExecutionLogs(tenantId, executionId, minLevel, taskRunId, taskIds, attempt);
+        return new ByteArrayInputStream(logs.stream().map(LogEntry::toPrettyString).collect(Collectors.joining("\n")).getBytes());
+    }
+
+    public List<LogEntry> getExecutionLogs(final String tenantId,
+                                           final String executionId,
+                                           final Level minLevel,
+                                           final String taskRunId,
+                                           final List<String> taskIds,
+                                           final Integer attempt) {
+        if (taskIds != null) {
+            return taskIds.size() == 1 ?
+                logRepository.findByExecutionIdAndTaskId(tenantId, executionId, taskIds.getFirst(), minLevel):
+                getExecutionLogs(tenantId, executionId, minLevel, taskIds).toList();
+        }
+
+        if (taskRunId != null) {
+            return attempt != null ?
+                logRepository.findByExecutionIdAndTaskRunIdAndAttempt(tenantId, executionId, taskRunId, minLevel, attempt) :
+                logRepository.findByExecutionIdAndTaskRunId(tenantId, executionId, taskRunId, minLevel);
+        }
+        return logRepository.findByExecutionId(tenantId, executionId, minLevel);
+    }
+
+    public Stream<LogEntry> getExecutionLogs(String tenantId,
+                                             String executionId,
+                                             Level minLevel,
+                                             List<String> taskIds) {
+        return logRepository.findByExecutionId(tenantId, executionId, minLevel)
             .stream()
-            .filter(logEntry -> levels.contains(logEntry.getLevel().name()));
+            .filter(data -> taskIds.isEmpty() || taskIds.contains(data.getTaskId()));
     }
 }

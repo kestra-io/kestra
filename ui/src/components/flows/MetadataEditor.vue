416e693943c1ea02bbae6ad1604ed37300a4e105
MilosPaunovic <paun992@hotmail.com>
2024-11-15 14:42:35 +0100

    chore(ui): minor tweaks

--- ui/src/components/flows/MetadataEditor.vue
+++ ui/src/components/flows/MetadataEditor.vue
@@ -18,10 +18,10 @@
                     <span class="flex-grow-1">{{ $t("description") }}</span>
                     <el-button-group size="small" class="my-1">
                         <el-button @click="preview = false">
-                            <Pencil class="me-1" /> Edit
+                            <Pencil class="me-1" /> {{ $t("edit") }}
                         </el-button>
                         <el-button @click="preview = true">
-                            <Eye class="me-1" /> Preview
+                            <Eye class="me-1" /> {{ $t("preview") }}
                         </el-button>
                     </el-button-group>
                 </div>

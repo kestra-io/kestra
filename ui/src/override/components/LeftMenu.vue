01075a3c7922463aafe4e1173f36c7dbd7b465ad
Bart Ledoux <bledoux@kestra.io>
2024-11-19 17:12:13 +0100

    use overrides system

--- ui/src/override/components/LeftMenu.vue
+++ ui/src/override/components/LeftMenu.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script setup>
-    import {useLeftMenu} from "./useLeftMenu";
+    import {useLeftMenu} from "override/components/useLeftMenu";
     import SideBar from "../../components/layout/SideBar.vue"
 
     const {generateMenu} = useLeftMenu();

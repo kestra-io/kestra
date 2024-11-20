38ca24a2875e0367099c41fbc733cf9a69da76b7
Loïc Mathieu <loikeseke@gmail.com>
2024-11-20 10:21:04 +0100

    feat(core): add a path traversal guard to the fileURI Pebble function

--- core/src/main/java/io/kestra/core/runners/pebble/functions/FileURIFunction.java
+++ core/src/main/java/io/kestra/core/runners/pebble/functions/FileURIFunction.java
@@ -25,7 +25,14 @@ public class FileURIFunction implements Function {
             throw new PebbleException(null, ERROR_MESSAGE, lineNumber, self.getName());
         }
 
-        Object path = args.get("path");
+        String path = (String) args.get("path");
+        if (path.startsWith("/")) {
+            path = path.substring(1);
+        }
+        if (path.contains("../")) {
+            throw new IllegalArgumentException("Path must not contain '../'");
+        }
+
         @SuppressWarnings("unchecked")
         Map<String, String> flow = (Map<String, String>) context.getVariable("flow");
         return StorageContext.KESTRA_PROTOCOL + StorageContext.namespaceFilePrefix(flow.get("namespace")) + "/" + path;

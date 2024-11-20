36d7bafe8733b239ff09e71a606c2bad726ac968
Loïc Mathieu <loikeseke@gmail.com>
2024-11-20 10:21:04 +0100

    feat(core): add a path traversal guard to the fileURI Pebble function

--- core/src/test/java/io/kestra/core/runners/pebble/functions/FileURIFunctionTest.java
+++ core/src/test/java/io/kestra/core/runners/pebble/functions/FileURIFunctionTest.java
@@ -4,6 +4,8 @@ import java.util.Map;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.Test;
 
 import io.kestra.core.exceptions.IllegalVariableEvaluationException;
@@ -12,9 +14,9 @@ import io.kestra.core.runners.VariableRenderer;
 import jakarta.inject.Inject;
 
 @KestraTest
-public class FileURIFunctionTest {
-    @Inject 
-    VariableRenderer variableRenderer;
+class FileURIFunctionTest {
+    @Inject
+    private VariableRenderer variableRenderer;
 
     @Test
     void fileURIFunction() throws IllegalVariableEvaluationException{
@@ -29,6 +31,22 @@ public class FileURIFunctionTest {
         );
         String render = variableRenderer.render("{{ fileURI(fileA) }}", variables);
         assertThat(render, is("kestra:///my/namespace/_files/test"));
+    }
+
+    @Test
+    void fileURIFunctionShouldThrowForIncorrectPath() throws IllegalVariableEvaluationException{
+        String namespace = "my.namespace";
+        String flowId = "flow";
+
+        Map<String, Object> variables = Map.of(
+            "flow", Map.of(
+                "id", flowId,
+                "namespace", namespace),
+            "fileA", "../test"
+        );
+
+        var exception = assertThrows(IllegalArgumentException.class, () -> variableRenderer.render("{{ fileURI(fileA) }}", variables));
+        assertThat(exception.getMessage(), is("Path must not contain '../'"));
     }
 
 }

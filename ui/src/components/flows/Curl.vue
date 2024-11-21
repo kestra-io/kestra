34dfe1e28485b45b9f56148c1bd5abd412994c43
Bart Ledoux <bledoux@kestra.io>
2024-11-21 15:43:13 +0100

    fix curl url

--- ui/src/components/flows/Curl.vue
+++ ui/src/components/flows/Curl.vue
@@ -109,7 +109,7 @@
                     .filter((label) => label.key !== null && label.value !== null && label.key !== "" && label.value !== "")
                     .map((label) => this.generateExecutionLabel(label.key, label.value));
 
-                const origin = baseUrl ? apiUrl(this.$store) : `${location.origin}${basePath()}`;
+                const origin = typeof baseUrl === "string" ? apiUrl(this.$store) : `${location.origin}${basePath()}`;
 
                 var url = `${origin}/executions/${this.flow.namespace}/${this.flow.id}`;
 

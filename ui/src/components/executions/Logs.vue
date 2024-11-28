c1d3418932781772e0682e90a8b79300b819c88b
Ludovic DEHON <tchiot.ludo@gmail.com>
2024-11-28 15:18:00 +0100

    fix(ui): revert bar on the execution > logs page
    close #6092

--- ui/src/components/executions/Logs.vue
+++ ui/src/components/executions/Logs.vue
@@ -1,10 +1,23 @@
 <template>
     <div data-component="FILENAME_PLACEHOLDER">
-        <KestraFilter
-            prefix="execution_logs"
-            :include="['level']"
-        />
         <collapse>
+            <el-form-item>
+                <el-input
+                    v-model="filter"
+                    @update:model-value="onChange"
+                    :placeholder="$t('search')"
+                >
+                    <template #suffix>
+                        <magnify />
+                    </template>
+                </el-input>
+            </el-form-item>
+            <el-form-item>
+                <log-level-selector
+                    v-model="level"
+                    @update:model-value="onChange"
+                />
+            </el-form-item>
             <el-form-item v-for="logLevel in currentLevelOrLower" :key="logLevel">
                 <log-level-navigator
                     v-if="countByLogLevel[logLevel] > 0"
@@ -96,7 +109,9 @@
     import TaskRunDetails from "../logs/TaskRunDetails.vue";
     import {mapState} from "vuex";
     import Download from "vue-material-design-icons/Download.vue";
+    import Magnify from "vue-material-design-icons/Magnify.vue";
     import Kicon from "../Kicon.vue";
+    import LogLevelSelector from "../logs/LogLevelSelector.vue";
     import LogLevelNavigator from "../logs/LogLevelNavigator.vue";
     import {DynamicScroller, DynamicScrollerItem} from "vue-virtual-scroller";
     import "vue-virtual-scroller/dist/vue-virtual-scroller.css"
@@ -106,20 +121,20 @@
     import LogLine from "../logs/LogLine.vue";
     import Restart from "./Restart.vue";
     import LogUtils from "../../utils/logs";
-    import KestraFilter from "../filter/KestraFilter.vue"
 
     export default {
         components: {
             LogLine,
             TaskRunDetails,
+            LogLevelSelector,
             LogLevelNavigator,
             Kicon,
             Download,
+            Magnify,
             Collapse,
             Restart,
             DynamicScroller,
             DynamicScrollerItem,
-            KestraFilter
         },
         data() {
             return {
@@ -283,7 +298,7 @@
 </script>
 
 <style lang="scss" scoped>
-@import "@kestra-io/ui-libs/src/scss/variables";
+    @import "@kestra-io/ui-libs/src/scss/variables";
     .attempt-wrapper {
         background-color: var(--bs-white);
 
@@ -298,7 +313,7 @@
         .attempt-wrapper & {
             border-radius: .25rem;
         }
-        }
+    }
     .log-lines {
         max-height: calc(100vh - 335px);
         transition: max-height 0.2s ease-out;
@@ -323,5 +338,5 @@
         &::-webkit-scrollbar-thumb {
             background: var(--bs-primary);
         }
-        }
+    }
 </style>

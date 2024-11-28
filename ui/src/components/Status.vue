3a63ce623a75d4b921cb5f6498f2b81a900e6bac
Ludovic DEHON <tchiot.ludo@gmail.com>
2024-11-28 15:38:38 +0100

    chore(ui): suppress some console warning

--- ui/src/components/Status.vue
+++ ui/src/components/Status.vue
@@ -1,5 +1,5 @@
 <template>
-    <el-button data-component="FILENAME_PLACEHOLDER" data-test-id="execution-status" @click="$emit('click', $event)" class="status" :size="size" :class="cls" :style="style">
+    <el-button data-component="FILENAME_PLACEHOLDER" data-test-id="execution-status" @click="$emit('click', $event)" class="status" :size="size" :style="style">
         <template v-if="label">
             {{ title || $filters.cap(status) }}
         </template>

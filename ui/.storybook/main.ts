14508d5dab12d0aba159bc13c2aae3b1cccd6088
Bart Ledoux <bledoux@kestra.io>
2024-11-29 14:19:11 +0100

    add storybook to kestra oss

---
+++ ui/.storybook/main.ts
@@ -0,0 +1,16 @@
+import type { StorybookConfig } from "@storybook/vue3-vite";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-onboarding",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/vue3-vite",
+    options: {},
+  },
+};
+export default config;
---
+++ ui/.storybook/preview.ts
@@ -0,0 +1,14 @@
+import type { Preview } from "@storybook/vue3";
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;

a4b9307ef1d985f2c22d644cae5e0792cf08d26e
Bart Ledoux <bledoux@kestra.io>
2024-12-03 11:07:33 +0100

    fix: allow open in a new tab and main menu in docs

--- ui/src/components/docs/ContextDocsLink.vue
+++ ui/src/components/docs/ContextDocsLink.vue
@@ -30,7 +30,7 @@
     <a v-if="isRemote" :href="finalHref" @click="emit('click')" target="_blank" rel="noopener noreferrer">
         <slot />
     </a>
-    <a v-else :href="`/docs${finalHref}`" @click.prevent="() => {navigateInVuex();emit('click');}">
+    <RouterLink v-else :to="{name:'docs/view', path: finalHref}`/docs${finalHref}`" @click.prevent="() => {navigateInVuex();emit('click');}">
         <slot />
-    </a>
+    </RouterLink>
 </template>

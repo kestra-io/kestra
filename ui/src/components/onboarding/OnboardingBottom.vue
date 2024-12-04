5ee8dc1341d87b2d432dd6a26dea13afba37c2d7
Piyush Bhaskar <102078527+Piyush-r-bhaskar@users.noreply.github.com>
2024-12-04 11:59:23 +0100

    chore(ui): Improvement in Welcome Page. (#6077)
    * chore(ui): Improvement in Welcome Page.
    
    * Update Welcome.vue | scoped the styling
    
    * fix bad merge
    
    * remove special behavior of navbar on welcome
    
    * finish the welcome page (thank you)
    
    * fix: better adaptive layout
    
    * use container queries and flex for better responsive design
    
    * chore(translations): auto generate values for languages other than english
    
    ---------
    
    Co-authored-by: Barthélémy Ledoux <ledouxb@me.com>
    Co-authored-by: Bart Ledoux <bledoux@kestra.io>
    Co-authored-by: Miloš Paunović <paun992@hotmail.com>
    Co-authored-by: GitHub Action <actions@github.com>

--- ui/src/components/onboarding/OnboardingBottom.vue
+++ ui/src/components/onboarding/OnboardingBottom.vue
@@ -1,23 +1,14 @@
 <template>
-    <el-row justify="space-between" :gutter="20">
-        <el-col
+    <div class="onboarding-bottom">
+        <onboarding-card
             v-for="card in cards"
             :key="card.title"
-            :xs="24"
-            :sm="12"
-            :md="12"
-            :lg="6"
-            :xl="6"
-            class="pb-4"
-        >
-            <onboarding-card
-                :title="card.title"
-                :content="card.content"
-                :category="card.category"
-                :link="card.link"
-            />
-        </el-col>
-    </el-row>
+            :title="card.title"
+            :content="card.content"
+            :category="card.category"
+            :link="card.link"
+        />
+    </div>
 </template>
 <script>
     import {mapGetters} from "vuex";
@@ -31,16 +22,12 @@
             return {
                 cards: [
                     {
-                        title: this.$t("welcome.started.title"),
-                        category: "started",
-                    },
-                    {
                         title: this.$t("welcome.product-tour.title"),
                         category: "product",
 
                     },
                     {
-                        title: this.$t("welcome.doc.title"),
+                        title: this.$t("welcome.tutorial.title"),
                         category: "docs",
                     },
                     {
@@ -55,3 +42,14 @@
         }
     }
 </script>
+
+<style lang="scss" scoped>
+    .onboarding-bottom {
+        display: flex;
+        gap: 1rem;
+        margin-top: 1.5rem;
+        justify-items: center;
+        flex-wrap: wrap;
+        max-width: 1000px;
+    }
+</style>

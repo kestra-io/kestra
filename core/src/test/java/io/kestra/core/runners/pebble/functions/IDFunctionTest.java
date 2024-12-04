5c66903a9c33a59f35c22ca9a9b6db03a5c83685
RizviSyed <rizvi1997@gmail.com>
2024-12-04 14:54:46 +0300

    #6208 Refactored based on comments

--- core/src/test/java/io/kestra/core/runners/pebble/functions/IDFunctionTest.java
+++ core/src/test/java/io/kestra/core/runners/pebble/functions/IDFunctionTest.java
@@ -1,25 +1,23 @@
 package io.kestra.core.runners.pebble.functions;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import io.kestra.core.exceptions.IllegalVariableEvaluationException;
 import io.kestra.core.junit.annotations.KestraTest;
 import io.kestra.core.runners.VariableRenderer;
 import jakarta.inject.Inject;
 import java.util.Collections;
-
 import org.junit.jupiter.api.Test;
 
 @KestraTest
-class UUIDBase62FunctionTest {
+class IDFunctionTest {
     @Inject VariableRenderer variableRenderer;
 
     @Test
-    void checkMaxUUIDValue() throws IllegalVariableEvaluationException {
+    void checkMaxKestraUUIDLength() throws IllegalVariableEvaluationException {
         String rendered =
             variableRenderer.render(
-                "{{ generateUUID() }}", Collections.emptyMap());
+                "{{ generateKestraId() }}", Collections.emptyMap());
         assertThat(rendered, rendered.length() <=22);
     }
 }

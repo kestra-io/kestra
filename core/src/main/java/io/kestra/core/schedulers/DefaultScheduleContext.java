feb5ab51c8264122fb1ab4aaa8a1969d270a112c
Loïc Mathieu <loikeseke@gmail.com>
2024-12-11 15:35:06 +0100

    feat(core,jdbc): small trigger / scheduler improvements

--- core/src/main/java/io/kestra/core/schedulers/DefaultScheduleContext.java
+++ core/src/main/java/io/kestra/core/schedulers/DefaultScheduleContext.java
@@ -1,4 +1,11 @@
 package io.kestra.core.schedulers;
 
+import java.util.function.Consumer;
+
 // For tests purpose
-public class DefaultScheduleContext implements ScheduleContextInterface {}
+public class DefaultScheduleContext implements ScheduleContextInterface {
+    @Override
+    public void doInTransaction(Consumer<ScheduleContextInterface> consumer) {
+        consumer.accept(this);
+    }
+}

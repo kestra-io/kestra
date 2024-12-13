7c4633980deab0d32477b19ca60e6278d756e2dd
Bart Ledoux <bledoux@kestra.io>
2024-12-13 12:10:53 +0100

    protect against empty request data

--- ui/src/utils/axios.js
+++ ui/src/utils/axios.js
@@ -117,7 +117,7 @@ export default (callback, store, router) => {
                 const originalRequest = errorResponse.config
 
                 if (!refreshing) {
-                    const originalRequestData = JSON.parse(originalRequest.data);
+                    const originalRequestData = JSON.parse(originalRequest.data ?? "{}");
 
                     // if we already tried refreshing the token,
                     // the user simply does not have access to this feature

678205cfc020ee0adbc0090b11b8fdd064973037
Loïc Mathieu <loikeseke@gmail.com>
2024-12-13 10:35:09 +0100

    fix(core): serialize default inputs

--- core/src/main/java/io/kestra/core/models/flows/Input.java
+++ core/src/main/java/io/kestra/core/models/flows/Input.java
@@ -1,6 +1,5 @@
 package io.kestra.core.models.flows;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonSetter;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
@@ -39,7 +38,6 @@ import jakarta.validation.constraints.Pattern;
     @JsonSubTypes.Type(value = TimeInput.class, name = "TIME"),
     @JsonSubTypes.Type(value = URIInput.class, name = "URI")
 })
-@JsonInclude(JsonInclude.Include.NON_DEFAULT)
 public abstract class Input<T> implements Data {
     @NotNull
     @NotBlank

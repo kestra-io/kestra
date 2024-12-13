2eca20cc27806d255dca1a5a837c90e12daade13
nKwiatkowski <nkwiatkowski@kestra.io>
2024-12-13 11:37:59 +0100

    clean(tests): use existing interface to wrap the test execution

--- jdbc/src/test/java/io/kestra/jdbc/loader/FlowLoader.java
+++ jdbc/src/test/java/io/kestra/jdbc/loader/FlowLoader.java
@@ -3,11 +3,10 @@ package io.kestra.jdbc.loader;
 import io.kestra.core.models.flows.FlowWithSource;
 import io.kestra.core.repositories.FlowRepositoryInterface;
 import io.kestra.core.repositories.LocalFlowRepositoryLoader;
+import io.kestra.core.utils.Rethrow.RunnableChecked;
 import io.kestra.core.utils.TestsUtils;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
-import java.io.IOException;
-import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Objects;
 
@@ -19,7 +18,7 @@ public class FlowLoader {
     @Inject
     protected LocalFlowRepositoryLoader repositoryLoader;
 
-    public void executeWithFlow(List<String> flowPaths, Executor executor)
+    public void executeWithFlow(List<String> flowPaths, RunnableChecked runnableChecked)
         throws Exception {
 
         for (String path : flowPaths) {
@@ -27,16 +26,12 @@ public class FlowLoader {
         }
 
         try {
-            executor.execute();
+            runnableChecked.run();
         } catch (Exception e){
             throw e;
         } finally {
             flowRepository.findAllForAllTenants().forEach(flow -> flowRepository.delete(
                 FlowWithSource.of(flow, "unused")));
         }
-    }
-
-    public interface Executor {
-        void execute() throws Exception;
     }
 }

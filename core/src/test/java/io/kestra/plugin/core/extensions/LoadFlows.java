dddf4db75adbe516731873980c633f91d4543699
Nicolas K. <nk_mikmak@hotmail.com>
2024-12-16 11:41:55 +0100

    test(runner tests): add logs to track race condition (#6455)
    * test(runner tests): add logs to track race condition
    
    * feat(tests): add extension to load flows
    
    * clean(tests): remove logs
    
    ---------
    
    Co-authored-by: nKwiatkowski <nkwiatkowski@kestra.io>

---
+++ core/src/test/java/io/kestra/plugin/core/extensions/LoadFlows.java
@@ -0,0 +1,17 @@
+package io.kestra.plugin.core.extensions;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@ExtendWith(FlowLoaderExtension.class)
+@Test
+public @interface LoadFlows {
+    String[] value();
+}

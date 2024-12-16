70676441500128b7375cf96c89d238f2039781f6
Ludovic DEHON <tchiot.ludo@gmail.com>
2024-12-16 18:51:09 +0100

    refactor(test): use application context from the current test suite on FlowLoad

--- tests/src/main/java/io/kestra/core/junit/extensions/FlowLoaderExtension.java
+++ tests/src/main/java/io/kestra/core/junit/extensions/FlowLoaderExtension.java
@@ -8,24 +8,33 @@ import io.micronaut.context.ApplicationContext;
 
 import java.net.URL;
 
+import io.micronaut.test.extensions.junit5.MicronautJunit5Extension;
 import org.junit.jupiter.api.extension.AfterEachCallback;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
 public class FlowLoaderExtension implements BeforeEachCallback, AfterEachCallback {
-    private static ApplicationContext applicationContext;
+    private static final ExtensionContext.Namespace NAMESPACE = ExtensionContext.Namespace.create(KestraTestExtension.class);
+
+    private ApplicationContext applicationContext;
 
     @Override
     public void beforeEach(ExtensionContext extensionContext) throws Exception {
         if (applicationContext == null) {
-            applicationContext = ApplicationContext.run();
+            extensionContext.getRoot().getStore(NAMESPACE).put("test", "bla");
+
+            applicationContext = extensionContext.getRoot().getStore(NAMESPACE).get(ApplicationContext.class, ApplicationContext.class);
+
+            if (applicationContext == null) {
+                throw new IllegalStateException("No application context, to use '@LoadFlows' annotation, you need to add '@KestraTest'");
+            }
         }
 
         LocalFlowRepositoryLoader repositoryLoader = applicationContext.getBean(LocalFlowRepositoryLoader.class);
 
         LoadFlows loadFlows = extensionContext.getTestMethod()
-                .orElseThrow()
-                .getAnnotation(LoadFlows.class);
+            .orElseThrow()
+            .getAnnotation(LoadFlows.class);
 
         if (loadFlows != null) {
             for (String path : loadFlows.value()) {

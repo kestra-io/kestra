165983ec20f9cfcb33895115bbb778572f4c114a
Bart Ledoux <bledoux@kestra.io>
2024-12-16 10:31:20 +0100

    fix(ui): update global constants to use window scope

--- ui/src/utils/init.js
+++ ui/src/utils/init.js
@@ -87,10 +87,10 @@ export default (app, routes, stores, translations) => {
     let store = createStore(stores);
     app.use(store);
 
-    /* eslint-disable no-undef */
+
     // router
     let router = createRouter({
-        history: createWebHistory(KESTRA_UI_PATH),
+        history: createWebHistory(window.KESTRA_UI_PATH),
         routes
     });
 
@@ -120,16 +120,16 @@ export default (app, routes, stores, translations) => {
     app.use(router)
 
     // Google Analytics
-    if (KESTRA_GOOGLE_ANALYTICS !== null) {
+    if (window.KESTRA_GOOGLE_ANALYTICS !== null) {
         app.use(
             VueGtag,
             {
-                config: {id: KESTRA_GOOGLE_ANALYTICS}
+                config: {id: window.KESTRA_GOOGLE_ANALYTICS}
             },
             router
         );
     }
-    /* eslint-enable no-undef */
+
 
 
     // l18n

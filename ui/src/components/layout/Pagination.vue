599b26ddc3207d90d28d74d2435c9896120a442a
MilosPaunovic <paun992@hotmail.com>
2025-02-06 13:07:38 +0100

    chore(ui): minor tweaks

--- ui/src/components/layout/Pagination.vue
+++ ui/src/components/layout/Pagination.vue
@@ -136,39 +136,18 @@
             padding: 0 4px;
             line-height: 1.85;
             font-size: var(--el-font-size-extra-small);
-            color: var(--ks-content-secondary); // Ensure it uses non-primary color
+            color: var(--ks-content-secondary);
             white-space: nowrap;
         }
 
         :deep(.el-pagination .el-pager li) {
-            background-color: var(--ks-button-background-secondary); // Non-primary background for non-selected pages
-            border: 1px solid var(--ks-border-primary); // Non-primary border color
-            color: var(--ks-content-primary); // Non-primary text color for non-selected pages
+            background-color: var(--ks-button-background-secondary);
+            border: 1px solid var(--ks-border-primary);
+            color: var(--ks-content-primary);
 
-            &:hover {
-                background-color: var(--ks-button-background-secondary-hover); // Purple background on hover
-                border: 1px solid var(--ks-border-active); // Purple border on hover
-                color: white; // White text when hovered
-            }
-
-            &.is-active {
-                background-color: var(--ks-button-background-secondary-active); // Purple background for active page
-                border: 1px solid var(--ks-border-active); // Purple border for active page
-                color: white; // White text for the selected page
-            }
-        }
-
-        @media (prefers-color-scheme: light) {
-            :deep(.el-pagination .el-pager li) {
-                background-color: white; // Set background to white in light mode for non-selected pages
-                color: var(--ks-content-primary); // Non-primary text color for non-selected pages
-                border: 1px solid var(--ks-border-primary); // Border color for non-selected pages
-            }
-
-            :deep(.el-pagination .el-pager li.is-active) {
-                background-color: var(--ks-button-background-secondary-active); // Purple background for active page
-                border: 1px solid var(--ks-border-active); // Purple border for active page
-                color: white; // White text for the selected page
+            &:hover, &.is-active {
+                background-color: var(--ks-button-background-secondary-hover);
+                border: 1px solid var(--ks-border-active);
             }
         }
     }

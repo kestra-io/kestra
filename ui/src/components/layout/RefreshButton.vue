d29e96161ff8aa1425d6f6f553a493c6e4b2235b
Pravesh-Sudha <programmerpravesh@gmail.com>
2025-02-11 13:24:10 +0530

    Removed the styling from RefreshButton.vue file

--- ui/src/components/layout/RefreshButton.vue
+++ ui/src/components/layout/RefreshButton.vue
@@ -1,10 +1,10 @@
 <template>
-    <el-button :disabled="!canAutoRefresh" :active="autoRefresh" @click="toggleAutoRefresh" data-test-id="toggle-aut-refresh-button" class="refresh-button">
+    <el-button :disabled="!canAutoRefresh" :active="autoRefresh" @click="toggleAutoRefresh" data-test-id="toggle-aut-refresh-button">
         <kicon :tooltip="$t('toggle periodic refresh each 10 seconds')" placement="bottom">
             <component :is="autoRefresh ? 'auto-renew' : 'auto-renew-off'" class="auto-refresh-icon" />
         </kicon>
     </el-button>
-    <el-button @click="triggerRefresh" data-test-id="trigger-refresh-button" class="refresh-button">
+    <el-button @click="triggerRefresh" data-test-id="trigger-refresh-button">
         <kicon :tooltip="$t('trigger refresh')" placement="bottom">
             <refresh />
         </kicon>
@@ -92,8 +92,5 @@
         100% {
             rotate: 360deg;
         }
-    }
-    .refresh-button {
-    margin-left: 5px; 
     }    
 </style>

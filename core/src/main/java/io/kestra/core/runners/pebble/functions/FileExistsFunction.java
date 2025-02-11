4ec9d517fa9ba2d48b4fab3f3c123bd60396501f
Loïc Mathieu <loikeseke@gmail.com>
2025-02-11 11:19:52 +0100

    chore(core): refactor file related Pebble function to share code

--- core/src/main/java/io/kestra/core/runners/pebble/functions/FileExistsFunction.java
+++ core/src/main/java/io/kestra/core/runners/pebble/functions/FileExistsFunction.java
@@ -1,9 +1,7 @@
 package io.kestra.core.runners.pebble.functions;
 
 import io.kestra.core.storages.StorageInterface;
-import io.kestra.core.utils.Slugify;
 import io.pebbletemplates.pebble.error.PebbleException;
-import io.pebbletemplates.pebble.extension.Function;
 import io.pebbletemplates.pebble.template.EvaluationContext;
 import io.pebbletemplates.pebble.template.PebbleTemplate;
 import jakarta.inject.Inject;
@@ -15,12 +13,8 @@ import java.util.List;
 import java.util.Map;
 
 @Singleton
-public class FileExistsFunction implements Function {
+public class FileExistsFunction extends AbstractFileFunction {
     private static final String ERROR_MESSAGE = "The 'fileExists' function expects an argument 'path' that is a path to the internal storage URI.";
-    private static final String KESTRA_SCHEME = "kestra:///";
-    private static final String TRIGGER = "trigger";
-    private static final String NAMESPACE = "namespace";
-    private static final String ID  = "id";
 
     @Inject
     private StorageInterface storageInterface;
@@ -32,7 +26,6 @@ public class FileExistsFunction implements Function {
 
     @Override
     public Object execute(Map<String, Object> args, PebbleTemplate self, EvaluationContext context, int lineNumber) {
-
         if (!args.containsKey("path")) {
             throw new PebbleException(null, ERROR_MESSAGE, lineNumber, self.getName());
         }
@@ -48,52 +41,12 @@ public class FileExistsFunction implements Function {
 
     }
 
-    private URI getUriFromThePath(Object path, int lineNumber, PebbleTemplate self) {
-        if (path instanceof URI u) {
-            return u;
-        } else if (path instanceof String str && str.startsWith(KESTRA_SCHEME)) {
-            return URI.create(str);
-        } else {
-            throw new PebbleException(null, "Unable to create the URI from the path " + path, lineNumber, self.getName());
-        }
-    }
-
     @SuppressWarnings("unchecked")
     private boolean checkFileExistsFromInternalStorage(EvaluationContext context, URI path) throws IOException {
+        // check if the file is from the current execution or the parent execution
+        checkAllowedFile(context, path);
+
         Map<String, String> flow = (Map<String, String>) context.getVariable("flow");
-        Map<String, String> execution = (Map<String, String>) context.getVariable("execution");
-
-        boolean isFileFromCurrentExecution = isFileUriValid(flow.get(NAMESPACE), flow.get(ID), execution.get(ID), path);
-
-        if (!isFileFromCurrentExecution) {
-            checkIfFileFromParentExecution(context, path);
-        }
-
         return storageInterface.exists(flow.get("tenantId"), flow.get("namespace"), path);
-    }
-
-    private void checkIfFileFromParentExecution(EvaluationContext context, URI path) {
-        if (context.getVariable(TRIGGER) != null) {
-            // if there is a trigger of type execution, we also allow accessing a file from the parent execution
-            Map<String, String> trigger = (Map<String, String>) context.getVariable(TRIGGER);
-
-            if (!isFileUriValid(trigger.get(NAMESPACE), trigger.get("flowId"), trigger.get("executionId"), path)) {
-                throw new IllegalArgumentException("Unable to read the file '" + path + "' as it didn't belong to the current execution");
-            }
-        }
-        else {
-            throw new IllegalArgumentException("Unable to read the file '" + path + "' as it didn't belong to the current execution");
-        }
-    }
-
-    private boolean isFileUriValid(String namespace, String flowId, String executionId, URI path) {
-        // Internal storage URI should be: kestra:///$namespace/$flowId/executions/$executionId/tasks/$taskName/$taskRunId/$random.ion or kestra:///$namespace/$flowId/executions/$executionId/trigger/$triggerName/$random.ion
-        // We check that the file is for the given flow execution
-        if (namespace == null || flowId == null || executionId == null) {
-            return false;
-        }
-
-        String authorizedBasePath = KESTRA_SCHEME + namespace.replace(".", "/") + "/" + Slugify.of(flowId) + "/executions/" + executionId + "/";
-        return path.toString().startsWith(authorizedBasePath);
     }
 }

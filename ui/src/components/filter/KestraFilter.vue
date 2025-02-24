13e96bdf03e321e6616efcd8085eb9a49c7969ad
MilosPaunovic <paun992@hotmail.com>
2025-02-24 11:18:11 +0100

    chore(ui): prevent sending random strings as child filter values

--- ui/src/components/filter/KestraFilter.vue
+++ ui/src/components/filter/KestraFilter.vue
@@ -637,9 +637,13 @@
                 ["labels", "details"].includes(wholeSearchContent.at(-2)?.label) ||
                 wholeSearchContent.at(-2)?.value?.length === 0
             ) {
-                // Adding value to preceding empty filter
-                // TODO Provide a way for user to escape infinite labels & details loop (you can never fallback to a new filter, any further text will be added as a value to the filter)
-                wholeSearchContent.at(-2)?.value?.push(wholeSearchContent.at(-1));
+                if(wholeSearchContent.at(-2)?.label === "child") {
+                    if (typeof wholeSearchContent.at(-1) === "string") wholeSearchContent = [];
+                } else {
+                    // Adding value to preceding empty filter
+                    // TODO Provide a way for user to escape infinite labels & details loop (you can never fallback to a new filter, any further text will be added as a value to the filter)
+                    wholeSearchContent.at(-2)?.value?.push(wholeSearchContent.at(-1));
+                }
             } else {
                 // Adding text search string
                 const label = t("filters.options.text");

84814c06be34e6bcd108b709ed66e8e666211781
Loïc Mathieu <loikeseke@gmail.com>
2025-02-25 17:50:30 +0100

    WIP

--- core/src/main/java/io/kestra/core/models/executions/Variables.java
+++ core/src/main/java/io/kestra/core/models/executions/Variables.java
@@ -18,7 +18,6 @@ import io.micronaut.context.event.StartupEvent;
 import io.micronaut.runtime.event.annotation.EventListener;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
-import lombok.NoArgsConstructor;
 
 import java.io.*;
 import java.net.URI;
@@ -27,7 +26,6 @@ import java.util.*;
 
 @JsonSerialize(using = Variables.Serializer.class)
 @JsonDeserialize(using = Variables.Deserializer.class)
-@NoArgsConstructor
 public class Variables {
     public static final String TYPE = "io.kestra.datatype:outputs";
 
@@ -37,10 +35,10 @@ public class Variables {
     private URI storageUri;
 
     private transient Map<String, Object> delegate;
-    private transient boolean expanded = false;
+    private transient State state;
 
     private Map<String, Object> loadFromStorage() {
-        if (!this.expanded) {
+        if (this.state != State.DEFLATED) {
             StorageInterface storage = applicationContext.getBean(StorageInterface.class);
             if (storage == null) {
                 throw new IllegalStateException("Internal storage is not initialized");
@@ -51,7 +49,8 @@ public class Variables {
 
             try (InputStream file = storage.get(null, null, storageUri)) { // FIXME
                 delegate = ION_MAPPER.readValue(file, JacksonMapper.MAP_TYPE_REFERENCE);
-                expanded = true;
+                System.out.println("Reading " + delegate);
+                state = State.DEFLATED;
             } catch (IOException e) {
                 throw new UncheckedIOException(e);
             }
@@ -61,20 +60,46 @@ public class Variables {
     }
 
     public static Variables of(Map<String, Object> map) {
-        return new Variables(map);
+        StorageInterface storage = applicationContext.getBean(StorageInterface.class);
+
+        if (map == null || map.isEmpty()) {
+            return new Variables(map, null);
+        }
+
+        try {
+            System.out.println("Storing " + map);
+            File file = Files.createTempFile("output-", ".json").toFile();
+            ION_MAPPER.writeValue(file, map);
+            try (InputStream is = new FileInputStream(file)) {
+                URI storageUri = URI.create(file.getName()); // FIXME
+                storage.put(null, null, storageUri, is); // FIXME
+                return new Variables(map, storageUri);
+            }
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+
     }
 
     public static Variables of(URI uri) {
         return new Variables(uri);
     }
 
+    private Variables(Map<String, Object> map, URI uri) {
+        this.storageUri = uri;
+
+        this.delegate = map;
+        this.state = State.DEFLATED;
+    }
+
     private Variables(Map<String, Object> map) {
         this.delegate = map;
-        this.expanded = true;
+        this.state = State.LEGACY;
     }
 
     private Variables(URI uri) {
         this.storageUri = uri;
+        this.state = State.INIT;
     }
 
     public Map<String, Object> toMap() {
@@ -98,19 +123,14 @@ public class Variables {
             if (value == null) {
                 gen.writeNull();
             } else {
-                if (value.storageUri == null) { // FIXME this causes outputs existing before Variables to recreate the file each time they are serialized (for ex when listing executions) :(
-                    File file = Files.createTempFile("output-", ".json").toFile();
-                    ION_MAPPER.writeValue(file, value.loadFromStorage());
-                    try (InputStream is = new FileInputStream(file)) {
-                        value.storageUri = URI.create(file.getName()); // FIXME
-                        storage.put(null, null, value.storageUri, is); // FIXME
-                    }
+                if (value.state == State.LEGACY) {
+                    serializers.findValueSerializer(Map.class).serialize(value.delegate, gen, serializers);
+                } else {
+                    gen.writeStartObject();
+                    gen.writeStringField("type", TYPE); // marker to be sure at deserialization time we are a Variable not some random Map
+                    gen.writeStringField("storageUri", value.storageUri != null ? value.storageUri.toString() : null);
+                    gen.writeEndObject();
                 }
-
-                gen.writeStartObject();
-                gen.writeStringField("type", TYPE); // marker to be sure at deserialization time we are a Variable not some random Map
-                gen.writeStringField("storageUri", value.storageUri.toString());
-                gen.writeEndObject();
             }
         }
     }
@@ -128,8 +148,12 @@ public class Variables {
                 // deserialize as map
                 Map<String, Object> ret = ctx.readValue(parser, Map.class); // FIXME
                 if (TYPE.equals(ret.get("type"))) {
-                    URI storageUri = URI.create((String) ret.get("storageUri"));
-                    return new Variables(storageUri);
+                    String uriString = (String) ret.get("storageUri");
+                    if (uriString != null) {
+                        URI storageUri = URI.create((String) ret.get("storageUri"));
+                        return new Variables(storageUri);
+                    }
+                    return new Variables(Collections.emptyMap(), null);
                 }
 
                 // If the type is not TYPE, a real map has been serialized so we build a Variable with it.
@@ -150,5 +174,7 @@ public class Variables {
             Variables.applicationContext = this.applicationContext;
         }
     }
+
+    enum State {LEGACY, INIT, DEFLATED}
 }
 

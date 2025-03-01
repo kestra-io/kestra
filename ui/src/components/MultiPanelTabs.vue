47e3696c8b2ef9591dd563a589c00b5f8eb215a0
Bart Ledoux <bledoux@kestra.io>
2025-03-01 22:44:19 +0100

    non working reorder

--- ui/src/components/MultiPanelTabs.vue
+++ ui/src/components/MultiPanelTabs.vue
@@ -1,8 +1,26 @@
 <template>
     <Splitpanes class="default-theme">
         <Pane v-for="(panel, panelIndex) in panels" :key="panelIndex">
-            <div class="editor-tabs" @dragenter.stop.prevent="dragover" @dragleave.stop.prevent="dragleave" @drop="drop" :data-panel-index="panelIndex">
-                <button v-for="(tab, tabIndex) in panel.tabs" :key="tab.value" class="editor-tab" draggable="true" @dragstart="(e) => dragstart.bind(null, tab.value)(e)" @click="panel.activeTabIndex = tabIndex">
+            <div
+                class="editor-tabs"
+                @dragenter.prevent="dragover"
+                @dragleave.prevent="dragleave"
+                @dragover.prevent
+                @drop="drop"
+                :data-panel-index="panelIndex"
+            >
+                <button
+                    v-for="(tab, tabIndex) in panel.tabs"
+                    :key="tab.value"
+                    class="editor-tab"
+                    draggable="true"
+                    @dragstart="(e) => dragstart.bind(null, [panelIndex, tab.value])(e)"
+                    @dragenter.prevent="dragover"
+                    @dragleave.prevent="dragleave"
+                    @dragover.prevent
+                    @drop.stop="drop"
+                    @click="panel.activeTabIndex = tabIndex"
+                >
                     <component :is="tab.button.icon" />
                     {{ tab.button.label }}
                 </button>
@@ -37,32 +55,63 @@
         }
     }))
 
-    function dragstart(editorElementName: string, event: DragEvent) {
-        event.dataTransfer?.setData("text/plain", editorElementName);
+    function dragstart(editorElementPath: [number, string], event: DragEvent) {
+        event.dataTransfer?.setData("text/plain", JSON.stringify(editorElementPath));
     }
 
     function dragover(event: DragEvent) {
-        event.preventDefault();
-        event.stopPropagation();
         if(event.target instanceof HTMLElement){
-            if(event.target.classList.contains("editor-tabs")) {
-                event.target.style.backgroundColor = "var(--ks-background-info)";
-            }
+            event.target.classList.add("dragover");
         }
     }
 
     function dragleave(event: DragEvent) {
-        event.preventDefault();
-        event.stopPropagation();
         if(event.target instanceof HTMLElement){
-            event.target.style.backgroundColor = "";
+            event.target.classList.remove("dragover");
         }
     }
 
     function drop(event: DragEvent) {
-        console.log("drop", event)
-        const data = event.dataTransfer?.getData("text/plain");
-        console.log("data", data)
+        const [originalPanelIndex, tabId] = JSON.parse(event.dataTransfer?.getData("text/plain") ?? "[]");
+        if(!event.target || !(event.target instanceof HTMLElement)) {
+            return;
+        }
+        const targetPanelIndexAttrValue = event.target.getAttribute("data-panel-index") ?? event.target.closest(".editor-tabs")?.getAttribute("data-panel-index");
+        if(!targetPanelIndexAttrValue) {
+            return;
+        }
+        const targetPanelIndex = parseInt(targetPanelIndexAttrValue);
+
+        // find the tab object
+        const tabIndex = panels.value[originalPanelIndex].tabs.findIndex((tab) => tab.value === tabId);
+
+        const movedTab = panels.value[originalPanelIndex].tabs[tabIndex];
+
+        if(event.target.classList.contains("editor-tabs")){
+            // add the tab to the target panel
+            panels.value[targetPanelIndex].tabs.push(movedTab);
+        } else if(event.target.classList.contains("editor-tab")) {
+            //get the index of the hovered tab
+            const hoveredTabIndex = Array.from(event.target.parentElement?.children ?? []).indexOf(event.target);
+
+            // add the tab to the target panel just before the hovered tab
+            panels.value[targetPanelIndex].tabs.splice(hoveredTabIndex, 0, movedTab);
+        }
+
+        // if the leaving panel after removing is empty, delete the panel
+        if(panels.value[originalPanelIndex].tabs.length <= 1) {
+            panels.value.splice(originalPanelIndex, 1);
+        }else {
+            // if the tab was the active tab, and that tab was the last one, set the active tab to the previous one
+            if(panels.value[originalPanelIndex].activeTabIndex === tabIndex && panels.value[originalPanelIndex].tabs.length === tabIndex + 1) {
+                panels.value[originalPanelIndex].activeTabIndex = tabIndex - 1;
+            }
+
+            // finally, remove the tab from the original panel
+            panels.value[originalPanelIndex].tabs.splice(tabIndex, 1);
+        }
+
+        event.target.classList.remove("dragover");
     }
 </script>
 
@@ -72,6 +121,9 @@
         padding: .25rem;
         padding-bottom: 0;
         border-bottom: 1px solid var(--ks-border-primary);
+        &.dragover {
+            background-color: #e3e3e3;
+        }
     }
 
     .editor-tabs .editor-tab{
@@ -81,7 +133,12 @@
         border-bottom: none;
         background-color: var(--ks-background-card);
         display: flex;
+        flex-wrap:nowrap;
+        white-space: nowrap;
         align-items: center;
         gap: .5rem;
+        &.dragover {
+            background-color: #e3e3e3;
+        }
     }
 </style>

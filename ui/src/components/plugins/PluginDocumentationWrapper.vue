099eb47a80cf3ccd3887dfe6926a045a94fd23fd
Bart Ledoux <bledoux@kestra.io>
2025-03-18 23:32:53 +0100

    fix docs layout

--- ui/src/components/plugins/PluginDocumentationWrapper.vue
+++ ui/src/components/plugins/PluginDocumentationWrapper.vue
@@ -1,8 +1,26 @@
 <template>
-    <div style="padding: 1px 1rem; background-color: var(--ks-background-body);">
-        <PluginDocumentation />
+    <div class="plugin-doc" :class="{editorPlugin: plugin}">
+        <PluginDocumentation :key="theme" />
     </div>
 </template>
 <script lang="ts" setup>
+    import {computed} from "vue";
+    import {useStore} from "vuex";
     import PluginDocumentation from "./PluginDocumentation.vue"
+
+    const store = useStore();
+    const plugin = computed(() => store.state.plugin.editorPlugin);
+    const theme = computed(() => store.getters["misc/theme"]);
 </script>
+
+<style scoped lang="scss">
+.plugin-doc {
+    padding: 1px .5rem;
+    background-color: var(--ks-background-body);
+    height: 100%;
+}
+
+.editorPlugin{
+    padding: 1rem .5rem;
+}
+</style>

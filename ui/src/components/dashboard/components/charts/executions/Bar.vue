63b1853e434b0b2904559db3b6c0cea1291084a1
Rajat Singh <rs2382001@gmail.com>
2025-03-26 23:18:34 +0530

    Fix/Duration-Switch

--- ui/src/components/dashboard/components/charts/executions/Bar.vue
+++ ui/src/components/dashboard/components/charts/executions/Bar.vue
@@ -16,7 +16,10 @@
                 </p>
             </div>
 
-            <div class="switch-container w-100">
+            <div 
+                v-if="total > 0 || loading"
+                class="switch-container w-100"
+            >
                 <div
                     class="d-flex justify-content-end align-items-center switch-content"
                 >

f65c7081aec018a89f119e0089a97d171d94b321
Bart Ledoux <bledoux@kestra.io>
2025-03-27 12:08:13 +0100

    update vuex types

--- ui/src/vuex.d.ts
+++ ui/src/vuex.d.ts
@@ -1,9 +1,10 @@
+import {Store} from "vuex/types/index.d.ts";
+
 declare module "vuex"{
     export * from "vuex/types/index.d.ts"
 }
 
-declare module "@vue/runtime-core" {
-    import {Store} from "vuex";
+declare module "vue" {
     interface ComponentCustomProperties {
         $store: Store;
     }

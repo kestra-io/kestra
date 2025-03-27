6d719697ca5fa3d387d8f8d9a326ebee41b8ad47
Bart Ledoux <bledoux@kestra.io>
2025-03-27 11:58:13 +0100

    add vuex store types in options API

--- ui/src/vuex.d.ts
+++ ui/src/vuex.d.ts
@@ -1,3 +1,10 @@
 declare module "vuex"{
     export * from "vuex/types/index.d.ts"
 }
+
+declare module "@vue/runtime-core" {
+    import {Store} from "vuex";
+    interface ComponentCustomProperties {
+        $store: Store;
+    }
+}

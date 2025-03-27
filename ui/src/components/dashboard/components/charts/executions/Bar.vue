7a771120c8a3688cea7c3f0d372033f99a8a3b56
MilosPaunovic <paun992@hotmail.com>
2025-03-27 15:47:15 +0100

    chore(ui): minor tweak

--- ui/src/components/dashboard/components/charts/executions/Bar.vue
+++ ui/src/components/dashboard/components/charts/executions/Bar.vue
@@ -16,10 +16,7 @@
                 </p>
             </div>
 
-            <div 
-                v-if="total > 0"
-                class="switch-container w-100"
-            >
+            <div v-if="total > 0" class="switch-container w-100">
                 <div
                     class="d-flex justify-content-end align-items-center switch-content"
                 >

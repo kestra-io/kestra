9ab37a9ff468324eea206cf72cb5ba6128890882
satvik2131 <satvik213161@gmail.com>
2025-04-02 01:52:23 +0530

    done

--- ui/src/components/filter/KestraFilter.vue
+++ ui/src/components/filter/KestraFilter.vue
@@ -356,6 +356,7 @@
             .findIndex((item) => item.label === option.value.label);
         if (existingFilterIndex !== -1) {
             // If it exists, update current filter index
+            dropdowns.value.first = {shown: false, value: option};
             dropdowns.value.second = {shown: true, index: existingFilterIndex};
             activeParentFilter.value = option.value.label;
             lastClickedParent.value = option.value.label;
@@ -459,8 +460,12 @@
                 } else {
                     const values = currentFilters.value[parentIndex].value;
                     const index = values.indexOf(filter.value);
-                    if (index === -1) values.push(filter.value);
-                    else values.splice(index, 1);
+                    if (index === -1) {
+                        values.push(filter.value);
+                    }else{ 
+                        if(currentFilters.value[index].value.length === 1)currentFilters.value.splice(index, 1);
+                        else values.splice(index,1)
+                    }
                 }
             }
         } else {

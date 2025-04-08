3af6e0e18b2955289f6b67298e57e1ea242339b3
MilosPaunovic <paun992@hotmail.com>
2025-04-08 13:33:16 +0200

    chore(ui): pass proper argument to maximize the height of namespace file editor

--- ui/src/components/namespaces/utils/useHelpers.ts
+++ ui/src/components/namespaces/utils/useHelpers.ts
@@ -105,7 +105,7 @@ export function useHelpers() {
             name: "files",
             title: t("files"),
             component: EditorView,
-            props: {namespace, isNamespace: true, isReadOnly: false},
+            props: {namespace, isNamespace: true, isReadOnly: false, maximized: true},
         },
     ];
 

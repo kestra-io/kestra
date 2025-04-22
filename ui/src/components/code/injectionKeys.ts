d52c4d2cb1597b6116c0f8b7659ca3f56d93bd07
Bart Ledoux <bledoux@kestra.io>
2025-04-22 11:48:49 +0200

    make the creation key editable again

--- ui/src/components/code/injectionKeys.ts
+++ ui/src/components/code/injectionKeys.ts
@@ -1,8 +1,8 @@
-import type {InjectionKey, Ref, ComputedRef} from "vue"
+import type {InjectionKey, Ref} from "vue"
 
 export const FLOW_INJECTION_KEY = Symbol("flow-injection-key") as InjectionKey<string>
 export const SECTION_INJECTION_KEY = Symbol("section-injection-key") as InjectionKey<Ref<string>>
 export const TASKID_INJECTION_KEY = Symbol("taskid-injection-key") as InjectionKey<Ref<string>>
 export const POSITION_INJECTION_KEY = Symbol("position-injection-key") as InjectionKey<"after" | "before">
-export const CREATING_INJECTION_KEY = Symbol("creating-injection-key") as InjectionKey<ComputedRef<boolean>>
+export const CREATING_INJECTION_KEY = Symbol("creating-injection-key") as InjectionKey<Ref<boolean>>
 export const SAVEMODE_INJECTION_KEY = Symbol("flow-id-injection-key") as InjectionKey<"button" | "auto">

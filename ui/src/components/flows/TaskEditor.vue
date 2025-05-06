cdbb19d2bb4fd9d90a9218da2d1c93b1d9d36da0
Bart Ledoux <bledoux@kestra.io>
2025-05-06 11:39:39 +0200

    refactor: make taskeditor typescript

--- ui/src/components/flows/TaskEditor.vue
+++ ui/src/components/flows/TaskEditor.vue
@@ -24,100 +24,99 @@
         :definitions="plugin.schema.definitions"
     />
 </template>
-<script>
-    import TaskRoot from "./tasks/TaskRoot.vue";
+<script lang="ts" setup>
+    import {onBeforeMount, onBeforeUnmount, ref, watch} from "vue";
     import {YamlUtils as YAML_UTILS} from "@kestra-io/ui-libs";
+    import TaskRoot from "./tasks/TaskRoot.vue";
     import PluginSelect from "../../components/plugins/PluginSelect.vue";
-    import {mapGetters} from "vuex";
+    import {useStore} from "vuex";
     import {SECTIONS} from "../../utils/constants";
+    import {NoCodeElement, Schemas} from "../code/utils/types";
 
-    export default {
+    defineOptions({
+        name: "TaskEditor",
         inheritAttrs: false,
-        computed: {
-            ...mapGetters("flow", ["taskError"]),
-        },
-        components: {
-            TaskRoot,
-            PluginSelect
-        },
-        emits: ["update:modelValue"],
-        created() {
-            if (this.modelValue) {
-                this.setup()
-            }
-        },
-        watch: {
-            modelValue: {
-                handler() {
-                    if (!this.modelValue) {
-                        this.taskObject = {};
-                        this.selectedTaskType = undefined;
-                    }
-                }
-            }
-        },
-        beforeUnmount() {
-            this.$store.commit("flow/setTaskError", undefined);
-        },
-        props: {
-            modelValue: {
-                type: String,
-                required: false,
-                default: undefined,
-            },
-            section: {
-                type: String,
-                required: true,
-                default: undefined,
-            }
-        },
-        data() {
-            return {
-                selectedTaskType: undefined,
-                taskObject: {},
-                isLoading: false,
-                plugin: undefined,
-            };
-        },
-        methods: {
-            setup() {
-                this.taskObject = YAML_UTILS.parse(this.modelValue);
-                this.selectedTaskType = this.taskObject.type;
-                this.$store.dispatch("flow/validateTask", {task: this.modelValue, section: this.section})
+    });
 
-                this.load();
-            },
-            load() {
-                this.isLoading = true;
-                this.$store
-                    .dispatch("plugin/load", {
-                        cls: this.selectedTaskType,
-                        all: true
-                    })
-                    .then((response) => {
-                        this.plugin = response;
-                        this.isLoading = false;
-                    })
-
-            },
-            onInput(value) {
-                this.taskObject = value;
-                this.$emit("update:modelValue", YAML_UTILS.stringify(value));
-            },
-            onTaskTypeSelect() {
-                this.load();
-                const value = {
-                    type: this.selectedTaskType
-                };
-
-                if (this.section !== SECTIONS.TRIGGERS && this.section !== SECTIONS.TASK_RUNNERS) {
-                    value["id"] = this.taskObject && this.taskObject.id ? this.taskObject.id : undefined;
-                }
-
-                this.onInput(value);
-            },
+    const emit = defineEmits(["update:modelValue"]);
+    const props = defineProps({
+        modelValue: {
+            type: String,
+            required: false,
+            default: undefined,
         },
-    };
+        section: {
+            type: String,
+            required: true,
+            default: undefined,
+        },
+    });
+
+    const store = useStore();
+
+    onBeforeMount(() => {
+        if (props.modelValue) {
+            setup()
+        }
+    })
+
+    onBeforeUnmount(() => {
+        store.commit("flow/setTaskError", undefined);
+    })
+
+    watch(() => props.modelValue, (v) => {
+        if (!v) {
+            taskObject.value = {};
+            selectedTaskType.value = undefined;
+        }
+    })
+
+    type PartialCodeElement = Partial<NoCodeElement>;
+
+    const taskObject = ref<PartialCodeElement | undefined>({});
+    const selectedTaskType = ref<string>();
+    const isLoading = ref(false);
+    const plugin = ref<{schema: Schemas}>();
+
+    function setup() {
+        taskObject.value = YAML_UTILS.parse<PartialCodeElement>(props.modelValue);
+        selectedTaskType.value = taskObject.value?.type;
+        store.dispatch("flow/validateTask", {task: props.modelValue, section: props.section})
+
+        load();
+    }
+
+    function load() {
+        isLoading.value = true;
+        store
+            .dispatch("plugin/load", {
+                cls: selectedTaskType.value,
+                all: true
+            })
+            .then((response) => {
+                plugin.value = response;
+                isLoading.value = false;
+            })
+
+    }
+
+    function onInput(val: PartialCodeElement | undefined) {
+        taskObject.value = val;
+        emit("update:modelValue", YAML_UTILS.stringify(val));
+    }
+
+    function onTaskTypeSelect() {
+        load();
+        const value: PartialCodeElement = {
+            type: selectedTaskType.value ?? ""
+        };
+
+        if (props.section !== SECTIONS.TRIGGERS && props.section !== SECTIONS.TASK_RUNNERS) {
+            value["id"] = taskObject.value && taskObject.value.id ? taskObject.value.id : undefined;
+        }
+
+        onInput(value);
+    }
 </script>
 <style lang="scss" scoped>
     .type-div {

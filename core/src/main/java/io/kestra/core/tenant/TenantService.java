631a1a7d76575672a3300b0f06fa8dc6d72ec84e
nKwiatkowski <nkwiatkowski@kestra.io>
2025-05-07 11:36:00 +0200

    feat(core): clean the PR

--- core/src/main/java/io/kestra/core/tenant/TenantService.java
+++ core/src/main/java/io/kestra/core/tenant/TenantService.java
@@ -9,7 +9,7 @@ public class TenantService {
 
     /**
      * Resolve the current tenant and return its identifier.
-     * If the tenant is the default tenant, it returns null, which is always the case on OSS as Tenant is an EE feature.
+     * If the tenant is the default tenant, it returns main, which is always the case on OSS as Tenant is an EE feature.
      *
      * @return the current tenant identifier
      */

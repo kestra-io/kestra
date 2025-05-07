b77e067fb027397ba1d3531014db2bf80d320d2d
nKwiatkowski <nkwiatkowski@kestra.io>
2025-05-07 10:35:15 +0200

    feat(core): make tenant required in create user command

--- core/src/test/java/io/kestra/core/runners/FlowTriggerCaseTest.java
+++ core/src/test/java/io/kestra/core/runners/FlowTriggerCaseTest.java
@@ -1,7 +1,6 @@
 package io.kestra.core.runners;
 
 import io.kestra.core.models.executions.Execution;
-import io.kestra.core.models.executions.LogEntry;
 import io.kestra.core.models.flows.State;
 import io.kestra.core.queues.QueueException;
 import io.kestra.core.queues.QueueFactoryInterface;

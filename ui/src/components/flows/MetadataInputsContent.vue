140e276585aa45e371211b4b575a17eb63d2763c
MilosPaunovic <paun992@hotmail.com>
2025-05-14 14:36:26 +0200

    fix(core): properly handle inputs creation and editng via no code

--- ui/src/components/flows/MetadataInputsContent.vue
+++ ui/src/components/flows/MetadataInputsContent.vue
@@ -105,14 +105,14 @@
                 } else {
                     this.panel = undefined;
                     this.breadcrumbs.pop();
-                    this.$emit("update:modelValue", [...this.inputs]);
+                    this.$emit("update:modelValue", [...this.newInputs]);
                 }
             },
             updateSelected(value) {
-                if (!this.selectedIndex) {
-                    return;
+                if (this.selectedIndex >= 0) {
+                    this.newInputs[this.selectedIndex] = value;
+                    this.$emit("update:modelValue", [...this.newInputs]);
                 }
-                this.newInputs[this.selectedIndex] = value;
             },
             deleteInput(index) {
                 this.newInputs.splice(index, 1);

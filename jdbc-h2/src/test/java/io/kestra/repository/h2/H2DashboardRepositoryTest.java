948b2f74c7360d29933669d3b235ce849fba2c6b
rkampani <rkampani@gmail.com>
2025-05-16 12:06:15 -0500

    add missing H2DashboardRepositoryTest

---
+++ jdbc-h2/src/test/java/io/kestra/repository/h2/H2DashboardRepositoryTest.java
@@ -0,0 +1,17 @@
+package io.kestra.repository.h2;
+
+
+import io.kestra.jdbc.repository.AbstractJdbcDashboardRepositoryTest;
+import org.junit.jupiter.api.Test;
+
+class H2DashboardRepositoryTest extends AbstractJdbcDashboardRepositoryTest {
+    @Test
+    protected void findTaskRun() {
+
+    }
+
+    @Test
+    protected void taskRunsDailyStatistics() {
+
+    }
+}

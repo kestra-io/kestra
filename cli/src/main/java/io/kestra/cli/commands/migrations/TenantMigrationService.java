671cf94174619ac02b0e08095e141ec087b96758
nKwiatkowski <nkwiatkowski@kestra.io>
2025-05-19 16:10:38 +0200

    fix(core): user synchronisation command and clean PR

--- cli/src/main/java/io/kestra/cli/commands/migrations/TenantMigrationService.java
+++ cli/src/main/java/io/kestra/cli/commands/migrations/TenantMigrationService.java
@@ -1,10 +1,14 @@
 package io.kestra.cli.commands.migrations;
 
+import static io.kestra.core.tenant.TenantService.MAIN_TENANT;
+
 import com.github.javaparser.utils.Log;
+import io.kestra.core.exceptions.KestraRuntimeException;
 import io.kestra.core.repositories.TenantMigrationInterface;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 
 @Singleton
 @Slf4j
@@ -14,8 +18,12 @@ public class TenantMigrationService {
     private TenantMigrationInterface tenantMigrationInterface;
 
     public void migrateTenant(String tenantId, String tenantName, boolean dryRun) {
+        if (StringUtils.isNotBlank(tenantId) && !MAIN_TENANT.equals(tenantId)){
+            throw new KestraRuntimeException("Tenant configuration is an enterprise feature. It can only be main in OSS");
+        }
+
         Log.info("🔁 Starting tenant migration...");
-        tenantMigrationInterface.migrateTenant("main", dryRun);
+        tenantMigrationInterface.migrateTenant(MAIN_TENANT, dryRun);
     }
 
 }

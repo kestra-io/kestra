65f1bbfd71ccf5aacb69d915f06b334402431baf
Roman Acevedo <roman.acevedo62@gmail.com>
2025-05-23 16:51:52 +0200

    refactor(core): regroup Test and Beta badges with same component

--- ui/src/components/global/BetaBadge.vue
+++ ui/src/components/global/BetaBadge.vue
@@ -1,16 +1,10 @@
 <template>
-    <el-button type="primary" size="small" class="ms-2 beta">
-        Beta
-    </el-button>
+    <Badge content="Beta" />
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+    import Badge from "./Badge.vue";
+</script>
 
 <style scoped lang="scss">
-.beta {
-    pointer-events: none;
-    --el-button-border-color: var(--ks-button-content-primary);
-    border-color: var(--ks-button-content-primary);
-    border-radius: calc(var(--el-border-radius-round) * 2);
-}
 </style>
--- ui/src/components/global/TestBadge.vue
+++ ui/src/components/global/TestBadge.vue
@@ -1,20 +1,10 @@
 <template>
-    <el-tooltip :content="$t('test-badge-tooltip')" placement="top">
-        <div style="display: inline-block;">
-            <el-button type="primary" size="small" class="ms-2 test">
-                {{ $t('test-badge-text') }}
-            </el-button>
-        </div>
-    </el-tooltip>
+    <Badge :content="$t('test-badge-text')" :tooltip="$t('test-badge-tooltip')" />
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+    import Badge from "./Badge.vue";
+</script>
 
 <style scoped lang="scss">
-.test {
-    pointer-events: none;
-    --el-button-border-color: var(--ks-button-content-primary);
-    border-color: var(--ks-button-content-primary);
-    border-radius: calc(var(--el-border-radius-round) * 2);
-}
 </style>

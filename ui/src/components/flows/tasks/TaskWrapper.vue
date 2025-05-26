eda9bf2847d7370dc1a98ff9bdba21ec6bdfcd4d
MilosPaunovic <paun992@hotmail.com>
2025-05-26 10:45:05 +0200

    feat(core): move complex task fields inline in no code editor
    Closes https://github.com/kestra-io/kestra/issues/8934

--- ui/src/components/flows/tasks/TaskWrapper.vue
+++ ui/src/components/flows/tasks/TaskWrapper.vue
@@ -1,21 +1,25 @@
 <template>
-    <div class="schema-wrapper">
+    <div :class="{'schema-wrapper': true, bordered: !merge}">
         <slot name="tasks" />
     </div>
 </template>
 
 <script setup>
     defineOptions({name: "TaskWrapper"});
+
+    defineProps({merge: {type: Boolean, default: false}});
 </script>
 
-<style lang= "scss" scoped>
+<style lang="scss" scoped>
 .schema-wrapper {
-    border: 1px solid var(--ks-border-secondary);
     padding: 1rem;
-    padding-bottom: .20rem;
+    padding-bottom: 0.2rem;
     border-radius: 8px;
     margin: 1rem 0;
     background: var(--ks-background-body);
+}
+.bordered {
+    border: 1px solid var(--ks-border-secondary);
     box-shadow: 0 0 0 1px var(--ks-border-primary) inset;
 }
 </style>

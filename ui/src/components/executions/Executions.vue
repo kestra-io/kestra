fc9f46b14e827e7b66240182bf5b07fff8617019
Miloš Paunović <paun992@hotmail.com>
2025-05-29 14:06:19 +0200

    fix(executions): properly guard against the npe error (#9147)

--- ui/src/components/executions/Executions.vue
+++ ui/src/components/executions/Executions.vue
@@ -669,7 +669,7 @@
             let queryHasChanged = false;
 
             const queryKeys = Object.keys(query);
-            if (this.namespace === undefined && defaultNamespace && !queryKeys.some(key => key.startsWith("filters[namespace]"))) {
+            if (this?.namespace === undefined && defaultNamespace && !queryKeys.some(key => key.startsWith("filters[namespace]"))) {
                 query["filters[namespace][EQUALS]"] = defaultNamespace;
                 queryHasChanged = true;
             }

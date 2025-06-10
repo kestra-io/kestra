4b7c63c00914cac186a236622551aa4128273144
Bart Ledoux <bledoux@kestra.io>
2025-06-10 09:50:28 +0200

    refactor: avoid duplicating vite alias

--- ui/vitest.config.js
+++ ui/vitest.config.js
@@ -3,6 +3,8 @@ import {coverageConfigDefaults} from "vitest/config";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 
+import viteConfig from "./vite.config.js";
+
 export default defineConfig({
     plugins: [
         vue(),
@@ -10,14 +12,7 @@ export default defineConfig({
     resolve: {
         alias: {
             "override/services/filterLanguagesProvider": path.resolve(__dirname, "tests/storybook/mocks/services/filterLanguagesProvider.mock.ts"),
-            "override": path.resolve(__dirname, "src/override/"),
-            "#imports": path.resolve(__dirname, "node_modules/@kestra-io/ui-libs/stub-mdc-imports.js"),
-            "#build/mdc-image-component.mjs": path.resolve(__dirname, "node_modules/@kestra-io/ui-libs/stub-mdc-imports.js"),
-            "#mdc-imports": path.resolve(__dirname, "node_modules/@kestra-io/ui-libs/stub-mdc-imports.js"),
-            "#mdc-configs": path.resolve(__dirname, "node_modules/@kestra-io/ui-libs/stub-mdc-imports.js"),
-            "shiki": path.resolve(__dirname, "node_modules/shiki/dist"),
-            "vuex": path.resolve(__dirname, "node_modules/vuex/dist/vuex.esm-bundler.js"),
-            "@storybook/addon-actions": "storybook/actions"
+            ...viteConfig.resolve.alias,
         },
     },
     test: {

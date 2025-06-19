aff782a36786050da905bf50e0fdd41747f79503
Bart Ledoux <bledoux@kestra.io>
2025-06-19 17:02:49 +0200

    fix: default value for expression cannot be null

--- ui/src/components/flows/tasks/TaskExpression.vue
+++ ui/src/components/flows/tasks/TaskExpression.vue
@@ -42,7 +42,7 @@
             return props.modelValue;
         }
 
-        return YAML_UTILS.stringify(props.modelValue);
+        return props.modelValue ? YAML_UTILS.stringify(props.modelValue) : "";
     })
 
     const localEditorValue = ref(editorValue.value)

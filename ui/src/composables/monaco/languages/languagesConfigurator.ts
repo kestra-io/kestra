1df90d88c11f0079c67a41de1d4d8c46b3befe44
Bart Ledoux <bledoux@kestra.io>
2025-06-24 13:57:30 +0200

    feat: set it up without changing the language

--- ui/src/composables/monaco/languages/languagesConfigurator.ts
+++ ui/src/composables/monaco/languages/languagesConfigurator.ts
@@ -6,12 +6,21 @@ import {FlowAutoCompletion} from "override/services/flowAutoCompletionProvider";
 import {YamlAutoCompletion} from "../../../services/autoCompletionProvider";
 import {useI18n} from "vue-i18n";
 import {usePluginsStore} from "../../../stores/plugins";
+import {ComputedRef} from "vue";
 
-export default async function configure(store: Store<Record<string, any>>, pluginsStore: ReturnType<typeof usePluginsStore>, t: ReturnType<typeof useI18n>["t"], editorInstance: editor.ICodeEditor | undefined, language: string, domain: string | undefined): Promise<() => void> {
+export default async function configure(
+    store: Store<Record<string, any>>,
+    pluginsStore: ReturnType<typeof usePluginsStore>,
+    t: ReturnType<typeof useI18n>["t"],
+    editorInstance: editor.ICodeEditor | undefined,
+    language: string,
+    domain?: string,
+    flowYaml?: ComputedRef<string>
+): Promise<() => void> {
     let disposables: IDisposable[] | undefined;
     if (language === "yaml") {
         const yamlAutoCompletion = domain === "flow" ? new FlowAutoCompletion(store, pluginsStore) : new YamlAutoCompletion();
-        disposables = await new YamlLanguageConfigurator(yamlAutoCompletion).configure(store, t, editorInstance);
+        disposables = await new YamlLanguageConfigurator(yamlAutoCompletion, flowYaml).configure(store, t, editorInstance);
     } else if (filterLanguages.some(languageRegex => languageRegex.test(language))) {
         disposables = await new FilterLanguageConfigurator(language, domain).configure(store, t, editorInstance);
     }

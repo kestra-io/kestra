ddec179682a650c37d920a381370c668793a9095
Bart Ledoux <bledoux@kestra.io>
2025-07-01 16:57:08 +0200

    fix: remove prefix to make it work

--- ui/src/components/code/segments/Editor.vue
+++ ui/src/components/code/segments/Editor.vue
@@ -107,7 +107,7 @@
 
     import {useStore} from "vuex";
     import TaskWrapper from "../../flows/tasks/TaskWrapper.vue";
-    import {usePluginsStore} from "../../../stores/plugins";
+    import {removeRefPrefix, usePluginsStore} from "../../../stores/plugins";
     import {removeNullAndUndefined} from "../utils/cleanUp";
     const store = useStore();
 
@@ -232,7 +232,7 @@
             title: t(`no_code.sections.${section}`),
             blockType: SECTION_BLOCK_MAP[section],
             section,
-            definitionKey: pluginsStore.flowSchema?.$ref ?? ""
+            definitionKey: removeRefPrefix(pluginsStore.flowSchema?.$ref) ?? ""
         }))
     });
 </script>

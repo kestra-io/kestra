2d69fd893e111cdda9b3e7acb17a265239818d86
Piyush-r-bhaskar <impiyush0012@gmail.com>
2025-07-03 15:40:41 +0530

    chore(core): remove Breadcrumbs

--- ui/src/components/code/NoCode.vue
+++ ui/src/components/code/NoCode.vue
@@ -1,9 +1,5 @@
 <template>
     <div class="no-code">
-        <Breadcrumbs />
-
-        <hr class="m-0">
-
         <Editor
             :metadata
             @update-metadata="(k, v) => emit('updateMetadata', {[k]: v})"
@@ -18,16 +14,15 @@
     import * as YAML_UTILS from "@kestra-io/ui-libs/flow-yaml-utils";
 
     import {
-        BREADCRUMB_INJECTION_KEY, CLOSE_TASK_FUNCTION_INJECTION_KEY,
+        CLOSE_TASK_FUNCTION_INJECTION_KEY,
         CREATING_TASK_INJECTION_KEY, BLOCKTYPE_INJECT_KEY,
         PANEL_INJECTION_KEY, POSITION_INJECTION_KEY,
         REF_PATH_INJECTION_KEY, PARENT_PATH_INJECTION_KEY,
         FLOW_INJECTION_KEY, FIELDNAME_INJECTION_KEY,
         EDITING_TASK_INJECTION_KEY,
     } from "./injectionKeys";
-    import Breadcrumbs from "./components/Breadcrumbs.vue";
     import Editor from "./segments/Editor.vue";
-    import {Breadcrumb, BlockType} from "./utils/types";
+    import {BlockType} from "./utils/types";
 
     const emit = defineEmits<{
         (e: "updateTask", yaml: string): void
@@ -70,14 +65,12 @@
 
     const metadata = computed(() => YAML_UTILS.getMetadata(props.flow));
 
-    const breadcrumbs = ref<Breadcrumb[]>([])
     const panel = ref()
 
     provide(FLOW_INJECTION_KEY, computed(() => props.flow));
     provide(PARENT_PATH_INJECTION_KEY, props.parentPath ?? "");
     provide(REF_PATH_INJECTION_KEY, props.refPath);
     provide(PANEL_INJECTION_KEY, panel)
-    provide(BREADCRUMB_INJECTION_KEY, breadcrumbs);
     provide(BLOCKTYPE_INJECT_KEY, props.blockType);
     provide(POSITION_INJECTION_KEY, props.position);
     provide(CREATING_TASK_INJECTION_KEY, props.creatingTask);
@@ -85,13 +78,7 @@
     provide(FIELDNAME_INJECTION_KEY, props.fieldName);
 
     provide(CLOSE_TASK_FUNCTION_INJECTION_KEY, () => {
-        if (breadcrumbs.value[breadcrumbs.value.length - 1].component) {
-            breadcrumbs.value.pop();
-        } else {
-            // only close the tab if saving a task not a value
-            emit("closeTask")
-        }
-
+        emit("closeTask")
     })
 </script>
 

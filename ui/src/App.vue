79cccb26c84ce741dca6d1c60965b7bb57fbb64f
Bart Ledoux <bledoux@kestra.io>
2025-07-09 10:31:13 +0200

    fix vuetour

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -5,7 +5,7 @@
         <component :is="$route.meta.layout ?? DefaultLayout" v-if="loaded && shouldRenderApp">
             <router-view />
         </component>
-        <VueTour v-if="thi=$route.name && !isAuthRoute" />
+        <VueTour v-if="$route.name && !isAuthRoute" />
     </el-config-provider>
 </template>
 

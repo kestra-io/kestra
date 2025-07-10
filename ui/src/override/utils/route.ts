e9694ce23e0daa0679bc4386faf6e03f999be474
Piyush-r-bhaskar <impiyush0012@gmail.com>
2025-07-10 18:56:34 +0530

    refactor(core): streamlining base URL creation

--- ui/src/override/utils/route.ts
+++ ui/src/override/utils/route.ts
@@ -1,4 +1,4 @@
-import {Store} from "vuex";
+import type {Store} from "vuex"
 
 declare global {
     interface Window {
@@ -6,23 +6,26 @@ declare global {
     }
 }
 
-let root = (import.meta.env.VITE_APP_API_URL || "") + window.KESTRA_BASE_PATH;
-if (root.endsWith("/")) {
-    root = root.substring(0, root.length - 1);
+const createBaseUrl = (): string => {
+    const root = (import.meta.env.VITE_APP_API_URL || "") + (window.KESTRA_BASE_PATH || "")
+    return root.trim() || window.location.origin
 }
 
-export const baseUrl = root;
+export const baseUrl = createBaseUrl().replace(/\/$/, "")
+export const basePath = "/api/v1/main"
 
-export const basePath = () => "/api/v1/main"
-
-export const apiUrl = (_: Store<any>) => {
-    const login = localStorage.getItem("basicAuthLogin");
-    const password = localStorage.getItem("basicAuthPassword");
-    if(!login || !password) {
-        return `${baseUrl}${basePath()}`
+export const apiUrl = (_: Store<any>): string => {
+    const login = localStorage.getItem("basicAuthLogin")
+    const password = localStorage.getItem("basicAuthPassword")
+    
+    if (!login || !password) return `${baseUrl}${basePath}`
+    
+    try {
+        const {protocol, host} = new URL(baseUrl)
+        return `${protocol}//${login}:${password}@${host}${basePath}`
+    } catch {
+        return `${baseUrl}${basePath}`
     }
-    const baseUrlParsed = new URL(baseUrl); // Validate baseUrl
-    return `${baseUrlParsed.protocol}//${login}:${password}@${baseUrlParsed.host}${basePath()}`
 }
 
-export const apiUrlWithoutTenants = () => `${baseUrl}/api/v1`
+export const apiUrlWithoutTenants = (): string => `${baseUrl}/api/v1`

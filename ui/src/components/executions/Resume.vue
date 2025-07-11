d503e578317bf9484de1297f2e8fa979d2c164ab
Anusha G H <anushah@sahaj.ai>
2025-07-11 22:25:28 +0530

    [9960] Remove cancel button and functionality from resume component

--- ui/src/components/executions/Resume.vue
+++ ui/src/components/executions/Resume.vue
@@ -17,9 +17,6 @@
             <inputs-form :initial-inputs="inputsList" :execution="execution" v-model="inputs" />
         </el-form>
         <template #footer>
-            <el-button @click="cancel()">
-                {{ $t('cancel') }}
-            </el-button>
             <el-button :icon="PlayBox" type="primary" @click="resumeWithInputs($refs.form)" native-type="submit">
                 {{ $t('resume') }}
             </el-button>
@@ -98,16 +95,6 @@
                     .then(() => {
                         this.isDrawerOpen = false;
                         this.$toast().success(this.$t("resumed done"));
-                    });
-            },
-            cancel() {
-                this.$store
-                    .dispatch("execution/kill", {
-                        id: this.execution.id,
-                    })
-                    .then(() => {
-                        this.isDrawerOpen = false;
-                        this.$toast().success(this.$t("killed done"));
                     });
             },
             loadDefinition() {

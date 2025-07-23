93259d4438e37238cf83e6e9727ca0f09421deca
Bart Ledoux <bledoux@kestra.io>
2025-07-23 11:08:23 +0200

    fix playground

--- ui/src/stores/playground.ts
+++ ui/src/stores/playground.ts
@@ -31,6 +31,43 @@ export const usePlaygroundStore = defineStore("playground", () => {
     const store = useStore();
     const executionsStore = useExecutionsStore();
 
+    function getTargetNodes(graph: any, nodeUid?: string) {
+        if (!nodeUid) {
+            return undefined;
+        }
+        return graph.edges.filter((edge: any) => edge.source === nodeUid && edge.target);
+    }
+
+    async function getNextTaskId(taskId?: string) {
+        if (!taskId) {
+            return undefined;
+        }
+        const flow = store.state.flow.flow;
+        if (!flow) {
+            return undefined;
+        }
+
+        const graph = await store.dispatch("flow/loadGraph", {flow: store.state.flow.flow});
+
+        // find the node uid of the task with the given taskId
+        let taskUid = graph.nodes.find((node: any) => node?.task?.id === taskId).uid;
+
+        let edges:any[], nextTask:any;
+        // loop until we find a node that is not a cluster
+        do{
+            // find all the edges that are connected to this task
+            edges = getTargetNodes(graph, taskUid) as any[]
+            // if there are no edges, return undefined
+            if (edges.length === 0) {
+                return undefined;
+            }
+            taskUid = edges[0].target
+            nextTask = graph.nodes.find((node: any) => node.uid === taskUid && node.task)
+        }while(!nextTask)
+
+        return nextTask?.task.id;
+    }
+
     async function runUntilTask(taskId?: string){
         await store.dispatch("flow/saveAll")
 
@@ -39,12 +76,17 @@ export const usePlaygroundStore = defineStore("playground", () => {
             const {type, defaults} = input;
             defaultInputValues[input.id] = Inputs.normalize(type, defaults);
         }
+
+        // get the next task id to break on. If current task is provided to breakpoint,
+        // the task specified by the user will not be executed.
+        const nextTaskId = await getNextTaskId(taskId);
+
         const {data: execution} = await executionsStore.triggerExecution({
             id: store.state.flow.flow?.id,
             namespace: store.state.flow.flow?.namespace,
             formData: defaultInputValues,
             kind: "PLAYGROUND",
-            breakpoints: taskId ? [taskId] : undefined,
+            breakpoints: nextTaskId ? [nextTaskId] : undefined,
         })
         executionsStore.execution = execution;
 

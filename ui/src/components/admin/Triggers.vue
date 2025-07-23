bcb1edff7a3bd3d59301173882d5f7dae92f97f1
Roman Acevedo <roman.acevedo62@gmail.com>
2025-07-23 19:20:37 +0200

    fix(triggers): bulk action on triggers did not take into account this is async
    - fixes https://github.com/kestra-io/kestra-ee/issues/4388
    ugly quickwin hack

--- ui/src/components/admin/Triggers.vue
+++ ui/src/components/admin/Triggers.vue
@@ -339,6 +339,12 @@
                     }
                 });
             },
+            triggerLoadDataAfterBulkEditAction() {
+                this.loadData();
+                setTimeout(() => this.loadData(), 200);
+                setTimeout(() => this.loadData(), 1000);
+                setTimeout(() => this.loadData(), 5000);
+            },
             async unlock() {
                 const namespace = this.triggerToUnlock.namespace;
                 const flowId = this.triggerToUnlock.flowId;
@@ -391,10 +397,10 @@
                 this.triggerStore.update({...trigger, disabled: !value})
                     .then(updatedTrigger => {
                         this.triggers = this.triggers.map(t => {
-                            const triggerContextMatches = t.triggerContext && 
+                            const triggerContextMatches = t.triggerContext &&
                                 t.triggerContext.flowId === updatedTrigger.flowId &&
                                 t.triggerContext.triggerId === updatedTrigger.triggerId;
-                        
+
                             if (triggerContextMatches) {
                                 return {triggerContext: updatedTrigger, abstractTrigger: t.abstractTrigger};
                             }
@@ -404,7 +410,7 @@
             },
             genericConfirmAction(toast, queryAction, byIdAction, success, data) {
                 this.$toast().confirm(
-                    this.$t(toast, {"count": this.queryBulkAction ? this.total : this.selection.length}),
+                    this.$t(toast, {"count": this.queryBulkAction ? this.total : this.selection.length}) + ". " + this.$t("bulk action async warning"),
                     () => this.genericConfirmCallback(queryAction, byIdAction, success, data),
                     () => {
                     }
@@ -432,7 +438,7 @@
                         .then(data => {
                             this.$toast().success(this.$t(success, {count: data.count}));
                             this.toggleAllUnselected();
-                            this.loadData();
+                            this.triggerLoadDataAfterBulkEditAction();
                         })
                 } else {
                     const selection = this.selection;
@@ -442,7 +448,7 @@
                         .then(data => {
                             this.$toast().success(this.$t(success, {count: data.count}));
                             this.toggleAllUnselected();
-                            this.loadData();
+                            this.triggerLoadDataAfterBulkEditAction();
                         }).catch(e => {
                             this.$toast().error(e?.invalids.map(exec => {
                                 return {message: this.$t(exec.message, {triggers: exec.invalidValue})}

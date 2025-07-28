e4938b9c20714fd14b1a5fee46a9b1d65aee0be1
MilosPaunovic <paun992@hotmail.com>
2025-07-28 15:22:06 +0200

    chore(namespaces): properly handle file name field on flow run dialog if set from defaults

--- ui/src/components/inputs/InputsForm.vue
+++ ui/src/components/inputs/InputsForm.vue
@@ -153,16 +153,12 @@
                     <input
                         :data-testid="`input-form-${input.id}`"
                         :id="input.id+'-file'"
-                        class="el-input__inner"
+                        class="el-input__inner custom-file-input"
                         type="file"
                         @change="onFileChange(input, $event)"
                         autocomplete="off"
-                        :style="{display: isFile(inputsValues[input.id]) ? 'none': ''}"
                     >
-                    <label
-                        v-if="isFile(inputsValues[input.id])"
-                        :for="input.id+'-file'"
-                    >Kestra Internal Storage File</label>
+                    <span class="file-placeholder" v-html="getFilePlaceholder(inputsValues[input.id])" />
                 </div>
             </div>
             <div
@@ -581,9 +577,15 @@
 
                 this.updateArrayValue(input);
             },
-            isFile(data) {
-                return typeof data === "string" && (data.startsWith("kestra:///") || data.startsWith("file://") || data.startsWith("nsfile://"));
-            }
+            getFilePlaceholder(value) {
+                if (typeof value === "string" && value.startsWith("nsfile://")) {
+                    return this.$t("defaultsToNamespaceFile", {name: value.substring(10)});
+                }
+                if (value && typeof value.name === "string") {
+                    return value.name;
+                }
+                return this.$t("no_file_choosen");
+            },
         },
         watch: {
             flow () {
@@ -760,4 +762,19 @@
             overflow-x: hidden;
         }
     }
+
+.custom-file-input {
+  color: transparent;
+  width: 120px;
+}
+
+.custom-file-input::-webkit-file-upload-text {
+  visibility: hidden;
+}
+
+.file-placeholder {
+  margin-left: 8px;
+  color: var(--ks-content-secondary);
+  font-size: 0.9em;
+}
 </style>

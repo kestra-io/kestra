4e251c68c8f2437b381b85cd455b6aa8bb4f6eeb
MilosPaunovic <paun992@hotmail.com>
2025-07-30 11:36:41 +0200

    feat: introduce first version of table

---
+++ ui/src/components/dependencies/components/Table.vue
@@ -0,0 +1,9 @@
+<template>
+    <span>{{ props.elements }}</span>
+</template>
+
+<script setup lang="ts">
+    import type cytoscape from "cytoscape";
+
+    const props = defineProps<{ elements: cytoscape.ElementDefinition[] }>();
+</script>

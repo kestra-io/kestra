f786c93de43071b8cd396e90adfb912ade96741f
MilosPaunovic <paun992@hotmail.com>
2025-08-08 09:22:36 +0200

    chore: lower the wheelSensitivity on zooming actions

--- ui/src/components/dependencies/composables/useDependencies.ts
+++ ui/src/components/dependencies/composables/useDependencies.ts
@@ -23,9 +23,10 @@ import {getRandomNumber, getDependencies} from "../../../../tests/fixtures/depen
 import {edgeColors, style} from "../utils/style";
 const SELECTED = "selected", FADED = "faded",  HOVERED = "hovered", EXECUTIONS = "executions";
 
-const options: Omit<cytoscape.CytoscapeOptions, "container" | "elements"> & { elements?: Element[] } = {
-  minZoom: 0.1,
-  maxZoom: 2,
+const options: Omit<cytoscape.CytoscapeOptions, "container" | "elements"> & {elements?: Element[]} = {
+    minZoom: 0.1,
+    maxZoom: 2,
+    wheelSensitivity: 0.025,
 };
 
 /**

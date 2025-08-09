a209657ff3baadc75ed4f475b26b301a90ddb84c
Owen Warnack <owenwarnack@gmail.com>
2025-08-08 20:20:36 -0700

    fix(ui): show lock icon for namespace in No-Code editor

--- ui/src/components/flows/tasks/TaskNamespace.vue
+++ ui/src/components/flows/tasks/TaskNamespace.vue
@@ -1,5 +1,12 @@
 <template>
+    <InputText
+        v-if="disabled"
+        :model-value="modelValue"
+        disabled
+        class="w-100 disabled-field"
+    />
     <NamespaceSelect
+        v-else
         data-type="flow"
         :value="modelValue"
         allow-create
@@ -12,13 +19,20 @@
     import NamespaceSelect from "../../namespaces/components/NamespaceSelect.vue";
 
     import {useFlowStore} from "../../../stores/flow";
+    import InputText from "../../code/components/inputs/InputText.vue";
     export default {
-        components: {NamespaceSelect},
+        components: {InputText, NamespaceSelect},
         mixins: [Task],
+        props: {
+            disabled: {
+                type: Boolean,
+                default: false
+            }
+        },
         created() {
             const flowNamespace = this.flowStore.flow?.namespace;
             if (!this.modelValue && flowNamespace) {
-                this.onInput(flowNamespace)
+                this.onInput(flowNamespace);
             }
         },
         computed: {

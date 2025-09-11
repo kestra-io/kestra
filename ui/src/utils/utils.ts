a855c4152dc792055bec7dda80e2f6b94e49ec39
Bart Ledoux <bledoux@kestra.io>
2025-09-11 16:06:05 +0200

    make more tsc pass

--- ui/src/utils/utils.ts
+++ ui/src/utils/utils.ts
@@ -127,7 +127,7 @@ export default class Utils {
         return moment.duration(isoString).asMilliseconds() / 1000
     }
 
-    static humanDuration(value: string | number, options: humanizeDuration.HumanizerOptions) {
+    static humanDuration(value: string | number, options?: humanizeDuration.HumanizerOptions) {
         options = options || {maxDecimalPoints: 2};
         options.spacer = "";
         options.language = Utils.getLang();

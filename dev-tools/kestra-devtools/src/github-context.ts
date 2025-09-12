f190375a5bdf4b88fa4cda01a9bade5d3e2f7495
Roman Acevedo <roman.acevedo62@gmail.com>
2025-09-12 16:45:53 +0200

    bind token

--- dev-tools/kestra-devtools/src/github-context.ts
+++ dev-tools/kestra-devtools/src/github-context.ts
@@ -1,9 +1,10 @@
 import core from '@actions/core';
 import {context} from '@actions/github';
-const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
 import {strict as assert} from 'assert';
 
 export function getPRContext():{token: string, owner: string, repo: string, prNumber: number}{
+    const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN') || process.env.GITHUB_TOKEN;
+
     assert.ok(GITHUB_TOKEN, "GITHUB_TOKEN is mandatory");
     assert.ok(context.issue);
     assert.ok(context.issue.owner);

c5dec332d7192432a0618ae2b18b3a97d5d031f9
HARSH THAKARE <151563948+harsh21234i@users.noreply.github.com>
2025-09-15 15:51:40 +0530

    Update TimeSeries.vue

--- ui/src/components/dashboard/sections/TimeSeries.vue
+++ ui/src/components/dashboard/sections/TimeSeries.vue
@@ -185,7 +185,6 @@
                 .filter(key => !aggregatorKeys.includes(key))
                 .filter(key => key !== column);
 
-    correct-author-fix
             return array.reduce((acc, {...params}) => {
                 const stack = `${fields.map(field => params[field]).join(" ")}: ${aggregator.map(([key, meta]) => {
                     const value = isDuration(meta.field) ? Utils.humanDuration(params[key]) : params[key];
@@ -195,7 +194,6 @@
             return array.reduce((acc: any, {...params}) => {
                 const stack = `(${fields.map(field => params[field]).join(", ")}): ${aggregator.value.map(agg => agg[0] + " = " + (isDuration(agg[1].field) ? Utils.humanDuration(params[agg[0]]) : params[agg[0]])).join(", ")}`;
 
-        develop
                 if (!acc[stack]) {
                     acc[stack] = {
                         type: "bar",

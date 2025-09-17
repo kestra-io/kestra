73f04bdbc2a689b915b76c5b4c8ef7dd57c26829
Jeffrey Ricker <165683060+ricker-flow@users.noreply.github.com>
2025-09-17 15:30:16 -0400

    Update core/src/main/java/io/kestra/core/runners/pebble/functions/NanoIDFunction.java
    Co-authored-by: Roman Acevedo <roman.acevedo62@gmail.com>

--- core/src/main/java/io/kestra/core/runners/pebble/functions/NanoIDFunction.java
+++ core/src/main/java/io/kestra/core/runners/pebble/functions/NanoIDFunction.java
@@ -1,6 +1,5 @@
 package io.kestra.core.runners.pebble.functions;
 
-import io.pebbletemplates.pebble.error.PebbleException;
 import io.pebbletemplates.pebble.extension.Function;
 import io.pebbletemplates.pebble.template.EvaluationContext;
 import io.pebbletemplates.pebble.template.PebbleTemplate;

ff102f0aa19f4eb316bc4a21c7e31f185781dc24
Bart Ledoux <bledoux@kestra.io>
2025-09-19 15:35:37 +0200

    remove warning about autoimported vue functions

--- ui/src/components/layout/BulkSelect.vue
+++ ui/src/components/layout/BulkSelect.vue
@@ -20,7 +20,7 @@
     </div>
 </template>
 <script lang="ts" setup>
-    import {computed, defineProps, defineEmits} from "vue";
+    import {computed} from "vue";
 
     const props = defineProps<{
         total?: number;

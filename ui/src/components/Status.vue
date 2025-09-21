75d0a3e42326ab05b4e1eac2eb7666367d44f934
Bart Ledoux <bledoux@kestra.io>
2025-09-21 13:34:31 +0200

    remove warning about import

--- ui/src/components/Status.vue
+++ ui/src/components/Status.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script lang="ts" setup>
-    import {computed, defineProps} from "vue";
+    import {computed} from "vue";
     import * as FILTERS from "../utils/filters";
 
     const props = defineProps<{

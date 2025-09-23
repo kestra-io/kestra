cb85e12d93f0323a56ed65ed72a9c9009851593c
MilosPaunovic <paun992@hotmail.com>
2025-09-23 12:52:58 +0200

    chore: import image and use it

--- ui/src/components/layout/empty/images.ts
+++ ui/src/components/layout/empty/images.ts
@@ -1,23 +1,25 @@
-import AnnouncementsImage from "./assets/visuals/announcements.png";
-import AppsImage from "./assets/visuals/apps.png";
-import TestSuitesImage from "./assets/visuals/testSuites.png";
-import ConcurrencyExecutionsImage from "./assets/visuals/concurrency_executions.png";
-import ConcurrencyLimitImage from "./assets/visuals/concurrency_limit.png";
-import DependenciesImage from "./assets/visuals/dependencies.png";
-import PluginsImage from "./assets/visuals/plugins.png";
-import TriggersImage from "./assets/visuals/triggers.png";
-import VersionPluginImage from "./assets/visuals/versionPlugin.png";
-import PanelsImage from "./assets/visuals/panels.png";
+import announcements from "./assets/visuals/announcements.png";
+import apps from "./assets/visuals/apps.png";
+import testSuites from "./assets/visuals/testSuites.png";
+import concurrency_executions from "./assets/visuals/concurrency_executions.png";
+import concurrency_limit from "./assets/visuals/concurrency_limit.png";
+import dependencies from "./assets/visuals/dependencies.png";
+import plugins from "./assets/visuals/plugins.png";
+import triggers from "./assets/visuals/triggers.png";
+import versionPlugin from "./assets/visuals/versionPlugin.png";
+import panels from "./assets/visuals/panels.png";
 
-export const images = {
-  AnnouncementsImage,
-  AppsImage,
-  TestSuitesImage,
-  ConcurrencyExecutionsImage,
-  ConcurrencyLimitImage,
-  DependenciesImage,
-  PluginsImage,
-  TriggersImage,
-  VersionPluginImage,
-  PanelsImage,
+export const images: Record<string, string> = {
+    announcements,
+    apps,
+    testSuites,
+    concurrency_executions,
+    concurrency_limit,
+    "dependencies.FLOW": dependencies,
+    "dependencies.EXECUTION": dependencies,
+    "dependencies.NAMESPACE": dependencies,
+    plugins,
+    triggers,
+    versionPlugin,
+    panels,
 };

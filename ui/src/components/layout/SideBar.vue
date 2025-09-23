ce2882de3937478cfde93c02aeb2b4cdcfa433dd
Adekeye Emmanuel <adekeyeimmanuel@gmail.com>
2025-09-23 14:06:19 +0100

    fix(sidermenu): add StarIcon to Bookmark list(#11427)

--- ui/src/components/layout/SideBar.vue
+++ ui/src/components/layout/SideBar.vue
@@ -33,8 +33,7 @@
     import {
         onUpdated,
         ref,
-        computed,
-        shallowRef, h
+        computed, h
     } from "vue";
     import {useI18n} from "vue-i18n";
     import {useRoute} from "vue-router";
@@ -63,7 +62,7 @@
     const $route = useRoute()
     const {t} = useI18n({useScope: "global"});
 
-    function onToggleCollapse(folded) {
+    function onToggleCollapse(folded: boolean) {
         collapsed.value = folded;
         localStorage.setItem("menuCollapsed", folded ? "true" : "false");
         $emit("menu-collapse", folded);
@@ -71,15 +70,19 @@
         return folded;
     }
 
+    /**
+     * ⚠️ TODO: Review disabledCurrentRoute
+     */
     function disabledCurrentRoute(items) {
         return items
             .map(r => {
+             
                 if (r.href?.path === $route.path) {
                     r.disabled = true;
                 }
 
                 // route hack is still needed for blueprints
-                if (r.href !== "/" && ($route.path.startsWith(r.href) || r.routes?.includes($route.name))) {
+                if (r.href !== "/" && ($route.path.startsWith(r.href?.name) || r.routes?.includes($route.name))) {
                     r.class = "vsm--link_active";
                 }
 
@@ -111,13 +114,13 @@
             ...(bookmarksStore.pages?.length ? [{
                 title: t("bookmark"),
                 icon: {
-                    element: shallowRef(StarOutline),
+                    element: StarOutline,
                     class: "menu-icon",
                 },
                 child: [{
                     // here we use only one component for all bookmarks
                     // so when one edits the bookmark, it will be updated without closing the section
-                    component: () => h(BookmarkLinkList, {pages: bookmarksStore.pages}),
+                    component: () => h(BookmarkLinkList, {pages: bookmarksStore.pages.filter((page): page is { path: string, label: string } => typeof page.label === "string")}),
                 }]
             }] : []),
             ...(props.menu ? disabledCurrentRoute(props.menu) : [])

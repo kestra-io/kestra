789a47421811ab83d2b0c9b66511e188d475b33a
Eleftheria Batsou <eleftheriabatsou@gmail.com>
2025-09-30 14:49:44 +0300

    Update Markdown.vue

--- ui/src/components/layout/Markdown.vue
+++ ui/src/components/layout/Markdown.vue
@@ -116,7 +116,7 @@
         }
 
         mark {
-            background: var(--ks-background-additionLine);
+            background: var(--ks-background-success);
             color: var(--ks-content-success);
             font-size: var(--font-size-sm);
             padding: 2px 8px 2px 8px;

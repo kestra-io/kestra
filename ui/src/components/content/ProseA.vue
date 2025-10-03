661373c4da2606c10d0c678895f53fb7536cd1f8
Dhivya <dhivyadhi082@gmail.com>
2025-10-03 18:48:07 +0530

    ui: convert ProseA.vue to TypeScript (Composition API) (#11717)

--- ui/src/components/content/ProseA.vue
+++ ui/src/components/content/ProseA.vue
@@ -1,44 +1,40 @@
 <template>
-    <component :is="linkType" v-bind="linkProps">
-        <slot />
-    </component>
+  <component :is="linkType" v-bind="linkProps">
+    <slot />
+  </component>
 </template>
 
-<script setup>
-    import {computed, toRef} from "vue";
-    import {useRoute} from "vue-router";
-    import {useDocsLink} from "../docs/useDocsLink";
+<script setup lang="ts">
+import { computed, toRef } from "vue";
+import { useRoute } from "vue-router";
+import { useDocsLink } from "../docs/useDocsLink";
 
-    const route = useRoute();
+const route = useRoute();
 
-    const props = defineProps({
-        href: {
-            type: String,
-            default: ""
-        },
-        target: {
-            type: String,
-            default: undefined,
-            required: false
-        }
-    });
+const props = withDefaults(defineProps<{
+  href?: string;
+  target?: string;
+}>(), {
+  href: "",
+  target: undefined
+});
 
-    const {href, isRemote} = useDocsLink(toRef(props.href), computed(() => route.path));
+const { href, isRemote } = useDocsLink(
+  toRef(props, "href"),
+  computed(() => route.path)
+);
 
-    const linkType = computed(() => {
-        return isRemote.value ? "a" : "router-link";
-    });
+const linkType = computed(() => (isRemote.value ? "a" : "router-link"));
 
-    const linkProps = computed(() => {
-        if (isRemote.value) {
-            return {
-                href: href.value,
-                target: "_blank"
-            };
-        }
-
-        return {
-            to: href.value
-        };
-    });
+const linkProps = computed(() => {
+  if (isRemote.value) {
+    return {
+      href: href.value,
+      target: props.target ?? "_blank"
+    };
+  }
+  return {
+    to: href.value
+  };
+});
 </script>

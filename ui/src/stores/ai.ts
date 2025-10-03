9fcb12ad8bb4c32e997642410bc692cbaf2fef4c
Roman Acevedo <roman.acevedo62@gmail.com>
2025-10-03 16:22:20 +0200

    cleanup

--- ui/src/stores/ai.ts
+++ ui/src/stores/ai.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import {defineStore} from "pinia";
 import {apiUrl} from "override/utils/route";
+
 export const useAiStore = defineStore("ai", {
     actions: {
         async generateFlow({userPrompt, flowYaml, conversationId}: {userPrompt: string, flowYaml: string, conversationId: string}) {

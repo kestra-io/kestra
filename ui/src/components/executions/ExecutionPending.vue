c9e1b0e5a71009db6c1803baeea743cf3574aa80
dhinakaranst <dinastdhinast@gmail.com>
2025-10-07 21:13:27 +0530

    feat(ui): convert ExecutionPending to TypeScript

--- ui/src/components/executions/ExecutionPending.vue
+++ ui/src/components/executions/ExecutionPending.vue
@@ -19,21 +19,23 @@
     </EmptyTemplate>
 </template>
 
-<script setup>
+<script setup lang="ts">
     import EmptyTemplate from "../layout/EmptyTemplate.vue";
+    import { PropType } from 'vue';
+    import { Execution } from "@kestra-io/ui-libs/src/repository/index";
 
-    defineProps({
+    const props = defineProps({
         execution: {
-            type: Object,
+            type: Object as PropType<Execution>,
             required: true
         }
     });
 
-    const getStyle = (state) => ({
+    const getStyle = (state: string) => ({
         color: `var(--ks-content-${state.toLowerCase()})`,
         border: `1px solid var(--ks-border-${state.toLowerCase()})`,
         backgroundColor: `var(--ks-background-${state.toLowerCase()})`
-    })
+    });
 </script>
 
 <style lang="scss" scoped>

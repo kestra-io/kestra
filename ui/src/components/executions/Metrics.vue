b24ae731dc0ce68402ffbca358e1f43d097f4090
manshusainishab <manshupallav@gmail.com>
2025-10-08 19:49:42 +0530

    changes made as asked after review

--- ui/src/components/executions/Metrics.vue
+++ ui/src/components/executions/Metrics.vue
@@ -1,53 +1,43 @@
 <script setup lang="ts">
-import { ref, nextTick } from 'vue';
-import ChartAreaspline from 'vue-material-design-icons/ChartAreaspline.vue';
-import Drawer from '../Drawer.vue';
-import MetricsTable from './MetricsTable.vue';
+import { ref, nextTick } from 'vue'
+import ChartAreaspline from 'vue-material-design-icons/ChartAreaspline.vue'
+import Drawer from '../Drawer.vue'
+import MetricsTable from './MetricsTable.vue'
 
-// Props
-interface Props {
+const props = defineProps<{
     embed?: boolean;
     taskRun: Record<string, any>;
     execution: Record<string, any>;
-}
+}>();
 
-const props = defineProps<Props>();
+const embed = props.embed ?? true
+const isOpen = ref(false)
+const table = ref<InstanceType<typeof MetricsTable> | null>(null)
 
-// Default value for embed
-const embed = props.embed ?? true;
-
-// Refs
-const isOpen = ref(false);
-const table = ref<InstanceType<typeof MetricsTable> | null>(null);
-
-// Methods
 const onClick = async () => {
-    isOpen.value = !isOpen.value;
-    await nextTick();
-
-    if (table.value) {
-        table.value.loadData(table.value.onDataLoaded);
-    }
-};
+  isOpen.value = !isOpen.value
+  await nextTick()
+  table.value?.loadData(table.value.onDataLoaded)
+}
 </script>
 
 <template>
-    <el-dropdown-item
-        :icon="ChartAreaspline"
-        @click="onClick"
-    >
-        {{ $t('metrics') }}
-    </el-dropdown-item>
+  <el-dropdown-item
+    :icon="ChartAreaspline"
+    @click="onClick"
+  >
+    {{ $t('metrics') }}
+  </el-dropdown-item>
 
-    <Drawer
-        v-if="isOpen"
-        v-model="isOpen"
-        :title="$t('metrics')"
-    >
-        <MetricsTable
-            ref="table"
-            :taskRunId="props.taskRun.id"
-            :execution="props.execution"
-        />
-    </Drawer>
+  <Drawer
+    v-if="isOpen"
+    v-model="isOpen"
+    :title="$t('metrics')"
+  >
+    <MetricsTable
+      ref="table"
+      :taskRunId="props.taskRun.id"
+      :execution="props.execution"
+    />
+  </Drawer>
 </template>

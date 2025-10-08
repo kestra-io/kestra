419a4f55ed28ab6c458f3664cdd1c616f8726913
Bart Ledoux <bledoux@kestra.io>
2025-10-08 10:15:28 +0200

    remove changes on sections

--- ui/src/components/dashboard/sections/Pie.vue
+++ ui/src/components/dashboard/sections/Pie.vue
@@ -27,7 +27,7 @@
     import {Chart, getDashboard} from "../composables/useDashboards";
     import {useChartGenerator} from "../composables/useDashboards";
 
-
+    
     import NoData from "../../layout/NoData.vue";
     import Utils, {useTheme} from "../../../utils/utils";
 
@@ -153,7 +153,7 @@
 
         let results = Object.create(null);
 
-        generated.value.results?.forEach((value) => {
+        generated.value.results.forEach((value) => {
             const field = parseValue(value[aggregator.field.key]);
             const aggregated = value[aggregator.value.key];
 

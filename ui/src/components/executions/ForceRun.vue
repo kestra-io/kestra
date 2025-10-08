e61dfbf2f097617d969b0fb5044d20f55c797edb
Raj Gupta <150777419+Raj-G07@users.noreply.github.com>
2025-10-08 12:07:02 +0530

    Update ForceRun.vue

--- ui/src/components/executions/ForceRun.vue
+++ ui/src/components/executions/ForceRun.vue
@@ -30,62 +30,75 @@
     </el-dialog>
 </template>
 
-<script setup>
+<script lang="ts" setup>
+    import { ref, computed } from "vue";
     import RunFast from "vue-material-design-icons/RunFast.vue";
-</script>
-
-<script>
-    import {mapStores} from "pinia";
+    import QueueFirstInLastOut from "vue-material-design-icons/QueueFirstInLastOut.vue";
     import {State} from "@kestra-io/ui-libs";
     import permission from "../../models/permission";
     import action from "../../models/action";
     import {useExecutionsStore} from "../../stores/executions";
     import {useAuthStore} from "override/stores/auth"
 
-    export default {
-        props: {
-            execution: {
-                type: Object,
-                required: true
-            },
-            component: {
-                type: String,
-                default: "el-button"
-            },
-        },
-        data() {
-            return {
-                isDrawerOpen: false,
-            };
-        },
-        methods: {
-            click() {
-                this.$toast()
-                    .confirm(this.$t("force run confirm", {id: this.execution.id}), () => {
-                        return this.forceRun();
-                    });
-            },
-            forceRun() {
-                this.executionsStore.forceRun({
-                    id: this.execution.id
-                })
-                    .then(() => {
-                        this.isDrawerOpen = false;
-                        this.$toast().success(this.$t("force run done"));
-                    });
-            }
-        },
-        computed: {
-            ...mapStores(useExecutionsStore, useAuthStore),
-            enabled() {
-                if (!(this.authStore.user?.isAllowed(permission.EXECUTION, action.UPDATE, this.execution.namespace))) {
-                    return false;
-                }
+    interface ExecutionState {
+        current: string;
+    }
+    
+    interface Execution {
+        id: string;
+        namespace: string;
+        state: ExecutionState;
+    }
 
-                return State.isRunning(this.execution.state.current) || State.isQueued(this.execution.state.current);
-            }
-        },
+    interface Props {
+        execution: Execution;
+        component?: string;
+    }
+
+    const props = withDefaults(
+        defineProps<Props>(),
+        {
+            component: "el-button",
+        }
+    );
+
+    const isDrawerOpen = ref(false);
+    
+    const executionsStore = useExecutionsStore();
+    const authStore = useAuthStore();
+
+    const { t } = useI18n();
+    const toast = useToast();
+    
+    const click = () => {
+      toast.confirm(t("force run confirm", { id: props.execution.id }), () => {
+        return forceRun();
+      });
     };
+
+    const forceRun = async () => {
+      try {
+        await executionsStore.forceRun({ id: props.execution.id });
+        isDrawerOpen.value = false;
+        toast.success(t("force run done"));
+      } catch (err) {
+        toast.error(t("force run failed"));
+        console.error(err);
+      }
+    };
+    
+    const enabled = computed(() => {
+      const user = authStore.user;
+
+      if (!user?.isAllowed(permission.EXECUTION, action.UPDATE, props.execution.namespace)) {
+      return false;
+      }
+
+      return (
+      State.isRunning(props.execution.state.current) || State.isQueued(props.execution.state.current)
+      );
+    });
+    
 </script>
 
 <style lang="scss" scoped>

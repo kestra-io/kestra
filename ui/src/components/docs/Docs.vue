fc148148a0f7fc4a7a72649f1c54258f5e9222a2
Piyush Bhaskar <impiyush0012@gmail.com>
2025-10-08 15:17:59 +0530

    minor tweaks

--- ui/src/components/docs/Docs.vue
+++ ui/src/components/docs/Docs.vue
@@ -1,5 +1,5 @@
 <template>
-    <TopNavBar :title="routeInfo.title" :breadcrumb="routeInfo.breadcrumb" />
+    <TopNavBar :title="routeInfo.title" />
     <DocsLayout>
         <template #menu>
             <Toc />
@@ -18,6 +18,7 @@
     import TopNavBar from "../layout/TopNavBar.vue";
     import {useDocStore} from "../../stores/doc";
     import DocsLayout from "./DocsLayout.vue";
+    //@ts-expect-error no declaration
     import Toc from "./Toc.vue";
     import {computed,ref,watch,getCurrentInstance} from "vue";
     import {useRoute} from "vue-router";
@@ -29,32 +30,19 @@
 
     const ast = ref();
     const proseComponents = Object.fromEntries(
-        Object.keys(getCurrentInstance().appContext.components)
+        Object.keys(getCurrentInstance()?.appContext.components ?? {})
             .filter(name => name.startsWith("Prose"))
             .map(name => name.substring(5).replaceAll(/(.)([A-Z])/g, "$1-$2").toLowerCase())
             .map(name => [name, "prose-" + name])
     );
 
     const path = computed(() => {
-        const routePath = route.params.path;
+        const routePath = Array.isArray(route.params.path) ? route.params.path.join("/") : route.params.path;
         return routePath?.length > 0 ? routePath.replaceAll(/(^|\/)\.\//g, "$1") : undefined;
     });
 
     const routeInfo = computed(() => ({
         title: docStore.pageMetadata?.title ??t("docs"),
-        breadcrumb: [
-            {
-                label: t("docs"),
-                link: {name: "docs/view"}
-            },
-            ...(path.value?.split("/").map((part, index,arr) => ({
-                label: part,
-                link: {
-                    name: "docs/view",
-                    params: {path: arr.slice(0, index + 1).join("/")}
-                }
-            })) ?? [])
-        ]
     }));
 
     watch(

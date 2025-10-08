92619d03d4282a4a7230cad57edeff95b750ea64
MilosPaunovic <paun992@hotmail.com>
2025-10-08 12:40:24 +0200

    chore: format

--- ui/src/components/executions/ForceRun.vue
+++ ui/src/components/executions/ForceRun.vue
@@ -14,36 +14,50 @@
             :disabled="!enabled"
             class="ms-0 me-1"
         >
-            {{ $t('force run') }}
+            {{ $t("force run") }}
         </component>
     </el-tooltip>
 
-    <el-dialog v-if="isDrawerOpen" v-model="isDrawerOpen" destroyOnClose :appendToBody="true">
+    <el-dialog
+        v-if="isDrawerOpen"
+        v-model="isDrawerOpen"
+        destroyOnClose
+        :appendToBody="true"
+    >
         <template #header>
             <span v-html="$t('force run title', {id: execution.id})" />
         </template>
         <template #footer>
-            <el-button :icon="QueueFirstInLastOut" type="primary" @click="forceRun()" nativeType="submit">
-                {{ $t('force run') }}
+            <el-button
+                :icon="QueueFirstInLastOut"
+                type="primary"
+                @click="forceRun()"
+                nativeType="submit"
+            >
+                {{ $t("force run") }}
             </el-button>
         </template>
     </el-dialog>
 </template>
 
-<script lang="ts" setup>
-    import { ref, computed } from "vue";
-    import RunFast from "vue-material-design-icons/RunFast.vue";
-    import QueueFirstInLastOut from "vue-material-design-icons/QueueFirstInLastOut.vue";
+<script setup lang="ts">
+    import {ref, computed} from "vue";
     import {State} from "@kestra-io/ui-libs";
     import permission from "../../models/permission";
     import action from "../../models/action";
     import {useExecutionsStore} from "../../stores/executions";
-    import {useAuthStore} from "override/stores/auth"
+    import {useAuthStore} from "override/stores/auth";
+
+    import {useI18n} from "vue-i18n";
+    import {useToast} from "../../utils/toast";
+
+    import RunFast from "vue-material-design-icons/RunFast.vue";
+    import QueueFirstInLastOut from "vue-material-design-icons/QueueFirstInLastOut.vue";
 
     interface ExecutionState {
         current: string;
     }
-    
+
     interface Execution {
         id: string;
         namespace: string;
@@ -55,54 +69,56 @@
         component?: string;
     }
 
-    const props = withDefaults(
-        defineProps<Props>(),
-        {
-            component: "el-button",
-        }
-    );
+    const props = withDefaults(defineProps<Props>(), {
+        component: "el-button",
+    });
 
     const isDrawerOpen = ref(false);
-    
+
     const executionsStore = useExecutionsStore();
     const authStore = useAuthStore();
 
-    const { t } = useI18n();
+    const {t} = useI18n({useScope: "global"});
     const toast = useToast();
-    
+
     const click = () => {
-      toast.confirm(t("force run confirm", { id: props.execution.id }), () => {
-        return forceRun();
-      });
+        toast.confirm(t("force run confirm", {id: props.execution.id}), () => {
+            return forceRun();
+        });
     };
 
     const forceRun = async () => {
-      try {
-        await executionsStore.forceRun({ id: props.execution.id });
-        isDrawerOpen.value = false;
-        toast.success(t("force run done"));
-      } catch (err) {
-        toast.error(t("force run failed"));
-        console.error(err);
-      }
+        try {
+            await executionsStore.forceRun({id: props.execution.id});
+            isDrawerOpen.value = false;
+            toast.success(t("force run done"));
+        } catch (err) {
+            console.error(err);
+        }
     };
-    
+
     const enabled = computed(() => {
-      const user = authStore.user;
+        const user = authStore.user;
 
-      if (!user?.isAllowed(permission.EXECUTION, action.UPDATE, props.execution.namespace)) {
-      return false;
-      }
+        if (
+            !user?.isAllowed(
+                permission.EXECUTION,
+                action.UPDATE,
+                props.execution.namespace,
+            )
+        ) {
+            return false;
+        }
 
-      return (
-      State.isRunning(props.execution.state.current) || State.isQueued(props.execution.state.current)
-      );
+        return (
+            State.isRunning(props.execution.state.current) ||
+            State.isQueued(props.execution.state.current)
+        );
     });
-    
 </script>
 
 <style scoped lang="scss">
-    button.el-button {
-        cursor: pointer !important;
-    }
+button.el-button {
+    cursor: pointer !important;
+}
 </style>

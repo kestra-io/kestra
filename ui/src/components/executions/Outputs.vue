809f937b8dbb58d02769ade8fd2f439a26cd01a4
MilosPaunovic <paun992@hotmail.com>
2025-10-09 13:27:25 +0200

    chore: minor tweaks

--- ui/src/components/executions/Outputs.vue
+++ ui/src/components/executions/Outputs.vue
@@ -1,17 +1,9 @@
 <template>
-    <el-dropdown-item
-        :icon="LocationExit"
-        :disabled="isDisabled"
-        @click="isOpen = !isOpen"
-    >
-        {{ $t('outputs') }}
+    <el-dropdown-item :disabled :icon="LocationExit" @click="isOpen = !isOpen">
+        {{ $t("outputs") }}
     </el-dropdown-item>
 
-    <Drawer
-        v-if="isOpen"
-        v-model="isOpen"
-        :title="$t('outputs')"
-    >
+    <Drawer v-if="isOpen" v-model="isOpen" :title="$t('outputs')">
         <Vars
             :execution="props.execution"
             class="table-unrounded mt-1"
@@ -21,28 +13,25 @@
 </template>
 
 <script setup lang="ts">
-    import {computed, ref} from "vue";
-    import LocationExit from "vue-material-design-icons/LocationExit.vue";
-    import Vars from "../executions/Vars.vue";
-    import Drawer from "../Drawer.vue";
-    import {type PropType} from "vue";
+    import {computed, ref, type PropType} from "vue";
 
-    // Define props with TypeScript 
+    import Drawer from "../Drawer.vue";
+    import Vars from "../executions/Vars.vue";
+
+    import LocationExit from "vue-material-design-icons/LocationExit.vue";
+
     const props = defineProps({
         outputs: {
             type: Object as PropType<object>,
-            default: () => ({})
+            default: () => ({}),
         },
         execution: {
             type: Object as PropType<object>,
-            required: true
-        }
+            required: true,
+        },
     });
-    // Reactive state for the drawer
+
     const isOpen = ref(false);
 
-    // Computed property to determine if the button needs to be disabled
-    const isDisabled = computed(() => {
-        return !props.outputs || Object.keys(props.outputs).length === 0;
-    });
+    const disabled = computed(() => !props.outputs || Object.keys(props.outputs).length === 0);
 </script>

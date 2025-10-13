8ecb86494d4785569cda08f0d1374bff00f78cfc
Hemant M Mehta <hemant29mehta@gmail.com>
2025-10-13 17:48:54 +0530

    fix: merge conflicts

--- core/src/test/java/io/kestra/plugin/core/condition/DateTimeBetweenTest.java
+++ core/src/test/java/io/kestra/plugin/core/condition/DateTimeBetweenTest.java
@@ -38,7 +38,6 @@ class DateTimeBetweenTest {
     void valid(String date, ZonedDateTime before, ZonedDateTime after, boolean result) {
         Flow flow = TestsUtils.mockFlow();
         Execution execution = TestsUtils.mockExecution(flow, ImmutableMap.of());
-
         DateTimeBetween build = DateTimeBetween.builder()
             .date(Property.of(date))
             .date(date.startsWith("{{") ? Property.ofExpression(date) : Property.ofValue(date))

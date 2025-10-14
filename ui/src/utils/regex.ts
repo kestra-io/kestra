ce8ab957fc96395e97aa8c63b63d0e4206594ef6
brian.mulier <bmmulier@hotmail.fr>
2025-10-14 10:58:28 +0200

    fix(flows): pebble autocompletion performance optimization
    closes #11881

--- ui/src/utils/regex.ts
+++ ui/src/utils/regex.ts
@@ -1,23 +1,22 @@
 const maybeText = (allowSeparators: boolean) => "(?:\"[^\"]*\")|(?:'[^']*')|(?:(?:(?!\\}\\})" + (allowSeparators ? "[\\S\\n ]" : "[^~+,:\\n ]") + ")*)";
-const maybeAnotherPebbleExpression = "(?:[\\n ]*\\{\\{[\\n ]*" + maybeText(true) + "[\\n ]*\\}\\}[\\n ]*)*";
 const pebbleStart = "\\{\\{[\\n ]*";
-const fieldWithoutDotCapture = "([^\\(\\)\\}:~+.\\n '\"]*)(?![^\\(\\)\\}\\n ])";
-const dotAccessedFieldWithParentCapture = "([^\\(\\)\\}:~+\\n '\"]*)\\." + fieldWithoutDotCapture;
-const maybeTextFollowedBySeparator = "(?:" + maybeText(false) + "[~+ ]+)*";
-const paramKey = "[^\\n \\(\\)~+\\},:=]+";
-const paramValue = "(?:(?:(?:\"[^\"]*\"?)|(?:'[^']*'?)|[^,)]))*";
+const fieldWithoutDotCapture = "([^()}:~+.\\n '\"]*)(?![^()}\\n ])";
+const dotAccessedFieldWithParentCapture = "([^()}:~+\\n '\"]*)\\." + fieldWithoutDotCapture;
+const maybeTextFollowedBySeparator = "(?:" + maybeText(true) + "[\\n ]*(?:(?:[~+]+)|(?:\\}\\}[\\n ]*" + pebbleStart + "))[\\n ]*)*";
+const paramKey = "[^\\n ()~+},:=]+";
+const paramValue = "(?:(?:(?:\"[^\"]*\"?)|(?:'[^']*'?)|[^,)}]))*";
 const maybeParams = "(" +
     "(?:[\\n ]*" + paramKey + "[\\n ]*=[\\n ]*" + paramValue + "(?:[\\n ]*,[\\n ]*)?)+)?" +
-    "([^\\n \\(\\)~+\\},:=]*)?";
-const functionWithMaybeParams = "([^\\n\\(\\)\\},:~ ]+)\\(" + maybeParams
+    "([^\\n ()~+},:=]*)?";
+const functionWithMaybeParams = "([^\\n()},:~ ]+)\\(" + maybeParams
 
 export default {
     beforeSeparator: (additionalSeparators: string[] = []) => `([^\\}:\\n ${additionalSeparators.join("")}]*)`,
     /** [fullMatch, dotForbiddenField] */
-    capturePebbleVarRoot: `${maybeAnotherPebbleExpression}${pebbleStart}${maybeTextFollowedBySeparator}${fieldWithoutDotCapture}`,
+    capturePebbleVarRoot: `${pebbleStart}${maybeTextFollowedBySeparator}${fieldWithoutDotCapture}`,
     /** [fullMatch, parentFieldMaybeIncludingDots, childField] */
-    capturePebbleVarParent: `${maybeAnotherPebbleExpression}${pebbleStart}${maybeTextFollowedBySeparator}${dotAccessedFieldWithParentCapture}`,
+    capturePebbleVarParent: `${pebbleStart}${maybeTextFollowedBySeparator}${dotAccessedFieldWithParentCapture}`,
     /** [fullMatch, functionName, textBetweenParenthesis, maybeTypedWordStart] */
-    capturePebbleFunction: `${maybeAnotherPebbleExpression}${pebbleStart}${maybeTextFollowedBySeparator}${functionWithMaybeParams}`,
+    capturePebbleFunction: `${pebbleStart}${maybeTextFollowedBySeparator}${functionWithMaybeParams}`,
     captureStringValue: "^[\"']([^\"']+)[\"']$"
 }

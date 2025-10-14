43e1b5e17685d18074ff9521b9b6e183ddaf11bc
MilosPaunovic <paun992@hotmail.com>
2025-10-14 11:23:43 +0200

    chore: add plugin count component to prevent console warnings

---
+++ ui/src/components/docs/PluginCount.vue
@@ -0,0 +1,8 @@
+<template>
+    <span>{{ totalPlugins }}</span>
+</template>
+
+<script setup lang="ts">
+    // eslint-disable-next-line no-undef
+    const {totalPlugins} = usePluginsCount()
+</script>

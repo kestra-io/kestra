d2087a83e3088725f1a220b074617134f2b86d5b
Loïc Mathieu <loikeseke@gmail.com>
2025-10-15 10:34:52 +0200

    feat(flows): remove deprecated flow listeners
    Part-of: https://github.com/kestra-io/kestra-ee/issues/3238

--- core/src/test/java/io/kestra/core/runners/AbstractRunnerTest.java
+++ core/src/test/java/io/kestra/core/runners/AbstractRunnerTest.java
@@ -596,10 +596,4 @@ public abstract class AbstractRunnerTest {
     public void shouldCallTasksAfterError(Execution execution) {
         afterExecutionTestCase.shouldCallTasksAfterError(execution);
     }
-
-    @Test
-    @ExecuteFlow("flows/valids/after-execution-listener.yaml")
-    public void shouldCallTasksAfterListener(Execution execution) {
-        afterExecutionTestCase.shouldCallTasksAfterListener(execution);
-    }
 }

3a8286058f1f7e09df8af5d0ca0d6af26b8157b1
Loïc Mathieu <loikeseke@gmail.com>
2025-10-15 11:14:08 +0200

    feat(flows): remove deprecated ENUM inputs
    Part-of: https://github.com/kestra-io/kestra-ee/issues/3238

--- webserver/src/test/java/io/kestra/webserver/controllers/api/ExecutionControllerTest.java
+++ webserver/src/test/java/io/kestra/webserver/controllers/api/ExecutionControllerTest.java
@@ -96,7 +96,6 @@ class ExecutionControllerTest {
 
         return MultipartBody.builder()
             .addPart("string", "myString")
-            .addPart("enum", "ENUM_VALUE")
             .addPart("int", "42")
             .addPart("float", "42.42")
             .addPart("instant", "2019-10-06T18:27:49Z")

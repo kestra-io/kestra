84a1cc44ad7a84b70baa1141c561361de0d98234
mustafatarek <mustafatarek412@gmail.com>
2025-10-15 14:58:31 +0300

    refactor(tests): remove added code at ConditionService and transfer to tests directly

--- core/src/test/java/io/kestra/plugin/core/condition/PublicHolidayTest.java
+++ core/src/test/java/io/kestra/plugin/core/condition/PublicHolidayTest.java
@@ -1,9 +1,11 @@
 package io.kestra.plugin.core.condition;
 
 import com.google.common.collect.ImmutableMap;
+import io.kestra.core.models.conditions.ConditionContext;
 import io.kestra.core.models.executions.Execution;
 import io.kestra.core.models.flows.Flow;
 import io.kestra.core.models.property.Property;
+import io.kestra.core.runners.RunContextFactory;
 import io.kestra.core.services.ConditionService;
 import io.kestra.core.utils.TestsUtils;
 import io.kestra.core.junit.annotations.KestraTest;
@@ -11,12 +13,17 @@ import jakarta.inject.Inject;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @KestraTest
 class PublicHolidayTest {
     @Inject
     ConditionService conditionService;
+    @Inject
+    private RunContextFactory runContextFactory;
 
     @Test
     void valid() {
@@ -55,6 +62,44 @@ class PublicHolidayTest {
         assertThat(conditionService.isValid(publicHoliday, flow, execution)).isFalse();
     }
 
+    @Test
+    void validWithDynamicRender() {
+        Flow flow = TestsUtils.mockFlow();
+
+        Map<String, Object> variables = Map.of(
+            "trigger", Map.of("date", "2023-07-14")
+        );
+        Execution execution = TestsUtils.mockExecution(flow, ImmutableMap.of());
+        PublicHoliday publicHoliday = PublicHoliday.builder()
+            .country(Property.ofValue("FR"))
+            .build();
+       ConditionContext conditionContext= ConditionContext.builder()
+            .flow(flow)
+            .execution(execution)
+            .runContext(runContextFactory.of(flow,execution))
+            .variables(variables)
+            .build();
+        assertThat(conditionService.valid(flow, Collections.singletonList(publicHoliday), conditionContext)).isTrue();
+    }
+    @Test
+    void invalidWithDynamicRender() {
+        Flow flow = TestsUtils.mockFlow();
+
+        Map<String, Object> variables = Map.of(
+            "trigger", Map.of("date", "2023-01-02")
+        );
+        Execution execution = TestsUtils.mockExecution(flow, ImmutableMap.of());
+        PublicHoliday publicHoliday = PublicHoliday.builder()
+            .country(Property.ofValue("FR"))
+            .build();
+        ConditionContext conditionContext= ConditionContext.builder()
+            .flow(flow)
+            .execution(execution)
+            .runContext(runContextFactory.of(flow,execution))
+            .variables(variables)
+            .build();
+        assertThat(conditionService.valid(flow, Collections.singletonList(publicHoliday), conditionContext)).isFalse();
+    }
     @Test
     @Disabled("Locale is not deterministic on CI")
     void disabled() {

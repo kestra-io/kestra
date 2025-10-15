d2432f822e88fea4920726a107c67e51a2ff6397
Loïc Mathieu <loikeseke@gmail.com>
2025-10-15 10:34:52 +0200

    feat(flows): remove deprecated flow listeners
    Part-of: https://github.com/kestra-io/kestra-ee/issues/3238

--- core/src/main/java/io/kestra/core/models/flows/Flow.java
+++ core/src/main/java/io/kestra/core/models/flows/Flow.java
@@ -12,7 +12,6 @@ import io.kestra.core.exceptions.InternalException;
 import io.kestra.core.models.HasUID;
 import io.kestra.core.models.annotations.PluginProperty;
 import io.kestra.core.models.flows.sla.SLA;
-import io.kestra.core.models.listeners.Listener;
 import io.kestra.core.models.tasks.FlowableTask;
 import io.kestra.core.models.tasks.Task;
 import io.kestra.core.models.tasks.retrys.AbstractRetry;
@@ -86,10 +85,6 @@ public class Flow extends AbstractFlow implements HasUID {
     }
 
     @Valid
-    @Deprecated
-    List<Listener> listeners;
-
-    @Valid
     List<Task> afterExecution;
 
     @Valid
@@ -144,7 +139,7 @@ public class Flow extends AbstractFlow implements HasUID {
                 this.tasks != null ? this.tasks : Collections.<Task>emptyList(),
                 this.errors != null ? this.errors : Collections.<Task>emptyList(),
                 this._finally != null ? this._finally : Collections.<Task>emptyList(),
-                this.afterExecutionTasks()
+                this.afterExecution != null ? this.afterExecution : Collections.<Task>emptyList()
             )
             .flatMap(Collection::stream);
     }
@@ -285,13 +280,6 @@ public class Flow extends AbstractFlow implements HasUID {
         } else {
             return object;
         }
-    }
-
-    private List<Task> afterExecutionTasks() {
-        return ListUtils.concat(
-            ListUtils.emptyOnNull(this.getListeners()).stream().flatMap(listener -> listener.getTasks().stream()).toList(),
-            this.getAfterExecution()
-        );
     }
 
     public boolean equalsWithoutRevision(FlowInterface o) {

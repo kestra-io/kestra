02219abd28de6b3052439b0167c374c57c9b6006
Zaki-Mohd <zakilol21427@gmail.com>
2025-10-16 21:49:02 +0530

    refractor(ui)

--- ui/src/components/flows/FlowCreate.vue
+++ ui/src/components/flows/FlowCreate.vue
@@ -5,82 +5,78 @@
     </section>
 </template>
 
-<script>
-    import {mapStores} from "pinia";
+<script setup lang="ts">
+    import {computed, onMounted, onBeforeUnmount} from "vue";
+    import {useRoute, onBeforeRouteLeave} from "vue-router";
+    import {useI18n} from "vue-i18n";
     import * as YAML_UTILS from "@kestra-io/ui-libs/flow-yaml-utils";
-    import RouteContext from "../../mixins/routeContext";
     import TopNavBar from "../../components/layout/TopNavBar.vue";
     import MultiPanelFlowEditorView from "./MultiPanelFlowEditorView.vue";
     import {useBlueprintsStore} from "../../stores/blueprints";
     import {useCoreStore} from "../../stores/core";
     import {editorViewTypes} from "../../utils/constants";
-
     import {getRandomID} from "../../../scripts/id";
     import {useEditorStore} from "../../stores/editor";
     import {useFlowStore} from "../../stores/flow";
     import {defaultNamespace} from "../../composables/useNamespaces";
+    import {useTour} from "vue-tour";
 
-    export default {
-        mixins: [RouteContext],
-        components: {
-            MultiPanelFlowEditorView,
-            TopNavBar
-        },
+    const route = useRoute();
+    const {t} = useI18n();
+    const tour = useTour("guidedTour");
 
-        created() {
-            this.flowStore.isCreating = true;
-            if (this.$route.query.reset) {
-                localStorage.setItem("tourDoneOrSkip", undefined);
-                this.coreStore.guidedProperties = {...this.coreStore.guidedProperties, tourStarted: true};
-                this.$tours["guidedTour"]?.start();
-            }
-            this.setupFlow()
-            this.editorStore.closeAllTabs()
-        },
-        beforeUnmount() {
-            this.flowStore.flowValidation = undefined;
-        },
-        methods: {
-            async setupFlow() {
-                const blueprintId = this.$route.query.blueprintId;
-                const blueprintSource = this.$route.query.blueprintSource;
-                let flowYaml = ""
-                if (this.$route.query.copy && this.flowStore.flow){
-                    flowYaml = this.flowStore.flow.source;
-                } else if (blueprintId && blueprintSource) {
-                    flowYaml = await this.blueprintsStore.getBlueprintSource({type: blueprintSource, kind: "flow", id: blueprintId});
-                } else {
-                    const selectedNamespace = this.$route.query.namespace || defaultNamespace() || "company.team";
-                    flowYaml = `id: ${getRandomID()}
-namespace: ${selectedNamespace}
+    const blueprintsStore = useBlueprintsStore();
+    const coreStore = useCoreStore();
+    const editorStore = useEditorStore();
+    const flowStore = useFlowStore();
 
-tasks:
-  - id: hello
-    type: io.kestra.plugin.core.log.Log
-    message: Hello World! 🚀`;
-                }
+    const setupFlow = async () => {
+        const blueprintId = route.query.blueprintId as string;
+        const blueprintSource = route.query.blueprintSource as string;
+        let flowYaml = "";
 
-                this.flowStore.flowYaml = flowYaml;
-                this.flowStore.flowYamlBeforeAdd = flowYaml;
-
-                this.flowStore.flow = {...YAML_UTILS.parse(this.flowYaml), source: this.flowStore.flowYaml};
-                this.flowStore.initYamlSource({viewTypes: editorViewTypes.SOURCE_DOC});
-            }
-        },
-        computed: {
-            ...mapStores(useBlueprintsStore, useCoreStore, useEditorStore, useFlowStore),
-            routeInfo() {
-                return {
-                    title: this.$t("flows")
-                };
-            },
-            flowParsed() {
-                return YAML_UTILS.parse(this.source);
-            }
-        },
-        beforeRouteLeave(to, from, next) {
-            this.flowStore.flow = undefined;
-            next();
+        if (route.query.copy && flowStore.flow) {
+            flowYaml = flowStore.flow.source;
+        } else if (blueprintId && blueprintSource) {
+            flowYaml = await blueprintsStore.getBlueprintSource({
+                type: blueprintSource,
+                kind: "flow",
+                id: blueprintId
+            });
+        } else {
+            const selectedNamespace = (route.query.namespace as string) || defaultNamespace() || "company.team";
+            flowYaml = `id: ${getRandomID()}\nnamespace: ${selectedNamespace}\n\ntasks:\n  - id: hello\n    type: io.kestra.plugin.core.log.Log\n    message: Hello World! 🚀`;
         }
+
+        flowStore.flowYaml = flowYaml;
+        flowStore.flowYamlBeforeAdd = flowYaml;
+
+        flowStore.flow = {...YAML_UTILS.parse(flowStore.flowYaml), source: flowStore.flowYaml};
+        flowStore.initYamlSource({viewTypes: editorViewTypes.SOURCE_DOC});
     };
+
+    const routeInfo = computed(() => {
+        return {
+            title: t("flows")
+        };
+    });
+
+    onMounted(() => {
+        flowStore.isCreating = true;
+        if (route.query.reset) {
+            localStorage.setItem("tourDoneOrSkip", "");
+            coreStore.guidedProperties = {...coreStore.guidedProperties, tourStarted: true};
+            tour.start();
+        }
+        setupFlow();
+        editorStore.closeAllTabs();
+    });
+
+    onBeforeUnmount(() => {
+        flowStore.flowValidation = undefined;
+    });
+
+    onBeforeRouteLeave(() => {
+        flowStore.flow = undefined;
+    });
 </script>

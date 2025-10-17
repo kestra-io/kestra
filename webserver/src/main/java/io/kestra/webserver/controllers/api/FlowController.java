380be4252315561414ac80fffcaf9e8f545a8543
YannC. <ycoornaert@kestra.io>
2025-10-17 10:18:37 +0200

    fix: validateTask & validateTrigger endpoint changes for SDK

--- webserver/src/main/java/io/kestra/webserver/controllers/api/FlowController.java
+++ webserver/src/main/java/io/kestra/webserver/controllers/api/FlowController.java
@@ -618,12 +618,13 @@ public class FlowController {
     @Post(uri = "/validate/task", consumes = MediaType.APPLICATION_JSON)
     @Operation(tags = {"Flows"}, summary = "Validate task")
     public ValidateConstraintViolation validateTask(
-        @RequestBody(description = "The task") @Schema(implementation = Object.class) @Body Task task
+        @RequestBody(description = "The task") @Schema(implementation = Object.class) @Body String task
     ) {
         ValidateConstraintViolation.ValidateConstraintViolationBuilder<?, ?> validateConstraintViolationBuilder = ValidateConstraintViolation.builder();
 
         try {
-            modelValidator.validate(task);
+            var parsedTask = parseTaskTrigger(task, Task.class);
+            modelValidator.validate(parsedTask);
         } catch (ConstraintViolationException e) {
             validateConstraintViolationBuilder.constraints(e.getMessage());
         } catch (RuntimeException re) {
@@ -642,12 +643,13 @@ public class FlowController {
     @Post(uri = "/validate/trigger", consumes = MediaType.APPLICATION_JSON)
     @Operation(tags = {"Flows"}, summary = "Validate trigger")
     public ValidateConstraintViolation validateTrigger(
-        @RequestBody(description = "The trigger") @Schema(implementation = Object.class) @Body AbstractTrigger trigger
+        @RequestBody(description = "The trigger") @Schema(implementation = Object.class) @Body String trigger
     ) {
         ValidateConstraintViolation.ValidateConstraintViolationBuilder<?, ?> validateConstraintViolationBuilder = ValidateConstraintViolation.builder();
 
         try {
-            modelValidator.validate(trigger);
+            var parsedTrigger = parseTaskTrigger(trigger, AbstractTrigger.class);
+            modelValidator.validate(parsedTrigger);
         } catch (ConstraintViolationException e) {
             validateConstraintViolationBuilder.constraints(e.getMessage());
         } catch (RuntimeException re) {

00f0134c2ac937c56dbd91191871d2b2a89bc92a
mohammadshahidbeigh <mohammadshahidbeigh@gmail.com>
2025-10-18 12:51:26 +0530

    Reverted monaco-editor.d.ts to original state

--- ui/src/monaco-editor.d.ts
+++ ui/src/monaco-editor.d.ts
@@ -1,8 +1,3 @@
-declare module "*.vue" {
-    import {DefineComponent} from "vue";
-    const component: DefineComponent<object, object, any>;
-    export default component;
-}
 declare module "monaco-editor/esm/vs/editor/common/services/languageFeatures" {
     export {ILanguageFeaturesService} from "monaco-editor/esm/vs/editor/common/services/languageFeatures";
 }

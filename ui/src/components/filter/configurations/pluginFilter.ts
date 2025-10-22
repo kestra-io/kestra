ddd2f7bad2dc1e0679190451a205924a281cc1dd
Piyush Bhaskar <impiyush0012@gmail.com>
2025-10-22 18:41:49 +0530

    fix count

--- ui/src/components/filter/configurations/pluginFilter.ts
+++ ui/src/components/filter/configurations/pluginFilter.ts
@@ -1,12 +1,12 @@
 import {FilterConfiguration} from "../../../components/filter/utils/filterTypes";
 import {useI18n} from "vue-i18n";
 
-export const pluginFilter = (count: number): FilterConfiguration => {
+export const pluginFilter = (): FilterConfiguration => {
     const {t} = useI18n();
 
     return {
         title: t("filter.titles.plugin_filters"),
-        searchPlaceholder: t("filter.search_placeholders.search_plugins", {count}),
+        searchPlaceholder: t("filter.search_placeholders.search_plugins", {count: 900}),
         keys: [],
     };
 };

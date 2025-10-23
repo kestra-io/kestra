fd7213e3474e3926914add5239609b299697132d
Loïc Mathieu <loikeseke@gmail.com>
2025-10-23 17:27:11 +0200

    feat(flows): remove deprecated Schedule.scheduleConditions
    Part-of: https://github.com/kestra-io/kestra-ee/issues/3238

--- core/src/test/java/io/kestra/core/docs/JsonSchemaGeneratorTest.java
+++ core/src/test/java/io/kestra/core/docs/JsonSchemaGeneratorTest.java
@@ -326,7 +326,7 @@ class JsonSchemaGeneratorTest {
     void pluginSchemaShouldNotResolveTaskAndTriggerSubtypes() {
         Map<String, Object> generate = jsonSchemaGenerator.properties(null, TaskWithSubTaskAndSubTrigger.class);
         var definitions = (Map<String, Map<String, Object>>) generate.get("$defs");
-        assertThat(definitions.size(), is(27));
+        assertThat(definitions.size(), is(22));
     }
 
     @SuppressWarnings("unchecked")

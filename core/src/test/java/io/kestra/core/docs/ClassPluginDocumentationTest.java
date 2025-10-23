b8f9fe918085714495583d9173e91cefb53a9f34
Loïc Mathieu <loikeseke@gmail.com>
2025-10-23 17:27:11 +0200

    feat(flows): remove deprecated Schedule.scheduleConditions
    Part-of: https://github.com/kestra-io/kestra-ee/issues/3238

--- core/src/test/java/io/kestra/core/docs/ClassPluginDocumentationTest.java
+++ core/src/test/java/io/kestra/core/docs/ClassPluginDocumentationTest.java
@@ -103,7 +103,7 @@ class ClassPluginDocumentationTest {
             PluginClassAndMetadata<AbstractTrigger> metadata = PluginClassAndMetadata.create(scan, Schedule.class, AbstractTrigger.class, null);
             ClassPluginDocumentation<? extends AbstractTrigger> doc = ClassPluginDocumentation.of(jsonSchemaGenerator, metadata, scan.version(), true);
 
-            assertThat(doc.getDefs().size()).isEqualTo(20);
+            assertThat(doc.getDefs().size()).isEqualTo(16);
             assertThat(doc.getDocLicense()).isNull();
 
             assertThat(((Map<String, Object>) doc.getDefs().get("io.kestra.core.models.tasks.WorkerGroup")).get("type")).isEqualTo("object");

8757b5caaeb065618310a7ddb8cc2dd489ba20a7
Loïc Mathieu <loikeseke@gmail.com>
2025-10-23 17:27:11 +0200

    feat(flows): remove deprecated Schedule.scheduleConditions
    Part-of: https://github.com/kestra-io/kestra-ee/issues/3238

--- core/src/main/java/io/kestra/core/models/conditions/ScheduleCondition.java
+++ core/src/main/java/io/kestra/core/models/conditions/ScheduleCondition.java
@@ -2,7 +2,11 @@ package io.kestra.core.models.conditions;
 
 import io.kestra.core.exceptions.InternalException;
 
-
+/**
+ * Conditions of type ScheduleCondition have a special behavior inside the {@link io.kestra.plugin.core.trigger.Schedule} trigger.
+ * They are evaluated specifically and would be taken into account when computing the next evaluation date.
+ * Only conditions based on date should be marked as ScheduleCondition.
+ */
 public interface ScheduleCondition {
     boolean test(ConditionContext conditionContext) throws InternalException;
 }

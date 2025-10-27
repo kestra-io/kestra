6b433b3814eefc4f6f76aa9e9656302d1b93e328
Piyush Bhaskar <impiyush0012@gmail.com>
2025-10-27 19:29:21 +0530

    fix: new no executions page

--- ui/src/components/flows/NoExecutions.vue
+++ ui/src/components/flows/NoExecutions.vue
@@ -1,168 +1,97 @@
 <template>
-    <div class="main">
-        <div class="section-1">
-            <div class="section-1-main">
-                <div class="section-content">
-                    <img
-                        :src="logo"
-                        alt="Kestra"
-                        class="section-1-img img-fluid"
-                        width="200px"
-                    >
-                    <h2 class="section-1-title">
-                        {{ $t("no-executions-view.title") }} <span class="highlight">Kestra</span>
-                    </h2>
-                    <p class="section-1-desc">
-                        {{ $t("no-executions-view.sub_title") }}
-                    </p>
-                    <div v-if="flow && !flow.deleted" class="trigger-wrapper">
-                        <TriggerFlow
-                            type="primary"
-                            :disabled="flow.disabled"
-                            :flowId="flow.id"
-                            :namespace="flow.namespace"
-                            :flowSource="flow.source"
-                        />
-                    </div>
-                </div>
-                <el-divider>
-                    {{ $t("welcome_page.guide") }}
-                </el-divider>
-                <OverviewBottom />
+    <EmptyTemplate>
+        <div class="content">
+            <img
+                :src="noexecutionimg"
+                alt="Kestra"
+                class="logo img-fluid"
+            >
+            <h2 class="title">
+                {{ $t("no-executions-view.title") }}
+            </h2>
+            <p class="desc">
+                {{ $t("no-executions-view.sub_title") }}
+            </p>
+            <div v-if="flow && !flow.deleted" class="trigger">
+                <TriggerFlow
+                    type="primary"
+                    :disabled="flow.disabled"
+                    :flowId="flow.id"
+                    :namespace="flow.namespace"
+                    :flowSource="flow.source"
+                />
             </div>
         </div>
-    </div>
+        <el-divider>
+            {{ $t("welcome_page.guide") }}
+        </el-divider>
+        <OverviewBottom class="bottom" />
+    </EmptyTemplate>
 </template>
 <script setup lang="ts">
     import {computed} from "vue"
-    import TriggerFlow from "../flows/TriggerFlow.vue"
     import OverviewBottom from "../onboarding/execution/OverviewBottom.vue";
+    import EmptyTemplate from "../layout/EmptyTemplate.vue";
     import noexecutionimg from "../../assets/onboarding/noexecution.svg"
     import {useFlowStore} from "../../stores/flow"
-    interface Props {
-        topbar?: boolean
-    }
+    //@ts-expect-error no declaration file
+    import TriggerFlow from "../flows/TriggerFlow.vue"
 
-    withDefaults(defineProps<Props>(), {
+    withDefaults(defineProps<{topbar?: boolean}>(), {
         topbar: true,
     })
 
     const flowStore = useFlowStore();
     const flow = computed(() => flowStore.flow)
-    const logo = computed(() => noexecutionimg)
-    // const logoDark = computed(() => noexecutionimgDark)
 </script>
 
 <style scoped lang="scss">
-    .main {
-        padding: 5rem 1rem 1rem;
-        background: var(--ks-background-body);
-        transform: translateY(-40px);
-        background: radial-gradient(ellipse at top, rgba(102,51,255,0.6) 0%, rgba(253, 253, 253, 0) 20%);
-        background-size: 4000px;
-        background-position: center;
-        height: 100%;
-        width: auto;
-        display: flex;
-        flex-direction: column;
-        container-type: inline-size;
+.content {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    max-width: 434px;
+    margin: 4rem auto;
 
-        @media (min-width: 768px) {
-            padding: 5rem 2rem 1rem;
-        }
-
-        @media (min-width: 992px) {
-            padding: 5rem 3rem 1rem;
-        }
-
-        @media (min-width: 1920px) {
-            padding: 5rem 10rem 1rem;
-        }
+    .title {
+        line-height: var(--el-font-line-height-primary);
+        text-align: center;
+        font-size: var(--el-font-size-extra-large);
+        font-weight: 600;
+        color: var(--ks-content-primary);
     }
 
-    .trigger-wrapper {
-        margin-top: 0.5rem;
-        margin-bottom: 2rem;
-        display: flex;
-        justify-content: center;
-    }
-     .highlight {
-                    color: var(--ks-content-link);
-                }
-
-    .product-link, .watch {
-        font-weight: 700;
-        border-radius: 5px;
-        text-decoration: none;
-        font-size: var(--el-font-size-small);
-        width: 200px;
-        margin: 0;
-        margin-bottom: 1rem;
+    .desc {
+        line-height: var(--el-font-line-height-primary);
+        font-weight: 300;
+        font-size: 16px;
+        line-height: 28px;
+        text-align: center;
+        color: var(--ks-content-primary);
     }
 
-    .watch {
-        font-weight: 500;
-        background-color: var(--ks-button-background-secondary);
-        font-size: var(--el-font-size-small);
-    }
-
-    .main .section-1 {
-        display: flex;
-        flex-grow: 1;
-        justify-content: center;
-        align-items: center;
-        border-radius: var(--bs-border-radius);
-    }
-    .section-1-main {
-        .section-content {
-            width: 100%;
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            margin-bottom: 2rem;
-            .section-1-img {
-                max-width: 120%;
-                height: auto;
-                object-fit: contain;
-            }
-            .section-1-title {
-                line-height: var(--el-font-line-height-primary);
-                text-align: center;
-                font-size: var(--el-font-size-extra-large);
-                font-weight: 600;
-                color: var(--ks-content-primary);
-            }
-
-            .section-1-desc {
-                line-height: var(--el-font-line-height-primary);
-                font-weight: 300;
-                font-size: 0.875rem;
-                text-align: center;
-                color: var(--ks-content-primary);
-            }
+    .trigger {
+        :deep(.el-button) {
+            width: 226px;
+            height: 45px;
         }
     }
+}
 
-    :deep(.el-divider__text) {
-        color: var(--ks-content-secondary);
-        white-space: nowrap;
-        font-size: var(--el-font-size-extra-small);
-    }
+:deep(.el-divider) {
+    max-width: 746px;
+    margin: 0 auto;
+}
 
-    @container (max-width: 20px) {
-        .main .section-1 .section-1-main {
-            width: 90%;
-        }
-    }
+:deep(.el-divider__text) {
+    color: var(--ks-content-secondary);
+    white-space: nowrap;
+    font-size: var(--el-font-size-extra-small);
+}
 
-    @container (max-width: 50px) {
-        .main .section-1 .section-1-main {
-            padding-top: 30px;
-        }
-
-        .section-1 .section-1-main .container {
-            width: 76%;
-        }
-    }
-
+.bottom {
+    max-width: 746px;
+    margin: 2rem auto;
+}
 </style>
--- ui/src/components/onboarding/execution/OverviewCard.vue
+++ ui/src/components/onboarding/execution/OverviewCard.vue
@@ -1,147 +1,90 @@
 <template>
-    <el-card class="box-card">
-        <div class="card-content">
-            <div class="">
-                <div class="card-header">
-                    <el-link underline="never" :href="getLink()" target="_blank">
-                        <el-icon class="el-icon--right">
-                            <OpenInNew />
-                        </el-icon>
-                    </el-link>
+    <el-card class="card">
+        <div v-if="showIcon" class="header">
+            <el-link underline="never" :href="link" target="_blank">
+                <el-icon class="el-icon--right">
+                    <OpenInNew />
+                </el-icon>
+            </el-link>
+        </div>
+        <div class="icon-row">
+            <el-icon :size="24">
+                <component :is="icon" />
+            </el-icon>
+            <div>
+                <h5 class="title">
+                    {{ title }}
+                </h5>
+                <div class="desc">
+                    <Markdown :source="$t(`execution_guide.${category}.text`)" />
                 </div>
-                <div class="icon-title">
-                    <el-icon size="25px">
-                        <component :is="getIcon()" />
-                    </el-icon>
-                    <div class="card">
-                        <h5 class="cat_title">
-                            {{ title }}
-                        </h5>
-                        <div class="cat_description">
-                            <Markdown :source="$t(`execution_guide.${category}.text`)" />
-                        </div>
-                    </div>
-                </div>
-                <!-- <div>
-                    <h5 class="overview_cat_title">
-                        {{ title }}
-                    </h5>
-                    <div>
-                        <Markdown :source="$t(`execution_guide.${category}.text`)" />
-                    </div>
-                    <el-link underline="never" :href="getLink()" target="_blank">
-                        {{ category === 'videos_tutorials' ? $t('watch') : $t('learn_more') }}
-                        <el-icon class="el-icon--right">
-                            <OpenInNew />
-                        </el-icon>
-                    </el-link>
-                </div> -->
             </div>
         </div>
     </el-card>
 </template>
 
-<script setup>
+<script setup lang="ts">
     import OpenInNew from "vue-material-design-icons/OpenInNew.vue";
-</script>
-
-<script>
     import Markdown from "../../layout/Markdown.vue";
-    import mdiVideoInputComponent from "vue-material-design-icons/videoInputComponent.vue";
-    import mdiRocketLaunchOutline from "vue-material-design-icons/rocketLaunchOutline.vue";
-    import mdiPlayBoxMultiple from "vue-material-design-icons/playBoxMultiple.vue";
 
-    export default {
-        name: "OverviewCard",
-        components: {Markdown},
-        props: {
-            title: {
-                type: String,
-                required: true,
-            },
-            category: {
-                type: String,
-                required: true,
-            },
-            content: {
-                type: String,
-                required: true,
-            },
-            link: {
-                type: String,
-                required: true,
-            },
-        },
-        methods: {
-            getIcon() {
-                return {
-                    videos_tutorials: mdiRocketLaunchOutline ,
-                    workflow_components: mdiVideoInputComponent,
-                    get_started: mdiPlayBoxMultiple,
-                }[this.category] ||mdiRocketLaunchOutline ;
-            },
-            getLink() {
-                const links = {
-                    videos_tutorials: "https://www.youtube.com/watch?v=6TqWWz9difM",
-                    workflow_components: "https://kestra.io/docs/workflow-components",
-                    get_started: "https://kestra.io/docs/getting-started/quickstart",
-                };
-                return links[this.category] || "#"; // Default to "#" if no link is found
-            },
-        },
-    };
+    defineProps<{
+        title: string;
+        category: string;
+        content?: string;
+        link?: string;
+        icon?: any;
+        showIcon?: boolean;
+    }>();
 </script>
 
 <style scoped lang="scss">
-a:hover {
-    text-decoration: none;
-}
-
 .el-card {
     background-color: var(--ks-background-card);
     border-color: var(--ks-border-primary);
-    box-shadow: var(--el-box-shadow);
+    box-shadow: 0px 2px 4px 0px var(--ks-card-shadow);
     position: relative;
-    min-width: 250px;
-    flex: 1;
+    min-width: 100%;
+    min-height: 8.625rem;
+    border-radius: 0.5rem;
     cursor: pointer;
-
-    &:deep(.el-card__header) {
-        padding: 0;
-    }
+    text-align: left;
 }
 
-.box-card {
-    .card-header {
+.card {
+    .header {
         position: absolute;
-        top: 5px;
-        right: 5px;
+        top: 0.3125rem;
+        right: 0.3125rem;
+
+        :deep(.el-icon) {
+            color: var(--ks-content-secondary);
+            font-size: 1rem;
+            position: absolute;
+            top: -0.875rem;
+            right: 0;
+
+            &:hover {
+                color: var(--ks-content-tertiary);
+            }
+        }
     }
 
-    .cat_title {
-        width: 100%;
-        margin: 3px 0 10px;
-        padding-left: 20px;
-        font-weight: 600;
-        font-size: var(--el-font-size-small);
+    .title {
+        font-weight: 700;
+        font-size: 0.875rem;
+        line-height: 1.375rem;
     }
 
-    .cat_description {
-        width: 100%;
+    .desc {
         margin: 0;
-        padding-left: 20px;
+        font-size: 0.75rem;
+        line-height: 1.25rem;
+        color: var(--ks-content-secondary);
     }
 }
 
-.icon-title {
+.icon-row {
     display: inline-flex;
-
-    &.icon-title-left {
-        margin-right: 10px;
-    }
-}
-
-.el-link {
-    font-size: 20px;
+    gap: 1rem;
 }
 </style>

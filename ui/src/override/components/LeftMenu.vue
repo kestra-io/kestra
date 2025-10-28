7ce468aef9fe7881d539a2d68f071a85450fbf82
Ashutosh Devgotra <ashutoshdevgotra@gmail.com>
2025-10-29 00:09:20 +0530

    LeftMenu.vue is converted to Typescript

--- ui/src/override/components/LeftMenu.vue
+++ ui/src/override/components/LeftMenu.vue
@@ -1,40 +1,46 @@
 <template>
-    <SideBar v-if="menu" :menu :showLink="showLink" @menu-collapse="onCollapse">
-        <template #footer>
-            <Auth />    
-        </template>
-    </SideBar>
+  <SideBar v-if="menu" :menu="menu" :showLink="showLink" @menu-collapse="onCollapse">
+    <template #footer>
+      <Auth />    
+    </template>
+  </SideBar>
 </template>
 
-<script lang="ts" setup>
-    import {useLeftMenu} from "override/components/useLeftMenu";
-    import SideBar from "../../components/layout/SideBar.vue";
-    import Auth from "../../override/components/auth/Auth.vue";
+<script setup lang="ts">
+import { useLeftMenu } from "override/components/useLeftMenu";
+import SideBar from "../../components/layout/SideBar.vue";
+import Auth from "../../override/components/auth/Auth.vue";
 
-    defineProps({showLink: {type: Boolean, default: true}})
+interface Props {
+  showLink?: boolean;
+}
 
-    const $emit = defineEmits(["menu-collapse"])
+const props = defineProps<Props>();
 
-    function onCollapse(folded: boolean) {
-        $emit("menu-collapse", folded);
-    }
+const emit = defineEmits<{
+  (event: "menu-collapse", folded: boolean): void;
+}>();
 
-    const {menu} = useLeftMenu();
+function onCollapse(folded: boolean): void {
+  emit("menu-collapse", folded);
+}
+
+const { menu } = useLeftMenu();
 </script>
 
 <style scoped lang="scss">
 #side-menu {
+  .el-select {
+    padding: 0 30px;
+    padding-bottom: 15px;
+    transition: all 0.2s ease;
+    background-color: transparent;
+  }
+  &.vsm_collapsed {
     .el-select {
-        padding: 0 30px;
-        padding-bottom: 15px;
-        transition: all 0.2s ease;
-        background-color: transparent;
+      padding-left: 5px;
+      padding-right: 5px;
     }
-    &.vsm_collapsed {
-        .el-select {
-            padding-left: 5px;
-            padding-right: 5px;
-        }
-    }
+  }
 }
 </style>

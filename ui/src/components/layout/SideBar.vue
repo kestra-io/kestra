b5608e08d80fc90940e5b4d4dcd5cc310ab38a0f
Piyush Bhaskar <impiyush0012@gmail.com>
2025-11-10 22:38:01 +0530

    refactor: minor tweak

--- ui/src/components/layout/SideBar.vue
+++ ui/src/components/layout/SideBar.vue
@@ -28,13 +28,7 @@
 </template>
 
 <script setup lang="ts">
-    import {
-        onUpdated,
-        ref,
-        computed,
-        h,
-        watch
-    } from "vue";
+    import {onUpdated, ref, computed, h, watch} from "vue";
     import {useI18n} from "vue-i18n";
     import {useRoute} from "vue-router";
     import {useMediaQuery} from "@vueuse/core";

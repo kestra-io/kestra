cb4ba5fe480db58eaebc5c1bc6e0d6fc8cbf19d8
Loïc Mathieu <loikeseke@gmail.com>
2025-11-10 16:05:11 +0100

    feat(core): remove Property deprecated methdso and constructors
    Part-of: https://github.com/kestra-io/kestra-ee/issues/3238

--- core/src/main/java/io/kestra/core/models/property/Property.java
+++ core/src/main/java/io/kestra/core/models/property/Property.java
@@ -54,12 +54,7 @@ public class Property<T> {
     private String expression;
     private T value;
 
-    /**
-     * @deprecated use {@link #ofExpression(String)} instead.
-     */
-    @Deprecated
-    // Note: when not used, this constructor would not be deleted but made private so it can only be used by ofExpression(String) and the deserializer
-    public Property(String expression) {
+    private Property(String expression) {
         this.expression = expression;
     }
 
@@ -121,14 +116,6 @@ public class Property<T> {
         Property<V> p = new Property<>(expression);
         p.value = value;
         return p;
-    }
-
-    /**
-     * @deprecated use {@link #ofValue(Object)} instead.
-     */
-    @Deprecated
-    public static <V> Property<V> of(V value) {
-        return ofValue(value);
     }
 
     /**

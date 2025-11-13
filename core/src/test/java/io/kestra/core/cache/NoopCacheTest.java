75f12cce871703a8893c2e703ce2cc85b07a9c29
YannC. <ycoornaert@kestra.io>
2025-11-13 14:26:30 +0100

    change

--- core/src/test/java/io/kestra/core/cache/NoopCacheTest.java
+++ core/src/test/java/io/kestra/core/cache/NoopCacheTest.java
@@ -14,7 +14,7 @@ class NoopCacheTest {
     private Cache<String, String> cache = new NoopCache<>();
 
     @Test
-    void getIfPresentChangeToBe() {
+    void getIfPresentChangeToBeremove() {
         cache.put("key", "value");
 
         assertThat(cache.getIfPresent("key")).isNull();

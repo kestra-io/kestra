88af056592509066818d1babc3f334e6ad6cecda
YannC. <ycoornaert@kestra.io>
2025-11-18 15:22:48 +0100

    fix: remove useless code that was generating duplicate taskrun

--- core/src/main/java/io/kestra/core/runners/FlowableUtils.java
+++ core/src/main/java/io/kestra/core/runners/FlowableUtils.java
@@ -157,8 +157,6 @@ public class FlowableUtils {
 
             if (currentTasks.size() > lastIndex + 1) {
                 return Collections.singletonList(currentTasks.get(lastIndex + 1).toNextTaskRunIncrementIteration(execution, parentTaskRun.getIteration()));
-            } else {
-                return Collections.singletonList(currentTasks.getFirst().toNextTaskRunIncrementIteration(execution, parentTaskRun.getIteration()));
             }
         }
 

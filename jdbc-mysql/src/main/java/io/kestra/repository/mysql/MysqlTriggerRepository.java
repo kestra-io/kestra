6733edb4a9676a8c258376d5aad34f0a50d749a8
Loïc Mathieu <loikeseke@gmail.com>
2025-11-19 18:26:16 +0100

    fix(system): compilation issue

--- jdbc-mysql/src/main/java/io/kestra/repository/mysql/MysqlTriggerRepository.java
+++ jdbc-mysql/src/main/java/io/kestra/repository/mysql/MysqlTriggerRepository.java
@@ -1,11 +1,9 @@
 package io.kestra.repository.mysql;
 
 import io.kestra.core.models.triggers.Trigger;
-import io.kestra.core.runners.ScheduleContextInterface;
 import io.kestra.core.queues.QueueService;
 import io.kestra.core.utils.DateUtils;
 import io.kestra.jdbc.repository.AbstractJdbcTriggerRepository;
-import io.kestra.jdbc.runner.JdbcSchedulerContext;
 import io.kestra.jdbc.services.JdbcFilterService;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
@@ -14,10 +12,6 @@ import org.jooq.Condition;
 import org.jooq.Field;
 import org.jooq.impl.DSL;
 
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
-import java.time.temporal.Temporal;
 import java.util.Date;
 import java.util.List;
 
@@ -39,12 +33,5 @@ public class MysqlTriggerRepository extends AbstractJdbcTriggerRepository {
     @Override
     protected Field<Date> formatDateField(String dateField, DateUtils.GroupType groupType) {
         return MysqlRepositoryUtils.formatDateField(dateField, groupType);
-    }
-
-    @Override
-    protected Temporal toNextExecutionTime(ZonedDateTime now) {
-        // next_execution_date in the table is stored in UTC
-        // convert 'now' to UTC LocalDateTime to avoid any timezone/offset interpretation by the database.
-        return now.withZoneSameInstant(ZoneOffset.UTC).toLocalDateTime();
     }
 }

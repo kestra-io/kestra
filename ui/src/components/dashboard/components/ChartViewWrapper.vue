b5c5540aabbe39ab92698ff04c2407d337991fb4
Vishnu Varthan <166686421+Vishnu-Varthan1@users.noreply.github.com>
2025-11-21 08:17:14 +0530

    fix(ui): simplify translations in ChartViewWrapper.vue by using global

--- ui/src/components/dashboard/components/ChartViewWrapper.vue
+++ ui/src/components/dashboard/components/ChartViewWrapper.vue
@@ -1,7 +1,5 @@
 <template>
-    <div
-        class="chart-view"
-    >
+    <div class="chart-view">
         <div v-if="dashboardStore.selectedChart" class="w-100">
             <Sections
                 :dashboard="{id: 'default', charts: [dashboardStore.selectedChart]}"
@@ -16,7 +14,7 @@
             <el-empty :image="EmptyVisualDashboard" :imageSize="200">
                 <template #description>
                     <h5>
-                        {{ t("dashboards.chart_preview") }}
+                        {{ $t("dashboards.chart_preview") }}
                     </h5>
                 </template>
             </el-empty>
@@ -25,12 +23,9 @@
 </template>
 
 <script lang="ts" setup>
-    import {useI18n} from "vue-i18n";
     import Sections from "../sections/Sections.vue";
     import EmptyVisualDashboard from "../../../assets/empty_visuals/Visuals_empty_dashboard.svg";
     import {useDashboardStore} from "../../../stores/dashboard";
-
-    const {t} = useI18n();
 
     const dashboardStore = useDashboardStore();
 </script>

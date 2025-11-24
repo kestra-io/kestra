9aa57900c01e8b382db9e30a724b02202bd9b435
MilosPaunovic <paun992@hotmail.com>
2025-11-24 12:12:57 +0100

    api component

---
+++ ui/src/components/executions/overview/components/actions/Api.vue
@@ -0,0 +1,25 @@
+<template>
+    <el-button
+        :icon="Api"
+        tag="a"
+        :href="`${apiUrl()}/executions/${props.execution.id}`"
+        target="_blank"
+        rel="noopener noreferrer"
+    >
+        {{ $t("api") }}
+    </el-button>
+
+    <a target="_blank">
+        <el-button :icon="Api" />
+    </a>
+</template>
+
+<script setup lang="ts">
+    import {apiUrl} from "override/utils/route";
+
+    import {Execution} from "../../../../../stores/executions";
+
+    import Api from "vue-material-design-icons/Api.vue";
+
+    const props = defineProps<{ execution: Execution }>();
+</script>

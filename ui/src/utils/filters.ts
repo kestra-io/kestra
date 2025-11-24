2122200f8a0d57b355a77e7c7745937a72f968bd
Yuvraj Karna <yuvraj.karna@iquadra.com>
2025-11-24 16:32:33 +0530

    fix(ui): remove getCurrentInstance from filters.ts and use direct moment instead

--- ui/src/utils/filters.ts
+++ ui/src/utils/filters.ts
@@ -1,5 +1,4 @@
 import Utils from "./utils";
-import {getCurrentInstance} from "vue";
 import {storageKeys} from "../utils/constants";
 import moment from "moment-timezone";
 
@@ -19,16 +18,13 @@ export function lower (value:string) {
     return value ? value.toString().toLowerCase() : "";
 }
 export function date (dateString:string, format?:string) {
-    const currentLocale = getCurrentInstance()?.appContext.config.globalProperties.$moment().locale();
-    const momentInstance = getCurrentInstance()?.appContext.config.globalProperties.$moment(dateString).locale(currentLocale);
     let f;
     if (format === "iso") {
         f = "YYYY-MM-DD HH:mm:ss.SSS";
     } else {
         f = format ?? localStorage.getItem(storageKeys.DATE_FORMAT_STORAGE_KEY) ?? "llll";
     }
-    // Apply timezone and format using the correct locale
-    return momentInstance
+    return moment(dateString)
         .tz(localStorage.getItem(storageKeys.TIMEZONE_STORAGE_KEY) ?? moment.tz.guess())
         .format(f);
 }

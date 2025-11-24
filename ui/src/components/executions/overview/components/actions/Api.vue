287e0f966f179d3edda469325f9b0b6c7f512816
MilosPaunovic <paun992@hotmail.com>
2025-11-24 12:18:23 +0100

    check if api could be shown

--- ui/src/components/executions/overview/components/actions/Api.vue
+++ ui/src/components/executions/overview/components/actions/Api.vue
@@ -1,5 +1,6 @@
 <template>
     <el-button
+        v-if="isAllowedEdit"
         :icon="Api"
         tag="a"
         :href="`${apiUrl()}/executions/${props.execution.id}`"
@@ -11,11 +12,28 @@
 </template>
 
 <script setup lang="ts">
+    import {computed} from "vue";
+
     import {apiUrl} from "override/utils/route";
 
     import {Execution} from "../../../../../stores/executions";
+    import {useAuthStore} from "override/stores/auth";
+
+    import permission from "../../../../../models/permission";
+    import action from "../../../../../models/action";
 
     import Api from "vue-material-design-icons/Api.vue";
 
     const props = defineProps<{ execution: Execution }>();
+
+    const isAllowedEdit = computed(() => {
+        return (
+            props.execution &&
+            useAuthStore().user?.isAllowed(
+                permission.FLOW,
+                action.UPDATE,
+                props.execution.namespace,
+            )
+        );
+    });
 </script>

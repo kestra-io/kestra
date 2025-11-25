ca523a09b505ce0f9fc88707de2580a64f2c1c7f
MilosPaunovic <paun992@hotmail.com>
2025-11-25 12:24:36 +0100

    chore: imrpved timeline

--- ui/src/components/executions/overview/components/sidebar/Timeline.vue
+++ ui/src/components/executions/overview/components/sidebar/Timeline.vue
@@ -3,7 +3,7 @@
         <el-timeline-item
             v-for="(entry, hIdx) in props.histories"
             :key="hIdx"
-            :timestamp="entry.date"
+            :timestamp="formatDate(entry.date)"
             :color="State.getStateColor(entry.state)"
         >
             {{ entry.state }}
@@ -15,6 +15,39 @@
     import type {Histories} from "../../../../../stores/executions";
 
     import {State} from "@kestra-io/ui-libs";
+    import {storageKeys} from "../../../../../utils/constants";
+
+    import moment from "moment";
 
     const props = defineProps<{ histories: Histories[] }>();
+
+    const F = localStorage.getItem(storageKeys.DATE_FORMAT_STORAGE_KEY) ?? "llll";
+    const formatDate = (date: string) => moment(date)?.format(F) ?? date;
 </script>
+
+<style scoped lang="scss">
+@import "@kestra-io/ui-libs/src/scss/variables";
+
+.el-timeline {
+    & :deep(.el-timeline-item) {
+        padding-bottom: $spacer;
+    }
+
+    & :deep(.el-timeline-item__content) {
+        font-size: $font-size-sm;
+        color: var(--ks-content-primary);
+    }
+
+    & :deep(.el-timeline-item__timestamp) {
+        margin-top: calc($spacer / 4);
+        color: var(--ks-content-tertiary);
+    }
+
+    & :deep(.el-timeline-item__tail) {
+        height: inherit;
+        top: 30%;
+        bottom: 10%;
+        border-left-width: 1px;
+    }
+}
+</style>

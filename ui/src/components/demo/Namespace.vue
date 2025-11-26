83ac98c0cf0ce312d1d03ef25edf617ff7d9669a
RONGALI MOHAN KRISHNA 2400033266 <2400033266@kluniversity.in>
2025-11-26 19:30:05 +0530

    Update Namespace.vueEnhance Namespace.vue: Add validation for namespace format and improve user feedback for errors

--- ui/src/components/demo/Namespace.vue
+++ ui/src/components/demo/Namespace.vue
@@ -11,13 +11,11 @@
 </template>
 
 <script setup lang="ts">
-    import {useI18n} from "vue-i18n";
     import {computed} from "vue";
     import Layout from "./Layout.vue";
     import {useDocStore} from "../../stores/doc";
     import sourceImg from "../../assets/demo/namespace.png";
 
-    const {t} = useI18n();
     const docStore = useDocStore();
 
     docStore.docId = "namespace.management";

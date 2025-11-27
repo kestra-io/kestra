fba6105ad87863fa0c6b4004c4f71cb330708ad3
Bart Ledoux <bledoux@kestra.io>
2025-11-27 15:12:20 +0100

    use element plus item to demo modularity

--- plugin-ui/src/components/Button.vue
+++ plugin-ui/src/components/Button.vue
@@ -10,19 +10,8 @@ const emit = defineEmits<{
 </script>
 
 <template>
- <button
-    style="
-      border: 0 solid #e2e8f0;
-      margin-top: 10px;
-      background-color: rgb(100, 149, 237);
-      border-radius: 0.25rem;
-      font-weight: 700;
-      padding: 0.5rem 1rem 0.5rem 1rem;
-      color: rgb(24, 24, 24);
-    "
-    @click="emit('click')"
-  >
-    <slot /> {{ label }} 
+  <el-button @click="emit('click')" type="secondary">
+    <slot /> {{ label }} <em>remote</em>
     <img :src="image" alt="Kestra Logo" style="width: 16px; height: 16px; margin-left: 5px;" />
-  </button>
+  </el-button>
 </template>

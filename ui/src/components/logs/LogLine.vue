1a1b85f678a921594419922d2375bc29c717e570
MilosPaunovic <paun992@hotmail.com>
2025-11-27 09:10:54 +0100

    chore: remove the log line width limitation

--- ui/src/components/logs/LogLine.vue
+++ ui/src/components/logs/LogLine.vue
@@ -206,7 +206,6 @@ div.line {
         display: inline-block;
         vertical-align: middle;
         /* prevent Firefox word breaks */
-        max-width: calc(100% - 6rem);
         overflow-wrap: anywhere;
         word-break: break-word;
         min-width: 0;

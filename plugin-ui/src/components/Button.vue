ff9261ea284001ff0a646546d4551bba9d3840b0
Bart Ledoux <bledoux@kestra.io>
2025-11-28 11:56:23 +0100

    fix build

--- plugin-ui/src/components/Button.vue
+++ plugin-ui/src/components/Button.vue
@@ -1,4 +1,7 @@
 <script lang="ts" setup>
+import { ref } from "vue";
+
+const onclickOn = ref(false)
 import image from "../remote_assets/logo.svg";
 defineProps<{
     label?: string
@@ -10,8 +13,8 @@ const emit = defineEmits<{
 </script>
 
 <template>
-  <el-button @click="emit('click')" type="success">
-    <slot /> {{ label }} <em>remote</em>
+  <el-button @click="onclickOn = true; emit('click')" type="success">
+    <slot /> {{ label }} <em>new remote</em>
     <img :src="image" alt="Kestra Logo" style="width: 16px; height: 16px; margin-left: 5px;" />
   </el-button>
 </template>

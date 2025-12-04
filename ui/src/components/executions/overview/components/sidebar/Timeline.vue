e170d22245767dcf539d376c5c49deaf5981f3bc
MilosPaunovic <paun992@hotmail.com>
2025-12-04 11:54:35 +0100

    chore: tweaks

--- ui/src/components/executions/overview/components/sidebar/Timeline.vue
+++ ui/src/components/executions/overview/components/sidebar/Timeline.vue
@@ -23,7 +23,6 @@
     import type {Histories} from "../../../../../stores/executions";
 
     import {getSchemeValue} from "../../../../../utils/scheme";
-    import {storageKeys} from "../../../../../utils/constants";
 
     import moment from "moment";
 
@@ -31,8 +30,9 @@
 
     const props = defineProps<{ histories: Histories[] }>();
 
-    const F = localStorage.getItem(storageKeys.DATE_FORMAT_STORAGE_KEY) ?? "llll";
-    const formatDate = (date: string) => moment(date)?.format(F === "llll" ? "YYYY-MM-DD HH:mm:ss.SSS" : F) ?? date;
+    const formatDate = (date: string) => {
+        return moment(date)?.format("YYYY-MM-DD HH:mm:ss.SSS") ?? date;
+    };
 </script>
 
 <style scoped lang="scss">
@@ -67,74 +67,38 @@
 }
 
 .el-timeline {
-    padding-left: 200px;
+    padding-left: 50%;
     margin-top: $spacer;
 
     & :deep(.el-timeline-item) {
         padding-bottom: $spacer;
-        position: relative;
+
+        & * {
+            line-height: 1.5;
+            font-size: $font-size-sm;
+        }
     }
 
     & :deep(.el-timeline-item__timestamp) {
         position: absolute;
+        top: 0;
         left: -210px;
         width: 190px;
-        text-align: right;
-        top: -3px;
         margin-top: 0;
-        line-height: 1.5;
-        font-size: $font-size-sm;
+        text-align: right;
         color: var(--ks-content-tertiary);
-        white-space: nowrap;
-        overflow: hidden;
-        text-overflow: ellipsis;
     }
 
     & :deep(.el-timeline-item__content) {
-        font-size: $font-size-sm;
         color: var(--ks-content-primary);
-        line-height: 1.5;
-        top: -3px;
-        position: relative;
-        min-height: auto;
-
-        .timeline-state {
-            font-weight: bold;
-        }
-    }
-
-    & :deep(.el-timeline-item__node) {
-        position: absolute;
-        z-index: 2;
-        top: 2px;
-        left: -1px;
-        width: 10px;
-        height: 10px;
     }
 
     & :deep(.el-timeline-item__tail) {
-        position: absolute;
-        display: block !important;
-        z-index: 0;
-        left: 3px;
-        top: 0;
-        bottom: 0;
-        height: 100%;
-        width: 2px;
-        background-color: var(--ks-border-color, #5c5c5c);
-    }
-
-    & :deep(.el-timeline-item:first-child .el-timeline-item__tail) {
-        top: 7px;
-        height: calc(100% - 7px);
-    }
-
-    & :deep(.el-timeline-item:last-child .el-timeline-item__tail) {
-        display: none !important;
-    }
-
-    .el-collapse-item {
-        background-color: transparent;
+        height: inherit;
+        top: 40%;
+        bottom: 10%;
+        left: 4.5px;
+        border-left-width: 1px;
     }
 }
 </style>

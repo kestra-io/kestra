aa6c0b9ec068c7c45d51b8ab0b6dbf37629e98a2
mustafatarek <mustafatarek412@gmail.com>
2025-12-06 12:13:25 +0200

    fix(tests): fix allValidTypedInputs() test by avoiding serialization of strings or other scalars, only serialize Maps and Collections

--- core/src/main/java/io/kestra/core/runners/FlowInputOutput.java
+++ core/src/main/java/io/kestra/core/runners/FlowInputOutput.java
@@ -504,7 +504,14 @@ public class FlowInputOutput {
                 }
                 case JSON -> JacksonMapper.toObject(current.toString());
                 case YAML -> {
-                    String yaml = YAML_MAPPER.writeValueAsString(current);
+                    String yaml;
+                    if(current instanceof Collection<?> || current instanceof Map){
+                        yaml = YAML_MAPPER.writeValueAsString(current);
+                    }
+                    else{
+                        yaml = current.toString();
+                    }
+
                     yield YAML_MAPPER.readValue(yaml, JacksonMapper.OBJECT_TYPE_REFERENCE);
                 }
                 case URI -> {

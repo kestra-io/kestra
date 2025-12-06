24e5b9c93967ad02e4a70a187bf57c338070b1b0
03YASWANTH <yaswanthbaratam2580@gmail.com>
2025-12-06 11:23:31 +0000

    removed unused i18 imports

--- ui/src/components/inputs/PlaygroundRunTaskButton.vue
+++ ui/src/components/inputs/PlaygroundRunTaskButton.vue
@@ -7,27 +7,24 @@
         :disabled="!playgroundStore.readyToStart"
     >
         <el-icon><Play /></el-icon>
-        <span>{{ t('playground.run_task') }}</span>
+        <span>{{ $t('playground.run_task') }}</span>
         <template #dropdown>
             <el-dropdown-menu>
                 <el-dropdown-item :icon="Play" @click="playgroundStore.runUntilTask(taskId)">
-                    {{ t('playground.run_this_task') }}
+                    {{ $t('playground.run_this_task') }}
                 </el-dropdown-item>
                 <el-dropdown-item :icon="PlayBoxMultiple" @click="playgroundStore.runUntilTask(taskId, true)">
-                    {{ t('playground.run_task_and_downstream') }}
+                    {{ $t('playground.run_task_and_downstream') }}
                 </el-dropdown-item>
             </el-dropdown-menu>
         </template>
     </el-dropdown>
 </template>
 
-<script setup lang="ts">
-    import {useI18n} from "vue-i18n";
+<script setup lang="ts">;
     import {usePlaygroundStore} from "../../stores/playground";
     import Play from "vue-material-design-icons/Play.vue";
     import PlayBoxMultiple from "vue-material-design-icons/PlayBoxMultiple.vue";
-
-    const {t} = useI18n();
     const playgroundStore = usePlaygroundStore();
 
     defineProps<{

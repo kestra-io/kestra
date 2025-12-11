e7ca608e2a4decc80d8b8dcceb2fbc0d296a0f81
ameenpj <ameenjami9@gmail.com>
2025-12-11 14:02:57 +0000

    removed inline styles

--- ui/src/components/logs/LogsWrapper.vue
+++ ui/src/components/logs/LogsWrapper.vue
@@ -29,7 +29,7 @@
                                 filter=""
                                 :excludeMetas="isFlowEdit ? ['namespace', 'flowId'] : []"
                                 :log="log"
-                                :style="{borderTop: i===0 ? 'none' : '1px solid var(--ks-border-primary)'}"
+                                :class="{'log-0': i === 0}"
                             />
                         </div>
 

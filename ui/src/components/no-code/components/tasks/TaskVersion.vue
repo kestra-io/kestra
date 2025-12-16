7cca3f5ddfc07937685dc7439b24c8f846938c65
Vamsi <vamsi.172323@gmail.com>
2025-12-16 09:53:26 +0530

    ui: use $t instead of useI18n in TaskVersion
    Simplifying TaskVersion.vue translations by using $t in template section

--- ui/src/components/no-code/components/tasks/TaskVersion.vue
+++ ui/src/components/no-code/components/tasks/TaskVersion.vue
@@ -2,14 +2,13 @@
     <InputText
         :modelValue="modelValue"
         :disabled
-        :placeholder="disabled ? t('no_code.version_oss_placeholder') : undefined"
+        :placeholder="disabled ? $t('no_code.version_oss_placeholder') : undefined"
         class="w-100"
     />
 </template>
 
 <script setup lang="ts">
     import {computed} from "vue";
-    import {useI18n} from "vue-i18n";
     import InputText from "../inputs/InputText.vue";
 
     import {useMiscStore} from "override/stores/misc";
@@ -17,7 +16,6 @@
     
     const disabled = computed(() => miscStore.configs?.edition === "OSS") 
 
-    const {t} = useI18n()
 
     const modelValue = defineModel<string>({default: ""})
 

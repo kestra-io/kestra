8b5af1f8a33cfbe13f72b5b0ab610a7f698a0cf7
Bart Ledoux <bledoux@kestra.io>
2025-12-16 17:31:01 +0100

    generate flat sdk to allow treeshaking

--- ui/openapi-ts.config.ts
+++ ui/openapi-ts.config.ts
@@ -1,5 +1,8 @@
 import {defineConfig} from "@hey-api/openapi-ts";
 
+const capitalize = (s: string) => s.charAt(0).toUpperCase() + s.slice(1);
+const deCapitalize = (s: string) => s.charAt(0).toLowerCase() + s.slice(1);
+
 export default defineConfig({
   input: "../webserver/build/classes/java/main/META-INF/swagger/kestra.yml",
   output: {
@@ -12,8 +15,10 @@ export default defineConfig({
     },
     {
         name: "@hey-api/sdk",
-        asClass: true,
         paramsStructure: "flat",
+        methodNameBuilder(operation) {
+            return `${deCapitalize(operation.tags?.[0] ?? "")}${capitalize(operation.operationId ?? "")}`;
+        }
     }
   ],
 });

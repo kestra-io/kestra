7ed456208da473c2da96c79e9b929739fce27391
Piyush Bhaskar <102078527+Piyush-r-bhaskar@users.noreply.github.com>
2025-12-16 17:38:09 +0530

    fix(plugins): avoid list flash when opening plugin (#13690)

--- ui/src/components/plugins/Plugin.vue
+++ ui/src/components/plugins/Plugin.vue
@@ -1,6 +1,6 @@
 <template>
     <TopNavBar :title="routeInfo.title" :breadcrumb="routeInfo?.breadcrumb" />
-    <template v-if="!pluginIsSelected">
+    <template v-if="isPluginList">
         <PluginHome v-if="filteredPlugins" :plugins="filteredPlugins" />
     </template>
     <DocsLayout v-else>
@@ -121,8 +121,9 @@
 
     const releaseNotesUrl = computed(() => getPluginReleaseUrl(pluginType.value));
 
-    const pluginIsSelected = computed(
-        () => pluginType.value !== undefined && pluginsStore.plugin !== undefined
+
+    const isPluginList = computed(
+        () => typeof route.name === "string" && route.name === "plugins/list"
     );
 
     function loadToc() {

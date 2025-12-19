242edbe3b960417e4e2271e4307123aa6324c993
Bart Ledoux <bledoux@kestra.io>
2025-12-19 16:27:37 +0100

    fix: use injection instead of store

--- ui/src/components/no-code/components/tasks/TaskNamespace.vue
+++ ui/src/components/no-code/components/tasks/TaskNamespace.vue
@@ -8,18 +8,17 @@
 </template>
 
 <script lang="ts" setup>
-    import {computed, onMounted} from "vue";
-    import {useFlowStore} from "../../../../stores/flow";
+    import {onMounted, inject, computed, provide} from "vue";
     import NamespaceSelect from "../../../namespaces/components/NamespaceSelect.vue";
+    import {CREATING_FLOW_INJECTION_KEY, DEFAULT_NAMESPACE_INJECTION_KEY} from "../../injectionKeys";
 
     const modelValue = defineModel<string>();
-
-    const flowStore = useFlowStore();
-
-    const isCreating = computed(() => flowStore.isCreating);
+    const isCreating = inject(CREATING_FLOW_INJECTION_KEY, false);
+    const defaultNamespace = inject(DEFAULT_NAMESPACE_INJECTION_KEY, computed(() => ""));
+    provide(DEFAULT_NAMESPACE_INJECTION_KEY, computed(() => modelValue.value || defaultNamespace.value));
 
     onMounted(() => {
-        const flowNamespace = flowStore.flow?.namespace;
+        const flowNamespace = defaultNamespace.value;
         if (!modelValue.value && flowNamespace) {
             modelValue.value = flowNamespace;
         }

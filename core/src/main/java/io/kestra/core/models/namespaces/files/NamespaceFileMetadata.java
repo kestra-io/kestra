68b5c113e07d0d0f7932751671faba4d2424d5ab
Mustafa Tarek <mustafatarek412@gmail.com>
2025-12-21 01:17:40 +0200

    fix(core): remove innecessary leadingBackSlash as it is always true

--- core/src/main/java/io/kestra/core/models/namespaces/files/NamespaceFileMetadata.java
+++ core/src/main/java/io/kestra/core/models/namespaces/files/NamespaceFileMetadata.java
@@ -94,7 +94,7 @@ public class NamespaceFileMetadata implements DeletedInterface, TenantInterface,
         return NamespaceFileMetadata.builder()
             .tenantId(tenantId)
             .namespace(namespaceFile.namespace())
-            .path(namespaceFile.path(true).toString())
+            .path(namespaceFile.path().toString())
             .version(namespaceFile.version())
             .build();
     }

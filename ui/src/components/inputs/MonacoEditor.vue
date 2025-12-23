68156830908e7f34bc75724c587b5a34417a3ed7
MilosPaunovic <paun992@hotmail.com>
2025-12-23 13:42:12 +0100

    chore: minor tweaks

--- ui/src/components/inputs/MonacoEditor.vue
+++ ui/src/components/inputs/MonacoEditor.vue
@@ -125,7 +125,7 @@
     import uniqBy from "lodash/uniqBy";
     import {useI18n} from "vue-i18n";
     import {ElDatePicker} from "element-plus";
-    import {Moment} from "moment";
+    import moment from "moment";
     import PlaceholderContentWidget from "../../composables/monaco/PlaceholderContentWidget";
     import Utils from "../../utils/utils";
     import {hashCode} from "../../utils/global";
@@ -369,7 +369,7 @@
         }
     }, {deep: true});
 
-    const nowMoment: Moment = moment().startOf("day");
+    const nowMoment = moment().startOf("day");
     function addedSuggestRows(mutations: MutationRecord[]) {
         return mutations.flatMap(({addedNodes}) => {
             const nodes = [...addedNodes];

b852740cbb9dbb035d720a7c67faaed6a9189db6
VAMSIKRISHNA BATRAJU <batrajuvamsikrishna@gmail.com>
2026-01-04 19:47:21 +0530

    ui: use $t helper in Instance.vue template

--- ui/src/components/demo/Instance.vue
+++ ui/src/components/demo/Instance.vue
@@ -1,8 +1,8 @@
 <template>
     <TopNavBar :title="routeInfo.title" v-if="!isFullScreen()" />
     <Layout
-        :title="t('demos.instance.title')"
-        :image="{source: sourceImg, alt: t('demos.instance.title')}"
+        :title="$t('demos.instance.title')"
+        :image="{ source: sourceImg, alt: $t('demos.instance.title') }"
         :video="{
             source: 'https://www.youtube.com/embed/pcC3OAJPQao',
         }"
@@ -14,20 +14,22 @@
 </template>
 
 <script setup lang="ts">
-    import {computed} from "vue";
-    import {useI18n} from "vue-i18n";
+    import { computed } from "vue";
+    import { useI18n } from "vue-i18n";
     import Layout from "./Layout.vue";
     import TopNavBar from "../../components/layout/TopNavBar.vue";
     import sourceImg from "../../assets/demo/instance.png";
     import useRouteContext from "../../composables/useRouteContext";
 
-    const {t} = useI18n();
+    const { t } = useI18n();
 
-    const routeInfo = computed(() => ({title: t("demos.instance.title")}));
+    const routeInfo = computed(() => ({ title: t("demos.instance.title") }));
 
     useRouteContext(routeInfo);
 
     function isFullScreen() {
-        return document.getElementsByTagName("html")[0].classList.contains("full-screen");
+        return document
+            .getElementsByTagName("html")[0]
+            .classList.contains("full-screen");
     }
 </script>

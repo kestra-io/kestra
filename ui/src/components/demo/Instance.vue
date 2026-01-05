86a4cb762cbd4515e30fffccebbdd0969b9c7c32
MilosPaunovic <paun992@hotmail.com>
2026-01-05 08:56:39 +0100

    chore: tweaks

--- ui/src/components/demo/Instance.vue
+++ ui/src/components/demo/Instance.vue
@@ -2,7 +2,7 @@
     <TopNavBar :title="routeInfo.title" v-if="!isFullScreen()" />
     <Layout
         :title="$t('demos.instance.title')"
-        :image="{ source: sourceImg, alt: $t('demos.instance.title') }"
+        :image="{source: sourceImg, alt: $t('demos.instance.title')}"
         :video="{
             source: 'https://www.youtube.com/embed/pcC3OAJPQao',
         }"
@@ -14,22 +14,20 @@
 </template>
 
 <script setup lang="ts">
-    import { computed } from "vue";
-    import { useI18n } from "vue-i18n";
+    import {computed} from "vue";
+    import {useI18n} from "vue-i18n";
     import Layout from "./Layout.vue";
     import TopNavBar from "../../components/layout/TopNavBar.vue";
     import sourceImg from "../../assets/demo/instance.png";
     import useRouteContext from "../../composables/useRouteContext";
 
-    const { t } = useI18n();
+    const {t} = useI18n();
 
-    const routeInfo = computed(() => ({ title: t("demos.instance.title") }));
+    const routeInfo = computed(() => ({title: t("demos.instance.title")}));
 
     useRouteContext(routeInfo);
 
     function isFullScreen() {
-        return document
-            .getElementsByTagName("html")[0]
-            .classList.contains("full-screen");
+        return document.getElementsByTagName("html")[0].classList.contains("full-screen");
     }
 </script>

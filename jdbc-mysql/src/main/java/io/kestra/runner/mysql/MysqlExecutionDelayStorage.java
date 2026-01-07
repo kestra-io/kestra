b7ecb542e5e1589de0db779d5e71e91c0f3dd9c7
Florian Hussonnois <fhussonnois@kestra.io>
2026-01-07 11:32:15 +0100

    fix(core): regression on execution delay (#14023)
    Fixes: #14023

--- jdbc-mysql/src/main/java/io/kestra/runner/mysql/MysqlExecutionDelayStorage.java
+++ jdbc-mysql/src/main/java/io/kestra/runner/mysql/MysqlExecutionDelayStorage.java
@@ -3,14 +3,24 @@ package io.kestra.runner.mysql;
 import io.kestra.core.runners.ExecutionDelay;
 import io.kestra.jdbc.runner.AbstractJdbcExecutionDelayStorage;
 import io.kestra.repository.mysql.MysqlRepository;
-import io.micronaut.context.ApplicationContext;
 import jakarta.inject.Named;
 import jakarta.inject.Singleton;
+
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.temporal.Temporal;
 
 @Singleton
 @MysqlQueueEnabled
 public class MysqlExecutionDelayStorage extends AbstractJdbcExecutionDelayStorage {
     public MysqlExecutionDelayStorage(@Named("executordelayed") MysqlRepository<ExecutionDelay> repository) {
         super(repository);
+    }
+
+    @Override
+    protected Temporal getNow() {
+        // 'date' column in the table is in UTC
+        // convert 'now' to UTC LocalDateTime to avoid any timezone/offset interpretation by the database.
+        return ZonedDateTime.now().withZoneSameInstant(ZoneOffset.UTC).toLocalDateTime();
     }
 }

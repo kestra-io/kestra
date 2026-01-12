6de40b28f6f7c86af1b2b7f76d8a796e56a08e70
张新奕 <zhangxinyi25@xiaomi.com>
2026-01-12 16:28:59 +0800

    增加随机数长度，避免podname重名问题

--- core/src/main/java/io/kestra/core/models/tasks/runners/ScriptService.java
+++ core/src/main/java/io/kestra/core/models/tasks/runners/ScriptService.java
@@ -234,7 +234,7 @@ public final class ScriptService {
     }
 
     /**
-     * Create a job name like {namespace}-{flowId}-{taskId}-{random} with random being 5 alphanumerical characters.
+     * Create a job name like {namespace}-{flowId}-{taskId}-{random} with random being 10 alphanumerical characters.
      * The Job name will be normalized based on the DNS Subdomain Names (RFC 1123) with a limit of 63 characters as used by Kubernetes
      */
     @SuppressWarnings("unchecked")
@@ -249,12 +249,12 @@ public final class ScriptService {
             task.get("id")
         ));
         String normalized = normalizeValue(name, true, true);
-        if (normalized.length() > 58) {
-            normalized = normalized.substring(0, 57);
+        if (normalized.length() > 53) {
+            normalized = normalized.substring(0, 52);
         }
 
-        // we add a suffix of 5 chars, this should be enough as it's the standard k8s way
-        String suffix = RandomStringUtils.secure().nextAlphanumeric(5).toLowerCase();
+        // we add a suffix of 10 chars, this is safer for high-concurrency scenarios
+        String suffix = RandomStringUtils.secure().nextAlphanumeric(10).toLowerCase();
         return normalized + "-" + suffix;
     }
 }

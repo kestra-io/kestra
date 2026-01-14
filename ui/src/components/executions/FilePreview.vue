d539b40bcbfcea7efe6f8564531ae01257cb48e6
Mahadeva Peruka <97960828+mahadevaperuka@users.noreply.github.com>
2026-01-14 10:44:43 -0600

    style is scoped

--- ui/src/components/executions/FilePreview.vue
+++ ui/src/components/executions/FilePreview.vue
@@ -234,8 +234,8 @@
         encoding.value = encodingOptions[0].value;
     });
 </script>
-<style lang="scss">
-    .editor-container {
+<style scoped lang="scss">
+    :deep(.editor-container) {
         min-height: 65px !important;
     }
 </style>

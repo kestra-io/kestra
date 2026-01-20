fc3c77e5dd0490c4ed6ac5a946b1ba6530ef0e3b
Loïc Mathieu <loikeseke@gmail.com>
2026-01-20 12:04:10 +0100

    fix(trigger): Expression and ExecutionOutputs should not be scheduled condition
    ScheduledCondition are special types of conditions used when computing the Schedule trigger next date.
    If no next date can be computed, the Scheduler will try to compute again the next date after 1s.
    So ScheduleConditions must be conditions that only works on date to limit the number of date a trigger should be scheduled on.
    
    Without that, it creates a kind of inifinte loop.
    In my local machine, this uses 5% CPU whereas with the fix the CPU becomes idle.
    On resource restricted computers this can cost way more.
    
    Fixes https://github.com/kestra-io/kestra-ee/issues/6444

--- core/src/main/java/io/kestra/core/models/conditions/ScheduleCondition.java
+++ core/src/main/java/io/kestra/core/models/conditions/ScheduleCondition.java
@@ -2,7 +2,10 @@ package io.kestra.core.models.conditions;
 
 import io.kestra.core.exceptions.InternalException;
 
-
+/**
+ * A ScheduleCondition is a special type of condition that will be used by the Schedule trigger when computing its next date.
+ * Such condition must be a condition on a date or there is a risk of infinite evaluation of the trigger (each on sec).
+ */
 public interface ScheduleCondition {
     boolean test(ConditionContext conditionContext) throws InternalException;
 }

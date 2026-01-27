8955873df56c0fcce48eed94dc72e6a7afc9ca0f
Hemant M Mehta <hemant29mehta@gmail.com>
2026-01-27 18:48:36 +0530

    fix: storybook component test
    Signed-off-by: Hemant M Mehta <hemant29mehta@gmail.com>

--- ui/.storybook/vitest.config.js
+++ ui/.storybook/vitest.config.js
@@ -1,5 +1,6 @@
 import path from "node:path";
 
+import {defineConfig} from "vite";
 import {defineProject, mergeConfig} from "vitest/config";
 
 import {storybookTest} from "@storybook/addon-vitest/vitest-plugin";
@@ -9,6 +10,21 @@ import initialConfig from "../vite.config.js"
 // More info at: https://storybook.js.org/docs/writing-tests/test-addon
 export default mergeConfig(
     initialConfig,
+    defineConfig({
+        test: {
+            name: "storybook",
+            browser: {
+                enabled: true,
+                headless: true,
+                name: "chromium",
+                provider: "playwright",
+            },
+            include: ["../**/*.stories.?(m)[jt]s?(x)"],
+            setupFiles: ["./.storybook/vitest.setup.ts"],
+            testTimeout: 30000,
+            hookTimeout: 30000,
+        },
+    }),
     defineProject({
             plugins: [
                 // The plugin will run tests for the stories defined in your Storybook config
@@ -24,6 +40,8 @@ export default mergeConfig(
                     instances: [{browser: "chromium"}],
                 },
                 setupFiles: ["vitest.setup.ts"],
+                testTimeout: 30000,
+                hookTimeout: 30000,
             },
             define: {
                 "process.env.RUN_TEST_WITH_PERSISTENT": JSON.stringify("false"), // Disable persistent mode for tests

9c60e28e8139cb724ffc7422140178f758e33a54
Hemant M Mehta <hemant29mehta@gmail.com>
2026-02-04 21:04:12 +0530

    feat(variables): fetch pebble filters & functions for autocompletion from backend
    closes https://github.com/kestra-io/kestra/issues/8350
    closes https://github.com/kestra-io/kestra/issues/7734

--- ui/src/services/autoCompletionProvider.ts
+++ ui/src/services/autoCompletionProvider.ts
@@ -1,7 +1,45 @@
 import {YamlElement} from "@kestra-io/ui-libs";
+import {apiUrlWithoutTenants} from "../override/utils/route";
+import {useAxios} from "../utils/axios";
 
 export const QUOTE = "'";
 
+let cachedFilters: string[] | null = null;
+let cachedFunctions: string[] | null = null;
+
+export function resetExpressionCache() {
+    cachedFilters = null;
+    cachedFunctions = null;
+}
+
+export function fillExpressionCache(filters: string[], functions: string[]) {
+    cachedFilters = filters;
+    cachedFunctions = functions;
+}
+
+async function fetchExpressionFilters(): Promise<string[]> {
+    if (cachedFilters === null) {
+        try {
+            const axios = useAxios();
+            cachedFilters = (await axios.get<string[]>(`${apiUrlWithoutTenants()}/pebble/filters`)).data;
+        } catch {
+            return [];
+        }
+    }
+    return cachedFilters;
+}
+
+async function fetchExpressionFunctions(): Promise<string[]> {
+    if (cachedFunctions === null) {
+        try {
+            const axios = useAxios();
+            cachedFunctions = (await axios.get<string[]>(`${apiUrlWithoutTenants()}/pebble/functions`)).data;
+        } catch {
+            return [];
+        }
+    }
+    return cachedFunctions;
+}
 
 export class PebbleAutoCompletion {
     rootFieldAutoCompletion(): Promise<string[]> {
@@ -14,6 +52,14 @@ export class PebbleAutoCompletion {
 
     functionAutoCompletion(_parsed: any | undefined, _functionName: string, _args: Record<string, string>): Promise<string[]> {
         return Promise.resolve([]);
+    }
+
+    filterAutoCompletion(): Promise<string[]> {
+        return fetchExpressionFilters();
+    }
+
+    functionNames(): Promise<string[]> {
+        return fetchExpressionFunctions();
     }
 }
 

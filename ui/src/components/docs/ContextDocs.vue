c764a96e7fa302267aad5c4f7742649b964cf532
Barthélémy Ledoux <bledoux@kestra.io>
2026-02-11 16:39:00 +0100

    fix(docs): help homepage render semi properly (#14542)

--- ui/src/components/docs/ContextDocs.vue
+++ ui/src/components/docs/ContextDocs.vue
@@ -62,6 +62,7 @@
     const {isOnline} = useNetwork()
     
     import Markdown from "../../components/layout/Markdown.vue";
+    import PluginCount from "./PluginCount.vue";
     const OFFLINE_MD = "You're seeing this because you are offline.\n\nHere's how to configure the right sidebar in Kestra to include custom links:\n\n```yaml\nkestra:\n  ee:\n    right-sidebar:\n      custom-links:\n        internal-docs:\n          title: \"Internal Docs\"\n          url: \"https://kestra.io/docs/\"\n        support-portal:\n          title: \"Support portal\"\n          url: \"https://kestra.io/support/\"\n```";
 
     const docStore = useDocStore();
@@ -118,6 +119,48 @@
         return contentArray.join("```");
     }
 
+    function extractMultilineJSXComponents(content: string) {
+        // first, find every line that start with < and a capital letter, and that doesn't end with />
+        const lines = content.split("\n");
+        const linesToRemove: number[] = [];
+        const removedComponents: Record<number, string> = {};
+        let startOfBlockLine = -1;
+        let componentName = "";
+
+        for(let i = 0; i < lines.length; i++){
+            if(startOfBlockLine > -1){
+                // if we have started a block, let's check if this line is the end of it.
+                // if so, we remove it and stop the next iterations until we find a new block
+                linesToRemove.push(i);
+                if(lines[i].match(/^\/>/)){
+                    removedComponents[startOfBlockLine] = lines.slice(startOfBlockLine, i).join("\n") + `\n></${componentName}>`;
+                    startOfBlockLine = -1;
+                    componentName = "";
+                }
+            }
+
+            if(lines[i].match(/^<([A-Z][\w]*)\b(?![^>]*\/>).*$/)){
+                componentName = lines[i].match(/^<([A-Z][\w]*)/)?.[1] ?? "";
+                startOfBlockLine = i;
+            }
+        }
+        
+        // in place of each removed block, we add a placeholder with the component name to keep track of where it was in the doc
+        for(const lineIndex in removedComponents){
+            lines[lineIndex] = `<!-- ${removedComponents[lineIndex]} -->`;
+        }
+        return {
+            content: lines.filter((_, i) => !linesToRemove.includes(i)).join("\n"),
+            removedComponents: removedComponents
+        };
+    }
+
+    function replaceSelfClosingTagsWithOpenClose(content: string): string {
+        // we want to replace every self closing tag with an open and close tag
+        // to keep compatibility with mdx files that use self closing tags for custom components
+        return content.replaceAll(/<([A-Z][\w]*)\b([^>]*)\/>/g, "<$1$2></$1>\n");
+    }
+
     async function setDocPageFromResponse(response: {metadata?: any, content:string}) {
         docStore.pageMetadata = response.metadata;
         let content = response.content;
@@ -127,16 +170,19 @@
 
         content = removeMDXImports(content);
 
-        
+        const {content: cleanedContent, removedComponents: _} = extractMultilineJSXComponents(content);
+
+        const noSelfClosingTagsContent = replaceSelfClosingTagsWithOpenClose(cleanedContent);
+
         const parse = await getMDCParser();
         // this hack alleviates a little the parsing load of the first render on big docs
         // by only rendering the first 50 lines of the doc on opening
         // since they are the only ones visible in the beginning
-        const firstLinesOfContent = content.split("---\n")[2].split("\n").slice(0, 50).join("\n") + "\nLoading the rest...\n";
+        const firstLinesOfContent = noSelfClosingTagsContent.split("---\n")[2].split("\n").slice(0, 50).join("\n") + "\nLoading the rest...\n";
         ast.value = await parse(firstLinesOfContent);
 
         setTimeout(async () => {
-            ast.value = await parse(content);
+            ast.value = await parse(noSelfClosingTagsContent);
         }, 50);
     }
 
@@ -181,7 +227,8 @@
             .map(name => [name, "prose-" + name]),
         ["a", ContextDocsLink],
         ["ChildCard", ContextChildCard],
-        ["ChildTableOfContents", ContextChildTableOfContents]
+        ["ChildTableOfContents", ContextChildTableOfContents],
+        ["PluginCount", PluginCount],
     ]);
 
     onMounted(() => {

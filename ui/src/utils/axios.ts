08a105fa42b9d9fb3d3cb1290542e8b3a81b0a80
Bart Ledoux <bledoux@kestra.io>
2026-02-19 15:57:57 +0100

    refactor: use an alias instead of "generated"

--- ui/src/utils/axios.ts
+++ ui/src/utils/axios.ts
@@ -9,7 +9,7 @@ import * as BasicAuth from "../utils/basicAuth"
 import {useAuthStore} from "override/stores/auth"
 import {useMiscStore} from "override/stores/misc";
 import {useUnsavedChangesStore} from "../stores/unsavedChanges"
-import {client} from "../generated/kestra-api/client.gen"
+import {client} from "kestra-api/client.gen"
 
 let pendingRoute = false
 let requestsTotal = 0

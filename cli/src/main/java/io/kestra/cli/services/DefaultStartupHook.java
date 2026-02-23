87fd478c6631b6ee5398648d8bb85c612c506fb1
Florian Hussonnois <fhussonnois@kestra.io>
2026-02-23 10:53:08 +0100

    fix(cli): do not execute startup hook for worker server

--- cli/src/main/java/io/kestra/cli/services/DefaultStartupHook.java
+++ cli/src/main/java/io/kestra/cli/services/DefaultStartupHook.java
@@ -2,6 +2,7 @@ package io.kestra.cli.services;
 
 import io.kestra.cli.AbstractCommand;
 import io.kestra.cli.commands.servers.ServerCommandInterface;
+import io.kestra.cli.commands.servers.WorkerCommand;
 import io.kestra.core.contexts.KestraContext;
 import io.kestra.core.models.Setting;
 import io.kestra.core.repositories.SettingRepositoryInterface;
@@ -19,8 +20,8 @@ public class DefaultStartupHook implements StartupHookInterface {
    private ApplicationContext applicationContext;
 
    @Override
-   public void start(AbstractCommand abstractCommand) {
-      if (abstractCommand instanceof ServerCommandInterface){
+   public void start(AbstractCommand cmd) {
+      if (cmd instanceof ServerCommandInterface && !(cmd instanceof WorkerCommand)){
          saveKestraVersion();
       }
    }

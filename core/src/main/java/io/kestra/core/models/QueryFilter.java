2186dc5fe6583e4063501405cce80c4e92cf3d6b
steven meek <smeek@kestra.io>
2026-02-23 21:30:00 +0000

    feat(core): implement User.group EQUALS and CONTAINS filter

--- core/src/main/java/io/kestra/core/models/QueryFilter.java
+++ core/src/main/java/io/kestra/core/models/QueryFilter.java
@@ -264,6 +264,12 @@ public record QueryFilter(
             public List<Op> supportedOp() {
                 return List.of(Op.EQUALS);
             }
+        },
+        GROUP("groupList") {
+            @Override
+            public List<Op> supportedOp() {
+                return List.of(Op.CONTAINS, Op.EQUALS);
+            }
         };
 
         private static final Map<String, Field> BY_VALUE = Arrays.stream(values())
@@ -344,7 +350,7 @@ public record QueryFilter(
         USER {
             @Override
             public List<Field> supportedField() {
-                return List.of(Field.QUERY, Field.USERNAME);
+                return List.of(Field.QUERY, Field.USERNAME, Field.GROUP);
             }
         },
         SECRET_METADATA {

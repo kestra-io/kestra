5cde8341a620ba9fa0065c99a2da88d55cdf5efa
Roman Acevedo <roman.acevedo62@gmail.com>
2026-02-25 19:11:23 +0100

    feat(dashboards): only display defaults button when user is permitted

--- ui/src/components/dashboard/components/selector/Item.vue
+++ ui/src/components/dashboard/components/selector/Item.vue
@@ -5,7 +5,7 @@
         </div>
 
         <div class="col-auto">
-            <el-button v-if="props.dashboard.id !== 'default'" link :icon="props.dashboard.isDefault ? Home : HomeOutline" class="mx-0" @click.stop="props.setAsDefault(props.dashboard.id)" title="set as default" />
+            <el-button v-if="isAllowedToSetDefaults" link :icon="props.dashboard.isDefault ? Home : HomeOutline" class="mx-0" @click.stop="props.setAsDefault(props.dashboard.id)" title="set as default" />
             <el-button v-if="props.dashboard.id !== 'default'" link :icon="Pencil" class="mx-0" @click.stop="props.edit(props.dashboard.id)" title="edit" />
             <el-button v-if="props.dashboard.id !== 'default' && props.remove" link :icon="DeleteOutline" class="mx-0" @click.stop="props.remove(props.dashboard)" title="delete" />
         </div>
@@ -17,10 +17,27 @@
     import Pencil from "vue-material-design-icons/Pencil.vue";
     import Home from "vue-material-design-icons/Home.vue";
     import HomeOutline from "vue-material-design-icons/HomeOutline.vue";
+    import {computed} from "vue";
+    import {useAuthStore} from "override/stores/auth.ts";
+    import {useMiscStore} from "override/stores/misc.ts";
 
     const props = defineProps<{
         dashboard: {id: string, title: string, isDefault: boolean},
         setAsDefault: (id: string) => void,
         edit: (id: string) => void,
         remove?: (dashboard: {id: string, title: string}) => void}>()
+
+    const authStore = useAuthStore();
+    const isOSS = computed(() => useMiscStore().configs?.edition === "OSS");
+    const isAllowedToSetDefaults = computed(() => {
+        const user = authStore.user as Record<string, unknown> | null;
+
+        return isOSS.value
+            || (
+                !!user
+                && Object.prototype.hasOwnProperty.call(user, "superAdmin")
+                && user["superAdmin"] === true
+            );
+    });
+
 </script>

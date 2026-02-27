84c99b83d444792487927d879de1474877682df4
Bart Ledoux <bledoux@kestra.io>
2026-02-27 10:20:02 +0100

    fix: move blueprints import

--- ui/src/override/components/flows/blueprints/Blueprints.vue
+++ ui/src/override/components/flows/blueprints/Blueprints.vue
@@ -36,7 +36,7 @@
     import TopNavBar from "../../../../components/layout/TopNavBar.vue";
     import DottedLayout from "../../../../components/layout/DottedLayout.vue";
     import BlueprintDetail from "override/components/flows/blueprints/BlueprintDetail.vue";
-    import BlueprintsBrowser from "override/components/flows/blueprints/BlueprintsBrowser.vue";
+    import BlueprintsBrowser from "../../../../components/flows/blueprints/BlueprintsBrowser.vue";
     import DemoBlueprints from "../../../../components/demo/Blueprints.vue";
     import useRouteContext from "../../../../composables/useRouteContext";
 

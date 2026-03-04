12a2c0a23c027700d0ca943b8dd17d01c66967a7
steven meek <smeek@kestra.io>
2026-03-04 13:20:57 +0000

    chore(core): add query filter tests for INVITATION Resource.

--- core/src/test/java/io/kestra/core/models/QueryFilterTest.java
+++ core/src/test/java/io/kestra/core/models/QueryFilterTest.java
@@ -302,6 +302,23 @@ public class QueryFilterTest {
                     Op.EQUALS,
                     Op.IN
                 )
+            ),
+
+            buildQueryFiltersForOperations(Field.EMAIL, Resource.INVITATION,
+                Set.of(
+                    Op.EQUALS
+                )
+            ),
+
+            buildQueryFiltersForOperations(Field.STATUS, Resource.INVITATION,
+                Set.of(
+                    Op.EQUALS,
+                    Op.NOT_EQUALS
+                )
+            ),
+
+            buildQueryFiltersForOperations(Field.EXPIRED_AT, Resource.INVITATION,
+                Set.of()
             )
         ).flatMap(s -> s);
     }
@@ -636,6 +653,57 @@ public class QueryFilterTest {
                     Op.REGEX,
                     Op.PREFIX,
                     Op.NOT_EQUALS
+                )
+            ),
+
+            buildQueryFiltersForOperations(Field.EMAIL, Resource.INVITATION,
+                Set.of(
+                    Op.NOT_EQUALS,
+                    Op.GREATER_THAN,
+                    Op.LESS_THAN,
+                    Op.GREATER_THAN_OR_EQUAL_TO,
+                    Op.LESS_THAN_OR_EQUAL_TO,
+                    Op.IN,
+                    Op.NOT_IN,
+                    Op.STARTS_WITH,
+                    Op.ENDS_WITH,
+                    Op.CONTAINS,
+                    Op.REGEX,
+                    Op.PREFIX
+                )
+            ),
+
+            buildQueryFiltersForOperations(Field.STATUS, Resource.INVITATION,
+                Set.of(
+                    Op.GREATER_THAN,
+                    Op.LESS_THAN,
+                    Op.GREATER_THAN_OR_EQUAL_TO,
+                    Op.LESS_THAN_OR_EQUAL_TO,
+                    Op.IN,
+                    Op.NOT_IN,
+                    Op.STARTS_WITH,
+                    Op.ENDS_WITH,
+                    Op.CONTAINS,
+                    Op.REGEX,
+                    Op.PREFIX
+                )
+            ),
+
+            buildQueryFiltersForOperations(Field.EXPIRED_AT, Resource.INVITATION,
+                Set.of(
+                    Op.EQUALS,
+                    Op.NOT_EQUALS,
+                    Op.GREATER_THAN,
+                    Op.LESS_THAN,
+                    Op.GREATER_THAN_OR_EQUAL_TO,
+                    Op.LESS_THAN_OR_EQUAL_TO,
+                    Op.IN,
+                    Op.NOT_IN,
+                    Op.STARTS_WITH,
+                    Op.ENDS_WITH,
+                    Op.CONTAINS,
+                    Op.REGEX,
+                    Op.PREFIX
                 )
             )
         ).flatMap(s -> s);

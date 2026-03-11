40e21f039b52398da25b9d1cde16cdda7bac2bf7
MilosPaunovic <paun992@hotmail.com>
2026-03-11 13:53:19 +0100

    style the tags properly

--- ui/src/components/onboarding/Welcome.vue
+++ ui/src/components/onboarding/Welcome.vue
@@ -9,18 +9,29 @@
         </template>
     </TopNavBar>
 
-    <section class="container">
+    <section id="welcome" class="container">
         <el-row justify="center">
             <el-col :xs="24" :sm="24" :md="18" :lg="16" :xl="14">
                 <AiCopilot :flow :conversationId />
 
-                <el-tag
-                    v-for="(label, index) in labels"
-                    :key="index"
-                    @click="flow = flows[label]"
-                >
-                    {{ label }}
-                </el-tag>
+                <div class="mt-3">
+                    <el-tag
+                        v-for="(label, index) in visibleLabels"
+                        :key="index"
+                        round
+                        @click="flow = flows[label]"
+                    >
+                        {{ label }}
+                    </el-tag>
+
+                    <el-tag
+                        v-if="labels.length > 5"
+                        round
+                        @click="allLabelsShown = !allLabelsShown"
+                    >
+                        {{ allLabelsShown ? $t("showLess") : $t("showMore") }}
+                    </el-tag>
+                </div>
             </el-col>
         </el-row>
     </section>
@@ -62,6 +73,25 @@
 
     const flow = ref<string>(flows.initial);
     const conversationId = ref<string>(Utils.uid());
+
+    const allLabelsShown = ref(false);
+    const visibleLabels = computed(() => {
+        return allLabelsShown.value ? labels : labels.slice(0, 5);
+    });
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+section#welcome {
+    .el-tag {
+        cursor: pointer;
+        margin: calc(1rem / 4);
+        border: 1px solid var(--ks-border-primary);
+        background-color: var(--ks-button-background-secondary);
+        color: var(--ks-content-primary);
+
+        &:hover {
+            background-color: var(--ks-button-background-secondary-hover);
+        }
+    }
+}
+</style>

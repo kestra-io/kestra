082141379c374aa22df4e603fd868d8cac7dca34
MilosPaunovic <paun992@hotmail.com>
2026-03-12 11:40:38 +0100

    icon

--- ui/src/components/onboarding/Success.vue
+++ ui/src/components/onboarding/Success.vue
@@ -4,7 +4,80 @@
             <el-col :xs="24" :sm="22" :md="18" :lg="16" :xl="14">
                 <div class="welcome-success-hero">
                     <div class="welcome-success-icon">
-                        <CheckBold />
+                        <svg viewBox="0 0 109 109" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                            <g filter="url(#filter0_dd_success)">
+                                <g clip-path="url(#clip0_success)">
+                                    <path d="M20 26.7136C20 22.4535 23.4535 19 27.7136 19H81.2864C85.5465 19 89 22.4535 89 26.7136V80.2864C89 84.5465 85.5465 88 81.2864 88H27.7136C23.4535 88 20 84.5465 20 80.2864V26.7136Z" fill="#2C0059" />
+                                    <g filter="url(#filter1_diii_success)">
+                                        <path d="M48.757 69.2879L37.1133 57.6441L42.4195 52.3379L48.757 58.6941L67.282 40.1504L72.5883 45.4566L48.757 69.2879Z" fill="url(#paint0_linear_success)" />
+                                    </g>
+                                </g>
+                                <rect
+                                    x="20.3069"
+                                    y="19.3069"
+                                    width="68.3862"
+                                    height="68.3862"
+                                    rx="19.6931"
+                                    stroke="#E1E3E5"
+                                    stroke-width="0.613795"
+                                    shape-rendering="crispEdges"
+                                />
+                            </g>
+                            <defs>
+                                <filter id="filter0_dd_success" x="0" y="0" width="109" height="109" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                                    <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                                    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                                    <feOffset dy="1" />
+                                    <feGaussianBlur stdDeviation="10" />
+                                    <feColorMatrix type="matrix" values="0 0 0 0 0.0790828 0 0 0 0 1 0 0 0 0 0.539541 0 0 0 0.05 0" />
+                                    <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_success" />
+                                    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                                    <feOffset dy="-1" />
+                                    <feGaussianBlur stdDeviation="4" />
+                                    <feComposite in2="hardAlpha" operator="out" />
+                                    <feColorMatrix type="matrix" values="0 0 0 0 0.432809 0 0 0 0 0.127517 0 0 0 0 0.96013 0 0 0 0.72 0" />
+                                    <feBlend mode="normal" in2="effect1_dropShadow_success" result="effect2_dropShadow_success" />
+                                    <feBlend mode="normal" in="SourceGraphic" in2="effect2_dropShadow_success" result="shape" />
+                                </filter>
+                                <filter id="filter1_diii_success" x="29.8818" y="31" width="49" height="51" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                                    <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                                    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                                    <feOffset dy="4" />
+                                    <feGaussianBlur stdDeviation="1" />
+                                    <feComposite in2="hardAlpha" operator="out" />
+                                    <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.06 0" />
+                                    <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_success" />
+                                    <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_success" result="shape" />
+                                    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                                    <feOffset />
+                                    <feGaussianBlur stdDeviation="1.67407" />
+                                    <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+                                    <feColorMatrix type="matrix" values="0 0 0 0 0.0235045 0 0 0 0 0.0724697 0 0 0 0 0.118555 0 0 0 0.5 0" />
+                                    <feBlend mode="soft-light" in2="shape" result="effect2_innerShadow_success" />
+                                    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                                    <feOffset dy="1.67407" />
+                                    <feGaussianBlur stdDeviation="1.67407" />
+                                    <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+                                    <feColorMatrix type="matrix" values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 1 0" />
+                                    <feBlend mode="soft-light" in2="effect2_innerShadow_success" result="effect3_innerShadow_success" />
+                                    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                                    <feOffset dx="2.51111" />
+                                    <feGaussianBlur stdDeviation="0.837037" />
+                                    <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+                                    <feColorMatrix type="matrix" values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 1 0" />
+                                    <feBlend mode="soft-light" in2="effect3_innerShadow_success" result="effect4_innerShadow_success" />
+                                </filter>
+                                <linearGradient id="paint0_linear_success" x1="54.8508" y1="40.1504" x2="54.8508" y2="69.2879" gradientUnits="userSpaceOnUse">
+                                    <stop stop-color="#F62E76" />
+                                    <stop offset="0.259615" stop-color="#A950FF" />
+                                    <stop offset="0.663462" stop-color="#CC8EFF" />
+                                    <stop offset="1" stop-color="#E9C1FF" />
+                                </linearGradient>
+                                <clipPath id="clip0_success">
+                                    <rect x="20" y="19" width="69" height="69" rx="20" fill="white" />
+                                </clipPath>
+                            </defs>
+                        </svg>
                     </div>
 
                     <h1>{{ $t("welcome_copilot.success_page.title") }}</h1>
@@ -52,7 +125,6 @@
     import {computed} from "vue";
     import {useI18n} from "vue-i18n";
     import {useRoute} from "vue-router";
-    import CheckBold from "vue-material-design-icons/CheckBold.vue";
     import PlayBoxMultiple from "vue-material-design-icons/PlayBoxMultiple.vue";
     import BookOpenVariant from "vue-material-design-icons/BookOpenVariant.vue";
     import ChevronRight from "vue-material-design-icons/ChevronRight.vue";
@@ -127,7 +199,7 @@
         margin-bottom: 2rem;
 
         h1 {
-            margin: 1.5rem 0 1rem;
+            margin: 0.5rem 0 1rem;
             color: var(--ks-content-primary);
             font-size: $font-size-lg;
             font-weight: 700;
@@ -143,17 +215,14 @@
     }
 
     .welcome-success-icon {
-        display: grid;
-        place-items: center;
-        width: 68px;
-        height: 68px;
-        border-radius: 18px;
-        background: linear-gradient(180deg, #35006c 0%, #240047 100%);
-        color: #f2b8ff;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 100px;
+        height: 100px;
 
-        :deep(svg) {
-            width: 34px;
-            height: 34px;
+        svg {
+            display: block;
         }
     }
 

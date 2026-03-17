6344df1664406a16fad2b6140d44b295cc6d27d1
brian.mulier <bmmulier@hotmail.fr>
2026-03-17 16:29:53 +0100

    feat(core): introduce new onboarding

--- ui/src/components/ai/AiIcon.vue
+++ ui/src/components/ai/AiIcon.vue
@@ -7,6 +7,7 @@
         height: 20px;
         min-width: 20px;
         background: center url("../../assets/icons/ai-agent.svg#file");
+        background-repeat: no-repeat;
 
         html.light & {
             background: center url("../../assets/icons/ai-agent-light.svg#file");

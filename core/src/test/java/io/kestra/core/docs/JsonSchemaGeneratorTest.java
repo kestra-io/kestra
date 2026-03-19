e04f13085dc3b93381a67d4db7eaa17d10059604
Anna Geller <anna.m.geller@gmail.com>
2026-03-19 09:07:07 +0100

    fix(core): condition must be required in the if task
    closes https://github.com/kestra-io/kestra-ee/issues/7055

--- core/src/test/java/io/kestra/core/docs/JsonSchemaGeneratorTest.java
+++ core/src/test/java/io/kestra/core/docs/JsonSchemaGeneratorTest.java
@@ -25,6 +25,7 @@ import io.kestra.plugin.core.dashboard.data.Executions;
 import io.kestra.plugin.core.debug.Echo;
 import io.kestra.plugin.core.debug.Return;
 import io.kestra.plugin.core.flow.Dag;
+import io.kestra.plugin.core.flow.If;
 import io.kestra.plugin.core.log.Log;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.inject.Inject;
@@ -197,6 +198,20 @@ class JsonSchemaGeneratorTest {
 
             var dag = definitions.get(Dag.class.getName());
             assertThat((List<String>) dag.get("required"), not(contains("errors")));
+        });
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    void ifTask() throws URISyntaxException {
+        Helpers.runApplicationContext((applicationContext) -> {
+            JsonSchemaGenerator jsonSchemaGenerator = applicationContext.getBean(JsonSchemaGenerator.class);
+
+            Map<String, Object> generate = jsonSchemaGenerator.schemas(If.class);
+
+            var definitions = (Map<String, Map<String, Object>>) generate.get("definitions");
+            var ifTask = definitions.get(If.class.getName());
+            assertThat((List<String>) ifTask.get("required"), hasItems("condition", "then"));
         });
     }
 

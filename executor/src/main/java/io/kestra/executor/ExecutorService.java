dfe80bc20530aed54aaea388a88658ec7de10db8
steven meek <smeek@kestra.io>
2026-03-22 22:46:20 +0000

    chore: remove flowExecutorInterface method and use provider directly.

--- executor/src/main/java/io/kestra/executor/ExecutorService.java
+++ executor/src/main/java/io/kestra/executor/ExecutorService.java
@@ -99,15 +99,6 @@ public class ExecutorService {
     @Inject
     private Provider<FlowMetaStoreInterface> flowMetaStoreInterfaceProvider;
 
-    private FlowMetaStoreInterface flowExecutorInterface() {
-        // bean is injected late, so we need to wait
-        if (this.flowExecutorInterface == null) {
-            this.flowExecutorInterface = flowMetaStoreInterfaceProvider.get();
-        }
-
-        return this.flowExecutorInterface;
-    }
-
     public ExecutionRunning processExecutionRunning(FlowInterface flow, int runningCount, ExecutionRunning executionRunning) {
         // if concurrency was removed, it can be null as we always get the latest flow definition
         if (flow.getConcurrency() != null && runningCount >= flow.getConcurrency().getLimit()) {
@@ -1020,7 +1011,7 @@ public class ExecutorService {
                         executor.getExecution(),
                         executableTaskRun
                     );
-                    List<SubflowExecution<?>> subflowExecutions = executableTask.createSubflowExecutions(runContext, flowExecutorInterface(), executor.getFlow(), executor.getExecution(), executableTaskRun);
+                    List<SubflowExecution<?>> subflowExecutions = executableTask.createSubflowExecutions(runContext, flowMetaStoreInterfaceProvider.get(), executor.getFlow(), executor.getExecution(), executableTaskRun);
                     if (subflowExecutions.isEmpty()) {
                         // if no executions we move the task to SUCCESS immediately
                         executor.withExecution(

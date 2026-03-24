a436a7e2b630f00a382a96adf2e79fd78e4482d2
steven meek <smeek@kestra.io>
2026-03-24 13:26:49 +0000

    chore: use BeanProvider where beans is not able to be resolved at injection time but may be resolved after injection.

--- cli/src/main/java/io/kestra/cli/AbstractCommand.java
+++ cli/src/main/java/io/kestra/cli/AbstractCommand.java
@@ -7,6 +7,7 @@ import io.kestra.core.plugins.PluginManager;
 import io.kestra.core.plugins.PluginRegistry;
 import io.kestra.webserver.services.FlowAutoLoaderService;
 import io.micronaut.context.ApplicationContext;
+import io.micronaut.context.BeanProvider;
 import io.micronaut.context.env.yaml.YamlPropertySourceLoader;
 import io.micronaut.http.uri.UriBuilder;
 import io.micronaut.management.endpoint.EndpointDefaultConfiguration;
@@ -47,7 +48,7 @@ public abstract class AbstractCommand extends BaseCommand implements Callable<In
     private Optional<EmbeddedServer> embeddedServer;
 
     @Inject
-    private Optional<FlowAutoLoaderService> flowAutoLoaderService;
+    private BeanProvider<FlowAutoLoaderService> flowAutoLoaderService;
 
     @Inject
     protected Provider<PluginRegistry> pluginRegistryProvider;

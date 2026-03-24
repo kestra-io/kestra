ee4aea886a7d531933816f70ec19fe8a9a45f76c
steven meek <smeek@kestra.io>
2026-03-24 16:08:01 +0000

    chore: remove redundant FlowMetaStoreInterfaceProvider.

--- executor/src/main/java/io/kestra/executor/ExecutorService.java
+++ executor/src/main/java/io/kestra/executor/ExecutorService.java
@@ -94,9 +94,6 @@ public class ExecutorService {
     @Inject
     private TaskOutputService taskOutputService;
 
-    @Inject
-    private Provider<FlowMetaStoreInterface> flowMetaStoreInterfaceProvider;
-
     public ExecutionRunning processExecutionRunning(FlowInterface flow, int runningCount, ExecutionRunning executionRunning) {
         // if concurrency was removed, it can be null as we always get the latest flow definition
         if (flow.getConcurrency() != null && runningCount >= flow.getConcurrency().getLimit()) {
@@ -1125,7 +1122,7 @@ public class ExecutorService {
                         executableTaskRun
                     );
                     List<SubflowExecution<?>> subflowExecutions = executableTask
-                        .createSubflowExecutions(runContext, flowMetaStoreInterfaceProvider.get(), executor.getFlow(), executor.getExecution(), executableTaskRun);
+                        .createSubflowExecutions(runContext, flowExecutorInterface, executor.getFlow(), executor.getExecution(), executableTaskRun);
                     if (subflowExecutions.isEmpty()) {
                         // if no executions we move the task to SUCCESS immediately
                         executor.withExecution(

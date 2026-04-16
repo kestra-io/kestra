25cdb9a1cf552762f634459f11643df8cef136ac
nKwiatkowski <nkwiatkowski@kestra.io>
2026-04-16 17:52:19 +0200

    clean(core): merge with develop

--- jdbc-postgres/src/main/resources/migrations/postgres/V3_3__execution_parent_id.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE executions ADD COLUMN IF NOT EXISTS "parent_id" VARCHAR(100) GENERATED ALWAYS AS (value #>> '{parentId}') STORED;
-ALTER TABLE executions ADD COLUMN IF NOT EXISTS "loop_run_index" INT GENERATED ALWAYS AS ((value #>> '{loopRun,index}')::INT) STORED;
-
-CREATE INDEX IF NOT EXISTS executions_parent_id ON executions ("deleted", "tenant_id", "parent_id");

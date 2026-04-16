87c53427ba859cab15ead8d9cf203920c39b7069
MilosPaunovic <paun992@hotmail.com>
2026-04-16 10:20:29 +0200

    Revert "feat(core): add resources prop to WelcomeCopilot to allow per-page customization of help section"
    This reverts commit 31bbc10e817df728368a560f3ddb715054fd734d.

--- ui/src/components/onboarding/WelcomeCopilot.vue
+++ ui/src/components/onboarding/WelcomeCopilot.vue
@@ -47,7 +47,7 @@
                     </el-tag>
                 </div>
 
-                <div v-if="welcomeResources.length > 0" class="welcome-help-section">
+                <div class="welcome-help-section">
                     <p class="welcome-help-title">
                         {{ $t("welcome_copilot.need_help") }}
                     </p>
@@ -65,7 +65,6 @@
     import AiCopilot from "../ai/AiCopilot.vue";
     import OnboardingResourceList from "./OnboardingResourceList.vue";
     import {useOnboardingResources} from "./useOnboardingResources";
-    import type {OnboardingResourceItem} from "./OnboardingResourceList.vue";
 
     import type {AiGenerationType} from "../../utils/constants";
     import Utils from "../../utils/utils";
@@ -82,7 +81,6 @@
         generationType?: AiGenerationType;
         examples: WelcomeCopilotExample[];
         namespace?: string;
-        resources?: OnboardingResourceItem[];
     }>();
 
     const emit = defineEmits<{
@@ -108,9 +106,7 @@
     );
 
     const {onboardingResources} = useOnboardingResources();
-    const welcomeResources = computed(() =>
-        props.resources !== undefined ? props.resources : onboardingResources.value.slice(0, 3),
-    );
+    const welcomeResources = computed(() => onboardingResources.value.slice(0, 3));
 
     function selectExample(index: number) {
         activeIndex.value = index;

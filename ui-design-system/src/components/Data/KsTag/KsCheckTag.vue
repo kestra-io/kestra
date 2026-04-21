5b260210ddf9eb40cc93f507cbc349e178121f13
MilosPaunovic <paun992@hotmail.com>
2026-04-21 14:45:10 +0200

    feat(core): add size prop to KsCheckTag defaulting to small

--- ui-design-system/src/components/Data/KsTag/KsCheckTag.vue
+++ ui-design-system/src/components/Data/KsTag/KsCheckTag.vue
@@ -1,6 +1,7 @@
 <template>
     <ElCheckTag
         v-bind="({...filteredProps(), ...$attrs} as any)"
+        :class="`kel-check-tag--${size}`"
         @change="emit('change', $event)"
     >
         <template v-if="$slots.default" #default>
@@ -17,12 +18,15 @@
 
     defineOptions({inheritAttrs: false})
 
-    const props = defineProps<{
+    const props = withDefaults(defineProps<{
         checked?: boolean
         disabled?: boolean
-    }>()
+        size?: "large" | "default" | "small"
+    }>(), {
+        size: "small",
+    })
 
-    const filteredProps = useFilteredProps(props)
+    const filteredProps = useFilteredProps(props, ["size"])
 
     const emit = defineEmits<{
         change: [checked: boolean]
@@ -36,4 +40,21 @@
 <style lang="scss">
     @use '../../../assets/styles/el-ns';
     @use 'element-plus/theme-chalk/src/check-tag';
+
+    .kel-check-tag {
+        &--small {
+            --kel-font-size-base: var(--kel-font-size-extra-small);
+            padding: 3px 8px;
+        }
+
+        &--default {
+            --kel-font-size-base: var(--kel-font-size-base);
+            padding: 7px 15px;
+        }
+
+        &--large {
+            --kel-font-size-base: var(--kel-font-size-medium);
+            padding: 9px 18px;
+        }
+    }
 </style>

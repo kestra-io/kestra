ca2a471e483b1cbb52ac5175959a225abcbcb755
RemiBarthe <barthe.rem@gmail.com>
2026-04-22 17:38:37 +0200

    feat(breadcrumb): refactor breadcrumb rendering for improved clarity

--- ui/src/components/layout/Breadcrumb.vue
+++ ui/src/components/layout/Breadcrumb.vue
@@ -1,8 +1,6 @@
 <template>
     <div class="breadcrumb">
-        <template v-for="(item, index) in visibleItems" :key="index">
-            <span v-if="index > 0" class="separator">/</span>
-
+        <template v-for="item in visibleItems" :key="item.label">
             <el-dropdown v-if="item.ellipsis" trigger="click" :showArrow="false" size="large">
                 <button class="ellipsis-btn">
                     ...
@@ -23,21 +21,19 @@
                 </template>
             </el-dropdown>
 
-            <RouterLink
-                v-else-if="item.link && !item.disabled && !item.last"
-                class="item"
-                :to="item.link"
-            >
+            <RouterLink v-else-if="item.link && !item.disabled" class="item" :to="item.link">
                 {{ item.label }}
             </RouterLink>
 
-            <span v-else :class="['item', {'item--last': item.last}]">
-                <template v-if="item.last">
-                    <slot name="title">{{ item.label }}</slot>
-                </template>
-                <template v-else>{{ item.label }}</template>
-            </span>
+            <span v-else class="item">{{ item.label }}</span>
+            <span class="separator">/</span>
         </template>
+
+        <h1 class="item item--last">
+            <slot name="title">
+                {{ title }}
+            </slot>
+        </h1>
     </div>
 </template>
 
@@ -46,39 +42,26 @@
     import {RouterLink} from "vue-router";
     import type {BreadcrumbItem} from "./breadcrumbTypes";
 
-    const props = defineProps<{
+    const {items, title} = defineProps<{
         items: BreadcrumbItem[];
         title: string;
     }>();
 
-    const allItems = computed<(BreadcrumbItem & {last: boolean; ellipsis: false})[]>(() => [
-        ...props.items.map(item => ({...item, last: false, ellipsis: false as const})),
-        {label: props.title, last: true, ellipsis: false as const},
-    ]);
+    const COLLAPSE_THRESHOLD = 4;
 
-    const COLLAPSE_THRESHOLD = 5;
-    const shouldCollapse = computed(() => allItems.value.length >= COLLAPSE_THRESHOLD);
+    type VisibleItem = BreadcrumbItem & {ellipsis?: boolean};
 
-    type VisibleItem = BreadcrumbItem & {last: boolean; ellipsis: boolean};
+    const shouldCollapse = computed(() => items.length >= COLLAPSE_THRESHOLD);
 
-    const visibleItems = computed<VisibleItem[]>(() => {
-        const items = allItems.value;
-        if (!shouldCollapse.value) {
-            return items;
-        }
-        return [
-            items[0],
-            {label: "...", last: false, ellipsis: true},
-            items[items.length - 2],
-            items[items.length - 1],
-        ];
-    });
+    const visibleItems = computed<VisibleItem[]>(() =>
+        shouldCollapse.value
+            ? [items[0], {label: "...", ellipsis: true}, items[items.length - 1]]
+            : items
+    );
 
-    const collapsedItems = computed(() => {
-        if (!shouldCollapse.value) return [];
-        const items = allItems.value;
-        return items.slice(1, items.length - 2);
-    });
+    const collapsedItems = computed<BreadcrumbItem[]>(() =>
+        shouldCollapse.value ? items.slice(1, items.length - 1) : []
+    );
 </script>
 
 <style scoped lang="scss">
@@ -104,6 +87,7 @@
                 font-size: var(--font-size-base);
                 font-weight: 700;
                 color: var(--ks-content-primary);
+                margin: 0;
             }
         }
 

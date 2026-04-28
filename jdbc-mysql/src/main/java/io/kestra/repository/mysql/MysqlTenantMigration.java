96ac496c76728b1b74e61960ad5d296bb2702bb0
nKwiatkowski <nkwiatkowski@kestra.io>
2026-04-28 12:44:10 +0200

    fix(cli): update tenant migration

--- jdbc-mysql/src/main/java/io/kestra/repository/mysql/MysqlTenantMigration.java
+++ jdbc-mysql/src/main/java/io/kestra/repository/mysql/MysqlTenantMigration.java
@@ -1,12 +1,14 @@
 package io.kestra.repository.mysql;
 
-import static io.kestra.core.tenant.TenantService.MAIN_TENANT;
+import org.jooq.DSLContext;
+import org.jooq.Table;
 
 import io.kestra.jdbc.JooqDSLContextWrapper;
 import io.kestra.jdbc.repository.AbstractJdbcTenantMigration;
+
 import jakarta.inject.Singleton;
-import org.jooq.DSLContext;
-import org.jooq.Table;
+
+import static io.kestra.core.tenant.TenantService.MAIN_TENANT;
 
 @Singleton
 @MysqlRepositoryEnabled
@@ -28,13 +30,23 @@ public class MysqlTenantMigration extends AbstractJdbcTenantMigration {
     @Override
     protected int updateTenantIdFieldAndKey(Table<?> table, DSLContext context) {
         String query = """
-            UPDATE `%s`
-            SET
-                `key` = CONCAT(?, '_', `key`),
-                `value` = JSON_SET(`value`, '$.tenantId', ?)
-            WHERE JSON_UNQUOTE(JSON_EXTRACT(`value`, '$.tenantId')) IS NULL
-        """.formatted(table.getName());
+                UPDATE `%s`
+                SET
+                    `key` = CONCAT(?, '_', `key`),
+                    `value` = JSON_SET(`value`, '$.tenantId', ?)
+                WHERE JSON_UNQUOTE(JSON_EXTRACT(`value`, '$.tenantId')) IS NULL
+            """.formatted(table.getName());
 
         return context.execute(query, MAIN_TENANT, MAIN_TENANT);
+    }
+
+    @Override
+    protected String selectExecutionsQuery() {
+        return "SELECT `key`, `value` FROM `executions` WHERE JSON_LENGTH(`value`, '$.taskRunList') > 0 LIMIT ? OFFSET ?";
+    }
+
+    @Override
+    protected String updateExecutionQuery() {
+        return "UPDATE `executions` SET `value` = ? WHERE `key` = ?";
     }
 }

f7142ab6515214c5813633fd0348c3a0e04df1d4
nKwiatkowski <nkwiatkowski@kestra.io>
2026-04-29 19:19:07 +0200

    feat(api): remove NOtNull on field that are nullable

--- core/src/main/java/io/kestra/core/models/flows/AbstractFlow.java
+++ core/src/main/java/io/kestra/core/models/flows/AbstractFlow.java
@@ -37,7 +37,6 @@ public abstract class AbstractFlow implements FlowInterface {
     @Size(min = 1, max = 150)
     String namespace;
 
-    @NotNull
     @Min(value = 1)
     Integer revision;
 
@@ -61,7 +60,6 @@ public abstract class AbstractFlow implements FlowInterface {
     @Builder.Default
     boolean deleted = false;
 
-    @NotNull
     @Hidden
     @Pattern(regexp = "^[a-z0-9][a-z0-9_-]*")
     String tenantId;

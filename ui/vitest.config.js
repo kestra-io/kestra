ee3f2bbf813cd18a2ed3ae557978c31cf8e2193e
Bart Ledoux <bledoux@kestra.io>
2026-05-04 10:13:15 +0200

    silence warning by removing storybook project annotations

--- ui/vitest.config.js
+++ ui/vitest.config.js
@@ -42,7 +42,6 @@ export default defineConfig({
                             },
                         ],
                     },
-                    setupFiles: [".storybook/vitest.setup.ts"],
                 },
             }),
         ],

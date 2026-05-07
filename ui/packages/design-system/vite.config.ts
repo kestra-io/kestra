b1f5d5ee72924492ef34b7daf0166c336f8ff911
Bart Ledoux <bledoux@kestra.io>
2026-05-07 13:58:05 +0200

    feat: add TypeScript configuration and styles for design system

--- ui/packages/design-system/vite.config.ts
+++ ui/packages/design-system/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [
     vue(),
     dts({
-        tsconfigPath: "./tsconfig.json", 
+        tsconfigPath: "./tsconfig.app.json", 
         exclude: ["tests/**"]
     })
   ],

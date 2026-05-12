50bbbb9db6da2ee9130023502c29adfab3d8abb6
Roman Acevedo <roman.acevedo62@gmail.com>
2026-05-12 20:12:04 +0200

    fix(storage): parenTransversalGuard was not handling encoded characters
    - fixes https://github.com/kestra-io/kestra-ee/issues/7686

--- core/src/test/java/io/kestra/core/utils/FileUtilsTest.java
+++ core/src/test/java/io/kestra/core/utils/FileUtilsTest.java
@@ -1,6 +1,10 @@
 package io.kestra.core.utils;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.net.URI;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -14,5 +18,26 @@ class FileUtilsTest {
         assertThat(FileUtils.getExtension("/file/hello.txt")).isEqualTo(".txt");
         assertThat(FileUtils.getExtension("/file/hello.file with spaces.txt")).isEqualTo(".txt");
         assertThat(FileUtils.getExtension("/file/hello.file.with.multiple.dots.txt")).isEqualTo(".txt");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+        "",
+        "kestra:///ns/flow/exec/abc/output",
+        "kestra:///ns/flow/exec/abc/.output",
+    })
+    void isParentTraversal_false(String path) {
+        assertThat(FileUtils.isParentTraversal(URI.create(path))).isFalse();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+        "kestra:///ns/flow/exec/abc/../../../etc/passwd",
+        "kestra:///ns/flow/exec/abc/%2E%2E/%2E%2E/%2E%2E/etc/passwd",
+        "kestra:///ns/flow/exec/abc/%2e%2e/%2e%2e/%2e%2e/etc/passwd",
+        "kestra:///ns/flow/exec/abc%2F%2E%2E%2Fetc%2Fpasswd"
+    })
+    void isParentTraversal_true(String path) {
+        assertThat(FileUtils.isParentTraversal(URI.create(path))).isTrue();
     }
 }

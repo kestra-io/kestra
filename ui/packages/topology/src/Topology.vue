4e8540a1ccf74079abb7eda9e5da88809ab499f2
François Delbrayelle <fdelbrayelle@kestra.io>
2026-05-18 16:08:00 +0200

    fix(topology): stop click propagation on all Controls buttons
    ControlButton clicks were bubbling up to parent router-link elements,
    causing the router to navigate to the nearest tab route (e.g. /edit),
    resulting in a 404. Adding .stop to all ControlButton @click handlers
    prevents the bubble from reaching the tab router-link.

--- ui/packages/topology/src/Topology.vue
+++ ui/packages/topology/src/Topology.vue
@@ -92,19 +92,19 @@
         </template>
 
         <Controls v-if="controlsShown" :showInteractive="false" :showFitView="false">
-            <ControlButton @click="showExtraDetails = !showExtraDetails" :class="{'active': showExtraDetails}">
+            <ControlButton @click.stop="showExtraDetails = !showExtraDetails" :class="{'active': showExtraDetails}">
                 <Information />
             </ControlButton>
-            <ControlButton @click="fitView()">
+            <ControlButton @click.stop="fitView()">
                 <svg viewBox="0 0 32 32" style="width:12px;height:12px"><path d="M3.692 4.63c0-.53.4-.938.939-.938h5.215V0H4.708C2.13 0 0 2.054 0 4.63v5.216h3.692V4.631zM27.354 0h-5.2v3.692h5.17c.53 0 .984.4.984.939v5.215H32V4.631A4.624 4.624 0 0 0 27.354 0zm.954 24.83c0 .532-.4.94-.939.94h-5.215v3.768h5.215c2.577 0 4.631-2.13 4.631-4.707v-5.139h-3.692v5.139zm-23.677.94a.919.919 0 0 1-.939-.94v-5.138H0v5.139c0 2.577 2.13 4.707 4.708 4.707h5.138V25.77H4.631z" fill="currentColor" /></svg>
             </ControlButton>
-            <ControlButton @click="uncollapseAll()" v-if="collapsed.size > 0">
+            <ControlButton @click.stop="uncollapseAll()" v-if="collapsed.size > 0">
                 <ArrowExpandAll />
             </ControlButton>
-            <ControlButton @click="emit('toggle-orientation', $event)" v-if="toggleOrientationButton">
+            <ControlButton @click.stop="emit('toggle-orientation', $event)" v-if="toggleOrientationButton">
                 <component :is="isHorizontal ? SplitCellsHorizontal : SplitCellsVertical" />
             </ControlButton>
-            <ControlButton @click="toggleDropdown">
+            <ControlButton @click.stop="toggleDropdown">
                 <Download />
             </ControlButton>
             <ul v-if="isDropdownOpen" class="exporting">

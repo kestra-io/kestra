d44cb9ec69c71f1a8c486ff3ceafb375b6e3bfea
François Delbrayelle <fdelbrayelle@kestra.io>
2026-05-18 11:01:04 +0200

    feat(topology): add uncollapse all nodes button
    Adds an ArrowExpandAll control button that appears when there are
    collapsed cluster nodes. Clicking it clears all collapsed state and
    re-generates the graph with all nodes expanded.

--- ui/packages/topology/src/Topology.vue
+++ ui/packages/topology/src/Topology.vue
@@ -98,6 +98,9 @@
             <ControlButton @click="fitView()">
                 <svg viewBox="0 0 32 32" style="width:12px;height:12px"><path d="M3.692 4.63c0-.53.4-.938.939-.938h5.215V0H4.708C2.13 0 0 2.054 0 4.63v5.216h3.692V4.631zM27.354 0h-5.2v3.692h5.17c.53 0 .984.4.984.939v5.215H32V4.631A4.624 4.624 0 0 0 27.354 0zm.954 24.83c0 .532-.4.94-.939.94h-5.215v3.768h5.215c2.577 0 4.631-2.13 4.631-4.707v-5.139h-3.692v5.139zm-23.677.94a.919.919 0 0 1-.939-.94v-5.138H0v5.139c0 2.577 2.13 4.707 4.708 4.707h5.138V25.77H4.631z" fill="currentColor" /></svg>
             </ControlButton>
+            <ControlButton @click="uncollapseAll()" v-if="collapsed.size > 0">
+                <ArrowExpandAll />
+            </ControlButton>
             <ControlButton @click="emit('toggle-orientation', $event)" v-if="toggleOrientationButton">
                 <component :is="isHorizontal ? SplitCellsHorizontal : SplitCellsVertical" />
             </ControlButton>
@@ -132,6 +135,7 @@
     import SplitCellsHorizontal from "./assets/icons/SplitCellsHorizontal.vue"
     import Download from "vue-material-design-icons/Download.vue"
     import Information from "vue-material-design-icons/Information.vue"
+    import ArrowExpandAll from "vue-material-design-icons/ArrowExpandAll.vue"
     import {cssVar as cssVariable} from "@kestra-io/design-system"
     import {CLUSTER_PREFIX} from "./utils/constants"
     import * as flowYamlUtils from "./utils/flowYamlUtils"
@@ -482,6 +486,14 @@
         generateGraph()
     }
 
+
+    const uncollapseAll = () => {
+        collapsed.value = new Set()
+        hiddenNodes.value = []
+        edgeReplacer.value = {}
+        clusterToNode.value = []
+        generateGraph()
+    }
 
     const controlsShown = ref(true)
     const isDropdownOpen = ref(false)

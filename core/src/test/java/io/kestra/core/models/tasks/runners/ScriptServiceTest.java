90063d6e58368b4859945da04f1efac53ed3a12d
Sagar Khandagre <sagar.khandagre998@gmail.com>
2026-05-31 10:56:56 +0530

    remove unsupported functionality from test , now its supported

--- core/src/test/java/io/kestra/core/models/tasks/runners/ScriptServiceTest.java
+++ core/src/test/java/io/kestra/core/models/tasks/runners/ScriptServiceTest.java
@@ -129,10 +129,6 @@ class ScriptServiceTest {
 
     @Test
     void shouldRejectOutputFileWithUnsupportedChars() {
-        assertThatThrownBy(() -> ScriptService.validateStoragePath("file🐸name.txt"))
-            .isInstanceOf(IOException.class)
-            .hasMessageContaining("unsupported characters");
-
         assertThatThrownBy(() -> ScriptService.validateStoragePath("file name.txt"))
             .isInstanceOf(IOException.class)
             .hasMessageContaining("unsupported characters");

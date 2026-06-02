53d393971e13c4e610cb7f1db9cbec37d0dc8c07
RemiBarthe <barthe.rem@gmail.com>
2026-06-02 11:58:00 +0200

    fix(executions): handle purged output files in the outputs panel

--- ui/src/components/executions/VarValue.vue
+++ ui/src/components/executions/VarValue.vue
@@ -16,7 +16,17 @@
             ({{ humanSize }})
         </KsButton>
     </KsButtonGroup>
-    <KsButtonGroup v-else-if="isURI(value)">
+    <KsButtonGroup v-else-if="Utils.isFile(value) && fileStatus === 'loading'">
+        <KsButton disabled loading size="small" type="primary">
+            {{ $t('download') }}
+        </KsButton>
+    </KsButtonGroup>
+    <KsTooltip v-else-if="Utils.isFile(value) && fileStatus === 'missing'" :content="$t('file unavailable description')">
+        <KsButton disabled size="small" type="primary" :icon="FileAlertOutline">
+            {{ $t('file unavailable') }}
+        </KsButton>
+    </KsTooltip>
+    <KsButtonGroup v-else-if="isURI(value) && !Utils.isFile(value)">
         <KsButton
             type="primary"
             tag="a"
@@ -57,6 +67,7 @@
     import {ref, watch, onMounted} from "vue"
     import Download from "vue-material-design-icons/Download.vue"
     import OpenInNew from "vue-material-design-icons/OpenInNew.vue"
+    import FileAlertOutline from "vue-material-design-icons/FileAlertOutline.vue"
     import FilePreview from "./FilePreview.vue"
     import {KsEditor} from "@kestra-io/design-system"
     import {useEditorBindings} from "../../composables/useEditorBindings"
@@ -85,6 +96,7 @@
     const editorBindings = useEditorBindings()
 
     const humanSize = ref<string>("")
+    const fileStatus = ref<"loading" | "available" | "missing">("loading")
 
     const isFileValid = (value: unknown): boolean => {
         return Utils.isFile(value) && humanSize.value.length > 0 && humanSize.value !== "0B"
@@ -149,11 +161,17 @@
 
     const getFileSize = async (): Promise<void> => {
         if (Utils.isFile(props.value) && props.execution?.id) {
-            try {
-                const response = await axios.get<FileMetadata>(`${apiUrl()}/executions/${props.execution.id}/file/metas?path=${props.value}`)
+            humanSize.value = ""
+            fileStatus.value = "loading"
+            const response = await axios.get<FileMetadata>(
+                `${apiUrl()}/executions/${props.execution.id}/file/metas?path=${props.value}`,
+                {validateStatus: (status: number) => status === 200 || status === 404 || status === 422},
+            )
+            if (response.status === 200) {
                 humanSize.value = Utils.humanFileSize(response.data.size)
-            } catch (error) {
-                console.error("Failed to fetch file size:", error)
+                fileStatus.value = "available"
+            } else {
+                fileStatus.value = "missing"
             }
         }
     }

dc154bd266d08547a5de16452d5afbebf903a665
Bart Ledoux <bledoux@kestra.io>
2026-06-09 16:14:45 +0200

    fix file preview for big files

--- ui/src/components/executions/FilePreview.vue
+++ ui/src/components/executions/FilePreview.vue
@@ -1,5 +1,16 @@
 <template>
-    <div v-if="!preview">
+    <div v-if="bigFileWarning">
+        <KsAlert type="warning" :closable="false">
+            {{ $t("executions.file_preview.big_file_warning") }}
+        </KsAlert>
+        <KsButton
+            type="primary"
+            @click="bigFileWarning = false;loadPreview()"
+        >
+            {{ $t("executions.file_preview.load_anyway") }}
+        </KsButton>
+    </div>
+    <div v-else-if="!preview">
         Loading...
     </div>
     <template v-else>
@@ -15,6 +26,8 @@
 
 <script lang="ts" setup>
     import {computed, onMounted, ref, watch} from "vue"
+    import {type FileMetas} from "@kestra-io/kestra-sdk"
+    import * as ExecutionsAPI from "@kestra-io/kestra-sdk/executions"
     import FilePreviewForm, {type EncodingOption} from "./FilePreviewForm.vue"
     import RawPreview, {type Preview} from "./RawPreview.vue"
     import {useExecutionsStore} from "../../stores/executions.ts"
@@ -31,20 +44,47 @@
     const encodingModel = ref<EncodingOption["value"]>()
     const forceEditor = ref<boolean>()
     const preview = ref<Preview>()
+    const bigFileWarning = ref<boolean>(false)
+    const metadata = ref<FileMetas>()
 
-    const emits = defineEmits(["preview"])
+    async function getFileMeta() {
+        return await ExecutionsAPI.fileMetadatasFromExecution({
+            executionId: props.executionId,
+            path: props.path,
+        })
+    }
+
+    async function loadPreview() {
+        preview.value = await executionsStore
+            .filePreview({
+                executionId: props.executionId,
+                path: props.path,
+                maxRows: maxPreview.value,
+                encoding: encodingModel.value,
+            })
+    }
 
     watch(
         [maxPreview, encodingModel],
         async ([maxRows, encoding]) => {
             if(maxRows === undefined || encoding === undefined) return
-            preview.value = await executionsStore
-                .filePreview({
-                    executionId: props.executionId,
-                    path: props.path,
-                    maxRows,
-                    encoding,
-                })
+            metadata.value = await getFileMeta()
+            if(metadata.value.size === 0) {
+                preview.value = {
+                    type: "RAW",
+                    content: "",
+                    truncated: false,
+                }
+                return
+            }
+            bigFileWarning.value = metadata.value.size >= maxRows * 10_000
+            if(bigFileWarning.value) {
+                // For big files, we want to signal the user that it can take 
+                // significant time to load the preview, so we set maxRows to 
+                // undefined to disable the limit and load the full file.
+                return
+            }
+            await loadPreview()
         },
         {immediate: true},
     )

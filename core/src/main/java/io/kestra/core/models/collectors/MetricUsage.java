28d03e663b721efe925691806cc9d76498a32716
Loïc Mathieu <loikeseke@gmail.com>
2026-07-07 16:19:24 +0200

    feat(system): add metrics for all actions on an execution
    And forward them to the metric usage report.
    
    Fixes https://github.com/kestra-io/kestra-ee/issues/8971

--- core/src/main/java/io/kestra/core/models/collectors/MetricUsage.java
+++ core/src/main/java/io/kestra/core/models/collectors/MetricUsage.java
@@ -16,6 +16,7 @@ public class MetricUsage {
     private final QueueMetric queue;
     private final WorkerMetric worker;
     private final ServerMetric server;
+    private final WebserverMetric webserver;
 
     public static MetricUsage of(MetricRegistry metricRegistry) {
         return MetricUsage.builder()
@@ -23,6 +24,7 @@ public class MetricUsage {
             .queue(QueueMetric.of(metricRegistry))
             .worker(WorkerMetric.of(metricRegistry))
             .server(ServerMetric.of(metricRegistry))
+            .webserver(WebserverMetric.of(metricRegistry))
             .build();
     }
 
@@ -86,6 +88,38 @@ public class MetricUsage {
         static ServerMetric of(MetricRegistry metricRegistry) {
             return ServerMetric.builder()
                 .maintenanceEnterCount(fromCounter(metricRegistry, MetricRegistry.METRIC_MAINTENANCE_ENTER_COUNT))
+                .build();
+        }
+    }
+
+    @SuperBuilder
+    @Getter
+    public static class WebserverMetric {
+        private final Double restartCount;
+        private final Double replayCount;
+        private final Double pauseCount;
+        private final Double resumeCount;
+        private final Double resumeFromBreakpointCount;
+        private final Double forceRunCount;
+        private final Double killCount;
+        private final Double changeStatusCount;
+        private final Double updateLabelsCount;
+        private final Double unqueueCount;
+        private final Double taskRunChangeStateCount;
+
+        static WebserverMetric of(MetricRegistry metricRegistry) {
+            return WebserverMetric.builder()
+                .restartCount(fromCounter(metricRegistry, MetricRegistry.METRIC_WEBSERVER_EXECUTION_RESTART_TOTAL))
+                .replayCount(fromCounter(metricRegistry, MetricRegistry.METRIC_WEBSERVER_EXECUTION_REPLAY_TOTAL))
+                .pauseCount(fromCounter(metricRegistry, MetricRegistry.METRIC_WEBSERVER_EXECUTION_PAUSE_TOTAL))
+                .resumeCount(fromCounter(metricRegistry, MetricRegistry.METRIC_WEBSERVER_EXECUTION_RESUME_TOTAL))
+                .resumeFromBreakpointCount(fromCounter(metricRegistry, MetricRegistry.METRIC_WEBSERVER_EXECUTION_RESUME_FROM_BREAKPOINT_TOTAL))
+                .forceRunCount(fromCounter(metricRegistry, MetricRegistry.METRIC_WEBSERVER_EXECUTION_FORCE_RUN_TOTAL))
+                .killCount(fromCounter(metricRegistry, MetricRegistry.METRIC_WEBSERVER_EXECUTION_KILL_TOTAL))
+                .changeStatusCount(fromCounter(metricRegistry, MetricRegistry.METRIC_WEBSERVER_EXECUTION_CHANGE_STATUS_TOTAL))
+                .updateLabelsCount(fromCounter(metricRegistry, MetricRegistry.METRIC_WEBSERVER_EXECUTION_UPDATE_LABELS_TOTAL))
+                .unqueueCount(fromCounter(metricRegistry, MetricRegistry.METRIC_WEBSERVER_EXECUTION_UNQUEUE_TOTAL))
+                .taskRunChangeStateCount(fromCounter(metricRegistry, MetricRegistry.METRIC_WEBSERVER_TASKRUN_CHANGE_STATE_TOTAL))
                 .build();
         }
     }

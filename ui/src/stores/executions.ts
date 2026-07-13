bca88132a441e8610c3d1dc6dc3966a1d7c43bab
Barthélémy Ledoux <bledoux@kestra.io>
2026-07-13 17:08:21 +0200

    fix(executions): add flow outputs to execution type and variable explorer

--- ui/src/stores/executions.ts
+++ ui/src/stores/executions.ts
@@ -83,6 +83,7 @@ export type Execution = Omit<Optional<SDKExecution, "deleted">, "taskRunList"> &
     tenantId?: string;
     taskRunList?: Optional<TaskRun, "namespace" | "executionId" | "flowId">[];
     inputs?: Record<string, any>;
+    outputs?: Record<string, any>;
     variables?: Record<string, any>;
 }
 

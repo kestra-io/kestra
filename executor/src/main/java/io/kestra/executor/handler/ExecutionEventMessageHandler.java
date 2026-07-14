5c17bfa07306beb9d37828685d5284a813ccc07c
Hemant M Mehta <hemant29mehta@gmail.com>
2026-07-14 19:03:32 +0530

    fix: failure test
    Signed-off-by: Hemant M Mehta <hemant29mehta@gmail.com>

--- executor/src/main/java/io/kestra/executor/handler/ExecutionEventMessageHandler.java
+++ executor/src/main/java/io/kestra/executor/handler/ExecutionEventMessageHandler.java
@@ -171,16 +171,6 @@ public class ExecutionEventMessageHandler implements ExecutorMessageHandler<Exec
                                         case CANCEL -> execution.withState(State.Type.CANCELLED);
                                     };
 
-                            ExecutionRunning processed = concurrencyLimitStateStore.countThenProcess(flow, (txContext, concurrencyLimit) ->
-                            {
-                                Integer queueSize = flow.getConcurrency().getQueueSize();
-                                int queuedCount = queueSize == null ? 0 : executionQueuedStateStore.count(txContext, flow.getTenantId(), flow.getNamespace(), flow.getId());
-                                ExecutionRunning computed = executorService.processExecutionRunning(flow, concurrencyLimit.getRunning(), queuedCount, executionRunning.withExecution(execution)); // be sure that the execution running contains the latest value of the execution
-                                if (computed.getConcurrencyState() == ExecutionRunning.ConcurrencyState.RUNNING && !computed.getExecution().getState().isTerminated()) {
-                                    return Pair.of(computed, concurrencyLimit.withRunning(concurrencyLimit.getRunning() + 1));
-                                }
-                                if (computed.getConcurrencyState() == ExecutionRunning.ConcurrencyState.QUEUED) {
-                                    executionQueuedStateStore.save(txContext, ExecutionQueued.fromExecutionRunning(computed));
                                     metricRegistry
                                         .counter(
                                             MetricRegistry.METRIC_EXECUTOR_QUOTA_EXCEEDED_COUNT, MetricRegistry.METRIC_EXECUTOR_QUOTA_EXCEEDED_COUNT_DESCRIPTION, metricRegistry.tags(execution)
@@ -203,10 +193,13 @@ public class ExecutionEventMessageHandler implements ExecutorMessageHandler<Exec
 
                                 ExecutionRunning processed = concurrencyLimitStateStore.countThenProcess(flow, (txContext, concurrencyLimit) ->
                                 {
-                                    ExecutionRunning computed = executorService.processExecutionRunning(flow, concurrencyLimit.getRunning(), executionRunning.withExecution(execution)); // be sure that the execution running contains the latest value of the execution
+                                    Integer queueSize = flow.getConcurrency().getQueueSize();
+                                    int queuedCount = queueSize == null ? 0 : executionQueuedStateStore.count(txContext, flow.getTenantId(), flow.getNamespace(), flow.getId());
+                                    ExecutionRunning computed = executorService.processExecutionRunning(flow, concurrencyLimit.getRunning(), queuedCount, executionRunning.withExecution(execution)); // be sure that the execution running contains the latest value of the execution
                                     if (computed.getConcurrencyState() == ExecutionRunning.ConcurrencyState.RUNNING && !computed.getExecution().getState().isTerminated()) {
                                         return Pair.of(computed, concurrencyLimit.withRunning(concurrencyLimit.getRunning() + 1));
-                                    } else if (computed.getConcurrencyState() == ExecutionRunning.ConcurrencyState.QUEUED) {
+                                    }
+                                    if (computed.getConcurrencyState() == ExecutionRunning.ConcurrencyState.QUEUED) {
                                         executionQueuedStateStore.save(txContext, ExecutionQueued.fromExecutionRunning(computed));
                                     }
                                     return Pair.of(computed, concurrencyLimit);

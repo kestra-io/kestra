0161826404e62a6c1274a729e3b35dba2dd23c66
atharva-kamat <atharvakamat10@gmail.com>
2026-07-19 07:57:04 +0000

    added support for the anchor/alias yaml input. This commit adheres to the new parsing and processing framework.

--- core/src/main/java/io/kestra/core/services/FlowParsingService.java
+++ core/src/main/java/io/kestra/core/services/FlowParsingService.java
@@ -144,10 +144,14 @@ public class FlowParsingService {
 
         // The block below should only be reached during testing for failure scenarios
         try {
-            Flow parsed = YAML_MAPPER_NON_DEFAULT.readValue(flow.getSource(), Flow.class);
+            // Flow parsed = YAML_MAPPER_NON_DEFAULT.readValue(flow.getSource(), Flow.class);
+            Flow parsed = YamlParser.parse(flow.getSource(), Flow.class);
             return FlowWithSource.of(parsed, flow.getSource());
-        } catch (JsonProcessingException e) {
-            throw new KestraRuntimeException("Failed to read flow from source", e);
+        } catch (Exception e) {
+            if (e instanceof JsonProcessingException je) {
+                throw new KestraRuntimeException("Failed to read flow from source", e);
+            }
+            throw e;
         }
     }
 
@@ -198,7 +202,8 @@ public class FlowParsingService {
     protected Map<String, Object> readFlowAsMap(@Nullable final String tenantId,
         @Nullable final String namespace,
         final String source) throws JsonProcessingException, FlowProcessingException {
-        Map<String, Object> mapFlow = YAML_MAPPER.readValue(source, JacksonMapper.MAP_TYPE_REFERENCE);
+        Map<String, Object> mapFlow = YamlParser.parse(source, JacksonMapper.MAP_TYPE_REFERENCE);
+        // Map<String, Object> mapFlow = YAML_MAPPER.readValue(source, JacksonMapper.MAP_TYPE_REFERENCE);
         return injectPluginVersions(tenantId, namespace == null ? (String) mapFlow.get("namespace") : namespace, mapFlow);
     }
 

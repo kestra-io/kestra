5f0394fc728c0ac74860e6f0aaed5b563104fb7c
Loïc Mathieu <loikeseke@gmail.com>
2026-08-12 17:53:46 +0200

    fix(core): compilation issue

--- core/src/main/java/io/kestra/plugin/core/execution/Labels.java
+++ core/src/main/java/io/kestra/plugin/core/execution/Labels.java
@@ -117,7 +117,7 @@ public class Labels extends Task implements ExecutionUpdatableTask {
                     Collectors.toMap(
                         Map.Entry::getKey,
                         Map.Entry::getValue,
-                        (_, second) -> second
+                        (ignored, second) -> second
                     )
                 );
         } else if (labels instanceof Map<?, ?> map) {

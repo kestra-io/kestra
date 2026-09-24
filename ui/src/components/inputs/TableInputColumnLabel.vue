2cb14bd984a661a2dbaaaa3c07f69a30f9c1ddb6
Barthélémy Ledoux <bledoux@kestra.io>
2026-09-24 14:57:30 +0200

    feat(inputs): refactor TableInput to use TableInputColumnLabel component

---
+++ ui/src/components/inputs/TableInputColumnLabel.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <span class="table-input-header" role="columnheader">
+            {{ columnLabel }} 
+        </span>
+        <span class="table-input-type">
+            {{ column.type }}
+        </span>
+    </div>
+</template>
+
+<script lang="ts" setup>
+    import {computed} from "vue"
+    import type {InputMetaData} from "../../stores/executions"
+
+
+    const column = defineProps<InputMetaData>()
+    const columnLabel = computed(() => column.displayName ?? column.id)
+</script>
+
+<style lang="scss" scoped>
+    .table-input-header {
+        color: var(--ks-text-primary);
+        font-size: var(--ks-font-size-sm);
+        font-weight: var(--ks-font-weight-bold);
+    }
+
+    .table-input-type {
+        color: var(--ks-text-secondary);
+        font-size: var(--ks-font-size-xs);
+        margin-left: var(--ks-spacing-1);
+    }
+</style>

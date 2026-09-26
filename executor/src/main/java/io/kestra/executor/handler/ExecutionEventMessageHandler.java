d1bf638d6adc054f542b731143429036795d0fa8
Hemant M Mehta <hemant29mehta@gmail.com>
2026-09-26 20:55:00 +0530

    fix: resolve syntax error from bad merge conflict in ExecutionEventMessageHandler
    Signed-off-by: Hemant M Mehta <hemant29mehta@gmail.com>

--- executor/src/main/java/io/kestra/executor/handler/ExecutionEventMessageHandler.java
+++ executor/src/main/java/io/kestra/executor/handler/ExecutionEventMessageHandler.java
@@ -198,7 +198,7 @@ public class ExecutionEventMessageHandler implements ExecutorMessageHandler<Exec
                                     }
                                     case CANCEL -> execution.withState(State.Type.CANCELLED);
                                 };
-                            
+
                                 return executor.withExecution(newExecution, "processQuotas");
                             }
 
@@ -214,20 +214,6 @@ public class ExecutionEventMessageHandler implements ExecutorMessageHandler<Exec
                                     .concurrencyState(ExecutionRunning.ConcurrencyState.CREATED)
                                     .build();
 
-                                ExecutionRunning processed = concurrencyLimitStateStore.countThenProcess(flow, concurrencyLimits, (txContext, runningCounts) ->
-                                {
-                                    Integer queueSize = flow.getConcurrency().getQueueSize();
-                                    int queuedCount = queueSize == null ? 0 : executionQueuedStateStore.count(txContext, flow.getTenantId(), flow.getNamespace(), flow.getId());
-                                    ExecutionRunning computed = executorService.processExecutionRunning(flow, concurrencyLimit.getRunning(), queuedCount, executionRunning.withExecution(execution)); // be sure that the execution running contains the latest value of the execution
-                                    if (computed.getConcurrencyState() == ExecutionRunning.ConcurrencyState.RUNNING && !computed.getExecution().getState().isTerminated()) {
-                                        return Pair.of(computed, concurrencyLimit.withRunning(concurrencyLimit.getRunning() + 1));
-                                    }
-                                    if (computed.getConcurrencyState() == ExecutionRunning.ConcurrencyState.QUEUED) {
-                                    ExecutionRunning computed = executorService.processExecutionRunning(concurrencyLimits, runningCounts, executionRunning.withExecution(execution)); // be sure that the execution running contains the latest value of the execution
-                                    if (computed.getConcurrencyState() == ExecutionRunning.ConcurrencyState.RUNNING && !computed.getExecution().getState().isTerminated()) {
-                                        return Pair.of(computed, true);
-                                    } else if (computed.getConcurrencyState() == ExecutionRunning.ConcurrencyState.QUEUED) {
-                                        executionQueuedStateStore.save(txContext, ExecutionQueued.fromExecutionRunning(computed));
                                 ExecutionRunning processed = concurrencyLimitStateStore.countThenProcess(
                                     flow,
                                     concurrencyLimits,
